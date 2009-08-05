de845220a8c9aef7fca7b13f4ebb3e676a479f9a
dthain <dthain@nd.edu>
2009-08-05 19:36:40 +0000

    Use default port number, so that it can be passed via environment variable.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@295 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/batch_job.c
+++ dttools/src/batch_job.c
@@ -641,7 +641,7 @@ struct batch_queue * batch_queue_create( batch_queue_type_t type )
 	q->logfile = strdup("condor.logfile");
 
 	if(type==BATCH_QUEUE_TYPE_WORK_QUEUE) {
-		q->work_queue = work_queue_create(9123, time(0)+60);
+		q->work_queue = work_queue_create(0,time(0)+60);
 	} else {
 		q->work_queue = 0;
 	}
