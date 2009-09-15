26fc24eed02dff31d109b4170a232c7e0e3ad0f5
cmoretti <cmoretti@nd.edu>
2009-09-15 20:07:14 +0000

    Added options for error, output, x86 only, and specific machines
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@318 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/worker_condor_submit.c
+++ dttools/src/worker_condor_submit.c
@@ -1,30 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_HOSTNAME_LENGTH 256
+#define MAX_FILENAME_LENGTH 1024
+
 
 void print_usage() {
-    printf("Usage:\nworker_condor_submit #Workers MasterHostname MasterPort [MachineGroup] [MachineGroup] ...\n");
+    printf("Usage:\nworker_condor_submit [options] #Workers MasterHostname MasterPort [MachineGroup] [MachineGroup] ...\n");
+    printf("-x: Use only x86_64 machines for workers. (Default: use both 32-bit and 64-bit machines)\n");
+    printf("-o: Include output stream file specification in Condor submit file. (Default: worker's stdout comes back in .debug file)\n");
+    printf("-e: Include error stream file specification in Condor submit file. (Default: worker's stderr is discarded)\n");
+    printf("-y [file]: Use only machines listed in given file for workers. (Default: do not restrict, except by given MachineGroups)\n");
 }
 
 int main(int argc, char** argv) {
 
     int i;
+    int include_output = 0;
+    int include_error = 0;
+    int only_x86 = 0;
+    char opt_file[MAX_FILENAME_LENGTH] = "";
+    char c;
     
-    if(argc < 4) {
+    
+
+    while((c=getopt(argc,argv,"xoey:"))!=(char)-1) {
+	switch(c) {
+	case 'x':
+	    only_x86 = 1;
+	    break;
+	case 'o':
+	    include_output = 1;
+	    break;
+	case 'e':
+	    include_error = 1;
+	    break;
+	case 'y':
+	    strcpy(opt_file,optarg);
+	    break;
+	default:
+	    print_usage();
+	    exit(0);
+	    break;
+	}
+    }
+
+    if( (argc-optind)<3) {
 	print_usage();
 	exit(1);
     }
-
+    
     int jobs;
-    jobs = atoi(argv[1]);
+    jobs = atoi(argv[optind]);
     
     char hostname[MAX_HOSTNAME_LENGTH];
-    strcpy (hostname, argv[2]);
+    strcpy (hostname, argv[optind+1]);
 
     int port;
-    port = atoi(argv[3]);
+    port = atoi(argv[optind+2]);
 
     
 
@@ -33,14 +68,18 @@ int main(int argc, char** argv) {
     fprintf(subp,"executable = worker\n"); // set executable; it's a worker 
     fprintf(subp,"arguments = -o worker.$(PROCESS).debug %s %i\n",hostname,port);
 
-    // set first "third" of requirements string
+    // set architecture portion of requirements string
     char reqstring[4096];
-    sprintf(reqstring,"Requirements = (Arch==\"INTEL\" || Arch == \"X86_64\") ");
-
-    if(argc > 4) {
+    if(only_x86)
+	sprintf(reqstring,"Requirements = (Arch == \"X86_64\") ");
+    else
+	sprintf(reqstring,"Requirements = (Arch==\"INTEL\" || Arch == \"X86_64\") ");
+    
+    // set group portion of requirements string
+    if((argc-optind) > 3) {
 	char reqclose[10];
 	int rsl;
-  
+
 	char groupstring[4096] = "";
 	char goodstring[4096] = "";
 
@@ -48,15 +87,15 @@ int main(int argc, char** argv) {
 
 
 
-	for(i = 4; i < argc; i++) {
+	for(i = optind+3; i < argc; i++) {
 	    strcat(groupstring,argv[i]);
 	    strcat(groupstring,",");
 	}
 	groupstring[strlen(groupstring)-1] = '\0';
-	sprintf(reqclose,"\") )"); // set third "third" of requirements string
-	rsl = strlen(reqstring)+2+strlen(reqclose); // measure the length of the requirements string's 1st and 3rd parts
+	sprintf(reqclose,"\") )"); // set string to close off stringListIMember
+	rsl = strlen(reqstring)+2+strlen(reqclose); // measure the length of the requirements string's architecture part and stringListIMember's remaining requirement
 	
-	char* nextgroup = strtok(groupstring,","); // go through "forward" full set until we exhaust the character limit.
+	char* nextgroup = strtok(groupstring,","); // go through given groups until we exhaust the character limit.
 	while(nextgroup != NULL && ((strlen(nextgroup)+1+rsl)<2046)) {
 	    if(goodstring[0] != '\0')
 		strcat(goodstring,",");
@@ -65,15 +104,59 @@ int main(int argc, char** argv) {
 	}
 	nextgroup = NULL;
 	
-	strcat(reqstring,goodstring); // build full reqstring from three thirds.
+	strcat(reqstring,goodstring); // add group requirements to full requirements
 	strcat(reqstring,reqclose);
     }
 
+    // set hosts portion of requirements string
+    if(strcmp(opt_file,"")) {
+	char reqclose[10];
+	int rsl;
+
+	FILE* yfd;
+	yfd = fopen(opt_file,"r");
+	if(yfd) {
+	    char MSNstring[4096] = "";
+	    char goodstring[4096] = "";
+	    char line[4096] = "";
+	    strcat(reqstring,"&& ( stringListIMember(MachineShortName, \"");
+	    
+	    
+	    //open file, read all items into MSNstring
+	    
+	    while(fscanf(yfd,"%s",line) == 1) {
+		strcat(MSNstring,line);
+		strcat(MSNstring,",");
+	    }
+	    MSNstring[strlen(MSNstring)-1] = '\0';
+	    sprintf(reqclose,"\") )"); // set string to close off stringListIMember
+	    rsl = strlen(reqstring)+2+strlen(reqclose); // measure the length of the requirements string's architecture and group parts and stringListIMember's remaining requirement
+	    
+	    char* nexthost = strtok(MSNstring,","); // go through given hosts until we exhaust the character limit.
+	    while(nexthost != NULL && ((strlen(nexthost)+1+rsl)<2046)) {
+		if(goodstring[0] != '\0')
+		    strcat(goodstring,",");
+		strcat(goodstring,nexthost);
+		nexthost = strtok(NULL,",");
+	    }
+	    nexthost = NULL;
+	    
+	    strcat(reqstring,goodstring); // add host requirements to full requirements
+	    strcat(reqstring,reqclose);
+	}
+	else {
+	    printf("Could not open given host file %s\n",opt_file);
+	}
+    }
+
+   
     fprintf(subp,"%s\n",reqstring); // print it to the submit file
 
     // finish off the submit file
-    fprintf(subp, "#output = worker.$(PROCESS).output\n");
-    fprintf(subp, "#error = worker.$(PROCESS).error\n");
+    if(include_output)
+	fprintf(subp, "output = worker.$(PROCESS).output\n");
+    if(include_error)
+	fprintf(subp, "error = worker.$(PROCESS).error\n");
     fprintf(subp, "transfer_files = always\n");
     fprintf(subp, "transfer_output_files = worker.$(PROCESS).debug\n");
     fprintf(subp, "+JobMaxSuspendTime = 10\n");
