197b688268d24e94b7782f286bc8309e796b4e39
dthain <dthain@nd.edu>
2009-10-28 19:12:20 +0000

    Cleaned up the map program to work with work queue only, and transparently handle failures via resubmission.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@353 a4d8336d-3463-0410-8bba-c098c45d37a8

--- abstractions/map/src/map.c
+++ abstractions/map/src/map.c
@@ -2,17 +2,7 @@
 Copyright (C) 2009- The University of Notre Dame
 This software is distributed under a BSD-style license.
 See the file COPYING for details.
-*/
-
-/*
-	Connor Keenan
-	Submits all jobs in Worker Queue mode
-	Reads a file listing input and output files
-	Then runs the executable on then input and outputs to the specified file
-	----------------
-	To run workers:
-	system: condor_submit_workers <hostname> <port> <number>
-	Default batch_job_work_queue port: 9123
+First version written by Connor Keenan in 2009.
 */
 
 #include <errno.h>
@@ -21,29 +11,21 @@ See the file COPYING for details.
 #include <stdio.h>
 #include <time.h>
 
-#include "chirp_reli.h"
+#include "list.h"
 #include "debug.h"
 #include "auth_all.h"
-#include "batch_job.h"
+#include "work_queue.h"
+#include "stringtools.h"
 
-int read_Numberoflines(FILE *fin)
+const char * get_file_extension( const char *path )
 {
-	int numlines = 0;
-	char check = '\n'; // Default is new line, in case the file is empty
-
-	if (fin)
-	{
-		while((check = getc(fin)) != EOF)
-		{
-			if (check == '\n')
-			{
-				// For each new line character, increment 1
-				numlines++;
-			}
-		}
+	char *p = strrchr(path,'.');
+	if(p) {
+		p++;
+		return p;
+	} else {
+		return "";
 	}
-	rewind(fin);
-	return numlines;
 }
 
 static void show_version(const char *cmd)
@@ -53,48 +35,44 @@ static void show_version(const char *cmd)
 
 static void show_help( const char *cmd )
 {
-	printf("use: %s [options] <executable> <datafile>\n",cmd);
+	printf("use: %s [options] <executable> <input-list> <output-dir> <output-type>\n",cmd);
 	printf("where options are:\n");
-	printf(" -R         Retry failures until all jobs complete.\n");
-	printf(" -T <type>  Batch queue type: unix, condor, wq, or sge\n");
+	printf(" -p         Port number to listen on.  (default=%d)\n",WORK_QUEUE_DEFAULT_PORT);
+	printf(" -r         Retry application level failures.\n");
+	printf(" -f <a,b,c> Extra files to send along to each job.\n"); 
 	printf(" -d <flag>  Enable debugging for this subsystem.\n");
 	printf(" -v         Show version string.\n");
 	printf(" -h         This message.\n");
-	printf("details of the work_queue submissions are stored in <datafile>.log\n");
 }
 
 int main(int argc, char *argv[])
 {
-	char inFile[500], outFile[500], cmdLine[1000], logfile[1024];	
-	int submit_to_start, start_to_finish;
-	int total_time = 0, completed = 0, total_lines = 0, resubmitted = 0, jobs = 0, failed = 0;
-	int i;
-	int resubmission = 0; 
-	long stf_total = 0;
-	struct batch_queue *q;
-	struct batch_job_info batchinfo;
-	batch_job_id_t jobid;
-	batch_job_id_t result;
-	time_t starttime;
+	int retry_failures = 0;
 	char c;
-	int run_complete = 0;
-	batch_queue_type_t queue_type = BATCH_QUEUE_TYPE_WORK_QUEUE;
 
-	while((c=getopt(argc,argv,"rRT:d:vh"))!=(char)-1) {
+	int port = WORK_QUEUE_DEFAULT_PORT;
+
+	struct work_queue *q;
+	struct work_queue_task *t;
+	struct list *extra_files = list_create();
+
+	while((c=getopt(argc,argv,"p:f:rd:vh"))!=(char)-1) {
 		switch(c) {
+			case 'p':
+				port = atoi(optarg);
+				break;
+			case 'r':
+				retry_failures = 1;
+				break;
+			case 'f':
+				list_push_head(extra_files,strdup(optarg));
+				break;
 			case 'd':
 				debug_flags_set(optarg);
-				break;
-			case 'T':
-				queue_type = batch_queue_type_from_string(optarg);
 				break;
 			case 'v':
 				show_version(argv[0]);
 				exit(0);
-				break;
-			case 'r':
-			case 'R':
-				run_complete = 1;
 				break;
 			case 'h':
 				show_help(argv[0]);
@@ -103,147 +81,103 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if(argc-optind!=2) {
+	if(argc-optind!=4) {
 		show_help(argv[0]);
 		return 0;
 	}
 
-	const char *executable = argv[optind];
-	const char *datafile = argv[optind+1];
+	const char *command = argv[optind];
+	const char *inputlist = argv[optind+1];
+	const char *outputdir = argv[optind+2];
+	const char *outputtype = argv[optind+3];
 
 	debug_config(argv[0]);
 
-	q = batch_queue_create(queue_type);
-
-	// Open the input/output files
-	sprintf(logfile, "%s.log", datafile);
-	FILE *fin = fopen(datafile, "r");
-	FILE *fout = fopen(logfile, "w");
-	if (fin == NULL)
-	{
-		printf("Error opening file\n");
+	FILE *file = fopen(inputlist,"r");
+	if(!file) {
+		fprintf(stderr,"map: couldn't open %s: %s\n",inputlist,strerror(errno));
 		return 1;
 	}
-	total_lines = read_Numberoflines(fin);
-	starttime = time(NULL);
-	fprintf(fout, "%d Jobs Submitted\n", total_lines);
 
-	while (1)
-	{
-		if (jobs < 1000 || feof(fin))
-		{
-			// Read file line and Submit Jobs
-			fscanf(fin, "%s %s", inFile, outFile);
-			if (!feof(fin))
-			{
-				sprintf(cmdLine, "%s %s %s", executable, inFile, outFile);
-				do {
-					jobid = batch_job_submit_simple(q, cmdLine, inFile, outFile);
-					} while (jobid < 0);
-				jobs++;
-			}
-		}
-
-		if (jobs >= 1000 || feof(fin))
-		{
-			// Wait for Next Job to finish
-			result = batch_job_wait(q, &batchinfo);
-			if (result > 0)
-			{
-				completed++;
-				submit_to_start = (int)(batchinfo.started-batchinfo.submitted);
-				start_to_finish = (int)(batchinfo.finished-batchinfo.started);
-				stf_total += start_to_finish;
-				total_time = (int)(time(NULL) - starttime);
-				printf("JobID: %d Completed\n", result);
-				printf("TOTAL TIME: %d\n", total_time);
-				printf("%d out of %d Jobs Finished\n\n", completed, total_lines);
-
-				// Output File not Created Successfully:
-				if (batchinfo.exit_code == 1) failed++;
-				jobs--;
-			// WorkQueue is empty, Stop waiting
-			} else if (result == 0) break;
-		}
+	q = work_queue_create(port,time(0)+60);
+	if(!q) {
+		fprintf(stderr,"couldn't create work queue: perhaps port %d is already in use?\n",port);
+		return 1;
 	}
-	printf("Failed: %d\n", failed);
-	fprintf(fout, "First Submission: %d out of %d Failed\n\n", failed, total_lines);
 
-	// IF run_complete == TRUE, begin RESUBMISSIONS
-	if (run_complete==1)
-	{
-	FILE *ffail;
-	FILE *ftempout;
-	while (failed > 0)
-	{
-		// Make a failed.out file
-		ffail = fopen("failed.out", "w");
-		rewind(fin);
-		for (i=0;i<total_lines;i++)
-		{
-			fscanf(fin, "%s %s", inFile, outFile);
-			ftempout = fopen(outFile, "r");
-			if (ftempout == NULL)
-			{
-				fprintf(ffail, "%s %s\n", inFile, outFile);
-			} else fclose(ftempout);
+	printf("map: listening for workers on port %d...\n",port);
+
+	while(1) {
+		if(feof(file) && work_queue_empty(q)) break;
+
+		while(!feof(file) && work_queue_hungry(q)) {
+
+			char local_infile[WORK_QUEUE_LINE_MAX];
+			char local_outfile[WORK_QUEUE_LINE_MAX];
+
+			char remote_infile[WORK_QUEUE_LINE_MAX];
+			char remote_outfile[WORK_QUEUE_LINE_MAX];
+
+			char cmdline[WORK_QUEUE_LINE_MAX];
+
+			if(!fgets(local_infile,sizeof(local_infile),file)) break;
+
+			string_chomp(local_infile);
+			sprintf(remote_infile,"a.%s",get_file_extension(local_infile));
+			sprintf(remote_outfile,"b.%s",outputtype);
+			sprintf(local_outfile,"%s/%s",outputdir,string_basename(local_infile));
+
+			char *p = strrchr(local_outfile,'.');
+			if(p) *p = 0;
+
+			strcat(local_outfile,".");
+			strcat(local_outfile,outputtype);
+
+			if(access(local_outfile,R_OK)==0) {
+				printf("skipping:  %s because %s already exists\n",local_infile,local_outfile);
+				continue;
+			}
+
+			sprintf(cmdline,"./%s %s %s",string_basename(command),remote_infile,remote_outfile);
+
+			t = work_queue_task_create(cmdline);
+
+			work_queue_task_specify_tag(t,local_infile);
+			work_queue_task_specify_input_file(t,command,string_basename(command));
+			work_queue_task_specify_input_file(t,local_infile,remote_infile);
+			work_queue_task_specify_output_file(t,remote_outfile,local_outfile);
+
+			char *f;
+
+			list_first_item(extra_files);
+			while((f=list_next_item(extra_files))) {
+				work_queue_task_specify_input_file(t,f,string_basename(f));
+			}
+
+			work_queue_submit(q,t);
 		}
-		fclose(ffail);
-		failed = 0;
-		jobs = 0;
-		completed = 0;
-		ffail = fopen("failed.out", "r");
-		resubmitted = read_Numberoflines(ffail);
-		// Go through each line resubmitting and waiting
-		while (1)
-		{
-			if (jobs < 1000 || feof(ffail))
-			{
-				// Read file and Submit Jobs
-				fscanf(ffail, "%s %s", inFile, outFile);
-				if (!feof(ffail))
-				{
-					sprintf(cmdLine, "%s %s %s", executable, inFile, outFile);
-					do {
-						jobid = batch_job_submit_simple(q, cmdLine, inFile, outFile);
-						} while (jobid < 0);
-					jobs++;
+
+		t = work_queue_wait(q,WORK_QUEUE_WAITFORTASK);
+		if(t) {
+			if(t->return_status!=0) {
+				fprintf(stderr,"failed:   %s\n",t->tag);
+				fprintf(stderr,"with output: %s\n",t->output);
+				if(retry_failures) {
+					work_queue_submit(q,t);
+				} else {
+					work_queue_task_delete(t);
 				}
-			}
-			if (jobs >= 1000 || feof(ffail))
-			{
-				result = batch_job_wait(q, &batchinfo);
-				if (result > 0)
-				{
-					completed++;
-					submit_to_start = (int)(batchinfo.started-batchinfo.submitted);
-					start_to_finish = (int)(batchinfo.finished-batchinfo.started);
-					stf_total += start_to_finish;
-					total_time = (int)(time(NULL) - starttime);
-					printf("JobID: %d Resubmitted\n", result);
-					printf("TOTAL TIME: %d\n", total_time);
-					printf("%d out of %d Resubmitted Jobs Finished\n\n", completed, resubmitted);
-					if (batchinfo.exit_code == 1) failed++;
-					jobs--;
-				} else if (result == 0) break;
+			} else {
+				printf("complete: %s\n",t->tag);
+				work_queue_task_delete(t);
 			}
 		}
-		fclose(ffail);
-		resubmission++;
-		fprintf(fout, "Resubmission Group %d\n\t%d out of %d Failed\n\n", resubmission, failed, completed);
-	}
+
 	}
 
+	fclose(file);
+	work_queue_delete(q);
 
-	// Print Out Final Calculated Totals:
-	total_time = (int)(time(0) - starttime);
-	fprintf(fout, "---------------------------------------\n");
-	fprintf(fout, "Total Time = %d secs\nRaw Average Time per Job: %f secs\nStart to Finish Total CPU Time: %ld secs\nAverage Total CPU Time Per Job: %f secs", total_time, (float)((float)total_time/(float)total_lines), stf_total, (float)((float)stf_total/(float)total_lines));
-
-	batch_queue_delete(q);
-
-	fclose(fout);
-	fclose(fin);
 	return 0;
 }
 
