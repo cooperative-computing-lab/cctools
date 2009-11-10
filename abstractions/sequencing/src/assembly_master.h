5e85ad14e2a448dd7342a22f4924dbbefc3f5a79
dthain <dthain@nd.edu>
2009-11-10 19:00:08 +0000

    Updated a few incorrect copyright strings.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@369 a4d8336d-3463-0410-8bba-c098c45d37a8

--- abstractions/sequencing/src/assembly_master.h
+++ abstractions/sequencing/src/assembly_master.h
@@ -1,6 +1,5 @@
 /*
-Copyright (C) 2003-2004 Douglas Thain and the University of Wisconsin
-Copyright (C) 2005- The University of Notre Dame
+Copyright (C) 2009- The University of Notre Dame
 This software is distributed under the GNU General Public License.
 See the file COPYING for details.
 */
--- abstractions/wavefront/src/wavefront.c
+++ abstractions/wavefront/src/wavefront.c
@@ -1,7 +1,6 @@
 
 /*
-Copyright (C) 2003-2004 Douglas Thain and the University of Wisconsin
-Copyright (C) 2005- The University of Notre Dame
+Copyright (C) 2008- The University of Notre Dame
 This software is distributed under the GNU General Public License.
 See the file COPYING for details.
 */
