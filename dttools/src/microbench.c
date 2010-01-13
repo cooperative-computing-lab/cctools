ccd872d5daa734bf46a1b0c281d47531b5d227bc
pbui <pbui@nd.edu>
2010-01-13 05:18:18 +0000

    microbench: add fsync and optional write tests
    Patches from Matthew Farrellee
    
    
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@416 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/microbench.c
+++ dttools/src/microbench.c
@@ -19,18 +19,22 @@ See the file COPYING for details.
 enum {
 	OP_STAT,
 	OP_OPEN,
+	OP_WRITE,
 	OP_READ,
+	OP_FSYNC,
 	OP_CLOSE,
 	NOPS
 };
 
 #define BUFFER_SIZE 8192
 
-const char *OP_STRINGS[NOPS] = { "stat ", "open ", "read ", "close" };
+const char *OP_STRINGS[NOPS] = { "stat ", "open ", "write", "read ", "fsync", "close" };
+
+static int OPEN_FLAGS = O_RDONLY;
 
 static void show_help(const char *cmd)
 {
-	printf("Use: %s <path> <runs>\n", cmd);
+	printf("Use: %s <path> <runs> [write]\n", cmd);
 }
 
 static void do_stat(const char *path)
@@ -51,11 +55,29 @@ static void do_stat(const char *path)
 static void do_open(const char *path, int *fd)
 {
 	timer_start(OP_OPEN); 
-	*fd = open(path, O_RDONLY);
+	*fd = open(path, OPEN_FLAGS);
 	timer_stop(OP_OPEN);
 
 	if (*fd < 0) {
 		printf("could not open %s: %s\n", path, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void do_write(int fd)
+{
+	char buffer[BUFFER_SIZE];
+	int result, count = 0;
+
+	timer_start(OP_WRITE);
+	do {
+		result = write(fd, &buffer[count], BUFFER_SIZE - count);
+		count += result;
+	} while (result != -1 && count < BUFFER_SIZE);
+	timer_stop(OP_WRITE);
+
+	if (result < 0) {
+		printf("could not write: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 }
@@ -75,6 +97,20 @@ static void do_read(int fd)
 	}
 }
 
+static void do_fsync(int fd)
+{
+	int result;
+
+	timer_start(OP_FSYNC); 
+	result = fsync(fd);
+	timer_stop(OP_FSYNC);
+
+	if (result < 0) {
+		printf("could not fsync: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
 static void do_close(int fd)
 {
 	timer_start(OP_CLOSE); 
@@ -87,13 +123,17 @@ int main(int argc, char *argv[])
 	char *path;
 	int i, fd, runs;
 
-	if (argc != 3) {
+	if (argc < 3) {
 		show_help(argv[0]);
 		return (EXIT_FAILURE);
 	}
 
 	path = argv[1];
 	runs = atoi(argv[2]);
+
+	if (4 == argc && 0 == strcmp(argv[3], "write")) {
+		OPEN_FLAGS = O_RDWR;
+	}
 
 	timer_init(NOPS, OP_STRINGS);
 
@@ -103,7 +143,9 @@ int main(int argc, char *argv[])
 	for (i = 0; i < runs; i++) {
 		do_stat(path);
 		do_open(path, &fd);
+		if (O_RDWR == OPEN_FLAGS) do_write(fd);
 		do_read(fd);
+		do_fsync(fd);
 		do_close(fd);
 	}
 
