9a526cb3875b9cb1bd833f854a723fe7e282d5cc
kparting <kparting@nd.edu>
2010-01-29 22:07:06 +0000

    Separated dttools/get_line.h into .h and .c files. For some reason make fails, but this was the case before I made my changes (after running svn update to get to revision #439). Makeflow compiles.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@440 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/get_line.h
+++ dttools/src/get_line.h
@@ -8,69 +8,15 @@ See the file COPYING for details.
 #ifndef __LINEREADER_H__
 #define __LINEREADER_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /** Read a line of any length from a file.
 The function is invoked with a stack buffer ("buffer") of a set size.
-It will try to populate the stack buffer as much as it can 
+It will try to populate the stack buffer as much as it can, and if a newline
+is not found in the string (possibly meaning a line is too long), then a heap
+buffer ("other") allocated to the appropriate size will be used instead. The
+user does not know which buffer is returned (either the stack buffer or the
+heap buffer), nor does the user need to care. Even at EOF, the function must be
+called again, so the heap buffer will be freed.
  **/
-/** Delete a linked list.
-Note that this function only deletes the list itself,
-it does not delete the items referred to by the list.
-@param list The list to delete.
-*/
-static char * get_line( FILE *fp, char *buffer, int size )
-{
-	static char *other = NULL;
-
-	/* Free the other buffer, if we have used it. */
-	if (other)
-	{
-		free(other);
-		other = NULL;
-	}
-
-	if (!fgets(buffer, size, fp))
-	{
-		return NULL;
-	}
-
-	/* If the main buffer is completely filled and there is more
-	   to read... */
-	if (!strrchr(buffer, '\n'))
-	{
-		int s = size;
-
-		/* ...use the heap buffer ("other"), doubling its size
-		   repeatedly until it is filled. */
-		do {
-			char *tmp = realloc(other, 2*s * sizeof(char));
-
-			if (!tmp)
-			{
-				free(other);
-				other = NULL;
-				return NULL;
-			}
-			else
-			{
-				other = tmp;
-			}
-
-			strncpy(other, buffer, strlen(buffer));
-			fgets(other + s - 1, s + 1, fp);
-			s *= 2;
-
-		} while (!strrchr(other, '\n'));
-
-		return other;
-	}
-	else
-	{
-		return buffer;
-	}
-}
+char * get_line( FILE *fp, char *buffer, int size );
 
 #endif
