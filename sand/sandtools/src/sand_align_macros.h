ddb21dd8a901d857c9650d0d16afc2c5889316a8
cmoretti <cmoretti@nd.edu>
2010-02-06 20:32:17 +0000

    Put sand-specific headers and stuff into new sandtools library
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@455 a4d8336d-3463-0410-8bba-c098c45d37a8

---
+++ sand/sandtools/src/sand_align_macros.h
@@ -0,0 +1,9 @@
+/*
+	A set of macros to determine what kind of alignment program to make. By default only COMPRESSION is activated.
+*/
+
+//#define TRUNCATE 5
+//#define SPEEDTEST
+#define COMPRESSION
+//#define STATIC_MATRIX_SIZE 1400
+//#define UMD_COMPRESSION 1
