ceb2f056f2d0adde3c16440087bc5c741150994b
cmoretti <cmoretti@nd.edu>
2010-02-15 19:47:08 +0000

    Fixed symlink bug in example.sh
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@497 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/example/example.sh
+++ sand/example/example.sh
@@ -3,24 +3,37 @@
 CCTOOLS_HOME=../..
 SAND_HOME=../
 
+error_state=0;
+
 # link to all the necessary pieces.
 echo "Getting compress_reads"
-ln -s ${SAND_HOME}/formatting/src/compress_reads ./compress_reads || { echo "Please build sand first."; exit 1 ; }
+ln -s ${SAND_HOME}/formatting/src/compress_reads ./compress_reads || { echo "Please build sand first or delete conflicting file ./compress_reads ."; error_state=1 ; }
+if [ ! -f ./compress_reads ]; then echo "Please build sand first."; error_state=1 ; fi
 
 echo "Getting worker"
-ln -s ${CCTOOLS_HOME}/dttools/src/worker ./worker || { echo "Please build dttools first."; exit 1 ; }
+ln -s ${CCTOOLS_HOME}/dttools/src/worker ./worker || { echo "Please build dttools first or delete conflicting file ./worker ."; error_state=1 ; }
+if [ ! -f ./worker ]; then echo "Please build dttools first."; error_state=1 ; fi
 
 echo "Getting serial filter program"
-ln -s ${SAND_HOME}/filtering/src/filter_mer_seq ./filter_mer_seq || { echo "Please build sand first."; exit 1 ; }
+ln -s ${SAND_HOME}/filtering/src/filter_mer_seq ./filter_mer_seq || { echo "Please build sand first or delete conflicting file ./filter_mer_seq ."; error_state=1 ; }
+if [ ! -f ./filter_mer_seq ]; then echo "Please build sand first."; error_state=1 ; fi
 
 echo "Getting filter master"
-ln -s ${SAND_HOME}/filtering/src/sand_filter_master ./sand_filter_master || { echo "Please build sand first."; exit 1 ; }
+ln -s ${SAND_HOME}/filtering/src/sand_filter_master ./sand_filter_master || { echo "Please build sand first or delete conflicting file ./sand_filter_master ."; error_state=1 ; }
+if [ ! -f ./sand_filter_master ]; then echo "Please build sand first."; error_state=1 ; fi
 
 echo "Getting serial alignment program"
-ln -s ${SAND_HOME}/alignment/src/sw_alignment ./sw_alignment || { echo "Please build sand first."; exit 1 ; }
+ln -s ${SAND_HOME}/alignment/src/sw_alignment ./sw_alignment || { echo "Please build sand first or delete conflicting file ./sw_alignment ."; error_state=1 ; }
+if [ ! -f ./sw_alignment ]; then echo "Please build sand first."; error_state=1 ; fi
 
 echo "Getting alignment master"
-ln -s ${SAND_HOME}/alignment/src/sand_align_master ./sand_align_master || { echo "Please build sand first."; exit 1 ; }
+ln -s ${SAND_HOME}/alignment/src/sand_align_master ./sand_align_master || { echo "Please build sand first or delete conflicting file ./sand_align_master ."; error_state=1 ; }
+if [ ! -f ./sand_align_master ]; then echo "Please build sand first."; error_state=1 ; fi
+
+if(($error_state)); then
+    rm -f compress_reads worker filter_mer_seq sand_filter_master sw_alignment sand_align_master test_20.cand test_20.cfa;
+    exit 1;
+fi
 
 # compress reads
 echo "Compressing reads"
@@ -51,7 +64,7 @@ diff --brief test_20.ovl test_20.right && echo "Files test_20.ovl and test_20.ri
 echo "Waiting for worker to exit"
 wait $wpid
 echo "Removing created files"
-rm -f compress_reads worker filter_mer_seq filter_master sw_alignment sand_align test_20.cand test_20.cfa;
+rm -f compress_reads worker filter_mer_seq sand_filter_master sw_alignment sand_align_master test_20.cand test_20.cfa;
 rm -i test_20.ovl;
 
 exit 0;
