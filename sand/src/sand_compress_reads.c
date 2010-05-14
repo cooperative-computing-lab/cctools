c5871b1a9b83ed29fa26f2c2cfe6e789eda45e81
lyu2 <lyu2@nd.edu>
2010-05-14 19:29:15 +0000

    fixed the memory leak problem pointed out by Markiyan on his May 12,2010 email.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@626 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/src/sand_compress_reads.c
+++ sand/src/sand_compress_reads.c
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2009- The University of Notre Dame
+Copyright (C) 2009- The University of Notre Dame & University of Cambridge
 This software is distributed under the GNU General Public License.
 See the file COPYING for details.
 */
@@ -7,16 +7,18 @@ See the file COPYING for details.
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <ctype.h> 
+#include <ctype.h>
+#include <string.h>
 #include "sequence_alignment.h"
 #include "sequence_compression.h"
+#include "sand_align_macros.h"
 
 static void show_help(const char *cmd)
 {
         printf("Use: %s [options]  fasta_reads > compressed_reads\n", cmd);
         printf("where options are:\n");
 	printf(" -c             Remove Celera read_ids if file came from Celera's gatekeeper\n");
-	printf(" -i		Remove read_ids but leave the Celera internal ids if the file came from Celera's gatekeeper\n"); 
+	printf(" -i             Remove read_ids but leave the Celera internal ids if the file came from Celera's gatekeeper\n"); 
 	printf(" -h             Show this help screen\n");
 }
 
@@ -28,8 +30,9 @@ int main(int argc, char ** argv)
 	seq s;
 	cseq c;
 	char d;
-	int clip = 0; 
-	int internal = 0; 
+	char clip = 0;
+	char internal = 0;
+	char tmp_id[128];
 
         while((d=getopt(argc,argv,"chi"))!=(char)-1) {
                 switch(d) {
@@ -46,7 +49,7 @@ int main(int argc, char ** argv)
                 }
         }
 
-	if (argc == 2 && !clip && !internal) 
+	if (argc == 2 && clip == 0 && internal == 0)
 	{
 		input = fopen(argv[1], "r");
 		if (!input)
@@ -70,29 +73,22 @@ int main(int argc, char ** argv)
 		input = stdin;
 	}
 
-	//s = get_next_sequence(input);
 
 	while (!feof(input))
 	{
-		if(clip) { 
-			//fprintf(stderr, "clipping enabled"); 
-			s = get_next_sequence_clip(input); 
-		}
-		else if(internal) {
-			//fprintf(stderr, "internal reads enabled");
-			s = get_next_sequence_internal(input);
-		}	
-		else{
-			s = get_next_sequence(input); 
+		s = get_next_sequence(input); 
+		if(clip != 0 || internal != 0){
+			strcpy(tmp_id, s.id);
+			strcpy(s.id, strtok(tmp_id,","));//ext_id
+			if(internal != 0){
+				strcpy(s.id, strtok(NULL,","));//int_id
+			}
 		}
 		if (!s.id) { fprintf(stdout, ">>\n"); continue; }
-		//fprintf(stderr, "%s\n", s.id); 
 		c = compress_seq(s);
-		//fprintf(stderr, "freeing sequence"); 
-		//free_seq(s);
 		print_cseq(stdout, c);
-		//fprintf(stderr, "freeing compressed sequence"); 
 		free_cseq(c);
+		free_seq(s);
 	}
 
 	fclose(input);
