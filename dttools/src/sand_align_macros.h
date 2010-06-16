6026a4640567b34fa3ccb47d4dad96d301c9f9e5
dthain <dthain@nd.edu>
2010-06-16 17:58:13 +0000

    Removed duplicate file.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@687 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/sand_align_macros.h
+++
@@ -1,9 +0,0 @@
-/*
-	A set of macros to determine what kind of alignment program to make. By default only COMPRESSION is activated.
-*/
-
-//#define TRUNCATE 5
-//#define SPEEDTEST
-#define COMPRESSION
-//#define STATIC_MATRIX_SIZE 1400
-//#define UMD_COMPRESSION 1
