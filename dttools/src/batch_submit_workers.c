3a67c1a736a7b9d176382a3a83c05222631d66d8
pbui <pbui@nd.edu>
2010-07-08 06:51:42 +0000

    batch_submit_workers: remove -e, add -S, implement -s options
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@733 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/batch_submit_workers.c
+++ dttools/src/batch_submit_workers.c
@@ -43,7 +43,7 @@ static void show_help(const char *cmd)
 	printf("Use: %s [options] <hostname> <port> <count>\n", cmd);
 	printf("where batch options are:\n");
 	printf("  -d <subsystem> Enable debugging for this subsystem.\n");
-	printf("  -S <scratch>   Scratch directory. (default is /tmp/${USER}-workers)\n");
+	printf("  -s <scratch>   Scratch directory. (default is /tmp/${USER}-workers)\n");
 	printf("  -T <type>      Batch system type: unix, condor, sge, workqueue, xgrid. (default is unix)\n");
 	printf("  -W <path>      Path to worker executable.\n");
 	printf("  -h             Show this screen.\n");
@@ -51,6 +51,7 @@ static void show_help(const char *cmd)
 	printf("where worker options are:\n");
 	printf("  -a             Enable auto master selection mode.\n");
 	printf("  -e             Only select preferred master.\n");
+	printf("  -S             Run as a shared worker.\n");
 	printf("  -N <name>      Preferred master name.\n");
 	printf("  -t <time>      Abort after this amount of idle time.\n");
 }
@@ -69,7 +70,7 @@ int main(int argc, char *argv[])
 	struct batch_queue *q;
 	FILE *ifs, *ofs;
 
-	while ((c = getopt(argc, argv, "d:T:W:aeN:t:h")) >= 0) {
+	while ((c = getopt(argc, argv, "d:T:W:aSN:s:t:h")) >= 0) {
 		switch (c) {
 			case 'd':
 				debug_flags_set(optarg);
@@ -88,12 +89,15 @@ int main(int argc, char *argv[])
 				strncat(worker_args, " -a ", PATH_MAX);
 				auto_worker = 1;
 				break;
-			case 'e':
-				strncat(worker_args, " -e ", PATH_MAX);
+			case 'S':
+				strncat(worker_args, " -S ", PATH_MAX);
 				break;
 			case 'N':
 				strncat(worker_args, " -N ", PATH_MAX);
 				strncat(worker_args, optarg, PATH_MAX);
+				break;
+			case 's':
+				strncpy(scratch_dir, optarg, PATH_MAX);
 				break;
 			case 't':
 				strncat(worker_args, " -t ", PATH_MAX);
