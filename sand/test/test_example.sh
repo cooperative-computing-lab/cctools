b637766fd740c4aeed2a9c8434788e111053c6d8
dthain <dthain@nd.edu>
2010-09-29 14:52:03 +0000

    Updated test suite to correspond to the new code.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@851 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/test/test_example.sh
+++ sand/test/test_example.sh
@@ -17,21 +17,21 @@ echo "Worker is process $wpid"
 
 echo "Starting filter master"
 ln -s `which sand_filter_mer_seq` .
-sand_filter_master -s 10 -p $port -b test_20.cfa test_20.cand -d all -o filter.log || { echo "Error in filtering."; kill -9 $wpid; exit 1 ; }
+sand_filter_master -s 10 -p $port test_20.cfa test_20.cand -d all -o filter.log || { echo "Error in filtering."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking filtering results..."
 diff --brief test_20.cand test_20.cand.right && echo "Candidates match" || { kill -9 $wpid ; exit 1; }
 
 echo "Starting Smith-Waterman assembly..."
 ln -s `which sand_sw_alignment` .
-sand_align_master -n 1 -p $port sand_sw_alignment test_20.cand test_20.cfa test_20.sw.ovl -d all -o sw.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -p $port sand_sw_alignment test_20.cand test_20.cfa test_20.sw.ovl -d all -o sw.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking Smith-Waterman results"
 diff --brief test_20.sw.ovl test_20.sw.right && echo "Files test_20.sw.ovl and test_20.sw.right are the same" || { kill -9 $wpid ; exit 1 ; }
 
 echo "Starting banded assembly..."
 ln -s `which sand_banded_alignment` .
-sand_align_master -n 1 -p $port sand_banded_alignment test_20.cand test_20.cfa test_20.banded.ovl -d all -o sw.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -p $port sand_banded_alignment test_20.cand test_20.cfa test_20.banded.ovl -d all -o banded.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking banded results"
 diff --brief test_20.banded.ovl test_20.banded.right && echo "Files test_20.banded.ovl and test_20.banded.right are the same" || { kill -9 $wpid ; exit 1 ; }
