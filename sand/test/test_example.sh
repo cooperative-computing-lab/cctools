a9c76f9f9c9d3db7fc3dde276e21a8b6669f9eac
dthain <dthain@nd.edu>
2010-10-08 18:16:23 +0000

    Updated test script to invoke sand_align_master in the new way. (The test currently fails because filtering is still broken.)
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@877 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/test/test_example.sh
+++ sand/test/test_example.sh
@@ -23,15 +23,14 @@ echo "Checking filtering results..."
 diff --brief test_20.cand test_20.cand.right && echo "Candidates match" || { kill -9 $wpid ; exit 1; }
 
 echo "Starting Smith-Waterman assembly..."
-ln -s `which sand_sw_alignment` .
-sand_align_master -p $port sand_sw_alignment test_20.cand test_20.cfa test_20.sw.ovl -d all -o sw.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -p $port sand_align_kernel -e "-a sw" test_20.cand test_20.cfa test_20.sw.ovl -d all -o sw.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking Smith-Waterman results"
 diff --brief test_20.sw.ovl test_20.sw.right && echo "Files test_20.sw.ovl and test_20.sw.right are the same" || { kill -9 $wpid ; exit 1 ; }
 
 echo "Starting banded assembly..."
 ln -s `which sand_banded_alignment` .
-sand_align_master -p $port sand_banded_alignment test_20.cand test_20.cfa test_20.banded.ovl -d all -o banded.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -p $port sand_align_kernel -e "a banded" test_20.cand test_20.cfa test_20.banded.ovl -d all -o banded.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking banded results"
 diff --brief test_20.banded.ovl test_20.banded.right && echo "Files test_20.banded.ovl and test_20.banded.right are the same" || { kill -9 $wpid ; exit 1 ; }
