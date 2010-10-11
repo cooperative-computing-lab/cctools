13746b9d16742020e867a5d3b1561053cfe6bbbe
dthain <dthain@nd.edu>
2010-10-11 20:55:00 +0000

    Moved find_in_path back out to dttools.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@879 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/src/sand_align_master.c
+++ sand/src/sand_align_master.c
@@ -20,6 +20,7 @@ See the file COPYING for details.
 #include "hash_table.h"
 #include "stringtools.h"
 #include "macros.h"
+#include "find_in_path.h"
 
 #include "sequence.h"
 #include "compressed_sequence.h"
@@ -72,27 +73,6 @@ static void show_help(const char *cmd)
 	printf(" -o <file>      Send debugging to this file.\n");
 	printf(" -v             Show version string\n");
 	printf(" -h             Show this help screen\n");
-}
-
-static char * find_in_path( const char *cmd )
-{
-	char tmp[CAND_FILE_LINE_MAX];
-
-	if(access(cmd,X_OK)==0) return strdup(cmd);
-
-	char * path = strdup(getenv("PATH"));
-	char * p = strtok(path,":");
-	while(p) {
-		sprintf(tmp,"%s/%s",p,cmd);
-		if(access(tmp,X_OK)==0) {
-			free(path);
-			return strdup(tmp);
-		}
-		p = strtok(0,":");
-	}
-
-	free(path);
-	return 0;
 }
 
 static void display_progress(struct work_queue *q)
