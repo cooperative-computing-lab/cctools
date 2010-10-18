e528abd2c6f3a09b1f6e2047b82d183acb84d716
dthain <dthain@nd.edu>
2010-10-18 20:02:39 +0000

    Linking executables is no longer required.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@907 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/test/test_example.sh
+++ sand/test/test_example.sh
@@ -16,7 +16,6 @@ wpid=$!
 echo "Worker is process $wpid"
 
 echo "Starting filter master"
-ln -s `which sand_filter_mer_seq` .
 sand_filter_master -s 10 -p $port test_20.cfa test_20.cand -d all -o filter.log || { echo "Error in filtering."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking filtering results..."
@@ -29,7 +28,6 @@ echo "Checking Smith-Waterman results"
 diff --brief test_20.sw.ovl test_20.sw.right && echo "Files test_20.sw.ovl and test_20.sw.right are the same" || { kill -9 $wpid ; exit 1 ; }
 
 echo "Starting banded assembly..."
-ln -s `which sand_banded_alignment` .
 sand_align_master -p $port sand_align_kernel -e "a banded" test_20.cand test_20.cfa test_20.banded.ovl -d all -o banded.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking banded results"
