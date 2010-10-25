99ea3806345c46d9e28132fa83b18ffc045302c6
lyu2 <lyu2@nd.edu>
2010-10-25 21:26:28 +0000

    using Peter's test framework
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@944 a4d8336d-3463-0410-8bba-c098c45d37a8

---
+++ sand/test/TR_002_test_example.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+. ../../dttools/src/test_runner.common.sh
+
+prepare()
+{
+    cd ../src/; make
+	cd ../test/filter_verification
+   	./gen_random_sequence.pl
+	./gen_random_reads.pl
+	exec ./gen_candidates.sh
+}
+
+run()
+{
+	cd filter_verification
+	exec ./verify_candidates.pl
+}
+
+clean()
+{
+	cd filter_verification
+	exec ./cleanup.sh
+    exit 0
+}
+
+dispatch $@
--- sand/test/filter_verification/cleanup.sh
+++ sand/test/filter_verification/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -f random.seq random_revcom.seq random.fa random.cfa random.cand filter.log worker.log
+rm -f random.seq random_revcom.seq random.fa random.cfa random.cand filter.log filter.log.old worker.log
 
