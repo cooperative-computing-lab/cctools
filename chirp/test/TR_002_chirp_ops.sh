cc528844829e370dd125fd243471728861abf763
pbui <pbui@nd.edu>
2010-11-01 14:00:02 +0000

    chirp: test more operations
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@955 a4d8336d-3463-0410-8bba-c098c45d37a8

--- chirp/test/TR_002_chirp_ops.sh
+++ chirp/test/TR_002_chirp_ops.sh
@@ -16,7 +16,23 @@ prepare()
 
 run()
 {
-    exec ../src/chirp localhost:9095 df -g 
+    exec ../src/chirp localhost:9095 <<EOF
+help
+df -g
+audit -r
+whoami
+whoareyou localhost:9095
+ls
+mkdir _test
+ls
+cd _test
+ls
+put /etc/hosts hosts.txt
+cat hosts.txt
+getacl
+localpath hosts.txt
+exit
+EOF
 }
 
 clean()
@@ -25,6 +41,7 @@ clean()
     rm -f $TEST_FILE
     rm -f $PID_FILE
     rm -f .__acl
+    rm -fr _test
     exit 0
 }
 
