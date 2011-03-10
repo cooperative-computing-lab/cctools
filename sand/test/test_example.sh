72deb3dd97dbe30f5b076dff7301602c544a73fd
dthain <dthain@nd.edu>
2011-03-10 05:50:04 +0000

    Re-ordered command lines so options come first; this is apparently required by getopt on macos.  There is still at least one but on macos somewhere here yet.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1135 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/test/test_example.sh
+++ sand/test/test_example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-port=9091
+port=9092
 
 PATH=../src:../../dttools/src:$PATH
 export PATH
@@ -16,19 +16,19 @@ wpid=$!
 echo "Worker is process $wpid"
 
 echo "Starting filter master"
-sand_filter_master -s 10 -p $port test_20.cfa test_20.cand -d all -o filter.log || { echo "Error in filtering."; kill -9 $wpid; exit 1 ; }
+sand_filter_master -s 10 -p $port -d all -o filter.log test_20.cfa test_20.cand || { echo "Error in filtering."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking filtering results..."
 diff --brief test_20.cand test_20.cand.right && echo "Candidates match" || { kill -9 $wpid ; exit 1; }
 
 echo "Starting Smith-Waterman assembly..."
-sand_align_master -p $port sand_align_kernel -e "-a sw" test_20.cand test_20.cfa test_20.sw.ovl -d all -o sw.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -d all -o sw.log -p $port -e "-a sw" sand_align_kernel test_20.cand test_20.cfa test_20.sw.ovl || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking Smith-Waterman results"
 diff --brief test_20.sw.ovl test_20.sw.right && echo "Files test_20.sw.ovl and test_20.sw.right are the same" || { kill -9 $wpid ; exit 1 ; }
 
 echo "Starting banded assembly..."
-sand_align_master -p $port sand_align_kernel -e "-a banded" test_20.cand test_20.cfa test_20.banded.ovl -d all -o banded.log || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -d all -o banded.log -p $port -e "-a banded" sand_align_kernel test_20.cand test_20.cfa test_20.banded.ovl || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking banded results"
 diff --brief test_20.banded.ovl test_20.banded.right && echo "Files test_20.banded.ovl and test_20.banded.right are the same" || { kill -9 $wpid ; exit 1 ; }
