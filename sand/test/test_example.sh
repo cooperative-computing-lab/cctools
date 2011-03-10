d875a83abe580fda6b1ae032895e099238f3f6b6
dthain <dthain@nd.edu>
2011-03-10 17:09:08 +0000

    Removing valgrind, accidentally left in.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1137 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/test/test_example.sh
+++ sand/test/test_example.sh
@@ -19,19 +19,19 @@ wpid=$!
 echo "Worker is process $wpid"
 
 echo "Starting filter master"
-valgrind sand_filter_master -s 10 -p $port -d all -o filter.log test_20.cfa test_20.cand || { echo "Error in filtering."; kill -9 $wpid; exit 1 ; }
+sand_filter_master -s 10 -p $port -d all -o filter.log test_20.cfa test_20.cand || { echo "Error in filtering."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking filtering results..."
 diff --brief test_20.cand test_20.cand.right && echo "Candidates match" || { kill -9 $wpid ; exit 1; }
 
 echo "Starting Smith-Waterman assembly..."
-valgrind sand_align_master -d all -o sw.log -p $port -e "-a sw" sand_align_kernel test_20.cand test_20.cfa test_20.sw.ovl || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -d all -o sw.log -p $port -e "-a sw" sand_align_kernel test_20.cand test_20.cfa test_20.sw.ovl || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking Smith-Waterman results"
 diff --brief test_20.sw.ovl test_20.sw.right && echo "Files test_20.sw.ovl and test_20.sw.right are the same" || { kill -9 $wpid ; exit 1 ; }
 
 echo "Starting banded assembly..."
-valgrind sand_align_master -d all -o banded.log -p $port -e "-a banded" sand_align_kernel test_20.cand test_20.cfa test_20.banded.ovl || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
+sand_align_master -d all -o banded.log -p $port -e "-a banded" sand_align_kernel test_20.cand test_20.cfa test_20.banded.ovl || { echo "Error in alignment."; kill -9 $wpid; exit 1 ; }
 
 echo "Checking banded results"
 diff --brief test_20.banded.ovl test_20.banded.right && echo "Files test_20.banded.ovl and test_20.banded.right are the same" || { kill -9 $wpid ; exit 1 ; }
