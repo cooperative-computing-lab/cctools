86798abe61869a61456c87c19c2a4fef836630dd
pbui <pbui@nd.edu>
2011-03-11 02:42:01 +0000

    Fix if there are no ports in 9000 range open, try to fix OSX
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1148 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/test_runner.common.sh
+++ dttools/src/test_runner.common.sh
@@ -22,5 +22,20 @@ dispatch()
 
 find_free_port()
 {
-    netstat --numeric-ports --listening --numeric --protocol inet | grep tcp | awk '{print $4}' | grep 0.0.0.0 | awk -vFS=: '{print $2}' | sort --numeric-sort | grep --extended-regexp '9[0-9]{3}' | awk 'BEGIN {n = 9000} {if (n != $1) { print n; exit; } else { n = n+1; } }'
+    case `uname -s` in
+    	Darwin)
+    	netstat -n -f inet | grep tcp | awk '{print $4}' | awk -F . '{print $5}' | sort -n | awk 'BEGIN {n = 9000} {if (n != $1) { print n; exit; } else { n = n+1; } }'
+    	;;
+    	Linux)
+    	netstat --numeric-ports --listening --numeric --protocol inet | grep tcp | awk '{print $4}' | grep 0.0.0.0 | awk -vFS=: '{print $2}' | sort --numeric-sort | awk 'BEGIN {n = 9000} {if (n != $1) { print n; exit; } else { n = n+1; } }'
+    	;;
+    	*)
+    	netstat --numeric-ports --listening --numeric --protocol inet | grep tcp | awk '{print $4}' | grep 0.0.0.0 | awk -vFS=: '{print $2}' | sort --numeric-sort | awk 'BEGIN {n = 9000} {if (n != $1) { print n; exit; } else { n = n+1; } }'
+    	;;
+    esac
 }
+
+# For OS X
+if ! echo $PATH | grep /sbin > /dev/null 2>&1; then
+    export PATH=$PATH:/usr/sbin:/sbin
+fi
