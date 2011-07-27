1f1af8209125615dfad25678f4417ad9bb21935c
dthain <dthain@nd.edu>
2011-07-27 22:28:23 +0000

    Fixed bug in server-side access: the flags got converted from unix to chirp, but then passed right back to access.  (This should be applied to the trunk.)
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/branches/dthain/reorg@1391 a4d8336d-3463-0410-8bba-c098c45d37a8

--- chirp/src/chirp_server.c
+++ chirp/src/chirp_server.c
@@ -1256,13 +1256,13 @@ static void chirp_handler(struct link *l, const char *addr, const char *subject)
 		} else if(sscanf(line, "access %s %lld", path, &flags) == 2) {
 			if(!chirp_path_fix(path))
 				goto failure;
-			flags = chirp_acl_from_access_flags(flags);
+			int chirp_flags = chirp_acl_from_access_flags(flags);
 			/* If filename is a directory, then we change execute flags to list flags. */
-			if(cfs_isdir(path) && (flags & CHIRP_ACL_EXECUTE)) {
-				flags ^= CHIRP_ACL_EXECUTE; /* remove execute flag */
-				flags |= CHIRP_ACL_LIST; /* change to list */
+			if(cfs_isdir(path) && (chirp_flags & CHIRP_ACL_EXECUTE)) {
+				chirp_flags ^= CHIRP_ACL_EXECUTE; /* remove execute flag */
+				chirp_flags |= CHIRP_ACL_LIST; /* change to list */
 			}
-			if(!chirp_acl_check(path, subject, flags))
+			if(!chirp_acl_check(path, subject, chirp_flags))
 				goto failure;
 			result = chirp_alloc_access(path, flags);
 		} else if(sscanf(line, "chmod %s %lld", path, &mode) == 2) {
