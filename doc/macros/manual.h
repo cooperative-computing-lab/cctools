0caa8382b4229a08e54713dc58cb1e2c829f9d9f
pbui <pbui@nd.edu>
2011-09-28 05:54:44 +0000

    manual: add changecom to allow for # in documentation
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1465 a4d8336d-3463-0410-8bba-c098c45d37a8

--- doc/macros/manual.h
+++ doc/macros/manual.h
@@ -1,3 +1,4 @@
 ifdef(`HTML',`include(manual_html.h)',`include(manual_man.h)')dnl
+changecom(`@@')dnl
 define(COPYRIGHT_BOILERPLATE,The Cooperative Computing Tools are Copyright (C) 2003-2004 Douglas Thain and Copyright (C) 2005-2011 The University of Notre Dame.  This software is distributed under the GNU General Public License.  See the file COPYING for details.)dnl
 dnl
