7baabe20d6515456db4170cf8c743bab8ed4055b
dthain <dthain@nd.edu>
2011-10-11 14:03:04 +0000

    Fixed up some doc++ errors.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1525 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/mpi_queue.c
+++ dttools/src/mpi_queue.c
@@ -1,3 +1,9 @@
+/*
+Copyright (C) 2011- The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file COPYING for details.
+*/
+
 #include "mpi_queue.h"
 #include "link.h"
 #include "debug.h"
