7756467ec6bfd68cac51be193ea648a3b357bb87
pdonnel3 <pdonnel3@nd.edu>
2011-11-08 17:49:06 +0000

    added missing pfs_search.h
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/branches/pdonnel3/search-syscall@1605 a4d8336d-3463-0410-8bba-c098c45d37a8

---
+++ parrot/src/pfs_search.h
@@ -0,0 +1,33 @@
+/*
+Copyright (C) 2011- The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file COPYING for details.
+*/
+
+#ifndef PFS_SEARCH_H
+#define PFS_SEARCH_H
+
+#include <stdlib.h>
+
+struct parrot_search_args {
+  const char *paths;
+  const char *pattern;
+  char *buffer;
+  size_t buffer_length;
+  struct stat *stats;
+  size_t stats_length;
+  int flags;
+};
+
+#define PFS_SEARCH_DELIMITER   ':'
+#define PFS_SEARCH_DEPTH_MAX   200
+
+#define PFS_SEARCH_STOPATFIRST (1<<0)
+#define PFS_SEARCH_RECURSIVE   (1<<1)
+#define PFS_SEARCH_METADATA    (1<<2)
+#define PFS_SEARCH_INCLUDEROOT (1<<3)
+#define PFS_SEARCH_R_OK        (1<<4)
+#define PFS_SEARCH_W_OK        (1<<5)
+#define PFS_SEARCH_X_OK        (1<<6)
+
+#endif
