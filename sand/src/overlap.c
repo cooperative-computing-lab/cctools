2bd8e3aa878e23d70ed8afa1c9e22dd2a30effdd
athrash1 <athrash1@nd.edu>
2012-01-30 21:24:47 +0000

    Added functionality to the sand_align_kernel to output OVL records compatible with Celera 7.0. Also added a runCA_7.0 script to allow SAND and Celera 7.0 to integrate smoothly.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1685 a4d8336d-3463-0410-8bba-c098c45d37a8

--- sand/src/overlap.c
+++ sand/src/overlap.c
@@ -10,6 +10,63 @@ See the file COPYING for details.
 
 #include "overlap.h"
 #include "macros.h"
+void ovl_write(FILE * file, struct alignment *aln, const char * id1, const char * id2)
+{
+	int ahg, bhg;
+	int arh, brh;
+
+	// IDs of overlapping fragments.
+	fprintf(file, "%s ", id1);
+	fprintf(file, "%s ", id2);
+
+	// Orientation
+	fprintf(file, "%c ", aln->ori);
+
+	arh = aln->length1 - aln->end1;      // determine the right portions of sequences that 
+	brh = aln->length2 - aln->end2;      // are not in the alignment
+
+	// calculate overhangs assuming A is on the left
+	ahg = aln->start1 + aln->start2;
+	bhg = (aln->length2-1) - aln->end2;
+	if (bhg == 0) { bhg = aln->end1 - (aln->length1-1); }
+
+	if (aln->start2 <= aln->start1 && aln->end2 <= aln->end1) {  // A is on left or B is inside A
+
+
+	} 
+	else if (aln->start1 <= aln->start2 && aln->end1 <= aln->end2)  { // B is on left or A is inside B
+
+	  // recalculate overhangs given that B is on right.  Main difference is these should be negative
+	  ahg = (aln->start2 + aln->start1) * -1;
+	  bhg = ((aln->length1-1) - aln->end1) * -1;
+	  if (bhg == 0) { bhg = (aln->end1 - (aln->length1-1)) * -1; }
+	  
+
+	}
+
+	// How much each piece hangs off the end. If A is on left (or B is contained), these are positive
+	// If B is on the left, these are negative.
+	fprintf(file, "%d ", ahg);
+	fprintf(file, "%d ", bhg);
+
+	double qual = aln->quality; 
+	qual = qual * 100; 
+	
+	
+	// Celera defines the quality score as (gaps + mismatches) / MIN(end1, end2)
+	fprintf(file, "%.1f %.1f", qual, qual);
+
+	// This is the length of the overlap
+	//fprintf(file, "mno:%d\n", MIN(aln->end1 - aln->start1, aln->end2 - aln->start2));
+	//fprintf(file, "mxo:%d\n", aln->score);
+
+	// Polymorphism count.
+	//fprintf(file, "pct:0\n");  // Again, try to match Celera, where this is set to 0 and unchanged later
+
+	fprintf(file, "\n");
+
+
+}
 
 void overlap_write(FILE * file, struct alignment *aln, const char * id1, const char * id2)
 {
