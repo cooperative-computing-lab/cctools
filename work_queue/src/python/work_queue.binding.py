ad66d949d0feb769904123888b9b85fe8521b57e
dpandiar <dpandiar@nd.edu>
2012-04-12 22:31:22 +0000

    Revert back change 1939. The user should enable catalog mode through specify_master_mode() rather than it being done automatically.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1941 a4d8336d-3463-0410-8bba-c098c45d37a8

--- work_queue/src/python/work_queue.binding.py
+++ work_queue/src/python/work_queue.binding.py
@@ -361,8 +361,7 @@ class WorkQueue(_object):
     # @param self   Reference to the current work queue object.
     # @param name   The new project name.
     def specify_name(self, name):
-		work_queue_specify_master_mode(self._work_queue, WORK_QUEUE_MASTER_MODE_CATALOG)
-		return work_queue_specify_name(self._work_queue, name)
+        return work_queue_specify_name(self._work_queue, name)
 
     ##
     # Change the project priority for the given queue.
