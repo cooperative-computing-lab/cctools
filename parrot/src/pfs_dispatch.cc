cooperative-computing-lab/cctools

commit 2ab62b44e80241e3ad210d551ce699332c6e9e14

Added support for writable memory mapped files, which is required for the CMS software stack. The I/O channel now keeps a reference-counted list of the memory objects loaded. These are referred to by pfs_mmap structures in the pfs_mmap table. On an munmap, the correct map is looked up and flushed through to disk.
git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1846 a4d8336d-3463-0410-8bba-c098c45d37a8

parrot/src/pfs_dispatch.cc
@@ -7,7 +7,6 @@ See the file COPYING for details.
 
 #include "pfs_sysdeps.h"
 #include "pfs_channel.h"
-#include "pfs_channel_cache.h"
 #include "pfs_process.h"
 #include "pfs_sys.h"
 #include "pfs_poll.h"
@@ -125,7 +124,7 @@ static void decode_read( struct pfs_process *p, int entering, int syscall, INT64
 	char *local_addr;
 	
 	if(entering) {
-		if(!pfs_channel_alloc(length,&p->io_channel_offset)) {
+		if(!pfs_channel_alloc(0,length,&p->io_channel_offset)) {
 			divert_to_dummy(p,-ENOMEM);
 			return;
 		}
@@ -196,7 +195,7 @@ static void decode_write( struct pfs_process *p, int entering, int syscall, INT6
 	if(entering) {
 		void *uaddr = POINTER(args[1]);
 		INT64_T length = args[2];
-		if(!pfs_channel_alloc(length,&p->io_channel_offset)) {
+		if(!pfs_channel_alloc(0,length,&p->io_channel_offset)) {
 			divert_to_dummy(p,-ENOMEM);
 			return;
 		}
@@ -429,7 +428,7 @@ static void decode_stat( struct pfs_process *p, int entering, int syscall, INT64
 		}
 
 		if(p->syscall_result>=0) {
-			if(!pfs_channel_alloc(sizeof(kbuf64),&p->io_channel_offset)) {
+			if(!pfs_channel_alloc(0,sizeof(kbuf64),&p->io_channel_offset)) {
 				divert_to_dummy(p,-ENOMEM);
 			} else {
 				local_addr = pfs_channel_base() + p->io_channel_offset;
@@ -1235,38 +1234,52 @@ void decode_mmap( struct pfs_process *p, int syscall, int entering, INT64_T *arg
 		if(flags&MAP_ANONYMOUS) {
 			/* great, just do it. */
 			debug(D_SYSCALL,"mmap skipped b/c anonymous");
+		}
+
+		channel_offset = pfs_mmap_create(fd,source_offset,length,prot,flags);
+		if(channel_offset<0) {
+			divert_to_dummy(p,-errno);
+			return;
+		}
+
+		nargs[3] = flags & ~MAP_DENYWRITE;
+		nargs[4] = pfs_channel_fd();
+		nargs[5] = channel_offset+source_offset;
+
+		debug(D_SYSCALL,"channel_offset=0x%llx source_offset=0x%llx total=0x%x",channel_offset,source_offset,nargs[5]);
+
+		if(p->syscall==SYSCALL32_mmap) {
+			tracer_copy_out(p->tracer,nargs,POINTER(args[0]),sizeof(nargs));
 		} else {
-			char file_name[PFS_PATH_MAX];
-
-			if(pfs_get_full_name(fd,file_name)!=0) {
-				debug(D_SYSCALL,"mmap failed name: %s",strerror(errno));
-				divert_to_dummy(p,-errno);
-				return;
+			nargs[5] = nargs[5] / getpagesize();
+			for(int i=0;i<TRACER_ARGS_MAX;i++) {
+				args[i] = nargs[i];
 			}
+			tracer_args_set(p->tracer,p->syscall,args,6);
+			p->syscall_args_changed = 1;
+		}	
 
-			if(pfs_channel_cache_alloc(file_name,fd,&length,&channel_offset)) {
-				nargs[3] = flags & ~MAP_DENYWRITE;
-				nargs[4] = pfs_channel_fd();
-				nargs[5] = channel_offset+source_offset;
+		debug(D_SYSCALL,"mmap changed: fd=%d addr=0x%x",nargs[4],nargs[5]);
+	} else {
+		/*
+		On exit from the system call, retrieve the logical address of
+		the mmap as returned to the application.  Then, update the 
+		mmap record that corresponds to the proper channel offset.
+		On failure, we must unmap the object, which will have a logical
+		address of zero because it was never set.
+		*/
 
-				debug(D_SYSCALL,"channel_offset=0x%llx source_offset=0x%llx total=0x%x",channel_offset,source_offset,nargs[5]);
+		if(args[3]&MAP_ANONYMOUS) {
+			// nothing to do
+		} else {
+			tracer_result_get(p->tracer,&p->syscall_result);
 
-				if(p->syscall==SYSCALL32_mmap) {
-					tracer_copy_out(p->tracer,nargs,POINTER(args[0]),sizeof(nargs));
-				} else {
-					nargs[5] = nargs[5] / getpagesize();
-					for(int i=0;i<TRACER_ARGS_MAX;i++) {
-						args[i] = nargs[i];
-					}
-					tracer_args_set(p->tracer,p->syscall,args,6);
-					p->syscall_args_changed = 1;
-				}	
-				debug(D_SYSCALL,"mmap changed: fd=%d addr=0x%x",nargs[4],nargs[5]);
+			if(p->syscall_result!=-1) {
+				pfs_mmap_update(p->syscall_result,0);
 			} else {
-				debug(D_SYSCALL,"mmap failed cache: %s",strerror(errno));
-				divert_to_dummy(p,-errno);
-				return;
+				pfs_mmap_delete(0,0);
 			}
+
 		}
 	}
 }
@@ -1794,19 +1807,11 @@ void decode_syscall( struct pfs_process *p, int entering )
 			decode_mmap(p,p->syscall,entering,args);
 			break;
 
-		/*
-		XXX Incomplete but acceptable for now.
-		We permit the application to munmap whatever it likes.
-		This might be an anonymous map that we don't track,
-		or it could be file.  In the latter case, we would like
-		to decrement the reference count.  At the moment, we
-		can't tell the difference, nor can we reverse map the
-		application's space into our own address space.
-		Once loaded, mmaped objects just stay in until the
-		parent pfs process dies.
-		*/
-
 		case SYSCALL32_munmap:
+			if(entering) {
+				p->syscall_result = pfs_mmap_delete(args[0],args[1]);
+				divert_to_dummy(p,p->syscall_result);
+			}
 			break;
 
 		/*

parrot/src/Makefile
@@ -2,7 +2,7 @@ include ../../Makefile.config
 include ../../Makefile.rules
 
 TARGETS = parrot_run libparrot_client.a libparrot_helper.so parrot_lsalloc parrot_mkalloc parrot_getacl parrot_setacl parrot_whoami parrot_locate parrot_md5 parrot_cp parrot_timeout
-PARROT_OBJECTS =  pfs_main.o pfs_poll.o tracer.o pfs_dispatch.o pfs_dispatch64.o pfs_process.o pfs_channel_cache.o pfs_channel.o pfs_sys.o pfs_table.o pfs_resolve.o pfs_service.o pfs_file.o pfs_file_cache.o pfs_dir.o pfs_dircache.o pfs_pointer.o pfs_location.o ibox_acl.o pfs_service_local.o pfs_service_http.o pfs_service_grow.o pfs_service_chirp.o pfs_service_multi.o pfs_service_nest.o pfs_service_ftp.o pfs_service_gfal.o pfs_service_lfc.o pfs_service_rfio.o pfs_service_dcap.o pfs_service_irods.o irods_reli.o pfs_service_hdfs.o pfs_service_bxgrid.o pfs_service_s3.o pfs_service_xrootd.o pfs_service_cvmfs.o
+PARROT_OBJECTS =  pfs_main.o pfs_poll.o tracer.o pfs_dispatch.o pfs_dispatch64.o pfs_process.o pfs_channel.o pfs_sys.o pfs_table.o pfs_resolve.o pfs_service.o pfs_file.o pfs_file_cache.o pfs_dir.o pfs_dircache.o pfs_pointer.o pfs_location.o ibox_acl.o pfs_service_local.o pfs_service_http.o pfs_service_grow.o pfs_service_chirp.o pfs_service_multi.o pfs_service_nest.o pfs_service_ftp.o pfs_service_gfal.o pfs_service_lfc.o pfs_service_rfio.o pfs_service_dcap.o pfs_service_irods.o irods_reli.o pfs_service_hdfs.o pfs_service_bxgrid.o pfs_service_s3.o pfs_service_xrootd.o pfs_service_cvmfs.o
 
 LOCAL_LDFLAGS=-lchirp -ls3client -ldttools -lftp_lite -ldl ${CCTOOLS_INTERNAL_LDFLAGS}
 

parrot/src/pfs_channel.c
@@ -6,6 +6,8 @@ See the file COPYING for details.
 */
 
 #include "pfs_channel.h"
+
+#include "xxmalloc.h"
 #include "debug.h"
 
 #include <stdlib.h>
@@ -18,6 +20,7 @@ See the file COPYING for details.
 extern char pfs_temp_dir[];
 
 struct entry {
+	char *name;
 	pfs_size_t start;
 	pfs_size_t length;
 	int inuse;
@@ -30,12 +33,18 @@ static int channel_fd=-1;
 static char *channel_base=0;
 static pfs_size_t channel_size;
 
-static struct entry * entry_create( pfs_size_t start, pfs_size_t length, struct entry *prev, struct entry *next )
+static struct entry * entry_create( const char *name, pfs_size_t start, pfs_size_t length, struct entry *prev, struct entry *next )
 {
 	struct entry *e;
 
 	e = malloc(sizeof(*e));
 	if(!e) return 0;
+
+	if(name) {
+		e->name = xxstrdup(name);
+	} else {
+		e->name = 0;
+	}
 
 	e->start = start;
 	e->length = length;
@@ -63,6 +72,7 @@ static void entry_delete( struct entry *e )
 	if(head==e) head = e->next;
 	if(e->prev) e->prev->next = e->next;
 	if(e->next) e->next->prev = e->prev;
+	if(e->name) free(e->name);
 	free(e);
 }
 
@@ -84,7 +94,7 @@ int pfs_channel_init( pfs_size_t size )
 		return 0;
 	}
 
-	head = entry_create(0,size,0,0);
+	head = entry_create(0,0,size,0,0);
 	if(!head) {
 		close(channel_fd);
 		munmap(channel_base,size);
@@ -116,7 +126,7 @@ static pfs_size_t round_up( pfs_size_t x )
 	return x;
 }
 
-int pfs_channel_alloc( pfs_size_t length, pfs_size_t *start )
+int pfs_channel_alloc( const char *name, pfs_size_t length, pfs_size_t *start )
 {
 	struct entry *e = head;
 	struct entry *tail;
@@ -126,16 +136,14 @@ int pfs_channel_alloc( pfs_size_t length, pfs_size_t *start )
 
 	do {
 		if(!e->inuse) {
-			if(e->length==length) {
-				e->inuse = 1;
-				*start = e->start;
-				memset(channel_base+*start+length-page_size,0,page_size);
-				return 1;
-			} else if(e->length>length) {
-				struct entry *f;
-				f = entry_create(e->start+length,e->length-length,e,e->next);
-				if(!f) return 0;
+			if(e->length>=length) {
+				if(e->length>length) {
+					struct entry *f;
+					f = entry_create(0,e->start+length,e->length-length,e,e->next);
+					if(!f) return 0;
+				}
 
+				e->name = name ? xxstrdup(name) : 0;
 				e->length = length;
 				e->inuse = 1;
 				*start = e->start;
@@ -156,16 +164,32 @@ int pfs_channel_alloc( pfs_size_t length, pfs_size_t *start )
 		void *newbase;
 		newbase = mremap(channel_base,channel_size,newsize,MREMAP_MAYMOVE);
 		if(newbase!=MAP_FAILED) {
-			e = entry_create(channel_size,newsize-channel_size,tail,head);
+			e = entry_create(name,channel_size,newsize-channel_size,tail,head);
 			channel_size = newsize;
 			channel_base = newbase;
 			debug(D_CHANNEL,"channel expanded to 0x%x bytes at base 0x%x",(PTRINT_T)newsize,newbase);
-			return pfs_channel_alloc(length,start);
+			return pfs_channel_alloc(name,length,start);
 		}
 		ftruncate64(channel_fd,channel_size);
 	}
 
 	debug(D_CHANNEL|D_NOTICE,"out of channel space: %s",strerror(errno));
+
+	return 0;
+}
+
+int pfs_channel_lookup( const char *name, pfs_size_t *start )
+{
+	struct entry *e = head;
+
+	do {
+		if(e->name && !strcmp(e->name,name)) {
+			e->inuse++;
+			*start = e->start;
+			return 1;
+		}
+		e = e->next;
+	} while(e!=head);
 
 	return 0;
 }
@@ -176,19 +200,26 @@ void pfs_channel_free( pfs_size_t start )
 
 	do {
 		if(e->start==start) {
-			e->inuse=0;
-			if( (e->prev->start < e->start) && !e->prev->inuse) {
-				struct entry *f = e->prev;
-				f->length += e->length;
-				entry_delete(e);
-				e = f;
-			}
-			if( (e->next->start>e->start) && !e->next->inuse) {
-				struct entry *f = e->next;
-				f->start = e->start;
-				f->length += e->length;
-				entry_delete(e);
-				e = f;
+			e->inuse--;
+			if(e->inuse<=0) {
+				e->inuse=0;
+				if(e->name) {
+					free(e->name);
+					e->name = 0;
+				}
+				if( (e->prev->start < e->start) && !e->prev->inuse) {
+					struct entry *f = e->prev;
+					f->length += e->length;
+					entry_delete(e);
+					e = f;
+				}
+				if( (e->next->start>e->start) && !e->next->inuse) {
+					struct entry *f = e->next;
+					f->start = e->start;
+					f->length += e->length;
+					entry_delete(e);
+					e = f;
+				}
 			}
 			return;
 		}

parrot/src/pfs_channel.h
@@ -19,7 +19,8 @@ int    pfs_channel_init( pfs_size_t size );
 char * pfs_channel_base();
 int    pfs_channel_fd();
 
-int    pfs_channel_alloc( pfs_size_t length, pfs_size_t *start );
+int    pfs_channel_lookup( const char *name, pfs_size_t *start );
+int    pfs_channel_alloc( const char *name, pfs_size_t length, pfs_size_t *start );
 void   pfs_channel_free( pfs_size_t start );
 
 #ifdef __cplusplus

parrot/src/pfs_channel_cache.c
@@ -1,221 +1,0 @@
-/*
-Copyright (C) 2003-2004 Douglas Thain and the University of Wisconsin
-Copyright (C) 2005- The University of Notre Dame
-This software is distributed under the GNU General Public License.
-See the file COPYING for details.
-*/
-
-#define _POSIX_MAPPED_FILES
-#define POSIX_SYNCHRONIZED_IO
-
-
-#include "pfs_channel_cache.h"
-#include "pfs_channel.h"
-#include "debug.h"
-#include "pfs_sys.h"
-
-#include "hash_table.h"
-#include "macros.h"
-
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/mman.h>
-
-static struct hash_table * table=0;
-
-struct entry {
-	char *name;
-	pfs_size_t length;
-	pfs_size_t start;
-	int dirty;
-	int numrefs;
-};
-
-static struct entry * entry_create( const char *name, pfs_size_t length, pfs_size_t start )
-{
-	struct entry *e = malloc(sizeof(*e));
-	if(!e) return 0;
-
-	e->name = strdup(name);
-	if(!e->name) {
-		free(e);
-		return 0;
-	}
-
-	e->length = length;
-	e->start = start;
-	e->dirty = 0;
-	e->numrefs = 1;
-
-	return e;
-}
-
-static void entry_delete( struct entry *e )
-{
-	free(e->name);
-	free(e);
-}
-
-static void entry_deref( struct entry *e )
-{
-	e->numrefs--;
-	debug(D_CHANNEL,"deref %s start 0x%llx length 0x%llx",e->name);
-	if(e->numrefs<=0) {
-		debug(D_CHANNEL,"removed %s start 0x%llx length 0x%llx",e->name,e->start,e->length);
-		hash_table_remove(table,e->name);
-		pfs_channel_free(e->start);
-		entry_delete(e);
-	}
-}
-
-static int load_file( const char *name, int fd, pfs_size_t length, pfs_size_t start, pfs_size_t blocksize )
-{
-	pfs_size_t data_left = length;
-	pfs_size_t offset = 0;
-	pfs_size_t chunk, actual;
-
-	debug(D_CHANNEL,"loading: %s",name);
-
-	while(data_left>0) {
-		chunk = MIN(data_left,blocksize);
-		actual = pfs_pread(fd,pfs_channel_base()+start+offset,chunk,offset);
-		if(actual>0) {
-			offset += actual;
-			data_left -= actual;
-		} else if(actual==0) {
-			memset(pfs_channel_base()+start+offset,0,data_left);
-			offset += data_left;
-			data_left = 0;
-		} else {
-			break;
-		}
-	}
-
-	if(data_left) {
-		debug(D_CHANNEL,"loading: failed: %s",strerror(errno));
-		return 0;
-	} else {
-		/*
-		we must invalidate the others' mapping of this file,
-		otherwise, they will see old data that was in this place.
-		*/
-		msync(pfs_channel_base()+start,length,MS_INVALIDATE);
-		return 1;
-	}
-}
-
-int pfs_channel_cache_alloc( const char *name, int fd, pfs_size_t *length, pfs_size_t *start )
-{
-	struct entry *e;
-	struct pfs_stat buf;
-	int result;
-
-	if(!table) {
-		table = hash_table_create(0,0);
-		if(!table) return 0;
-	}
-
-	e = hash_table_lookup(table,name);
-	if(e) {
-		*start = e->start;
-		*length = e->length;
-		e->numrefs++;
-		debug(D_CHANNEL,"addref %s start 0x%llx length 0x%llx",name,e->start,e->length);
-		return 1;
-	} else {
-		result = pfs_fstat(fd,&buf);
-		if(result!=0) return 0;
-
-		*length = buf.st_size;
-
-		if(pfs_channel_alloc(*length,start)) {
-			if(load_file(name,fd,*length,*start,buf.st_blksize)) {
-				e = entry_create(name,*length,*start);
-				if(e) {
-					if(hash_table_insert(table,name,e)) {
-						debug(D_CHANNEL,"added %s start 0x%llx length 0x%llx",name,*start,*length);
-						return 1;
-					}
-					entry_delete(e);
-				}
-			}
-			pfs_channel_free(*start);
-		}
-	}
-
-	return 0;
-}
-
-int pfs_channel_cache_freename( const char *name )
-{
-	struct entry *e;
-	e = hash_table_lookup(table,name);
-	if(e) {
-		entry_deref(e);
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-void  pfs_channel_cache_freeaddr( pfs_size_t start, pfs_size_t length )
-{
-	char *key;
-	struct entry *e;
-
-	hash_table_firstkey(table);
-	while(hash_table_nextkey(table,&key,(void**)&e)) {
-		if(e->start==start && e->length==length) {
-			entry_deref(e);
-			break;
-		}
-	}
-}
-
-int   pfs_channel_cache_make_dirty( const char *name )
-{
-	struct entry *e;
-	e = hash_table_lookup(table,name);
-	if(e) {
-		e->dirty = 1;
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-int   pfs_channel_cache_is_dirty( const char *name )
-{
-	struct entry *e;
-	e = hash_table_lookup(table,name);
-	if(e) {
-		return e->dirty;
-	} else {
-		return 0;
-	}
-}
-
-int   pfs_channel_cache_refs( const char *name )
-{
-	struct entry *e;
-	e = hash_table_lookup(table,name);
-	if(e) {
-		return e->numrefs;
-	} else {
-		return 0;
-	}
-}
-
-int pfs_channel_cache_start( const char *name, pfs_size_t *start )
-{
-	struct entry *e;
-	e = hash_table_lookup(table,name);
-	if(e) {
-		*start = e->start;
-		return 1;
-	} else {
-		return 0;
-	}
-}
-

parrot/src/pfs_channel_cache.h
@@ -1,30 +1,0 @@
-/*
-Copyright (C) 2003-2004 Douglas Thain and the University of Wisconsin
-Copyright (C) 2005- The University of Notre Dame
-This software is distributed under the GNU General Public License.
-See the file COPYING for details.
-*/
-
-#ifndef PFS_CHANNEL_CACHE_H
-#define PFS_CHANNEL_CACHE_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "pfs_types.h"
-
-int   pfs_channel_cache_alloc( const char *name, int fd, pfs_size_t *length, pfs_size_t *start );
-int   pfs_channel_cache_freename( const char *name );
-void  pfs_channel_cache_freeaddr( pfs_size_t start, pfs_size_t length );
-
-int   pfs_channel_cache_make_dirty( const char *name );
-int   pfs_channel_cache_is_dirty( const char *name );
-int   pfs_channel_cache_refs( const char *name );
-int   pfs_channel_cache_start( const char *name, pfs_size_t *start );
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif

parrot/src/pfs_dispatch64.cc
@@ -19,7 +19,6 @@ int pfs_dispatch64( struct pfs_process *p, INT64_T signum )
 
 #include "pfs_sysdeps64.h"
 #include "pfs_channel.h"
-#include "pfs_channel_cache.h"
 #include "pfs_process.h"
 #include "pfs_sys.h"
 #include "pfs_poll.h"
@@ -137,7 +136,7 @@ static void decode_read( struct pfs_process *p, INT64_T entering, INT64_T syscal
 	char *local_addr;
 	
 	if(entering) {
-		if(!pfs_channel_alloc(length,&p->io_channel_offset)) {
+		if(!pfs_channel_alloc(0,length,&p->io_channel_offset)) {
 			divert_to_dummy(p,-ENOMEM);
 			return;
 		}
@@ -206,7 +205,7 @@ static void decode_write( struct pfs_process *p, INT64_T entering, INT64_T sysca
 	if(entering) {
 		void *uaddr = POINTER(args[1]);
 		INT64_T length = args[2];
-		if(!pfs_channel_alloc(length,&p->io_channel_offset)) {
+		if(!pfs_channel_alloc(0,length,&p->io_channel_offset)) {
 			divert_to_dummy(p,-ENOMEM);
 			return;
 		}
@@ -428,7 +427,7 @@ static void decode_stat( struct pfs_process *p, INT64_T entering, INT64_T syscal
 		}
 
 		if(p->syscall_result>=0) {
-			if(!pfs_channel_alloc(sizeof(kbuf),&p->io_channel_offset)) {
+			if(!pfs_channel_alloc(0,sizeof(kbuf),&p->io_channel_offset)) {
 				divert_to_dummy(p,-ENOMEM);
 			} else {
 				local_addr = pfs_channel_base() + p->io_channel_offset;
@@ -468,7 +467,7 @@ static void decode_statfs( struct pfs_process *p, INT64_T entering, INT64_T sysc
 		}
 
 		if(p->syscall_result>=0) {
-			if(!pfs_channel_alloc(sizeof(kbuf),&p->io_channel_offset)) {
+			if(!pfs_channel_alloc(0,sizeof(kbuf),&p->io_channel_offset)) {
 				divert_to_dummy(p,-ENOMEM);
 			} else {
 				local_addr = pfs_channel_base() + p->io_channel_offset;
@@ -838,7 +837,6 @@ static void decode_execve( struct pfs_process *p, INT64_T entering, INT64_T sysc
 	}
 }
 
-
 /*
 Memory mapped files are loaded into the channel,
 the whole file regardless of what portion is actually
@@ -848,53 +846,64 @@ mapped.  The channel cache keeps a reference count.
 static void decode_mmap( struct pfs_process *p, INT64_T syscall, INT64_T entering, INT64_T *args )
 {
 	if(entering) {
-		INT64_T addr, orig_length, prot, fd, flags;
-		pfs_size_t length, channel_offset, source_offset;
+		INT64_T addr, prot, fd, flags;
+		pfs_size_t length, source_offset, channel_offset;
 		INT64_T nargs[TRACER_ARGS_MAX];
 
 		memcpy(nargs,p->syscall_args,sizeof(p->syscall_args));
 
 		addr = nargs[0];
-		orig_length = nargs[1];
+		length = nargs[1];
 		prot = nargs[2];
 		flags = nargs[3];
 		fd = nargs[4];
-		//source_offset = nargs[5]*getpagesize();
 		source_offset = nargs[5];
 
-		debug(D_SYSCALL,"mmap addr=0x%x len=0x%x prot=0x%x flags=0x%x fd=%d offset=0x%llx",addr,orig_length,prot,flags,fd,source_offset);
+		debug(D_SYSCALL,"mmap addr=0x%x len=0x%x prot=0x%x flags=0x%x fd=%d offset=0x%llx",addr,length,prot,flags,fd,source_offset);
 
-		if(flags&MAP_ANONYMOUS) {
-			/* great, just do it. */
+		if(flags & MAP_ANONYMOUS) {
 			debug(D_SYSCALL,"mmap skipped b/c anonymous");
+			return;
+		}
+
+		channel_offset = pfs_mmap_create(fd,source_offset,length,prot,flags);
+		if(channel_offset<0) {
+			divert_to_dummy(p,-errno);
+			return;
+		}
+
+		nargs[3] = flags & ~MAP_DENYWRITE;
+		nargs[4] = pfs_channel_fd();
+		nargs[5] = channel_offset+source_offset;
+
+		debug(D_SYSCALL,"channel_offset=0x%llx source_offset=0x%llx total=0x%x",channel_offset,source_offset,nargs[5]);
+		debug(D_SYSCALL,"mmap changed: fd=%d offset=0x%x",nargs[4],nargs[5]);
+
+	      	tracer_args_set(p->tracer,p->syscall,nargs,6);
+		p->syscall_args_changed = 1;
+	} else {
+		/*
+		On exit from the system call, retrieve the logical address of
+		the mmap as returned to the application.  Then, update the 
+		mmap record that corresponds to the proper channel offset.
+		On failure, we must unmap the object, which will have a logical
+		address of zero because it was never set.
+		*/
+
+		if(args[3]&MAP_ANONYMOUS) {
+			// nothing to do
 		} else {
-			char file_name[PFS_PATH_MAX];
+			tracer_result_get(p->tracer,&p->syscall_result);
 
-			if(pfs_get_full_name(fd,file_name)!=0) {
-				debug(D_SYSCALL,"mmap failed name: %s",strerror(errno));
-				divert_to_dummy(p,-errno);
-				return;
-			}
-
-			if(pfs_channel_cache_alloc(file_name,fd,&length,&channel_offset)) {
-				nargs[3] = flags & ~MAP_DENYWRITE;
-				nargs[4] = pfs_channel_fd();
-				nargs[5] = channel_offset+source_offset;
-
-				debug(D_SYSCALL,"channel_offset=0x%llx source_offset=0x%llx total=0x%x",channel_offset,source_offset,nargs[5]);
-
-				debug(D_SYSCALL,"mmap changed: fd=%d offset=0x%x",nargs[4],nargs[5]);
-
-       				//nargs[5] = nargs[5] / getpagesize();
-	       			tracer_args_set(p->tracer,p->syscall,nargs,6);
-		       		p->syscall_args_changed = 1;
+			if(p->syscall_result!=-1) {
+				pfs_mmap_update(p->syscall_result,0);
 			} else {
-				debug(D_SYSCALL,"mmap failed cache: %s",strerror(errno));
-				divert_to_dummy(p,-errno);
-				return;
+				pfs_mmap_delete(0,0);
 			}
+
 		}
 	}
+
 }
 
 static INT64_T decode_ioctl_siocgifconf( struct pfs_process *p, INT64_T fd, INT64_T cmd, void *uaddr )
@@ -1677,19 +1686,12 @@ static void decode_syscall( struct pfs_process *p, INT64_T entering )
 			decode_mmap(p,p->syscall,entering,args);
 			break;
 
-		/*
-		XXX Incomplete but acceptable for now.
-		We permit the application to munmap whatever it likes.
-		This might be an anonymous map that we don't track,
-		or it could be file.  In the latter case, we would like
-		to decrement the reference count.  At the moment, we
-		can't tell the difference, nor can we reverse map the
-		application's space into our own address space.
-		Once loaded, mmaped objects just stay in until the
-		parent pfs process dies.
-		*/
 
 		case SYSCALL64_munmap:
+			if(entering) {
+				p->syscall_result = pfs_mmap_delete(args[0],args[1]);
+				divert_to_dummy(p,p->syscall_result);
+			}
 			break;
 
 		/*

parrot/src/pfs_mmap.h
@@ -1,0 +1,57 @@
+/*
+Copyright (C) 2012- The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file COPYING for details.
+*/
+
+#ifndef PFS_MMAP_H
+#define PFS_MMAP_H
+
+#include "pfs_types.h"
+#include "pfs_file.h"
+
+class pfs_mmap {
+public:
+	pfs_mmap( pfs_file *_file, pfs_size_t _logical_addr, pfs_size_t _channel_offset, pfs_size_t _map_length, pfs_size_t _file_offset, int _prot, int _flags )
+	{
+		file = _file;
+		logical_addr = _logical_addr;
+		channel_offset = _channel_offset;
+		map_length = _map_length;
+		file_offset = _file_offset;
+		prot = _prot;
+		flags = _flags;
+		file->addref();
+	}
+
+	pfs_mmap( pfs_mmap * m ) {
+		file = m->file;
+		logical_addr = m->logical_addr;
+		channel_offset = m->channel_offset;
+		map_length = m->map_length;
+		file_offset = m->file_offset;
+		prot = m->prot;
+		flags = m->flags;
+		file->addref();
+	}
+
+	~pfs_mmap()
+	{
+		file->delref();
+		if(file->refs()<1) {
+			file->close();
+			delete file;
+		}
+	}
+
+	pfs_file   *file;
+	pfs_size_t logical_addr;
+	pfs_size_t channel_offset;
+	pfs_size_t map_length;
+	pfs_size_t file_offset;
+	int	   prot;
+	int	   flags;
+	pfs_mmap   *next;
+};
+
+#endif

commit d344e4de2674d752b200702d23a97228bac05bb0

Merging r1952 from /trunk: ------------------------------------------------------------------------ r1952 | pdonnel3 | 2012-04-17 02:49:35 -0400 (Tue, 17 Apr 2012) | 3 lines
Add missing return for MAP_ANONYMOUS check caused by r1846.
I found this bug when trying to do `parrot_run /bin/bash`.

------------------------------------------------------------------------

git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/release/3.4.3@1953 a4d8336d-3463-0410-8bba-c098c45d37a8

parrot/src/pfs_dispatch.cc
@@ -1232,8 +1232,8 @@ void decode_mmap( struct pfs_process *p, int syscall, int entering, INT64_T *arg
 		debug(D_SYSCALL,"mmap addr=0x%x len=0x%x prot=0x%x flags=0x%x fd=%d offset=0x%llx",addr,length,prot,flags,fd,source_offset);
 
 		if(flags&MAP_ANONYMOUS) {
-			/* great, just do it. */
 			debug(D_SYSCALL,"mmap skipped b/c anonymous");
+			return;
 		}
 
 		channel_offset = pfs_mmap_create(fd,source_offset,length,prot,flags);