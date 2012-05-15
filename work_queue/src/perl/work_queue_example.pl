f2ae67e8fb601f503f6d08938076837482220366
dpandiar <dpandiar@nd.edu>
2012-05-15 19:49:18 +0000

    Make perl example equivalent to work_queue_example.c found under dttools/
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2016 a4d8336d-3463-0410-8bba-c098c45d37a8

--- work_queue/src/perl/work_queue_example.pl
+++ work_queue/src/perl/work_queue_example.pl
@@ -1,32 +1,42 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl 
 
 use strict;
 use work_queue;
 
-my $wq = work_queue::work_queue_create(int(*work_queue::WORK_QUEUE_RANDOM_PORT));
-
-work_queue::work_queue_specify_name($wq, "work_queue_perl");
-
-print work_queue::work_queue_port($wq) . "\n";
-
-for (my $i = 0; $i < 1000; $i++) {
-    my $task = work_queue::work_queue_task_create("date");
-
-    work_queue::work_queue_task_specify_algorithm($task, int(*work_queue::WORK_QUEUE_SCHEDULE_FCFS));
-    work_queue::work_queue_task_specify_tag($task, "current date/time " . $i);
-    work_queue::work_queue_task_specify_input_file($task, "/bin/date", "date");
-
-    print "$task->{worker_selection_algorithm}\n";
-    print "$task->{command_line}\n";
-    print "$task->{tag}\n";
-    
-    work_queue::work_queue_submit($wq, $task);
+if ($#ARGV < 0) {
+	print "work_queue_example <file1> [file2] [file3] ...\n";
+	print "Each file given on the command line will be compressed using a remote worker.\n";
+	exit 1;
 }
+
+my $wq = work_queue::work_queue_create($work_queue::WORK_QUEUE_DEFAULT_PORT);
+my $port = work_queue::work_queue_port($wq);
+print "listening on port $port...\n"; 
+
+for (my $i = 0; $i <= $#ARGV; $i++) {
+	my $infile = $ARGV[$i]; 
+	my $outfile = $ARGV[$i] . ".gz";
+	my $command = "/usr/bin/gzip < $infile > $outfile";
+
+    my $task = work_queue::work_queue_task_create($command);
+
+    work_queue::work_queue_task_specify_file($task, $infile, $infile, $work_queue::WORK_QUEUE_INPUT, $work_queue::WORK_QUEUE_CACHE);
+    work_queue::work_queue_task_specify_file($task, $outfile, $outfile, $work_queue::WORK_QUEUE_OUTPUT, $work_queue::WORK_QUEUE_CACHE);
+
+    work_queue::work_queue_submit($wq, $task);
+    print "submitted task (id# $task->{taskid}): $task->{command_line}\n";
+}
+
+print "waiting for tasks to complete...\n";
 
 while (not work_queue::work_queue_empty($wq)) {
-    my $task = work_queue::work_queue_wait($wq, 1);
+    my $task = work_queue::work_queue_wait($wq, 5);
 
     if (defined($task)) {
-    	print "$task->{command_line}, $task->{result}, $task->{output}\n";
+		print "task (id# $task->{taskid}) complete: $task->{command_line} (return code $task->{return_status})\n";
     }
 }
+
+print "all tasks complete!\n";
+
+exit 0;
