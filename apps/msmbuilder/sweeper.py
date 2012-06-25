1b945c989c3fbe0d5feb3b61999463f6b830e17c
cbauschk <cbauschk@nd.edu>
2012-06-25 15:12:11 +0000

    Disabled caching of input and output and added more documentation.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2149 a4d8336d-3463-0410-8bba-c098c45d37a8

--- apps/msmbuilder/sweeper.py
+++ apps/msmbuilder/sweeper.py
@@ -3,52 +3,63 @@
 # See the file COPYING for details.
 #
 # This program provides a simple api to sweep though a range of parameters
+# and store the output in progname/param/
 #
 
 from work_queue import *
 import sys, os, itertools
 
 class Sweeper:
-    """Provides a simple api for a program to sweep through a range of parameters."""
+    """Provides a simple api for a program to sweep through a range of parameters.
+        Usage: import sweeper
+               x = sweeper.Sweeper()"""
     def __init__(self):
         self.port = WORK_QUEUE_DEFAULT_PORT # 9123
-        self.progname = "" # the program to sweep with
-        self.envpath = "" # path to a environment set up script
-        self.paramvalues = [] # list of arguments e.g. -l -o 27
-        self.command = [] # list of the full command to be run
-        self.sweeps = [] # list of variables to sweep over
-        self.inputlist = [] # list of input files
-        self.outputlist = [] # list of output files
+        self.progname = ""                  # the program to sweep with
+        self.envpath = ""                   # path to a environment set up script
+        self.paramvalues = []               # list of arguments e.g. -l -o 27
+        self.command = []                   # list of the full command to be run
+        self.sweeps = []                    # list of variables to sweep over
+        self.inputlist = []                 # list of input files
+        self.outputlist = []                # list of output files
 
     def addprog(self, progname):
         """The program (sans parameters) to sweep with.
+            Usage: x.addprog("echo")
             @param progname The base program that will be run."""
         self.command.append(progname)
         self.progname, sep, tail = progname.partition('.') # remove file extension so we can create a nice directory
 
     def setenv(self, pathtoenv):
         """Set the path to a env script to set up an environment for the command to run in.
+            Usage: x.setenv("env.sh")
             @param pathtoenv The path to a script to run first."""
         self.envpath = pathtoenv
 
     def addparameter(self, param):
         """Add a parameter to the list of parameters(arguments).
+            Usage: x.addparameter("-n")
+                   x.addsweep(xrange(1,10,2))
+                   x.addparameter("> out")
             @param param The argument to be added."""
         self.command.append(str(param))
 
     def addinput(self, input):
         """Add a file (or directory) to the input list.
+            Usage: x.addinput("infile")
             @param input The file or directory to be included as input."""
         self.inputlist.append(input)
 
     def addoutput(self, output):
         """Add a file (or directory) to the output list.
             Output will be placed in progname/parameters/
+            Usage: a.addoutput("outfile")
             @param output The file to be recieved back from the worker as output."""
         self.outputlist.append(output)
 
     def addsweep(self, iterlist):
         """Add a sweep to the command.
+            Usage: x.addsweep(xrange(1,10,2))
             @param iterlist An iterable object that contains the values to sweep over."""
         self.command.append("%s")
         self.paramvalues.append("%s")
@@ -73,7 +84,11 @@ class Sweeper:
 
             os.system("mkdir -p %s/%s" % (self.progname, paramdir))
 
-            env = open(self.envpath).read()
+            if (self.envpath):
+                env = open(self.envpath).read()
+            else:
+                env = ""
+
             script = """
                     %(env)s
                     %(command)s
@@ -84,9 +99,9 @@ class Sweeper:
             t.specify_buffer(script, 'script.sh')
 
             for input in self.inputlist:
-                t.specify_file(input, input, WORK_QUEUE_INPUT, cache=True)
+                t.specify_file(input, input, WORK_QUEUE_INPUT, cache=False)
             for output in self.outputlist:
-                t.specify_file("%s/%s/%s" % (self.progname, paramdir, output), output, WORK_QUEUE_OUTPUT, cache=True)
+                t.specify_file("%s/%s/%s" % (self.progname, paramdir, output), output, WORK_QUEUE_OUTPUT, cache=False)
 
             taskid = self.q.submit(t)
             print "submitted task (id# %d): %s" % (taskid, t.command)
