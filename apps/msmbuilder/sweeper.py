c076bb4fc10f1e40f97d29a6e15a1de4d187c91b
cbauschk <cbauschk@nd.edu>
2012-07-16 13:29:43 +0000

    Fixed a try catch that was slowing down hashing files. When submitting to a MyWorkQueue database, duplicate input is now detected.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2213 a4d8336d-3463-0410-8bba-c098c45d37a8

--- apps/msmbuilder/sweeper.py
+++ apps/msmbuilder/sweeper.py
@@ -78,6 +78,7 @@ class Sweeper:
         r = []
         r.append(input)
         r.append(caching)
+        r.append(self._checksum(input))
         self.inputlist.append(r)
 
     def addoutput(self, output, caching=1):
@@ -130,7 +131,8 @@ class Sweeper:
 
             # combine an env script and the command into one
             script = """%(env)s%(command)s\n""" % {'env': env, 'command': command}
-            fo = open('%s-sweep/%s/%s-script' % (self.progname, commdir, commdir), 'w')
+            fo = open('%s-sweep/%s/%s-script' 
+                     % (self.progname, commdir, commdir), 'w')
             fo.write(script)
             fo.close
             # TODO add an option to specify the interpreter
@@ -153,7 +155,8 @@ class Sweeper:
                     t.specify_file("%s-sweep/%s/%s" % (self.progname, commdir, output[0]), output[0], WORK_QUEUE_OUTPUT, cache=False)
 
             taskid = self.q.submit(t)
-            print "submitted task (id# %d): %s" % (taskid, t.command)
+            print "submitted task (id# %d): %s" 
+                    % (taskid, t.command)
 
         print "waiting for tasks to complete..."
 
@@ -174,21 +177,25 @@ class Sweeper:
             @param pwfile A file containing the pw for the mysql server.
             @param interpreter The interpreter to run the script"""
         sqlscript = ""
+        # create the db object
         db = _mysql.connect(host=host, user=user, db=dbname, passwd=pw)
+        # we want to replace all illegal file characters with _
+        regex = re.compile('[:/" ()<>|?*]|(\\\)')
         for item in itertools.product(*self.sweeps):
             command  = ' '.join(self.command) % (item) # create the command
             commdir = '_'.join(self.command) % (item)
-            # we want to replace all illegal file characters with _
-            regex = re.compile('[:/" ()<>|?*]|(\\\)')
+            # replace illegal characters with _
             commdir = regex.sub('_', commdir)
+            outputdir = '%s-sweep/%s/' % (self.progname, commdir)
             # this is the location of the script on the master
             localpath = os.path.abspath('%s-sweep/%s/%s-script' % (self.progname, commdir, commdir))
             # location of the script on the worker
             remotepath = '%s-script' % (commdir)
             dbcommand = '%s ./' % (interpreter)+remotepath
-            
-	    for item in r.fetch_row():
-	      print item
+            if self._checkdup(dbcommand, db): 
+                print 'duplicate command', dbcommand
+                continue
+
             # create progname-sweep/commdir, this is where the output will go ex. BuildMSM-sweep/command_with_underscores/Data
             os.system("mkdir -p %s-sweep/%s" % (self.progname, commdir))
 
@@ -206,35 +213,86 @@ class Sweeper:
             #INSERT INTO commands VALUES (command_id, username, personal_id, name, command, status, stdout)
             #INSERT INTO files VALUES (fileid, command_id, local_path, remote_path, type, flags, checksum)
             # add the command to the table
-            db.query("""INSERT INTO %s.commands VALUES (command_id, \'%s\', personal_id, name, \'%s\', 2, stdout)""" % (dbname, user, dbcommand,))
+            db.query("""INSERT INTO %s.commands VALUES (command_id, \'%s\', personal_id, name, \'%s\', 2, stdout)""" 
+                    % (dbname, user, dbcommand,))
             
             # add script as input so it is sent to the worker - no caching
             # get the checksum of the input script (localpath)
-            db.query("""INSERT INTO %s.files VALUES (fileid, command_id, \'%s\', \'%s\', 1, 1, \'%s\')""" % (dbname, localpath, remotepath, self._checksum(localpath)))
+            db.query("""INSERT INTO %s.files VALUES (fileid, command_id, \'%s\', \'%s\', 1, 1, \'%s\')""" 
+                    % (dbname, localpath, remotepath, self._checksum(localpath)))
             # add the input files to the myworkqueue db
             for input in self.inputlist:
                 # TODO get the checksum of input files, check if the input is the same
-		db.query("""INSERT INTO %s.files VALUES (fileid, command_id, \'%s\', \'%s\', 1, %s, \'%s\')""" % (dbname, os.path.abspath(input[0]), input[0], input[1], self.checksum(input)))
+		        db.query("""INSERT INTO %s.files VALUES (fileid, command_id, \'%s\', \'%s\', 1, %s, \'%s\')""" 
+                        % (dbname, os.path.abspath(input[0]), input[0], input[1], input[2]))
             # add the output files to the myworkqueue db
             for output in self.outputlist:
-                outputdir = '%s-sweep/%s/' % (self.progname, commdir)
-                db.query("""INSERT INTO %s.files VALUES (fileid, command_id, \'%s\', \'%s\', 2, %s, checksum)""" % (dbname, os.path.abspath(outputdir+output[0]), output[0], output[1]))
+                db.query("""INSERT INTO %s.files VALUES (fileid, command_id, \'%s\', \'%s\', 2, %s, checksum)""" 
+                        % (dbname, os.path.abspath(outputdir+output[0]), output[0], output[1]))
                 # each command in the commands table has a unique command_id, this needs to be associated with the correct input/output files
-                db.query("""UPDATE %s.files SET files.command_id=(SELECT MAX(command_id) FROM %s.commands WHERE command=\'%s\' LIMIT 1) WHERE files.command_id=0""" % (dbname, dbname, dbcommand))
+                db.query("""UPDATE %s.files SET files.command_id=(SELECT MAX(command_id) FROM %s.commands WHERE command=\'%s\' LIMIT 1) WHERE files.command_id=0""" 
+                        % (dbname, dbname, dbcommand))
     
     def _checksum(self, filename):
-        try:
-            f = open(localpath)
-            # this is faster but doesn't seem to work in ND afs space
-            map = mmap.mmap(f.fileno(), 0, flags=mmap.MAP_PRIVATE, prot=mmap.PROT_READ)
-            return hashlib.sha1(map).hexdigest()
-        except:
-            # slightly slower but always works
+        #try:
+        #    f = open(localpath)
+        #    # this is faster but doesn't seem to work in ND afs space
+        #    map = mmap.mmap(f.fileno(), 0, flags=mmap.MAP_PRIVATE, prot=mmap.PROT_READ)
+        #    return hashlib.sha1(map).hexdigest()
+        #except:
+        # slightly slower but always works
+        #print filename
+        if os.path.isdir(filename) == True:
+            # it a dir, we have to hash slighly differently
+            SHAhash = hashlib.sha1()
+            if not os.path.exists(filename):
+                return -1
+            try:
+                for root, dirs, files, in os.walk(filename):
+                    for names in files:
+                        filepath = os.path.join(root, names)
+                        try:
+                            f1 = open(filepath, 'rb')
+                        except:
+                            f1.close()
+                            continue
+
+                        while 1:
+                            buf = f1.read(4096)
+                            if not buf : break
+                            SHAhash.update(hashlib.sha1(buf).hexdigest())
+                        f1.close()
+            except:
+                import traceback
+                traceback.print_exc()
+                return -2
+
+            return SHAhash.hexdigest()
+        else:
             return hashlib.sha1(open(filename, 'rb').read()).hexdigest()
-            
+
     def _checkdup(self, dbcommand, dbconn):
-       # TODO figure out how to get something back from the db
-       dbconn.query("""SELECT * FROM commands""")
-       r = dbconn.store_result()
-       print r.fetch_row()
-       return False
+        dbconn.query("""SELECT command_id FROM commands WHERE command='%s'""" % (dbcommand)) 
+        r = dbconn.store_result()
+        cid = -1 # we want the command_id to be -1 if we can't find a duplicate
+        # fetch_row returns a tuple, we only need the cid
+        for tuple in r.fetch_row():
+            for i in tuple:
+                # if we found a duplicate
+                cid = i
+
+        print cid
+        # if we did not find a duplicate
+        if cid < 0:
+            print 'new command'
+            return 0 # new command, input everything
+
+        # find the files associated with this commnand_id
+        dbconn.query("""SELECT remote_path,checksum FROM files WHERE command_id='%s'""" % (cid))
+        r = dbconn.store_result()
+        for file, hash in r.fetch_row(maxrows=0):
+            for input in self.inputlist:
+                if file in input and hash not in input:
+                    print 'file changed', file, hash
+                    return 1 # file changed, rerun command (set status to Avaliable) 
+        return 1
