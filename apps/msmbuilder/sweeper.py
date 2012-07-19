6123979b8056f5d78a25ce520b5a9d46f0eae7f1
cbauschk <cbauschk@nd.edu>
2012-07-19 19:22:00 +0000

    Added addmetadata() to the api which allows adding a key and value to the metadata table. Added associatemetadata() to the api which associates a command_id to a meta_id. If a duplicate command is entered with different input it is now added as a new command.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2225 a4d8336d-3463-0410-8bba-c098c45d37a8

--- apps/msmbuilder/sweeper.py
+++ apps/msmbuilder/sweeper.py
@@ -15,7 +15,7 @@ class Sweeper:
         Usage: import sweeper
                x = sweeper.Sweeper()"""
     def __init__(self, verbose=0):
-        self.vb = verbose              # turn on/off verbose mode
+        self.vb = verbose                   # turn on/off verbose mode
         self.port = WORK_QUEUE_RANDOM_PORT  # random, default is 9123
         self.progname = ""                  # the program to sweep with
         self.envpath = ""                   # path to a environment set up script
@@ -24,6 +24,7 @@ class Sweeper:
         self.sweeps = []                    # list of variables to sweep over
         self.inputlist = []                 # list of input files
         self.outputlist = []                # list of output files
+        self.keys = []                      # list of keys
 
     def addprog(self, progname):
         """The program (sans parameters) to sweep with.
@@ -53,7 +54,7 @@ class Sweeper:
         self.inputlist.append(r)
         if self.vb: print 'added environment directory = %s caching = %s' % (pathtoenvdir, caching)
 
-    def addtuple(self, flag, iterlist):
+    def addtuple(self, flag, iterlist, key=''):
         """Add a flag/sweep pair to the command. This has the same functionality as addparameter() and addsweep() used together.
             Usage: x.addtuple("-n", xrange(1,10,2))
             @param flag The flag to be added to the command.
@@ -63,6 +64,9 @@ class Sweeper:
         self.paramvalues.append('%s')
         self.sweeps.append(iterlist)
         if self.vb: print 'added tuple = %s sweep = %s' % (flag, iterlist)
+        self.keys.append(key)
+        if key is not '':
+            if self.vb: print 'added key = %s' % (key)
 
     def addparameter(self, param):
         """Add a parameter to the list of parameters(arguments).
@@ -95,7 +99,7 @@ class Sweeper:
         self.outputlist.append(r)
         if self.vb: print 'added output = %s caching = %s' % (output, caching)
 
-    def addsweep(self, iterlist):
+    def addsweep(self, iterlist, key=''):
         """Add a sweep to the command.
             Usage: x.addsweep(xrange(1,10,2))
             @param iterlist An iterable object that contains the values to sweep over."""
@@ -103,6 +107,15 @@ class Sweeper:
         self.paramvalues.append("%s")
         self.sweeps.append(iterlist)
         if self.vb: print 'added sweep = %s' % (iterlist)
+        self.keys.append(key)
+        if key is not '':
+            if self.vb: print 'added key = %s' % (key)
+
+    def addmetadata(self, key, value, dbconn):
+        dbconn.query("""INSERT INTO metadata VALUES (meta_id, \'%s\', \'%s\')""" % (key, value))
+
+    def associatemetadata(self, cid, mid, dbconn):
+        dbconn.query("""INSERT INTO cmdmeta VALUES (%d, %d)""" % (cid, mid))
 
     def sweep(self, interpreter='bash'):
         """Sweep over the command.
@@ -229,8 +242,15 @@ class Sweeper:
             #INSERT INTO commands VALUES (command_id, username, personal_id, name, command, status, stdout)
             #INSERT INTO files VALUES (fileid, command_id, local_path, remote_path, type, flags, checksum)
             # add the command to the table
-            db.query("""INSERT INTO %s.commands VALUES (command_id, \'%s\', personal_id, name, \'%s\', 2, stdout)""" 
+            db.query("""INSERT INTO %s.commands VALUES (command_id, \'%s\', personal_id, name, \'%s\', 2, stdout, env)""" 
                     % (dbname, user, dbcommand,))
+
+            #TODO metadata
+            for value,key in itertools.izip(item,self.keys):
+                if key is not '':
+                    self.addmetadata(key,value,db)
+                    db.query("""INSERT INTO cmdmeta VALUES ((SELECT MAX(command_id) FROM commands LIMIT 1), (SELECT MAX(meta_id) from metadata LIMIT 1))""")
+
             
             # add script as input so it is sent to the worker - no caching
             # get the checksum of the input script (localpath)
@@ -238,7 +258,6 @@ class Sweeper:
                     % (dbname, localpath, remotepath, self._checksum(localpath)))
             # add the input files to the myworkqueue db
             for input in self.inputlist:
-                # TODO get the checksum of input files, check if the input is the same
 		        db.query("""INSERT INTO %s.files VALUES (fileid, command_id, \'%s\', \'%s\', 1, %s, \'%s\')""" 
                         % (dbname, os.path.abspath(input[0]), input[0], input[1], input[2]))
             # add the output files to the myworkqueue db
@@ -302,15 +321,15 @@ class Sweeper:
             if self.vb: print 'new command =', dbcommand
             return 0 # new command, input everything
 
-        if slef.vb: print 'duplicte command = %s cid = %s' % (dbcommand, cid)
+        if self.vb: print 'duplicte command = %s cid = %s' % (dbcommand, cid)
         # find the files associated with this commnand_id
         dbconn.query("""SELECT remote_path,checksum FROM files WHERE command_id='%s'""" % (cid))
         r = dbconn.store_result()
         for file, hash in r.fetch_row(maxrows=0):
             for input in self.inputlist:
                 if file in input and hash not in input:
-                    if self.vb: print 'file changed', file, hash, 'updating cid', cid, 'to avaliable'
-                    dbconn.query("""UPDATE commands SET status=2 WHERE command_id='%s'""" % (cid))
-                    return 1 # file changed, rerun command (set status to Avaliable) 
-        if self.vb: print 'files identical, doing nothing to command = %s cid = %s' % (dbcommand, cid)
+                    if self.vb: print 'file changed', file, hash
+                    #dbconn.query("""UPDATE commands SET status=2 WHERE command_id='%s'""" % (cid)) update command to Avaliable
+                    return 0 # file changed, insert command anyway
+        if self.vb: print 'files identical, doing nothing to command = %s cid = %s (no new command inserted)' % (dbcommand, cid)
         return 1
