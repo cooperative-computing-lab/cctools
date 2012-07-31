f1b6aec3d9d94d04f1644f735699ce34c50652be
pdonnel3 <pdonnel3@nd.edu>
2012-07-31 15:55:17 +0000

    Merging r2269 from /trunk:
    > r2269 | dthain | 2012-07-31 09:44:01 -0400 (Tue, 31 Jul 2012) | 2 lines
    > Changed paths:
    >    M /trunk/dttools/src/work_queue_status.c
    > 
    > Squeezed in a few more columns.
    > 
    
    
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/release/3.5.3@2272 a4d8336d-3463-0410-8bba-c098c45d37a8

--- dttools/src/work_queue_status.c
+++ dttools/src/work_queue_status.c
@@ -25,13 +25,13 @@ char *catalog_host = NULL;
 int catalog_port = 0;
 
 static struct nvpair_header headers[] = {
-	{"project",       "PROJECT", NVPAIR_MODE_STRING, NVPAIR_ALIGN_LEFT, 20},
-	{"name",          "NAME",    NVPAIR_MODE_STRING, NVPAIR_ALIGN_LEFT, 20},
-	{"port",          "PORT",    NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 6},
+	{"project",       "PROJECT", NVPAIR_MODE_STRING, NVPAIR_ALIGN_LEFT, 18},
+	{"name",          "NAME",    NVPAIR_MODE_STRING, NVPAIR_ALIGN_LEFT, 22},
+	{"port",          "PORT",    NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 5},
 	{"tasks_waiting", "WAITING",    NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 8},
-	{"workers_busy",  "BUSY",    NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 8},
+	{"workers_busy",  "BUSY",    NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 5},
 	{"tasks_complete","COMPLETE",    NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 8},
-	{"workers",       "WORKERS", NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 8},
+	{"workers",       "WORKERS", NVPAIR_MODE_INTEGER, NVPAIR_ALIGN_RIGHT, 7},
 	{NULL,}
 };
 
