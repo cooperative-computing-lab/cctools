bc06c22e2d019330a9a88e940f2d48351deb5a2c
pdonnel3 <pdonnel3@nd.edu>
2012-08-29 22:27:57 +0000

    Squashed commit of patch generated from:
    svn diff file:///afs/nd.edu/user37/ccl/svn/cctools/branches/pdonnel3/search-syscall@1645 file:///afs/nd.edu/user37/ccl/svn/cctools/branches/pdonnel3/search-syscall@1659
    
    
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/branches/pdonnel3/search-syscall@2331 a4d8336d-3463-0410-8bba-c098c45d37a8

--- parrot/src/pfs_table.cc
+++ parrot/src/pfs_table.cc
@@ -1748,6 +1748,7 @@ static int search_directory (pfs_table *t, unsigned level, const char *base, cha
 	if (level == 0)
 		return 0;
 
+    int fnmatch_flags = flags & PFS_SEARCH_PERIOD ? FNM_PATHNAME | FNM_PERIOD : FNM_PATHNAME;
 	int found = 0;
 	char *current = dir+strlen(dir); /* point to end to current directory */
 	int fd;
@@ -1762,7 +1763,7 @@ static int search_directory (pfs_table *t, unsigned level, const char *base, cha
 
 			sprintf(current, "/%s", name);
 			r = t->stat(dir, &buf);
-			if (fnmatch(pattern, base, FNM_PATHNAME) == 0) {
+			if (((flags & PFS_SEARCH_RECURSIVE) && fnmatch(pattern, current, fnmatch_flags) == 0) || ((~flags & PFS_SEARCH_RECURSIVE) && fnmatch(pattern, base, fnmatch_flags) == 0)) {
 				const char *matched;
 				int access_flags = search_to_access(flags);
 
