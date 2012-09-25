39dac59e858260c2254a7147d4c12f6294fb216c
dpandiar <dpandiar@nd.edu>
2012-09-25 18:17:19 +0000

    Integrating Peter Bui's patch for the catalog_update feature that reports a bunch of information to the catalog server.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2362 a4d8336d-3463-0410-8bba-c098c45d37a8

---
+++ doc/man/catalog_update.m4
@@ -0,0 +1,73 @@
+include(manual.h)dnl
+HEADER(catalog_update)
+
+SECTION(NAME)
+BOLD(catalog_update) - send update to catalog server
+
+SECTION(SYNOPSIS)
+CODE(BOLD(catalog_update [options] [name=value] ..))
+
+SECTION(DESCRIPTION)
+
+PARA
+The CODE(catalog_update) tool allows users to manually send an update to a
+catalog server via a short UDP packet.
+
+SECTION(OPTIONS)
+
+OPTIONS_BEGIN
+OPTION_PAIR(-c,host)      Send update to this catalog host.
+
+PARA
+The CODE(catalog_update) tool also accepts a list of CODE(name)/CODE(value)
+pairs that will be sent to the catalog server as field entries.
+
+By default, the CODE(catalog_update) tool includes the following field entries
+in its update packet:
+
+LIST_BEGIN
+LIST_ITEM()CODE(BOLD(type)) This describes the node type (default is "node").
+LIST_ITEM()CODE(BOLD(version)) This is the version of CCTools.
+LIST_ITEM()CODE(BOLD(cpu)) This is CPU architecture of the machine.
+LIST_ITEM()CODE(BOLD(opsys)) This is operating system of the machine.
+LIST_ITEM()CODE(BOLD(opsysversion)) This is operating system version of the machine.
+LIST_ITEM()CODE(BOLD(load1)) This is 1-minute load of the machine.
+LIST_ITEM()CODE(BOLD(load5)) This is 5-minute load of the machine.
+LIST_ITEM()CODE(BOLD(load15)) This is 15-minute load of the machine.
+LIST_ITEM()CODE(BOLD(memory_total)) This is total amount of memory on the machine
+LIST_ITEM()CODE(BOLD(memory_avail)) This is amount of available memory on the machine
+LIST_ITEM()CODE(BOLD(cpus)) This is number of detected CPUs on the machine.
+LIST_ITEM()CODE(BOLD(uptime)) This how long the machine has been running.
+LIST_ITEM()CODE(BOLD(owner)) This is user who sent the update.
+LIST_END
+OPTIONS_END
+
+SECTION(ENVIRONMENT VARIABLES)
+
+LIST_BEGIN
+LIST_ITEM()CODE(BOLD(CATALOG_HOST)) Hostname of catalog server (same as CODE(-c)).
+LIST_END
+
+SECTION(EXIT STATUS)
+On success, returns zero.  On failure, returns non-zero.
+
+SECTION(EXAMPLES)
+PARA
+
+The following example sends an update to the catalog server located at
+CODE(chirp.cse.nd.edu) with the fields CODE(type) set to "node", CODE(name) set
+to "testnode00", and CODE(has_java) set to "yes".  These fields can be queried
+from the catalog server by clients using CODE(chirp_status).
+
+LONGCODE_BEGIN
+catalog_update -c chirp.cse.nd.edu type=node name=testnode00 has_java=yes
+LONGCODE_END
+
+SECTION(COPYRIGHT)
+COPYRIGHT_BOILERPLATE
+
+SECTION(SEE ALSO)
+SEE_ALSO_CHIRP
+
+FOOTER
+
---
+++ dttools/src/catalog_update.c
@@ -0,0 +1,126 @@
+/*
+Copyright (C) 2012- The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file COPYING for details.
+*/
+
+#include "buffer.h"
+#include "catalog_server.h"
+#include "datagram.h"
+#include "debug.h"
+#include "domain_name_cache.h"
+#include "int_sizes.h"
+#include "load_average.h"
+#include "memory_info.h"
+#include "stringtools.h"
+#include "username.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
+#include <sys/utsname.h>
+
+#define DEFAULT_TYPE	"node"
+
+void show_help(const char *cmd) {
+	fprintf(stdout, "Use: %s [options] <name=value> ...\n", cmd);
+	fprintf(stdout, "where options are:\n");
+	fprintf(stdout, " -c <catalog>\n");
+}
+
+int main(int argc, char *argv[]) {
+	char *host = CATALOG_HOST;
+	int   port = CATALOG_PORT;
+
+	int c;
+	while ((c = getopt(argc, argv, "c:")) != -1) {
+		switch (c) {
+			case 'c':
+				host = optarg;
+				break;
+			default:
+				show_help(argv[0]);
+				return EXIT_FAILURE;
+		}
+	}
+
+	struct datagram *d;
+	d = datagram_create(DATAGRAM_PORT_ANY);
+	if (!d) {
+		fatal("could not create datagram port!");
+	}
+
+	buffer_t *b;
+	const char *text;
+	size_t text_size;
+	b = buffer_create();
+	if (!b) {
+		fatal("could not create buffer!");
+	}
+
+	struct utsname name;
+	struct sysinfo info;
+	int cpus;
+	double load[3];
+	UINT64_T memory_total, memory_avail;
+	char owner[USERNAME_MAX];
+
+	uname(&name);
+	string_tolower(name.sysname);
+	string_tolower(name.machine);
+	string_tolower(name.release);
+	sysinfo(&info);
+	load_average_get(load);
+	cpus = load_average_get_cpus();
+	memory_info_get(&memory_avail, &memory_total);
+	username_get(owner);
+
+	buffer_printf(b, "type %s\nversion %d.%d.%d\ncpu %s\nopsys %s\nopsysversion %s\nload1 %0.02lf\nload5 %0.02lf\nload15 %0.02lf\nmemory_total %llu\nmemory_avail %llu\ncpus %d\nuptime %d\nowner %s\n",
+		DEFAULT_TYPE,
+		CCTOOLS_VERSION_MAJOR, CCTOOLS_VERSION_MINOR, CCTOOLS_VERSION_MICRO,
+		name.machine,
+		name.sysname,
+		name.release,
+		load[0],
+		load[1],
+		load[2],
+		memory_avail,
+		memory_total,
+		cpus,
+		info.uptime,
+		owner
+	);
+
+	int i;
+	for (i = optind; i < argc; i++) {
+		char *name;
+		char *value;
+
+		name  = argv[i];
+		value = strchr(name, '=');
+		if (!value) {
+			fatal("invalid name/value pair = %s", name);
+		} else {
+			*value++ = 0;
+		}
+
+		text = buffer_tostring(b, &text_size);
+		buffer_printf(b, "%s%s %s\n", text, name, value);
+	}
+
+        text = buffer_tostring(b, &text_size);
+
+	char address[DATAGRAM_ADDRESS_MAX];
+	if (domain_name_cache_lookup(host, address)) {
+		datagram_send(d, text, text_size, address, port);
+	} else {
+		fatal("unable to lookup address of host: %s", host);
+	}
+
+	buffer_delete(b);
+	datagram_delete(d);
+	return EXIT_SUCCESS;
+}
+
+/* vim: set sts=8 sw=8 ts=8 ft=c: */
