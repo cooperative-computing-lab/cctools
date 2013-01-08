64d7b502c89b7af92a1dec7716c2a3a18baa9b93
malbrec2 <malbrec2@nd.edu>
2013-01-08 19:27:22 +0000

    Added error message when trying to use automatic file renaming with non-WQ, non-Condor drivers.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2468 a4d8336d-3463-0410-8bba-c098c45d37a8

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -1647,9 +1647,15 @@ int dag_prepare_for_batch_system(struct dag *d) {
 				break;
 				
 			default:
+
+				if(f->remotename) {
+					fprintf(stderr, "makeflow: automatic file renaming (%s=%s) only works with Condor or Work Queue drivers\n", f->filename, f->remotename);
+					goto failure;
+				}
 				break;
 				
 			}
+
 			f = f->next;
 			
 			/* If we're done with the source files, switch to checking target files */
@@ -2765,7 +2771,10 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	dag_prepare_for_batch_system(d);
+	if(dag_prepare_for_batch_system(d)) {
+		return 1;
+	}
+
 	dag_prepare_gc(d);
 	dag_prepare_nested_jobs(d);
 
