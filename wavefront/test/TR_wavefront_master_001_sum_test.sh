68ea793c36c6cc0d54eb1404f9f2ec70013dfde4
btovar <btovar@nd.edu>
2013-01-24 22:27:56 +0000

    Added a couple of tests to wavefront and wavefront_master.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2486 a4d8336d-3463-0410-8bba-c098c45d37a8
    
    Conflicts:
    
    (adding missing files)
    
    	wavefront/test/TR_wavefront_001_sum_test.sh

---
+++ wavefront/test/TR_wavefront_master_001_sum_test.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+. ../../dttools/src/test_runner.common.sh
+
+TEST_INPUT=test.wmaster.input	
+TEST_OUTPUT=test.wmaster.output	
+PORT_FILE=wfm.port
+PIDMASTER_FILE=wfm.pid
+PIDWORKER_FILE=worker.pid
+
+prepare()
+{
+	rm -f $TEST_INPUT
+	rm -f $TEST_OUTPUT
+	rm -f $PORT_FILE
+	rm -f $PIDMASTER_FILE
+	rm -f $PIDWORKER_FILE
+
+	./gen_ints_wfm.sh $TEST_INPUT 10
+
+	../src/wavefront_master -Z $PORT_FILE ./sum_wfm.sh 10 10 $TEST_INPUT $TEST_OUTPUT &
+	pid=$!
+	echo $pid > $PIDMASTER_FILE
+
+	#wait at most five seconds for master to come alive.
+	wait_for_file_creation $PORT_FILE 5
+
+	exit 0
+}
+
+run()
+{
+	answer=1854882
+	port=`cat $PORT_FILE`
+
+	../../dttools/src/work_queue_worker -t60 localhost $port &
+	pid=$!
+	echo $pid > $PIDWORKER_FILE
+
+	#Wait for all the output to be written to the file. We wait until last
+	#modification to $TEST_OUTPUT is at least 3 second ago.
+	wait_for_file_creation $TEST_OUTPUT 5
+	wait_for_file_modification $TEST_OUTPUT 3
+
+	value=`sed -n 's/^9 9 \([[:digit:]]*\)/\1/p' $TEST_OUTPUT`
+
+	if [ -z $value ]
+	then
+		exit 1
+	else
+		exit $(($answer-$value))
+	fi
+}
+
+clean()
+{
+	[ -f $PIDWORKER_FILE ] && kill -9 `cat $PIDWORKER_FILE`
+	[ -f $PIDMASTER_FILE ] && kill -9 `cat $PIDMASTER_FILE`
+
+	rm -f $TEST_INPUT
+	rm -f $TEST_OUTPUT
+	rm -f $PORT_FILE
+	rm -f $PIDMASTER_FILE
+	rm -f $PIDWORKER_FILE
+
+    exit 0
+}
+
+dispatch $@
---
+++ wavefront/test/gen_ints_wfm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+filename=${1:-test.wmaster.input}
+N=${2:-10}
+
+I=0
+
+[ -f ${filename} ] && rm ${filename}
+
+while [ $I -lt $N ]; do
+		echo "$I 0 $I" >> ${filename}
+		echo "0 $I $I" >> ${filename}
+		((I++))
+done
+
+
