cooperative-computing-lab/cctools

commit 8f62f50bb3c39cfa8648209d8aad50f927075885

Segfault fix:
This commit includes the main fix for the segfault reported by Lee-Ping. This should also fix the segfault reported by Badi's group recently.

The cause for the segfaut is: if the master terminates while a worker is running a task for it, the worker would disconnect the master, kill the running task and mark its task_status as cancelled. When a new master comes in and uses the same worker, the worker would report the killed task's result (which is result with exit code 15) to the master. However, the master might have just connected the worker and haven't sent any task to it. This corrupts the state of the master and caused the segfault.

This fix entirely removes the TASK_CANCELLED state on the worker and correctified/simplified the task state transitions in work_queue_worker. Now the worker only has two possible task status: TASK_NONE, TASK_RUNNING. If the task status is TASK_NONE, there should be no task processes waiting to be reaped and the stdout buffer and other task related resources should be cleaned up. 

So, now in kill_task() function, we call wait_task_process(), which renamed and modified from collect_task_info(), to wait on the task info and clean up task related resources (currently stdout buffer/file).

Other fixes:

1. Removed the task_status != TASK_CANCELLED check in do_unlink and changed it to: if the path already does not exist, then return success.
2. set stdout_buffer to NULL after freeing it (in abort_worker()). This prevents segfault from stdout_buffer being used after it's free'ed. e.g. in cleaning up task resources in abort_worker().
3. updated debug messages to generate more useful messages in work_queue.c and work_queue_worker.c.




git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@2435 a4d8336d-3463-0410-8bba-c098c45d37a8

dttools/src/work_queue_worker.c
@@ -63,7 +63,6 @@ extern int setenv(const char *name, const char *value, int overwrite);
 
 #define TASK_NONE 0
 #define TASK_RUNNING 1
-#define TASK_CANCELLED 2
 
 struct task_info {
 	pid_t pid;
@@ -320,8 +319,10 @@ static void get_task_stdout(char **task_output, INT64_T *task_output_length) {
 	*task_output_length = length;
 }
 
-static int collect_task_info(struct task_info *ti) {
-	// Get task return status
+
+// Wait for the task process to terminate and collect information about the
+// task process if parameter 'ti' is not NULL
+static int wait_task_process(struct task_info *ti) {
 	pid_t tmp_pid;
 	int status;
 	struct rusage rusage;
@@ -331,25 +332,31 @@ static int collect_task_info(struct task_info *ti) {
 
 	if(tmp_pid != pid) {
 		return 0;
+	} 
+	
+	task_status = TASK_NONE;
+
+	char *output;
+	INT64_T output_length;
+	// Cleanup the file/buffer used to store task stdout 
+	get_task_stdout(&output, &output_length);
+
+	if(ti) {
+		memset(ti, 0, sizeof(struct task_info));
+		ti->pid = tmp_pid;
+		ti->status = status;
+		ti->rusage = rusage;
+		ti->execution_end = timestamp_get();
+		ti->output = output;
+		ti->output_length = output_length;
+	} else {
+		free(output);
 	}
-
-	// Fill task_info struct
-	clear_task_info(ti);
-
-	ti->pid = tmp_pid;
-	ti->status = status;
-	ti->rusage = rusage;
-	ti->execution_end = timestamp_get();
-
-	get_task_stdout(&(ti->output), &(ti->output_length));
 
 	return 1;
 }
 
 static int handle_task(struct link *master) {
-	struct task_info ti;
-	memset(&ti, 0, sizeof(ti));
-
 	int result = read_task_stdout(time(0) + short_timeout);
 
 	// Failed to retrieve task standard output
@@ -359,19 +366,17 @@ static int handle_task(struct link *master) {
 
 	if(result == 1) {
 		// Task has terminated.
-		if(!collect_task_info(&ti)) {
+		struct task_info ti;
+		if(!wait_task_process(&ti)) {
 			return 0;
 		}
 
-		if (WIFEXITED(ti.status)){
-			task_status = TASK_NONE;
-		} else {
-			//mark as cancelled if process did not exit normally.
-			debug(D_WQ, "Task (process %d) was aborted.\n", pid);
-			task_status = TASK_CANCELLED;
-		}	
+		if (!WIFEXITED(ti.status)){
+			debug(D_WQ, "Task (process %d) did not exit normally.\n", pid);
+		} 
 
 		report_task_complete(master, ti.status, ti.output, ti.output_length, ti.execution_end - execution_start);
+		clear_task_info(&ti);
 	} 
 
 	return 1;
@@ -633,7 +638,7 @@ static struct link *auto_link_connect(char *addr, int *port)
 	while((m = (struct work_queue_master *)select_master(ml, pool))) {
 		master = link_connect(m->addr, m->port, time(0) + master_timeout);
 		if(master) {
-			debug(D_WQ, "Talking to the Master at:\n");
+			debug(D_WQ, "talking to the master at:\n");
 			debug(D_WQ, "addr:\t%s\n", m->addr);
 			debug(D_WQ, "port:\t%d\n", m->port);
 			debug(D_WQ, "project:\t%s\n", m->proj);
@@ -809,7 +814,7 @@ static int poll_master_and_task(struct link *master, int task_pipe_fd, int timeo
 
 	ret = 0;
 
-	if(task_status != TASK_NONE) {
+	if(task_status == TASK_RUNNING) {
 		n = 2;
 	} else {
 		n = 1;
@@ -858,12 +863,12 @@ static void work_for_master(struct link *master) {
 		return;
 	}
 
-	debug(D_WQ, "Working for master at %s:%d.\n", actual_addr, actual_port);
+	debug(D_WQ, "working for master at %s:%d.\n", actual_addr, actual_port);
 
 	time_t idle_stoptime = time(0) + idle_timeout;
 	// Start serving masters
 	while(!abort_flag) {
-		if(time(0) > idle_stoptime && task_status != TASK_RUNNING) {
+		if(time(0) > idle_stoptime && task_status == TASK_NONE) {
 			debug(D_NOTICE, "work_queue_worker: giving up because did not receive any task in %d seconds.\n", idle_timeout);
 			abort_flag = 1;
 			break;
@@ -1001,12 +1006,15 @@ static int do_put(struct link *master, char *filename, INT64_T length, int mode)
 static int do_unlink(const char *path) {
 	//Use delete_dir() since it calls unlink() if path is a file.	
 	if(delete_dir(path) != 0) { 
-		if (task_status != TASK_CANCELLED) {
-			//recover only if it wasn't a cancelled task since it could
-			//have been cancelled before (output) file was generated.
-			fprintf(stderr, "Could not remove %s.(%s)\n", path, strerror(errno));
-			return 0;
-		}	
+		struct stat buf;
+		if(stat(path, &buf) != 0) {
+			if(errno == ENOENT) {
+				// If the path does not exist, return success
+				return 1;
+			}
+		}
+		// Failed to do unlink
+		return 0;
 	}	
 	return 1;
 }
@@ -1139,11 +1147,14 @@ static int do_thirdput(struct link *master, int mode, const char *filename, cons
 
 }
 
+// Kill task if there's one running
 static void kill_task() {
 	if(task_status == TASK_RUNNING) {
+		debug(D_WQ, "terminating the current running task - process %d", pid);
 		kill(pid, SIGTERM);
+		// This reaps the killed child process created by function execute_task
+		wait_task_process(NULL);
 	}
-	task_status = TASK_CANCELLED;
 }
 
 static int do_kill() {
@@ -1168,7 +1179,7 @@ static int handle_link(struct link *master) {
 	int mode, r;
 
 	if(link_readline(master, line, sizeof(line), time(0)+short_timeout)) {
-		debug(D_WQ, "Received command: %s.\n", line);
+		debug(D_WQ, "received command: %s.\n", line);
 		if(sscanf(line, "work %lld", &length)) {
 			r = do_work(master, length);
 		} else if(sscanf(line, "stat %s", filename) == 1) {
@@ -1211,6 +1222,7 @@ static int handle_link(struct link *master) {
 			r = 0;
 		}
 	} else {
+		debug(D_WQ, "Failed to read from master.\n");
 		r = 0;
 	}
 
@@ -1218,6 +1230,7 @@ static int handle_link(struct link *master) {
 }
 
 static void disconnect_master(struct link *master) {
+	debug(D_WQ, "Disconnecting the current master ...\n");
 	link_close(master);
 
 	if(auto_worker) {
@@ -1237,12 +1250,13 @@ static void disconnect_master(struct link *master) {
 }
 
 static void abort_worker() {
-	fprintf(stdout, "work_queue_worker: cleaning up %s\n", workspace);
-
 	// Free dynamically allocated memory
 	if(stdout_buffer) {
 		free(stdout_buffer);
+		stdout_buffer = NULL;
+		stdout_buffer_used = 0;
 	}
+
 	if(pool_name) {
 		free(pool_name);
 	}
@@ -1252,12 +1266,11 @@ static void abort_worker() {
 	free(os_name);
 	free(arch_name);
 
-	// Kill running task if needed
-	if(task_status == TASK_RUNNING) {
-		kill(pid, SIGTERM);
-	}
+	// Kill running task if any 
+    kill_task();	
 
 	// Remove workspace. 
+	fprintf(stdout, "work_queue_worker: cleaning up %s\n", workspace);
 	delete_dir(workspace);
 }
 

dttools/src/work_queue.c
@@ -1031,7 +1031,7 @@ static int receive_output_from_worker(struct work_queue *q, struct work_queue_wo
 	return 1;
 
       failure:
-	debug(D_WQ, "Removing worker %s (%s): cannot receive output.", w->hostname, w->addrport);
+	debug(D_WQ, "Failed to receive output from worker %s (%s).", w->hostname, w->addrport);
 	remove_worker(q, w);
 	return 0;
 }
@@ -1178,6 +1178,7 @@ static int process_result(struct work_queue *q, struct work_queue_worker *w, str
 		stoptime = time(0) + get_transfer_wait_time(q, w, (INT64_T) output_length);
 		actual = link_read(l, t->output, output_length, stoptime);
 		if(actual != output_length) {
+			debug(D_WQ, "Failure: actual received stdout size (%lld bytes) is different from expected (%lld bytes).", actual, output_length);
 			free(t->output);
 			t->output = 0;
 			return 0;
@@ -1408,7 +1409,6 @@ static void handle_worker(struct work_queue *q, struct link *l)
 	}
 
 	if(!keep_worker) {
-		debug(D_WQ, "Removing worker %s (%s): failed to process the worker's request", w->hostname, w->addrport);
 		remove_worker(q, w);
 	}
 }
@@ -2065,7 +2065,7 @@ static int start_task_on_worker(struct work_queue *q, struct work_queue_worker *
 		change_worker_state(q, w, WORKER_STATE_BUSY);
 		return 1;
 	} else {
-		debug(D_WQ, "Removing worker %s (%s): couldn't send task.", w->hostname, w->addrport);
+		debug(D_WQ, "Failed to send task to worker %s (%s).", w->hostname, w->addrport);
 		remove_worker(q, w);	// puts w->current_task back into q->ready_list
 		return 0;
 	}

commit 35c95aff32060ad00cc6f2fcfbccd6c8b046b702

Merging r2452 from /trunk:
> r2452 | dpandiar | 2012-12-05 18:06:42 -0500 (Wed, 05 Dec 2012) | 19 lines
> Changed paths:
>    M /trunk/dttools/src/work_queue_worker.c
> 
> Li and I noticed the following bug introduced by r2435 in the cancel task feature-
> 1. master sends kill message to cancel task at worker
> 2. master marks worker state as "cancelling"
> 3. worker gets message, kills task, and reaps it
> 4. worker transitions its task status from running to none
> 
> The bug is the worker never reports the result and output of cancelled task to
> the master before/after step 4. But the master maintains worker state as
> "cancelling" until it receives the result of the cancelled task. As a result,
> the master never sends new tasks to run after the worker cancels and reaps its
> current task. 
> 
> Using Li's inputs, this commit fixes this bug. It separates the functionality to
> kill tasks into two functions - kill_task() and kill_task_and_reap().
> kill_task() is exclusively used by the cancel task feature, while
> kill_task_and_reap() are used by the release worker, abort worker, and
> disconnect master features.
> 
> 


git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/release/3.6.2@2525 a4d8336d-3463-0410-8bba-c098c45d37a8

dttools/src/work_queue_worker.c
@@ -1162,14 +1162,19 @@ static int do_thirdput(struct link *master, int mode, const char *filename, cons
 
 }
 
-// Kill task if there's one running
 static void kill_task() {
 	if(task_status == TASK_RUNNING) {
 		debug(D_WQ, "terminating the current running task - process %d", pid);
 		kill(pid, SIGTERM);
-		// This reaps the killed child process created by function execute_task
-		wait_task_process(NULL);
 	}
+}
+
+// Kill task if there's one running
+static void kill_and_reap_task() {
+	kill_task();
+	// This reaps the killed child process created by function execute_task
+	if(task_status == TASK_RUNNING) 
+		wait_task_process(NULL);
 }
 
 static int do_kill() {
@@ -1178,13 +1183,11 @@ static int do_kill() {
 }
 
 static int do_release() {
-	kill_task();
+	kill_and_reap_task();
 	debug(D_WQ, "Released by master at %s:%d.\n", actual_addr, actual_port);
 	released_by_master = 1;
 	return 0;
 }
-
-
 
 static int handle_link(struct link *master) {
 	char line[WORK_QUEUE_LINE_MAX];
@@ -1220,7 +1223,7 @@ static int handle_link(struct link *master) {
 		} else if(!strncmp(line, "release", 8)) {
 			r = do_release();
 		} else if(!strncmp(line, "exit", 5)) {
-			kill_task();
+			kill_and_reap_task();
 			r = 0;
 		} else {
 			debug(D_WQ, "Unrecognized master message: %s.\n", line);
@@ -1242,7 +1245,7 @@ static void disconnect_master(struct link *master) {
 		record_bad_master(duplicate_work_queue_master(actual_master));
 	}
 
-	kill_task();
+	kill_and_reap_task();
 
 	// Remove the contents of the workspace. 
 	delete_dir_contents(workspace);
@@ -1272,7 +1275,7 @@ static void abort_worker() {
 	free(arch_name);
 
 	// Kill running task if any 
-    kill_task();	
+    kill_and_reap_task();	
 
 	// Remove workspace. 
 	fprintf(stdout, "work_queue_worker: cleaning up %s\n", workspace);