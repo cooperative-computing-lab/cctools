36faa0dca125bf2be5963c3385a141a054891bbe
pdonnel3 <pdonnel3@nd.edu>
2013-03-11 01:29:03 +0000

    Correct some compiler warnings.
    git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/branches/pdonnel3/search-syscall@2665 a4d8336d-3463-0410-8bba-c098c45d37a8

--- parrot/src/parrot_client.c
+++ parrot/src/parrot_client.c
@@ -11,10 +11,12 @@ See the file COPYING for details.
 #include "int_sizes.h"
 
 #include <unistd.h>
+
 #include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 int parrot_whoami( const char *path, char *buf, int size )
 {
@@ -70,7 +72,7 @@ int parrot_cp( const char *source, const char *dest )
 #endif
 }
 
-int parrot_mkalloc( const char *path, long long size, mode_t mode )
+int parrot_mkalloc( const char *path, INT64_T size, mode_t mode )
 {
 #ifdef CCTOOLS_CPU_I386
 	return syscall(SYSCALL32_parrot_mkalloc,path,&size,mode);
@@ -79,7 +81,7 @@ int parrot_mkalloc( const char *path, long long size, mode_t mode )
 #endif
 }
 
-int parrot_lsalloc( const char *path, char *alloc_path, long long *total, long long *inuse )
+int parrot_lsalloc( const char *path, char *alloc_path, INT64_T *total, INT64_T *inuse )
 {
 #ifdef CCTOOLS_CPU_I386
 	return syscall(SYSCALL32_parrot_lsalloc,path,alloc_path,total,inuse);
@@ -136,10 +138,10 @@ SEARCH *parrot_opensearch( const char *path, const char *pattern, int flags)
 static char *readsearch_next(char *data, int *i) {
 	data += *i;
 
-        if (*data=='\0') return NULL;
+	if (*data=='\0') return NULL;
 
 	char *tail = strchr(data, '|');
-	int length = (tail==NULL) ? strlen(data) : tail - data;
+	ptrdiff_t length = (tail==NULL) ? (ptrdiff_t)strlen(data) : tail - data;
 
 	if (length==0) {
 		(*i)++;
--- parrot/src/parrot_client.h
+++ parrot/src/parrot_client.h
@@ -11,6 +11,7 @@ See the file COPYING for details.
 #include <sys/stat.h>
 
 #include "pfs_search.h"
+#include "int_sizes.h"
 
 int parrot_whoami( const char *path, char *buf, int size );
 int parrot_locate( const char *path, char *buf, int size );
@@ -18,8 +19,8 @@ int parrot_getacl( const char *path, char *buf, int size );
 int parrot_setacl( const char *path, const char *subject, const char *rights );
 int parrot_md5( const char *filename, unsigned char *digest );
 int parrot_cp( const char *source, const char *dest );
-int parrot_mkalloc( const char *path, long long size, mode_t mode );
-int parrot_lsalloc( const char *path, char *alloc_path, long long *total, long long *inuse );
+int parrot_mkalloc( const char *path, INT64_T size, mode_t mode );
+int parrot_lsalloc( const char *path, char *alloc_path, INT64_T *total, INT64_T *inuse );
 int parrot_timeout( const char *time );
 SEARCH *parrot_opensearch(const char *path, const char *pattern, int flags);
 struct searchent *parrot_readsearch(SEARCH *search);
