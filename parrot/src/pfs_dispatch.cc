cooperative-computing-lab/cctools

commit fe89bc527c3f2ace3144f04a78919ab5445cefe4

[Bug Fix] pfs_dispatch should convert [tg]kill calls into a dummy, because the signal is actually delivered by pfs_process_raise. This fixes a bug in which processes in an identity box could send signals to those outside the box.
git-svn-id: file:///afs/nd.edu/user37/ccl/svn/cctools/trunk@1973 a4d8336d-3463-0410-8bba-c098c45d37a8

parrot/src/pfs_dispatch.cc
@@ -2399,14 +2399,18 @@ void decode_syscall( struct pfs_process *p, int entering )
 		case SYSCALL32_kill:
 		case SYSCALL32_tkill:
 			if(entering) {
-				pfs_process_raise(args[0],args[1],0);
+				debug(D_PROCESS,"tkill %d %d %d",args[0],args[1],args[2]);
+				p->syscall_result = pfs_process_raise(args[0],args[1],0);
+				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 
 
 		case SYSCALL32_tgkill:
 			if(entering) {
-				pfs_process_raise(args[0],args[2],0);
+				debug(D_PROCESS,"tgkill %d %d %d",args[0],args[1],args[2]);
+				p->syscall_result = pfs_process_raise(args[1],args[2],0);
+				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 

parrot/src/pfs_dispatch64.cc
@@ -2256,15 +2256,17 @@ static void decode_syscall( struct pfs_process *p, INT64_T entering )
 		case SYSCALL64_kill:
 		case SYSCALL64_tkill:
 			if(entering) {
-				debug(D_PROCESS,"tkill %d %d %d",args[0],args[1],args[2]);
-				pfs_process_raise(args[0],args[1],0);
+				debug(D_PROCESS,"%s %d %d %d",tracer_syscall64_name(p->syscall),args[0],args[1],args[2]);
+				p->syscall_result = pfs_process_raise(args[0],args[1],0);
+				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 
 		case SYSCALL64_tgkill:
 			if(entering) {
 				debug(D_PROCESS,"tgkill %d %d %d",args[0],args[1],args[2]);
-				pfs_process_raise(args[1],args[2],0);
+				p->syscall_result = pfs_process_raise(args[1],args[2],0);
+				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 

commit d9c5eb991209b08bae8c13cda48479735915af1a

This fixes the infamous lcg-ls bug [1] introduced by commit
fe89bc527c3f2ace3144f04a78919ab5445cefe4

which was trying to resolve an identity boxing bug:

commit fe89bc527c3f2ace3144f04a78919ab5445cefe4
Author: dthain <dthain@nd.edu>
Date:   Fri Apr 20 12:40:07 2012 +0000

[Bug Fix] pfs_dispatch should convert [tg]kill calls into a dummy,
because the signal is actually delivered by pfs_process_raise.
This fixes a bug in which processes in an identity box could
send signals to those outside the box.

In actuality, the signal is not sent by pfs_process_raise and so the
signal is never delivered because (a) the *kill system calls are
converted to dummies and (b) SYSCALL_tgkill and others do not set the
"really_sendit" flag when calling pfs_process_raise. The fact that
pfs_process_raise does not send the signal is actually easy to miss.
Parrot claims in debug output the signal is sent but it does not. You
have to either double check the code or strace parrot (which I did).

This comment above the *kill system calls says what the right behavior
is:

/*
Note that we call pfs_process_raise here so that the process data
structures are made aware of the signal propagation, possibly kicking
someone out of sleep.  However, we do *not* convert this call to a
dummy, so that the sender can deliver itself, thus getting the correct
data into the sa_info structure.
*/

The bug with identity boxing may now (again) exist but I think the real
reason the fe89bc527c3f2ace3144f04a78919ab5445cefe4 commit fixed the
bug:

"This fixes a bug in which processes in an identity box could
send signals to those outside the box."

in the first place was because parrot processes now cannot send signals
at all! :D I have made a passing attempt at resolving this issue by
wrapping one of the kills with a "really_sendit" check, but that may
not be correct.

Anyway, this resolves the hang but there are still issues I think
continue to exist in the way parrot handles threads, at least for this

parrot/src/pfs_dispatch.cc
@@ -2408,7 +2408,6 @@ void decode_syscall( struct pfs_process *p, int entering )
 				debug(D_PROCESS,"%s %d %d %d",tracer_syscall32_name(p->syscall),args[0],args[1],args[2]);
 				p->syscall_result = pfs_process_raise(args[0],args[1],0);
 				if (p->syscall_result == -1) p->syscall_result = -errno;
-				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 
@@ -2418,7 +2417,6 @@ void decode_syscall( struct pfs_process *p, int entering )
 				debug(D_PROCESS,"tgkill %d %d %d",args[0],args[1],args[2]);
 				p->syscall_result = pfs_process_raise(args[1],args[2],0);
 				if (p->syscall_result == -1) p->syscall_result = -errno;
-				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 

parrot/src/pfs_dispatch64.cc
@@ -2265,7 +2265,6 @@ static void decode_syscall( struct pfs_process *p, INT64_T entering )
 				debug(D_PROCESS,"%s %d %d %d",tracer_syscall64_name(p->syscall),args[0],args[1],args[2]);
 				p->syscall_result = pfs_process_raise(args[0],args[1],0);
 				if (p->syscall_result == -1) p->syscall_result = -errno;
-				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 
@@ -2274,7 +2273,6 @@ static void decode_syscall( struct pfs_process *p, INT64_T entering )
 				debug(D_PROCESS,"tgkill %d %d %d",args[0],args[1],args[2]);
 				p->syscall_result = pfs_process_raise(args[1],args[2],0);
 				if (p->syscall_result == -1) p->syscall_result = -errno;
-				divert_to_dummy(p,p->syscall_result);
 			}
 			break;
 

parrot/src/pfs_process.cc
@@ -447,7 +447,11 @@ int  pfs_process_raise( pid_t pid, int sig, int really_sendit )
 				debug(D_PROCESS,"ignoring attempt to send signal to parrot itself.");
 				result = 0;
 			} else {
-				result = kill(pid,sig);
+				if (really_sendit) {
+					result = kill(pid,sig);
+				} else {
+					result = 0;
+				}
 			}
 		}
 	} else {