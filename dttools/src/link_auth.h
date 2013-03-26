de374c8572fb298352c812e202ee91dfce6cccc7
Douglas Thain <dthain@nd.edu>
2013-03-26 11:31:24 -0400

    Updated copyright boilerplate.

--- dttools/src/link_auth.h
+++ dttools/src/link_auth.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2012- The University of Notre Dame
+Copyright (C) 2013- The University of Notre Dame
 This software is distributed under the GNU General Public License.
 See the file COPYING for details.
 */
