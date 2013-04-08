9da9a05971f24591139231b510e10bba08bb18aa
Casey Robinson <kc@rampantmonkey.com>
2013-04-08 14:57:55 -0400

    call makeflow from linker

--- linker/src/linker.c
+++ linker/src/linker.c
@@ -1,3 +1,44 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <limits.h>
+
+#include "hash_table.h"
+
+#define MAKEFLOW_PATH "makeflow"
+#define MAKEFLOW_BUNDLE_FLAG "-b"
+
+void initialize( char *output_directory, char *input_file, struct hash_table *ht){
+	pid_t pid;
+
+	char expanded_input[PATH_MAX];
+	realpath(input_file, expanded_input);
+
+	char ch;
+	switch ( pid = fork() ){
+	case -1:
+		fprintf( stderr, "Cannot fork. Exiting...\n" );
+		exit(1);
+	case 0:
+		/* Child process */
+
+		char * const args[5] = { "linking makeflow" , MAKEFLOW_BUNDLE_FLAG, output_directory, expanded_input, NULL };
+		execvp(MAKEFLOW_PATH, args); 
+		exit(1);
+	default:
+		/* Parent process */
+	}
+}
+
 int main(void){
+	char *output = "output_dir";
+	char *input = "test.mf";
+
+	struct hash_table *names;
+	names = hash_table_create(0, NULL);
+
+	initialize(output, input, names);
+
 	return 0;
 }
