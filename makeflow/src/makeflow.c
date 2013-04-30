9db61ac7a8955b8b37e1cddfba20e24443410de7
Kyle Joseph Mulholland <kmulholl@cclws05.cse.nd.edu>
2013-04-30 15:13:27 -0400

    Resolved conflict with master

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -2292,33 +2292,12 @@ int main(int argc, char *argv[])
 			char *value = string_format("%d", catalog_port);
 			setenv("CATALOG_PORT", value, 1);
 			free(value);
-	
-<<<<<<< HEAD
-				break;
-			case 'd':
-				debug_flags_set(optarg);
-				break;
-			case 'D':
-				if (ppm_mode) {
-					ppm_option = optarg;
-				}
-				if (strcasecmp(optarg, "c") == 0) condense_display = 1;
-				if (strcasecmp(optarg, "s") == 0) change_size = 1;
-				if (strcasecmp(optarg, "ppm") == 0) ppm_mode = 1;
-				display_mode = 1;
-				break;
-			case 'E':
-				work_queue_estimate_capacity_on = 1;
-				break;
-			case 'f':
-				write_summary_to = xxstrdup(optarg);
-=======
->>>>>>> 996363b520bedd60a62ef0ac3812c2cd0bb075be
 			break;
 		case 'd':
 			debug_flags_set(optarg);
 			break;
 		case 'D':
+			if (ppm_mode) ppm_option = optarg;
 			if (strcasecmp(optarg, "c") == 0) condense_display = 1;
 			if (strcasecmp(optarg, "s") == 0) change_size = 1;
 			if (strcasecmp(optarg, "ppm") == 0) ppm_mode = 1;
