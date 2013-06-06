22ef036552e374448afd69d5487f505e99392612
Patrick Donnelly <pdonnel3@nd.edu>
2013-06-06 11:40:05 -0400

    Improvements to test runner script.
    Test still fails. I'm looking into it.

--- parrot/test/TR_parrot_search_chirp.sh
+++ parrot/test/TR_parrot_search_chirp.sh
@@ -4,45 +4,51 @@
 
 chirp_server="../../chirp/src/chirp_server"
 psearch="../src/parrot_run ../src/parrot_search"
-out=output.txt
 expected=expected.txt
+
 cpid=cpid.txt
 debug=debug.txt
+out=output.txt
+port=port.txt
 
 prepare()
 {
-    $chirp_server -r "`pwd`/fixtures/a"  -p 9001 -d all -o $debug &
-    echo $! > $cpid
-    touch $out
+    $chirp_server -r ./fixtures/a -I 127.0.0.1 -Z $port -b -B $cpid -d all -o $debug
     make -C ../src
     exit 0
 }
 
 run()
 {
-    $psearch /chirp/localhost:9001/ bar >> $out
-    $psearch /chirp/localhost:9001/ /bar >> $out
-    $psearch /chirp/localhost:9001/ c/bar >> $out
-    $psearch /chirp/localhost:9001/ /c/bar >> $out
-    $psearch /chirp/localhost:9001/ b/bar >> $out
-    $psearch /chirp/localhost:9001/ /b/bar >> $out
-    $psearch /chirp/localhost:9001/ b/foo >> $out
-    $psearch /chirp/localhost:9001/ /b/foo >> $out
-    $psearch /chirp/localhost:9001/ "/*/foo" >> $out
-    $psearch /chirp/localhost:9001/ "*/*r" >> $out
+    $psearch /chirp/localhost:`cat $port`/ bar > $out
+    $psearch /chirp/localhost:`cat $port`/ /bar >> $out
+    $psearch /chirp/localhost:`cat $port`/ c/bar >> $out
+    $psearch /chirp/localhost:`cat $port`/ /c/bar >> $out
+    $psearch /chirp/localhost:`cat $port`/ b/bar >> $out
+    $psearch /chirp/localhost:`cat $port`/ /b/bar >> $out
+    $psearch /chirp/localhost:`cat $port`/ b/foo >> $out
+    $psearch /chirp/localhost:`cat $port`/ /b/foo >> $out
+    $psearch /chirp/localhost:`cat $port`/ "/*/foo" >> $out
+    $psearch /chirp/localhost:`cat $port`/ "*/*r" >> $out
 
     noerr=`cat $out | grep -v error`
     rm -f $out
     echo "$noerr" > $out
 
     failures=`diff --ignore-all-space $out $expected`
-    [ -z "$failures" ] && echo "all tests passed" || echo $failures
+    if [ -z "$failures" ]; then
+        echo "all tests passed"
+        exit 0
+    else
+        echo $failures
+        exit 1
+    fi
 }
 
 clean()
 {
     kill -9 `cat $cpid`
-    rm -f $out $cpid
+    rm -f $out $cpid $port $debug
     exit 0
 }
 
