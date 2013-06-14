0f79e6c5c89065e1a75bf5d1bc58556d60a645bb
Douglas Thain <dthain@nd.edu>
2013-06-14 10:22:52 -0400

    Modified SAND tests to use -Z for selecting a port. Removed find_free_port from the testing infrastructure: the technique has an inherent race condition.

--- sand/test/filter_verification/cleanup.sh
+++ sand/test/filter_verification/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -f random.seq random_revcom.seq random.fa random.cfa random.cand filter.log filter.log.old worker.log
+rm -f random.seq random_revcom.seq random.fa random.cfa random.cand filter.log filter.log.old worker.log port.file
