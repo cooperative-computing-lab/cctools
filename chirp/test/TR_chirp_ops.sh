94ed8cab1f5d3167aa6efbaf1998cb3acc68a8a5
Patrick Donnelly <batrick@batbytes.com>
2013-06-24 13:47:04 -0400

    use special wait function

--- chirp/test/TR_chirp_ops.sh
+++ chirp/test/TR_chirp_ops.sh
@@ -10,7 +10,9 @@ chirp_root=chirp.root
 prepare()
 {
 	../src/chirp_server -r "$chirp_root" -I 127.0.0.1 -Z "$chirp_port" -b -B "$chirp_pid" -d all -o "$chirp_debug"
-	sleep 1
+
+	wait_for_file_creation "$chirp_port" 5
+	wait_for_file_creation "$chirp_pid" 5
 }
 
 run()
