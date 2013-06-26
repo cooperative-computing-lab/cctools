2f84877626c5c0d6b1e970af268b4ad02c99f955
Patrick Donnelly <pdonnel3@nd.edu>
2013-06-26 19:37:01 -0400

    Move chunk test into script.

--- dttools/test/TR_002_chunk_test.sh
+++ dttools/test/TR_002_chunk_test.sh
@@ -2,21 +2,49 @@
 
 . ../../dttools/src/test_runner.common.sh
 
+exe="chunk.test"
+output="chunk.output"
+
 prepare()
 {
-    cd ../src/; make
-    exit 0
+	gcc -g -o "$exe" -I ../src/ -x c - -x none ../src/libdttools.a -lm <<EOF
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "chunk.h"
+
+int main(int argc, char **argv)
+{
+	char **files = malloc(2 * sizeof(*files));
+
+	files[0] = malloc(8 * sizeof(*files[0]));
+	strcpy(files[0], "../src/chunk.c");
+
+	files[1] = malloc(8 * sizeof(*files[0]));
+	strcpy(files[1], "../src/chunk.h");
+
+	if(!chunk_concat(argv[1], (char **) files, 2, "> ", NULL)) {
+		fprintf(stderr, "chunk_test: chunk_concat failed\n");
+		exit(1);
+	}
+
+	return 0;
+}
+EOF
+	return $?
 }
 
 run()
 {
-    cd ../src; exec ./chunk_test
+	./"$exe" "$output"
+	return $?
 }
 
 clean()
 {
-    rm -f ../src/large_chunk.txt
-    exit 0
+	rm -f "$exe" "$output"
+	return 0
 }
 
 dispatch $@
