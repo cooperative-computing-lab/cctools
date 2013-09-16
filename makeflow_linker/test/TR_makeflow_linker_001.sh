e5ff74a1ccc9c5ec193ad5fe2332f77a8da5e1c4
Casey Robinson <kc@rampantmonkey.com>
2013-09-16 08:47:58 -0400

    add abstract test which targets the driver integration

---
+++ makeflow_linker/test/TR_makeflow_linker_001.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+. ../../dttools/src/test_runner.common.sh
+
+
+out_dir=makeflow_linker.001.out
+expected=001
+workflow_description=001.mf
+
+prepare() {
+	cd ../src; make
+	exit $?
+}
+
+run() {
+	../src/makeflow_linker -o $out_dir input/001/$workflow_description
+	explicit_dependency=$(cat $out_dir/explicit | awk '{print $1}')
+	if [ "$explicit_dependency" != "Python" ]; then
+		exit 1
+	fi
+	cp expected/$expected/explicit $out_dir/explicit
+
+	diff -bur expected/$expected $out_dir
+	exit $?
+}
+
+clean() {
+	rm -rf $out_dir
+}
+
+dispatch "$@"
+
