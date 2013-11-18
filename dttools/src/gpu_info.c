202ef5f5b91f26a6a0dbe4d85342312f87d50af7
Casey Robinson <kc@rampantmonkey.com>
2013-11-18 07:56:09 -0700

    pass gpu count via stdout instead of exit codes.
    Exit codes can be ambiguous.

--- dttools/src/gpu_info.c
+++ dttools/src/gpu_info.c
@@ -7,6 +7,8 @@ See the file COPYING for details.
 #include "gpu_info.h"
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -14,19 +16,32 @@ See the file COPYING for details.
 int gpu_info_get()
 {
 	pid_t pid;
-	char *args[] = {"gpu_autodetect", NULL};
-	int status = 0;
+	int pipefd[2];
+	pipe(pipefd);
+
+	printf("Called gpu_info_get()\n");
 
 	switch (pid = fork() ) {
 	case -1:
 		return 0;
 	case 0:
+		close(pipefd[0]);
+		dup2(pipefd[1], fileno(stdout));
+		char *args[] = {"gpu_autodetect", NULL};
 		execvp("gpu_autodetect", args);
 		return 0;
 	default:
-		waitpid(pid, &status, 0);
-		if(WIFEXITED(status)) return WEXITSTATUS(status);
-		return 0;
+		close(pipefd[1]);
+		int status = 0;
+		int gpu_count = 0;
+		char buffer[10]; /* Enough characters to hold a decimal representation of a 32 bit int. */
+		if(read(pipefd[0], buffer, 10)){
+			waitpid(pid, &status, 0);
+			gpu_count = atoi(buffer);
+		}
+
+		close(pipefd[0]);
+		return gpu_count;
 	}
 }
 
