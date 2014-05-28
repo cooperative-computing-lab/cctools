3388170c4491d1ab1992f1012234be091d7f343f
Peter Ivie <pivie@nd.edu>
2014-05-28 09:01:23 -0400

    Removed expression library - I hope to incorporate it later.

--- deltadb/src/deltadb_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef DELTADB_FUNCTIONS_H
-#define DELTADB_FUNCTIONS_H
-
-#include "deltadb_value.h"
-
-struct deltadb_value * deltadb_function_call( const char *name, struct deltadb_value *args );
-
-#endif
