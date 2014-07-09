e5eaaeb836cf46390a0e92fa8d2287023db7eac3
Douglas Thain <dthain@nd.edu>
2014-07-09 11:21:31 -0400

    Simplified test code to reduce spurious test failures.

--- wavefront/test/TR_wavefront_master_001_sum_test.sh
+++ wavefront/test/TR_wavefront_master_001_sum_test.sh
@@ -2,68 +2,74 @@
 
 . ../../dttools/src/test_runner.common.sh
 
+PATH=../src:../../work_queue/src:$PATH
+
 TEST_INPUT=test.wmaster.input	
 TEST_OUTPUT=test.wmaster.output	
-PORT_FILE=wfm.port
-PIDMASTER_FILE=wfm.pid
-PIDWORKER_FILE=worker.pid
+PORT_FILE=master.port
+MASTER_PID=master.pid
+MASTER_LOG=master.log
+MASTER_OUTPUT=master.output
 
-prepare()
+cleanfiles()
 {
 	rm -f $TEST_INPUT
 	rm -f $TEST_OUTPUT
 	rm -f $PORT_FILE
-	rm -f $PIDMASTER_FILE
-	rm -f $PIDWORKER_FILE
+	rm -f $MASTER_PID
+	rm -f $MASTER_LOG
+	rm -f $MASTER_OUTPUT
+}
 
+prepare()
+{
+	cleanfiles
 	./gen_ints_wfm.sh $TEST_INPUT 10
-
-	../src/wavefront_master -Z $PORT_FILE ./sum_wfm.sh 10 10 $TEST_INPUT $TEST_OUTPUT &
-	pid=$!
-	echo $pid > $PIDMASTER_FILE
-
-	#wait at most five seconds for master to come alive.
-	wait_for_file_creation $PORT_FILE 5
-
-	exit 0
 }
 
 run()
 {
 	answer=1854882
-	port=`cat $PORT_FILE`
 
-	../../work_queue/src/work_queue_worker -t60 localhost $port &
+	echo "starting wavefront master"
+	wavefront_master -d all -o $MASTER_LOG -Z $PORT_FILE ./sum_wfm.sh 10 10 $TEST_INPUT $TEST_OUTPUT > $MASTER_OUTPUT &
 	pid=$!
-	echo $pid > $PIDWORKER_FILE
+    echo $pid > $MASTER_PID
 
-	#Wait for all the output to be written to the file. We wait until last
-	#modification to $TEST_OUTPUT is at least 3 second ago.
-	wait_for_file_creation $TEST_OUTPUT 5
-	wait_for_file_modification $TEST_OUTPUT 3
+	echo "waiting for port file to be created"
+	wait_for_file_creation $PORT_FILE 5
 
+	echo "running worker"
+	work_queue_worker --timeout 2 localhost `cat $PORT_FILE`
+
+	echo "extracting result"
 	value=`sed -n 's/^9 9 \([[:digit:]]*\)/\1/p' $TEST_OUTPUT`
 
-	if [ -z $value ]
+	echo "computed value is $value"
+
+	if [ X$value = X ]
 	then
+		echo "result is missing"
 		exit 1
+	elif [ $value = $answer ]
+	then
+		echo "result is correct"
+		exit 0
 	else
-		exit $(($answer-$value))
+		echo "result is incorrect"
+		exit 1
 	fi
 }
 
 clean()
 {
-	[ -f $PIDWORKER_FILE ] && /bin/kill -9 `cat $PIDWORKER_FILE`
-	[ -f $PIDMASTER_FILE ] && /bin/kill -9 `cat $PIDMASTER_FILE`
+	if [ -f $MASTER_PID ]
+	then
+		kill -9 `cat $MASTER_PID`
+	fi
 
-	rm -f $TEST_INPUT
-	rm -f $TEST_OUTPUT
-	rm -f $PORT_FILE
-	rm -f $PIDMASTER_FILE
-	rm -f $PIDWORKER_FILE
-
-    exit 0
+	cleanfiles
+	exit 0
 }
 
 dispatch $@
