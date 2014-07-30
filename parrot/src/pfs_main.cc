cooperative-computing-lab/cctools

commit 8c070ac70076bb6462b030e5876962759f4ce0e9

On execve, reopen /proc/X/mem.
After execve replaces the previous memory mapping, apparently any old open file
descriptor for /proc/X/mem becomes useless. This caused ptrace to unnecessarily
(and permanently) fallback to PTRACE_POKEDATA/PTRACE_PEEKDATA.

This patch also changes some tracer functions to use better types for buffer
length/return.

parrot/src/tracer.c
@@ -318,19 +318,20 @@ in and out of the target process.  We use a long here because
 that represents the natural type of the target platform.
 */
 
-static int tracer_copy_out_slow( struct tracer *t, const void *data, const void *uaddr, int length )
+static ssize_t tracer_copy_out_slow( struct tracer *t, const void *data, const void *uaddr, size_t length )
 {
 	UINT8_T *bdata = (UINT8_T *)data;
 	UINT8_T *buaddr = (UINT8_T *)uaddr;
-	UINT32_T size = length;
-	UINT32_T wordsize = sizeof(long);
+	size_t size = length;
 	long word;
+	size_t wordsize = sizeof(word);
 
 	/* first, copy whole words */ 
 
-	while(size>=sizeof(long)) {
+	while(size>=wordsize) {
 		word = *(long*)bdata;
-		ptrace(PTRACE_POKEDATA,t->pid,buaddr,word);
+		if (ptrace(PTRACE_POKEDATA,t->pid,buaddr,word) == -1)
+			return -1;
 		size -= wordsize;
 		buaddr += wordsize;
 		bdata += wordsize;
@@ -339,17 +340,18 @@ static int tracer_copy_out_slow( struct tracer *t, const void *data, const void 
 	/* if necessary, copy the last few bytes */
 
 	if(size>0) {
-		word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0);
+		if ((word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1)
+			return -1;
 		memcpy(&word,bdata,size);
-		ptrace(PTRACE_POKEDATA,t->pid,buaddr,word);
+		if (ptrace(PTRACE_POKEDATA,t->pid,buaddr,word) == -1)
+			return -1;
 	}
 
-	return length;		
+	return length;
 }
 
-int tracer_copy_out( struct tracer *t, const void *data, const void *uaddr, int length )
+ssize_t tracer_copy_out( struct tracer *t, const void *data, const void *uaddr, size_t length )
 {
-	int result;
 	static int has_fast_write=1;
 	UPTRINT_T iuaddr = (UPTRINT_T)uaddr;
 
@@ -360,32 +362,41 @@ int tracer_copy_out( struct tracer *t, const void *data, const void *uaddr, int 
 #endif
 
 	if(has_fast_write) {
-		result = full_pwrite64(t->memory_file,data,length,iuaddr);
-		if( result!=length ) {
-			has_fast_write = 0;
-			debug(D_SYSCALL,"writing to /proc/X/mem failed, falling back to slow ptrace write");
+		ssize_t result = full_pwrite64(t->memory_file,data,length,iuaddr);
+		if( (size_t)result!=length ) {
+			/* this may be because execve caused a remapping, we need to reopen */
+			close(t->memory_file);
+			char path[PATH_MAX];
+			sprintf(path,"/proc/%d/mem",t->pid);
+			t->memory_file = open64(path,O_RDWR);
+			result = full_pwrite64(t->memory_file,data,length,iuaddr);
+			if ((size_t)result == length) {
+				return result;
+			} else {
+				has_fast_write = 0;
+				debug(D_SYSCALL,"writing to /proc/%d/mem failed, falling back to slow ptrace write", t->pid);
+			}
 		} else {
 			return result;
 		}
 	}
 
-	result = tracer_copy_out_slow(t,data,(void*)iuaddr,length);
-
-	return result;
+	return tracer_copy_out_slow(t,data,(void*)iuaddr,length);
 }
 
-static int tracer_copy_in_slow( struct tracer *t, const void *data, const void *uaddr, int length )
+static ssize_t tracer_copy_in_slow( struct tracer *t, const void *data, const void *uaddr, size_t length )
 {
 	UINT8_T *bdata = (UINT8_T *)data;
 	UINT8_T *buaddr = (UINT8_T *)uaddr;
-	UINT32_T size = length;
-	UINT32_T wordsize = sizeof(long);
+	size_t size = length;
 	long word;
+	size_t wordsize = sizeof(word);
 
 	/* first, copy whole words */ 
 
-	while(size>=sizeof(long)) {
-		*((long*)bdata) = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0);
+	while(size>=wordsize) {
+		if ((*((long*)bdata) = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1)
+			return -1;
 		size -= wordsize;
 		buaddr += wordsize;
 		bdata += wordsize;
@@ -394,24 +405,26 @@ static int tracer_copy_in_slow( struct tracer *t, const void *data, const void *
 	/* if necessary, copy the last few bytes */
 
 	if(size>0) {
-		word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0);
+		if ((word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1)
+			return -1;
 		memcpy(bdata,&word,size);
 	}
 
-	return length;		
+	return length;
 }
 
-int tracer_copy_in_string( struct tracer *t, char *str, const void *uaddr, int length )
+ssize_t tracer_copy_in_string( struct tracer *t, char *str, const void *uaddr, size_t length )
 {
 	UINT8_T *bdata = (UINT8_T *)str;
 	UINT8_T *buaddr = (UINT8_T *)uaddr;
-	UINT32_T total = 0;
-	UINT32_T wordsize = sizeof(long);
+	ssize_t total = 0;
 	long word;
+	size_t wordsize = sizeof(word);
 	unsigned int i;
 
 	while(length>0) {
-		word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0);
+		if ((word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1)
+			return -1;
 		UINT8_T *worddata = (void*)&word;
 		for(i=0;i<wordsize;i++) {
 			*bdata = worddata[i];
@@ -448,36 +461,43 @@ is 32 or 64 bit, since the 32-bit pread() cannot read above the 2GB
 limit on a 32-bit process.
 */
 
-int tracer_copy_in( struct tracer *t, void *data, const void *uaddr, int length )
+ssize_t tracer_copy_in( struct tracer *t, void *data, const void *uaddr, size_t length )
 {
-	int result;
 	static int fast_read_success = 0;
 	static int fast_read_failure = 0;
 	static int fast_read_attempts = 100;
 
-	UPTRINT_T iuaddr = (UPTRINT_T)uaddr;
+	uintptr_t iuaddr = (UPTRINT_T)uaddr;
 
 #if !defined(CCTOOLS_CPU_I386)
 	if(!tracer_is_64bit(t)) iuaddr &= 0xffffffff;
 #endif
 
 	if(fast_read_success>0 || fast_read_failure<fast_read_attempts) {
-		result = full_pread64(t->memory_file,data,length,iuaddr);
+		ssize_t result = full_pread64(t->memory_file,data,length,iuaddr);
 		if(result>0) {
 			fast_read_success++;
 			return result;
 		} else {
-			fast_read_failure++;
-			// fall through to slow method, print message on the last attempt.
-			if(fast_read_success==0 && fast_read_failure>=fast_read_attempts) {
-				debug(D_SYSCALL,"reading from /proc/X/mem failed, falling back to slow ptrace read");
+			/* this may be because execve caused a remapping, we need to reopen */
+			close(t->memory_file);
+			char path[PATH_MAX];
+			sprintf(path,"/proc/%d/mem",t->pid);
+			t->memory_file = open64(path,O_RDWR);
+			result = full_pread64(t->memory_file,data,length,iuaddr);
+			if ((size_t)result == length) {
+				return result;
+			} else {
+				fast_read_failure++;
+				// fall through to slow method, print message on the last attempt.
+				if(fast_read_success==0 && fast_read_failure>=fast_read_attempts) {
+					debug(D_SYSCALL,"reading from /proc/%d/mem failed, falling back to slow ptrace read", t->pid);
+				}
 			}
 		}
 	}
 
-	result = tracer_copy_in_slow(t,data,(void*)iuaddr,length);
-
-	return result;
+	return tracer_copy_in_slow(t,data,(void *)iuaddr,length);
 }
 
 #include "tracer.table.c"

parrot/src/tracer.h
@@ -22,17 +22,17 @@ int tracer_continue( struct tracer *t, int signum );
 int tracer_listen( struct tracer *t );
 unsigned long tracer_getevent( struct tracer *t );
 
-int             tracer_args_get( struct tracer *t, INT64_T *syscall, INT64_T args[TRACER_ARGS_MAX] );
-int             tracer_args_set( struct tracer *t, INT64_T syscall, INT64_T args[TRACER_ARGS_MAX], int nargs );
+int tracer_args_get( struct tracer *t, INT64_T *syscall, INT64_T args[TRACER_ARGS_MAX] );
+int tracer_args_set( struct tracer *t, INT64_T syscall, INT64_T args[TRACER_ARGS_MAX], int nargs );
 
 void		tracer_has_args5_bug( struct tracer *t );
 
 int             tracer_result_get( struct tracer *t, INT64_T *result );
 int             tracer_result_set( struct tracer *t, INT64_T result );
 
-int             tracer_copy_out( struct tracer *t, const void *data, const void *uaddr, int length );
-int             tracer_copy_in( struct tracer *t, void *data, const void *uaddr, int length );
-int             tracer_copy_in_string( struct tracer *t, char *data, const void *uaddr, int maxlength );
+ssize_t tracer_copy_out( struct tracer *t, const void *data, const void *uaddr, size_t length );
+ssize_t tracer_copy_in( struct tracer *t, void *data, const void *uaddr, size_t length );
+ssize_t tracer_copy_in_string( struct tracer *t, char *data, const void *uaddr, size_t maxlength );
 
 int             tracer_is_64bit( struct tracer *t );
 

commit 87a23f3f84a346b361ac2860ee400b2547e3ebe0

Return a partial count for tracer read.
8c070ac70076bb6462b030e5876962759f4ce0e9 introduced new behavior which would
cause the tracer to return an error (-1) if any ptrace(PEEKDATA, ...) failed.
Certain system calls like ioctl expect a partial read instead. This commit
restores the old behavior.

I've also added some extra debug output on error to help identify these
failures in the future. Before any ptrace error would just be silently ignored
which could hide a problem that reappears as something worse later.

Fixes #518.

parrot/src/pfs_main.cc
@@ -324,6 +324,7 @@ the event and take the appropriate action.
 static void handle_event( pid_t pid, int status, struct rusage *usage )
 {
 	struct pfs_process *p;
+	unsigned long message;
 
 	p = pfs_process_lookup(pid);
 	if(!p) {
@@ -340,7 +341,9 @@ static void handle_event( pid_t pid, int status, struct rusage *usage )
 		struct pfs_process *child;
 		INT64_T clone_files;
 
-		cpid = tracer_getevent(p->tracer);
+		if (tracer_getevent(p->tracer, &message) == -1)
+			return;
+		cpid = message;
 		debug(D_PROCESS, "pid %d cloned %d",pid,cpid);
 
 		if(status>>8 == (SIGTRAP | (PTRACE_EVENT_FORK<<8)) || status>>8 == (SIGTRAP | (PTRACE_EVENT_VFORK<<8))) {
@@ -374,7 +377,9 @@ static void handle_event( pid_t pid, int status, struct rusage *usage )
 		struct rusage _usage;
 		if (status>>8 == (SIGTRAP | (PTRACE_EVENT_EXIT<<8))) {
 			debug(D_DEBUG, "pid %d received PTRACE_EVENT_EXIT",pid);
-			status = tracer_getevent(p->tracer);
+			if (tracer_getevent(p->tracer, &message) == -1)
+				return;
+			status = message;
 		}
 		if (WIFEXITED(status))
 			debug(D_PROCESS,"pid %d exited normally with code %d",pid,WEXITSTATUS(status));

parrot/src/tracer.c
@@ -29,6 +29,12 @@ See the file COPYING for details.
 
 #define FATAL fatal("tracer: %d %s",t->pid,strerror(errno));
 
+#define ERROR \
+	do {\
+		debug(D_DEBUG, "%s: ptrace error: %s", __func__, strerror(errno));\
+		return -1;\
+	} while (0)
+
 #ifndef PTRACE_OLDSETOPTIONS
 #	define PTRACE_OLDSETOPTIONS 21
 #endif
@@ -86,21 +92,21 @@ int tracer_attach (pid_t pid)
 		 * explanation, see pfs_main.cc where PTRACE_LISTEN is used.
 		 */
 		if (ptrace(PTRACE_SEIZE, pid, 0, (void *)options) == -1)
-			return -1;
+			ERROR;
 	} else {
 		if (ptrace(PTRACE_ATTACH, pid, 0, 0) == -1)
-			return -1;
+			ERROR;
 		if (linux_available(2,6,0)) {
 			if (ptrace(PTRACE_SETOPTIONS, pid, 0, (void *)options) == -1)
-				return -1;
+				ERROR;
 		} else {
 			if (ptrace(PTRACE_OLDSETOPTIONS, pid, 0, (void *)options) == -1)
-				return -1;
+				ERROR;
 		}
 	}
 
 	if (ptrace(PTRACE_SYSCALL, pid, NULL, (void *)SIGCONT) == -1)
-		return -1;
+		ERROR;
 
 	return 0;
 }
@@ -109,11 +115,11 @@ int tracer_listen (struct tracer *t)
 {
 	if (linux_available(3,4,0)) {
 		if (ptrace(PTRACE_LISTEN, t->pid, NULL, NULL) == -1)
-			return -1;
+			ERROR;
 	} else {
 		/* This version of Linux does not allow transparently listening for wake up from group-stop. We have no choice but to restart it... */
 		if (ptrace(PTRACE_SYSCALL, t->pid, NULL, 0) == -1)
-			return -1;
+			ERROR;
 	}
 	return 0;
 }
@@ -141,11 +147,11 @@ struct tracer *tracer_init (pid_t pid)
 	return t;
 }
 
-unsigned long tracer_getevent( struct tracer *t )
+int tracer_getevent( struct tracer *t, unsigned long *message )
 {
-	unsigned long message;
-	ptrace(PTRACE_GETEVENTMSG, t->pid, 0, &message);
-	return message;
+	if (ptrace(PTRACE_GETEVENTMSG, t->pid, 0, message) == -1)
+		ERROR;
+	return 0;
 }
 
 int tracer_is_64bit( struct tracer *t )
@@ -162,28 +168,26 @@ int tracer_is_64bit( struct tracer *t )
 	}
 }
 
-int tracer_detach( struct tracer *t )
+void tracer_detach( struct tracer *t )
 {
-	pid_t pid = t->pid;
+	ptrace(PTRACE_DETACH,t->pid,0,0); /* ignore failure */
 	close(t->memory_file);
 	free(t);
-	if (ptrace(PTRACE_DETACH,pid,0,0) == -1)
-		return -1;
-	return 0;
 }
 
 int tracer_continue( struct tracer *t, int signum )
 {
 	t->gotregs = 0;
 	if (ptrace(PTRACE_SYSCALL,t->pid,0,signum) == -1)
-		return -1;
+		ERROR;
 	return 0;
 }
 
 int tracer_args_get( struct tracer *t, INT64_T *syscall, INT64_T args[TRACER_ARGS_MAX] )
 {
 	if(!t->gotregs) {
-		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs)!=0) FATAL;
+		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs) == -1)
+			ERROR;
 		t->gotregs = 1;
 	}
 
@@ -235,7 +239,8 @@ void tracer_has_args5_bug( struct tracer *t )
 int tracer_args_set( struct tracer *t, INT64_T syscall, INT64_T args[TRACER_ARGS_MAX], int nargs )
 {
 	if(!t->gotregs) {
-		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs)!=0) FATAL;
+		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs) == -1)
+			ERROR;
 		t->gotregs = 1;
 	}
 
@@ -270,7 +275,8 @@ int tracer_args_set( struct tracer *t, INT64_T syscall, INT64_T args[TRACER_ARGS
 	}
 #endif
 
-	if(ptrace(PTRACE_SETREGS,t->pid,0,&t->regs)!=0) FATAL;
+	if(ptrace(PTRACE_SETREGS,t->pid,0,&t->regs) == -1)
+		ERROR;
 
 	return 1;
 }
@@ -278,7 +284,8 @@ int tracer_args_set( struct tracer *t, INT64_T syscall, INT64_T args[TRACER_ARGS
 int tracer_result_get( struct tracer *t, INT64_T *result )
 {
 	if(!t->gotregs) {
-		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs)!=0) FATAL;
+		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs) == -1)
+			ERROR;
 		t->gotregs = 1;
 	}
 
@@ -294,7 +301,8 @@ int tracer_result_get( struct tracer *t, INT64_T *result )
 int tracer_result_set( struct tracer *t, INT64_T result )
 {
 	if(!t->gotregs) {
-		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs)!=0) FATAL;
+		if(ptrace(PTRACE_GETREGS,t->pid,0,&t->regs) == -1)
+			ERROR;
 		t->gotregs = 1;
 	}
 
@@ -304,7 +312,8 @@ int tracer_result_set( struct tracer *t, INT64_T result )
 	t->regs.regs64.rax = result;
 #endif
 
-	if(ptrace(PTRACE_SETREGS,t->pid,0,&t->regs)!=0) FATAL;
+	if(ptrace(PTRACE_SETREGS,t->pid,0,&t->regs) == -1)
+		ERROR;
 
 	return 1;
 }
@@ -331,7 +340,7 @@ static ssize_t tracer_copy_out_slow( struct tracer *t, const void *data, const v
 		word = *(long*)bdata;
 		errno = 0;
 		if (ptrace(PTRACE_POKEDATA,t->pid,buaddr,word) == -1 && errno)
-			return -1;
+			ERROR;
 		size -= sizeof(word);
 		buaddr += sizeof(word);
 		bdata += sizeof(word);
@@ -342,11 +351,11 @@ static ssize_t tracer_copy_out_slow( struct tracer *t, const void *data, const v
 	if(size>0) {
 		errno = 0;
 		if ((word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1 && errno)
-			return -1;
+			ERROR;
 		memcpy(&word,bdata,size);
 		errno = 0;
 		if (ptrace(PTRACE_POKEDATA,t->pid,buaddr,word) == -1 && errno)
-			return -1;
+			ERROR;
 	}
 
 	return length;
@@ -365,7 +374,9 @@ ssize_t tracer_copy_out( struct tracer *t, const void *data, const void *uaddr, 
 
 	if(has_fast_write) {
 		ssize_t result = full_pwrite64(t->memory_file,data,length,iuaddr);
-		if( (size_t)result!=length ) {
+		if ((size_t)result == length) {
+			return length;
+		} else {
 			/* this may be because execve caused a remapping, we need to reopen */
 			close(t->memory_file);
 			char path[PATH_MAX];
@@ -378,8 +389,6 @@ ssize_t tracer_copy_out( struct tracer *t, const void *data, const void *uaddr, 
 				has_fast_write = 0;
 				debug(D_SYSCALL,"writing to /proc/%d/mem failed, falling back to slow ptrace write", t->pid);
 			}
-		} else {
-			return result;
 		}
 	}
 
@@ -390,30 +399,38 @@ static ssize_t tracer_copy_in_slow( struct tracer *t, void *data, const void *ua
 {
 	uint8_t *bdata = data;
 	const uint8_t *buaddr = uaddr;
-	size_t size = length;
+	size_t total = 0;
 	long word;
 
 	/* first, copy whole words */ 
 
-	while(size>=sizeof(word)) {
+	while((length-total)>=sizeof(word)) {
 		errno = 0;
-		if ((*((long*)bdata) = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1 && errno)
-			return -1;
-		size -= sizeof(word);
+		if ((*((long*)bdata) = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1 && errno) {
+			if (total)
+				return total;
+			else
+				ERROR;
+		}
+		total += sizeof(word);
 		buaddr += sizeof(word);
 		bdata += sizeof(word);
 	}
 
 	/* if necessary, copy the last few bytes */
 
-	if(size>0) {
+	if((length-total)>0) {
 		errno = 0;
-		if ((word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1 && errno)
-			return -1;
-		memcpy(bdata,&word,size);
+		if ((word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1 && errno) {
+			if (total)
+				return total;
+			else
+				ERROR;
+		}
+		memcpy(bdata,&word,(length-total));
 	}
 
-	return length;
+	return total;
 }
 
 ssize_t tracer_copy_in_string( struct tracer *t, char *str, const void *uaddr, size_t length )
@@ -428,7 +445,7 @@ ssize_t tracer_copy_in_string( struct tracer *t, char *str, const void *uaddr, s
 		const uint8_t *worddata;
 		errno = 0;
 		if ((word = ptrace(PTRACE_PEEKDATA,t->pid,buaddr,0)) == -1 && errno)
-			return -1;
+			ERROR;
 		worddata = (const uint8_t *)&word;
 		for(i=0;i<sizeof(word);i++) {
 			*bdata = worddata[i];
@@ -489,7 +506,7 @@ ssize_t tracer_copy_in( struct tracer *t, void *data, const void *uaddr, size_t 
 			sprintf(path,"/proc/%d/mem",t->pid);
 			t->memory_file = open64(path,O_RDWR);
 			result = full_pread64(t->memory_file,data,length,iuaddr);
-			if ((size_t)result == length) {
+			if ((size_t)result > 0) {
 				return result;
 			} else {
 				fast_read_failure++;

parrot/src/tracer.h
@@ -16,11 +16,11 @@ See the file COPYING for details.
 struct tracer;
 
 int tracer_attach( pid_t pid );
-int tracer_detach( struct tracer *t );
+void tracer_detach( struct tracer *t );
 struct tracer *tracer_init( pid_t pid );
 int tracer_continue( struct tracer *t, int signum );
 int tracer_listen( struct tracer *t );
-unsigned long tracer_getevent( struct tracer *t );
+int tracer_getevent( struct tracer *t, unsigned long *message );
 
 int tracer_args_get( struct tracer *t, INT64_T *syscall, INT64_T args[TRACER_ARGS_MAX] );
 int tracer_args_set( struct tracer *t, INT64_T syscall, INT64_T args[TRACER_ARGS_MAX], int nargs );