2a6b027a5054b0e5cd70c0d2f7b9e5cc288ca00f
Benjamin Tovar <btovar@nd.edu>
2014-08-29 11:42:17 -0400

    Foremen update catalog on disconnection.

--- work_queue/src/work_queue_worker.c
+++ work_queue/src/work_queue_worker.c
@@ -1079,7 +1079,12 @@ static void disconnect_master(struct link *master) {
 	if(foreman_q) {
 		debug(D_WQ, "Disconnecting all workers...\n");
 		release_all_workers(foreman_q); 
+
+		if(project_regex) {
+			update_catalog(foreman_q, master, 1);
+		}
 	}
+
 
 
 	if(released_by_master) {
