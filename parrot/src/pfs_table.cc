cooperative-computing-lab/cctools

commit be2321475b0b23c8627de32c4e455917b55cf939

Increment reference count by channel offset.
o Adds new pfs_channel_addref function. mmap constructors now add a reference.
o Move deleting the channel reference to the mmap destructor.

parrot/src/pfs_table.cc
@@ -2580,11 +2580,7 @@ int pfs_table::mmap_delete( pfs_size_t logical_addr, pfs_size_t length )
 				mmap_update(logical_addr+length,0);
 			}
 
-			// Decrement (and possibly free) the file in the channel.
-			pfs_channel_free(m->channel_offset);
-
-			// Delete the mapping, which may also delete the file object.
-			delete m;
+			delete m; // Delete the mapping, which may also delete the file object and free the channel.
 
 			return 0;
 		}

parrot/src/pfs_channel.c
@@ -194,6 +194,20 @@ int pfs_channel_lookup( const char *name, pfs_size_t *start )
 	return 0;
 }
 
+int pfs_channel_addref( pfs_size_t start )
+{
+	struct entry *e = head;
+
+	do {
+		if(e->start==start) {
+			e->inuse++;
+			return 1;
+		}
+		e = e->next;
+	} while(e!=head);
+	return 0;
+}
+
 int pfs_channel_update_name( const char *oldname, const char *newname )
 {
 	struct entry *e = head;

parrot/src/pfs_channel.h
@@ -20,6 +20,7 @@ char * pfs_channel_base();
 int    pfs_channel_fd();
 
 int    pfs_channel_lookup( const char *name, pfs_size_t *start );
+int    pfs_channel_addref( pfs_size_t start );
 int    pfs_channel_alloc( const char *name, pfs_size_t length, pfs_size_t *start );
 void   pfs_channel_free( pfs_size_t start );
 

parrot/src/pfs_mmap.h
@@ -25,18 +25,19 @@ public:
 		prot = _prot;
 		flags = _flags;
 		file->addref();
+		pfs_channel_addref(channel_offset);
 	}
 
 	pfs_mmap( pfs_mmap * m ) {
 		file = m->file;
 		logical_addr = m->logical_addr;
-		pfs_channel_lookup(file->get_name()->path,&channel_offset); /* increase refcount */
-		assert(channel_offset == m->channel_offset);
+		channel_offset = m->channel_offset;
 		map_length = m->map_length;
 		file_offset = m->file_offset;
 		prot = m->prot;
 		flags = m->flags;
 		file->addref();
+		pfs_channel_addref(channel_offset);
 	}
 
 	~pfs_mmap()
@@ -46,6 +47,7 @@ public:
 			file->close();
 			delete file;
 		}
+		pfs_channel_free(channel_offset);
 	}
 
 	pfs_file   *file;

commit d4d306a213e83a593cf58442ad46b5022e846ede

pfs_channel_alloc adds extra ref.
This bug was introduced in be2321475b0b23c8627de32c4e455917b55cf939.

Fixes #550.

parrot/src/pfs_table.cc
@@ -2255,11 +2255,16 @@ pfs_size_t pfs_table::mmap_create( int fd, pfs_size_t file_offset, pfs_size_t ma
 			pfs_channel_free(channel_offset);
 			return -1;
 		}
+
+		channel_offset = mmap_create_object(file, channel_offset, map_length, file_offset, prot, flags);
+
+		/* pfs_channel_alloc adds a ref and so does mmap_create_object, remove the extra: */
+		pfs_channel_free(channel_offset);
+		return channel_offset;
 	} else {
 		debug(D_CHANNEL,"%s cached at channel %llx",file->get_name()->path,(long long)channel_offset);
+		return mmap_create_object(file, channel_offset, map_length, file_offset, prot, flags);
 	}
-
-	return mmap_create_object(file, channel_offset, map_length, file_offset, prot, flags);
 }
 
 int pfs_table::mmap_update( pfs_size_t logical_addr, pfs_size_t channel_offset )