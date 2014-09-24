28728871198d12dbeef6cffaedeaa96b3afc6fa8
Patrick Donnelly <pdonnel3@nd.edu>
2014-09-24 15:08:52 -0400

    Use 64 bit buffer lengths.
    This is pulled out of the Confuga branch where we want to able to hash large
    files. This commit also changes the <hash>_file routines to use mmap if
    possible.

--- dttools/src/hmac.h
+++ dttools/src/hmac.h
@@ -7,6 +7,8 @@ See the file COPYING for details.
 #ifndef HMAC_H_
 #define HMAC_H_
 
+#include <stdlib.h>
+
 /** @file hmac.h
 Routines for computing Hash-based Message Authentication Codes.
 */
@@ -25,7 +27,7 @@ Routines for computing Hash-based Message Authentication Codes.
 @param block_size The size of the block used by the hash function in bytes.
 @param hash_func A function pointer to the hash function to be used.
 */
-int hmac(const char *buffer, int buffer_length, const char *key, int key_length, unsigned char *digest, int digest_len, int block_size, void (*hash_func) (const char *, int, unsigned char *));
+int hmac(const void *buffer, size_t buffer_length, const void *key, size_t key_length, unsigned char *digest, size_t digest_len, size_t block_size, void (*hash_func) (const void *, size_t, unsigned char *));
 
 /** Generate HMAC using md5 hash function
 Note that this function produces a digest in binary form which must be converted to a human readable form with md5_string.
@@ -35,7 +37,7 @@ Note that this function produces a digest in binary form which must be converted
 @param key_length The length of the key in bytes.
 @param digest Pointer to a buffer of size MD5_DIGEST_LENGTH to store the digest.
 */
-int hmac_md5(const char *buffer, int buffer_length, const char *key, int key_length, unsigned char digest[MD5_DIGEST_LENGTH]);
+int hmac_md5(const void *buffer, size_t buffer_length, const void *key, size_t key_length, unsigned char digest[MD5_DIGEST_LENGTH]);
 
 /** Generate HMAC using sha1 hash function
 Note that this function produces a digest in binary form which must be converted to a human readable form with sha1_string.
@@ -45,7 +47,7 @@ Note that this function produces a digest in binary form which must be converted
 @param key_length The length of the key in bytes.
 @param digest Pointer to a buffer of size SHA1_DIGEST_LENGTH to store the digest.
 */
-int hmac_sha1(const char *buffer, int buffer_length, const char *key, int key_length, unsigned char digest[SHA1_DIGEST_LENGTH]);
+int hmac_sha1(const void *buffer, size_t buffer_length, const void *key, size_t key_length, unsigned char digest[SHA1_DIGEST_LENGTH]);
 
 
 #endif
