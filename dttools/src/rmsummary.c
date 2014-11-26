98b43fc5831b09fa85cc1d339b6992f0971e921f
Benjamin Tovar <btovar@nd.edu>
2014-11-26 10:30:40 -0500

    Use struct buffer to read summaries.
    Conflicts:
    	dttools/src/rmsummary.c

--- dttools/src/rmsummary.c
+++ dttools/src/rmsummary.c
@@ -20,6 +20,7 @@
 
 #include <ctype.h>
 
+#include "buffer.h"
 #include "int_sizes.h"
 #include "stringtools.h"
 #include "xxmalloc.h"
@@ -28,6 +29,7 @@
 #include "rmsummary.h"
 
 #define ONE_MEGABYTE 1048576 /* this many bytes */
+#define MAX_LINE 1024
 
 #define rmsummary_assign_as_int_field(s, key, value, field)	\
 	if(!strcmp(#field, key)){				\
@@ -42,6 +44,7 @@
 
 #define rmsummary_assign_as_string_field(s, key, value, field)	\
 	if(!strcmp(#field, key)){				\
+     	if(s->field) free(s->field);        \
 		s->field = xxstrdup(value);			\
 		return 1;}
 
@@ -77,34 +80,28 @@ int rmsummary_assign_field(struct rmsummary *s, char *key, char *value)
     we simply read between markers (--) **/
 char *rmsummary_read_single_chunk(FILE *stream)
 {
-	int nmax   = 1024;
-	int ntotal = 0;
-	int nline  = 1024;
-	int n;
-
-	char *line = malloc( nline * sizeof(char) );
-	char *summ = malloc( nmax  * sizeof(char) );
+	struct buffer b;
+	char   line[MAX_LINE];
 
 	/* Skip comments, blank lines, and markers before the summary */
 	char c;
 	while( (c = getc(stream)) == '#' || isblank(c) || c == '-' )
 	{
 		ungetc(c, stream);
+		
 		/* Make sure we read complete comment lines */
-		n = nline;
-		while( n >= (nline - 1) )
-		{
-			fgets(line, nline, stream);
-			n = strlen(line);
-		}
+		do {
+			line[MAX_LINE - 1] = '\0';
+			fgets(line, MAX_LINE, stream);
+		} while(line[MAX_LINE - 1]);
 	}
 
 	if(feof(stream))
 	{
-		free(line);
-		free(summ);
 		return NULL;
 	}
+
+	buffer_init(&b);
 
 	ungetc(c, stream);
 	while( (c = getc(stream)) != EOF )
@@ -115,31 +112,20 @@ char *rmsummary_read_single_chunk(FILE *stream)
 
 		if(c == '-')
 			break;
-		
-		fgets(line, nline, stream);
-		n = strlen(line);
 
-		if( ntotal + n > nmax )
-		{
-			nmax = ntotal + n + nline;
-			summ = realloc(summ, nmax * sizeof(char) );
-			if(!summ)
-				fatal("Could not read summary file : %s.\n", strerror(errno)); 
-
-            fprintf(stderr, "::::: %s\n", line);
-
-		}
-		memcpy((summ + ntotal), line, n); 
-		ntotal += n;
+		fgets(line, MAX_LINE, stream);
+		buffer_printf(&b, "%s", line);
 	}
 
-	free(line);
+	char *summ = xxstrdup(buffer_tostring(&b));
+
+	buffer_free(&b);
+
 	return summ;
 }
 
 /* Parse a string for summary fields. Separator is usually '\n' or ',' */
-#define MAX_LINE 1024
-struct rmsummary *rmsummary_parse_from_str(char *buffer, char separator)
+struct rmsummary *rmsummary_parse_from_str(const char *buffer, const char separator)
 {
 	char key[MAX_LINE], value[MAX_LINE];
 	char *token, *saveptr;
@@ -147,13 +133,14 @@ struct rmsummary *rmsummary_parse_from_str(char *buffer, char separator)
 	if(!buffer)
 		return NULL;
 
-	buffer = xxstrdup(buffer);
+	// strtok does not work with const char.
+	char *buffer_copy = xxstrdup(buffer);
 
 	const char delim[] = { separator, '\n', '\0'}; 
 
 	struct rmsummary *s = make_rmsummary(-1);
 
-	token = strtok_r(buffer, delim, &saveptr);
+	token = strtok_r(buffer_copy, delim, &saveptr);
 	while(token)
 	{
 		if(sscanf(token, "%[^:]:%*[ \t]%[^\n]", key, value) >= 2) 
@@ -174,7 +161,7 @@ struct rmsummary *rmsummary_parse_from_str(char *buffer, char separator)
 		token = strtok_r(NULL, delim, &saveptr);
 	}
 
-	free(buffer);
+	free(buffer_copy);
 
 	return s;
 }
