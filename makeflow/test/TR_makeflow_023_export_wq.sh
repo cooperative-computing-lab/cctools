47c5ddbb24d6041ce3814c9a6f8da55ab3bfeaa6
Douglas Thain <dthain@nd.edu>
2014-12-09 09:21:26 -0500

    Fixed up test.

--- makeflow/test/TR_makeflow_023_export_wq.sh
+++ makeflow/test/TR_makeflow_023_export_wq.sh
@@ -2,10 +2,8 @@
 
 # Test that an environment variable can be set in the makeflow
 # and then propagated all the way to a work queue task, where
-# it is evaluated.  Note that HELLO is set within the makeflow,
-# while GOODBYE is simply exported, and its value is taken from
-# the environment in effect when makeflow runs.  The backslashes
-# ensure that the variables are evaluated by the task, not by makeflow.
+# it is evaluated.  The backslashes ensure that the variables
+# are evaluated by the task, not by makeflow.
 
 . ../../dttools/test/test_runner_common.sh
 
@@ -20,7 +18,7 @@ prepare()
 	cd $TEST_DIR
 	cat > test.mf <<EOF
 export HELLO=hello makeflow
-export GOODBYE
+export GOODBYE=goodbye makeflow
 output.txt:
 	echo \\\$HELLO \\\$GOODBYE > output.txt
 EOF
@@ -35,7 +33,6 @@ run()
 {
 	cd $TEST_DIR
 
-	export GOODBYE="goodbye makeflow"
 	../../src/makeflow -d all -T wq -Z master.port test.mf &
 
 	wait_for_file_creation master.port 5
