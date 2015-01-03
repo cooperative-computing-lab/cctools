41fdb1a8f3a31102ba920b6876450a7c8391e47b
Douglas Thain <dthain@nd.edu>
2015-01-03 09:37:37 -0500

    Visitors should use file_table instead of completed_files to be independent of the current dag state.

--- makeflow/src/visitors.c
+++ makeflow/src/visitors.c
@@ -680,10 +680,11 @@ void dag_to_dot(struct dag *d, int condense_display, int change_size)
 	fprintf(stdout, "digraph {\n");
 
 	if(change_size) {
-		hash_table_firstkey(d->completed_files);
-		while(hash_table_nextkey(d->completed_files, &label, (void **) &name)) {
-			stat(label, &st);
-			average += ((double) st.st_size) / ((double) hash_table_size(d->completed_files));
+		hash_table_firstkey(d->file_table);
+		while(hash_table_nextkey(d->file_table, &name, (void**)&f )) {
+			if(stat(name,&st)==0) {
+				average += ((double) st.st_size) / ((double) hash_table_size(d->completed_files));
+			}
 		}
 	}
 
