7e669fe7280b419ddb992a5d9c5e81191d237e7b
Benjamin Tovar <btovar@nd.edu>
2015-03-26 10:14:44 -0400

    Change cvmfs symlink error to ENOENT.
    As reported by Brian Bockelman. EXDEV is only valid for errors regarding
    hard links.

--- parrot/src/pfs_service_cvmfs.cc
+++ parrot/src/pfs_service_cvmfs.cc
@@ -942,7 +942,7 @@ static bool path_expand_symlink(struct pfs_name *path, struct pfs_name *xpath)
 
 			if(sscanf(link_target, "/cvmfs/%[^/]%[^\n]", xpath->host, path_head) < 1)
 			{
-				errno = EXDEV;
+				errno = ENOENT;
 				return false;
 			}
 
