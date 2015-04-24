b5fbb2c4e888c476f46184680afd38d40eb62f8d
Douglas Thain <dthain@nd.edu>
2015-04-24 10:54:23 -0400

    Pass flags to summary as args instead of globals.

--- makeflow/src/makeflow_summary.h
+++ makeflow/src/makeflow_summary.h
@@ -7,7 +7,7 @@ See the file COPYING for details.
 #ifndef MAKEFLOW_SUMMARY_H
 #define MAKEFLOW_SUMMARY_H
 
-void makeflow_summary_create( struct dag *d, const char *filename, const char *email_summary_to, timestamp_t runtime, timestamp_t time_completed, int argc, char *argv[], const char *dagfile, struct batch_queue *remote_queue );
+void makeflow_summary_create( struct dag *d, const char *filename, const char *email_summary_to, timestamp_t runtime, timestamp_t time_completed, int argc, char *argv[], const char *dagfile, struct batch_queue *remote_queue, int abort_flag, int failed_flag );
 
 #endif
 
