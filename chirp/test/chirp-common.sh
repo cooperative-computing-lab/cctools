de11f19be8fc24bb18a502bda602af0ce9384a0c
Patrick Donnelly <pdonnel3@nd.edu>
2015-05-18 16:54:52 -0400

    Run as nobody so tests run on Docker.

--- chirp/test/chirp-common.sh
+++ chirp/test/chirp-common.sh
@@ -24,12 +24,18 @@ chirp_start() {
 		root="$1"
 	fi
 	shift
-	if chirp_server --advertise=localhost --auth=unix --background --debug=all --debug-file="$debug" --debug-rotate-max=0 --interface=127.0.0.1 --pid-file="$pid" --port-file="$port" --root="$root" --transient="$transient" "$@"; then
+	if [ "$(id -u)" -eq 0 ]; then
+		chirp_server --advertise=localhost --auth=unix --background --debug=all --debug-file="$debug" --debug-rotate-max=0 --interface=127.0.0.1 --pid-file="$pid" --port-file="$port" --root="$root" --transient="$transient" --user=nobody "$@"
+	else
+		chirp_server --advertise=localhost --auth=unix --background --debug=all --debug-file="$debug" --debug-rotate-max=0 --interface=127.0.0.1 --pid-file="$pid" --port-file="$port" --root="$root" --transient="$transient" "$@"
+	fi
+	result=$?
+	if [ "$result" -eq 0 ]; then
 		i=0
 		while [ $i -lt 10 ]; do
 			if [ -s "$pid" -a -s "$port" ]; then
 				hostport="127.0.0.1:$(cat "$port")"
-				unset debug pid port transient
+				unset debug pid port result transient
 				return 0
 			fi
 			echo $i sleeping waiting for server to start
@@ -42,7 +48,7 @@ chirp_start() {
 	fi
 	touch "$debug"
 	cat "$debug"
-	unset debug pid port transient
+	unset debug pid port result transient
 	return 1
 }
 
