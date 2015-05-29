989806b45ad540be751032b54e94d9f0e0d3963f
Patrick Donnelly <pdonnel3@nd.edu>
2015-05-29 13:21:08 -0400

    Move executable memory fd to dttools.

---
+++ dttools/src/memfdexe.h
@@ -0,0 +1,12 @@
+/*
+Copyright (C) 2015- The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file COPYING for details.
+*/
+
+#ifndef MEMFDEXE_H
+#define MEMFDEXE_H
+
+int memfdexe (const char *name, const char *extradir);
+
+#endif /* MEMFDEXE_H */
