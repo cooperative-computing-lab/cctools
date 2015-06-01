a6c50f858e19c404ffdcfdb49636edaf1ca1fb30
Peter Ivie <pivie@nd.edu>
2015-06-01 14:53:42 -0400

    A few more cleanup items with prune installation

---
+++ prune/src/prune/prune.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+#
+
+# Copyright (c) 2010- The University of Notre Dame.
+# This software is distributed under the GNU General Public License.
+# See the file COPYING for details.
+
+from prune.script import Script
+
+import sys
+
+cctools_version = 'CCTOOLS_VERSION'
+cctools_releasedate = ''
+
+if __name__ == '__main__':
+	pass
