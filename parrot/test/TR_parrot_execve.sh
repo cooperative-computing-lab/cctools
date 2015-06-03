1d3c584dbad4591d881312408d09ebfba5396bd9
Patrick Donnelly <pdonnel3@nd.edu>
2015-06-03 11:18:53 -0400

    Test execve of static executables.
    Continuation of #793.

--- parrot/test/TR_parrot_execve.sh
+++ parrot/test/TR_parrot_execve.sh
@@ -7,6 +7,7 @@ set -e
 
 c="./hostport.$PPID"
 parrot_debug=parrot.debug
+exe="hello-static"
 
 parrot() {
 	../src/parrot_run --no-chirp-catalog --debug=all --debug-file="$parrot_debug" --timeout=5 --work-dir="/chirp/$hostport/" "$@"
@@ -16,6 +17,18 @@ prepare()
 {
 	chirp_start local
 	echo "$hostport" > "$c"
+
+	set +e
+	# -static requires "libc-devel" which is missing on some platforms
+	gcc -static -I../src/ -g -o "$exe" -x c - -x none <<EOF
+#include <stdio.h>
+int main (int argc, char *argv[])
+{
+	printf("Hello, world!\\n");
+	return 0;
+}
+EOF
+	set -e
 
 	return 0
 }
@@ -36,9 +49,15 @@ EOF2
 cp /usr/bin/python /bin/sh bin/
 chmod 700 bin/a.py bin/python bin/sh
 EOF1
+	if [ -x "$exe" ]; then
+		[ "$(parrot -- "$(pwd)/$exe" | tee -a /dev/stderr)" = 'Hello, world!' ]
+	fi
 	for loader in `find -L /lib64 /lib -name 'ld-linux*.so*' 2>/dev/null`; do
 		[ "$(parrot --ld-path="$loader" -- ./bin/a.py 1 2 | tee -a /dev/stderr)" = './bin/a.py 1 2' ]
 		[ "$(parrot --ld-path="$loader" -- ./bin/sh -c 'echo "$0"' | tee -a /dev/stderr)" = './bin/sh' ]
+		if [ -x "$exe" ]; then
+			[ "$(parrot --ld-path="$loader" -- "$(pwd)/$exe" | tee -a /dev/stderr)" = 'Hello, world!' ]
+		fi
 		return 0
 	done
 	echo No loader found!
@@ -48,7 +67,7 @@ EOF1
 clean()
 {
 	chirp_clean
-	rm -f "$c" "$parrot_debug"
+	rm -f "$c" "$parrot_debug" "$exe"
 }
 
 dispatch "$@"
