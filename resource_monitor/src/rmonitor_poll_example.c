3055a03fca104cb1caa88f372058b5901399896c
Benjamin Tovar <btovar@nd.edu>
2015-06-18 17:43:29 -0400

    Adds librmonitor_poll example.

---
+++ resource_monitor/src/rmonitor_poll_example.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "rmonitor_poll.h"
+
+int main(int argc, char **argv) {
+	sleep(2);
+
+	struct rmsummary resources;
+	rmonitor_measure_process(&resources, getpid());
+
+	fprintf(stdout, "wall time used (s): %3.0lf, ",
+			resources.wall_time/1000000.0);
+
+	fprintf(stdout, "total memory used (MB): %" PRId64 ", ",
+			resources.resident_memory + resources.swap_memory);
+
+	fprintf(stdout, "total cores used: %" PRId64 "\n",
+			resources.resident_memory + resources.swap_memory);
+
+	return 0;
+}
+
+/* vim: set noexpandtab tabstop=4: */
