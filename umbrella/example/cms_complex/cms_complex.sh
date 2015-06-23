be3e8ba0a7a8a492152b5d569279e11786083851
Haiyan Meng <hmeng@nd.edu>
2015-06-23 11:45:08 -0400

    Umbrella mount_env update
    Add mount_env into the umbrella spec syntax
    
    	mount_env allows the user's analysis code to ignore the data location, and
    	also makes the relocation of the dependency easier without changing the
    	analysis code and just change the spec file.
    
    Add a new umbrella example for cern_opendata
    
    Check HTTP_PROXY for Umbrella+Docker+Parrot app
    
    Use the right version of cctools
    
    	If the sandbox mode is parrot, use the cctools version which matches
    	the host machine.
    	If the sandbox mode is docker or chroot, and parrot is needed inside
    	the sandbox to deliver cvmfs, use the cctools version which matches
    	the specification os requirement.
    
    Modify cms_opendata.umbrella
    
    Create mountpoint for each level of a path.
    
    	If a mountitem '/a/b/c' is added into the parrot mountfile, and if '/a' does
    	not exist or '/a/b' does not exist, `cd` fails because it needs to stat /a and
    	stat /a/b.  Solution: create mountpoints for /a and /a/b when they do not
    	exist.
    
    Replace the cctools binary in umbrella archive
    
    	The new cctools binary is based on the commit:
    		Date:   2015-06-26 17:08:45 -0400
    		Commit: f2bb681fdf8403632aa3da767983bf566e40007d (master)
    
    Add fake mounts into parrot mount file
    
    Remove blank line at EOF
    
    Let condor execution engine use cctools_python
    
    Add --cvmfs_http_proxy option ot umbrella
    
    	This option specifies the value of HTTP_PROXY used by Parrot
    	to access cvmfs
    
    Update umbrella documentation

--- umbrella/example/cms_complex/cms_complex.sh
+++ umbrella/example/cms_complex/cms_complex.sh
@@ -4,14 +4,14 @@ rm -rf sim_job
 mkdir sim_job
 cd sim_job
 
-. /cvmfs/cms.cern.ch/cmsset_default.sh
+. ${CMS_DIR}/cmsset_default.sh
 scramv1 project -f CMSSW ${CMS_VERSION}
 cd ${CMS_VERSION}
 eval `scram runtime -sh`
 cd ..
 cmsDriver.py Hadronizer_MgmMatchTuneZ2star_8TeV_madgraph_cff.py -s GEN \
 --eventcontent=RAWSIM --datatier GEN -n 10 \
---filein=file:/tmp/final_events_2381.lhe \
+--filein=file:${INPUT_FILE} \
 --filetype=LHE --conditions=START52_V9::All
 
 # vim: set noexpandtab tabstop=4:
