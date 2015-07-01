148887022f0240fc51a3a3d509bb9db0db649837
Benjamin Tovar <btovar@nd.edu>
2015-07-01 07:16:45 -0400

    chirp jobs C wrappers to ease swig wrappers.

--- chirp/src/chirp_swig_wrap.h
+++ chirp/src/chirp_swig_wrap.h
@@ -5,3 +5,10 @@ struct chirp_stat *chirp_wrap_stat(const char *hostname, const char *path, time_
 char *chirp_wrap_listacl(const char *hostname, const char *path, time_t stoptime);
 
 char *chirp_wrap_whoami(const char *hostname, time_t stoptime);
+
+int64_t chirp_wrap_job_create(const char *host, const char *json, time_t stoptime);
+int64_t chirp_wrap_job_commit(const char *host, const char *json, time_t stoptime);
+int64_t chirp_wrap_job_kill  (const char *host, const char *json, time_t stoptime);
+int64_t chirp_wrap_job_reap  (const char *host, const char *json, time_t stoptime);
+char   *chirp_wrap_job_status(const char *host, const char *json, time_t stoptime);
+char   *chirp_wrap_job_wait  (const char *host, chirp_jobid_t id, int64_t timeout, time_t stoptime);
