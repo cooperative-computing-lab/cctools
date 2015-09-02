d28b62debc2fd39e1506a53d6ceb2eaf173fa3e7
nkremerh <nkremerh@nd.edu>
2015-09-02 13:41:06 -0400

    Fixes trailing whitespace

--- work_queue/src/work_queue.c
+++ work_queue/src/work_queue.c
@@ -3108,7 +3108,7 @@ struct work_queue_task *work_queue_task_create(const char *command_line)
 	t->cores = -1;
 	t->gpus = -1;
 	t->unlabeled = 1;
-	t->loop_mount = 0;	
+	t->loop_mount = 0;
 
 	return t;
 }
