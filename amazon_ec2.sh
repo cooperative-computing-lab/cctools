7f7fcde9e92c03a4e5bf7c543e574494435168b8
Charles Shinaver <bshinaver@gmail.com>
2015-10-08 03:44:21 -0400

    Added file sshes and refactors to amazon script

--- amazon_ec2.sh
+++ amazon_ec2.sh
@@ -4,7 +4,6 @@
 # Invocation:
 # $ ./amazon_ec2.sh $AWS_ACCESS_KEY $AWS_SECRET_KEY
 set -e
-
 EC2_TOOLS_DIR="../ec2-api-tools-1.7.5.1/bin"
 AMI_IMAGE="ami-4b630d2e"
 INSTANCE_TYPE="t1.micro"
@@ -25,6 +24,36 @@ cleanup () {
     echo "Temporary keypair deleted."
 }
 
+run_ssh_cmd () {
+    ssh -o StrictHostKeyChecking=no -i $KEYPAIR_NAME.pem $USERNAME@$PUBLIC_DNS \
+            "$1"
+}
+
+get_file_from_server_to_destination () {
+    echo "Copying file to $(pwd)"
+    scp -o StrictHostKeyChecking=no -i $KEYPAIR_NAME.pem \
+        $USERNAME@$PUBLIC_DNS:~/"$1" $2
+}
+
+generate_temp_keypair () {
+    # Generate temp key pair and save
+    echo "Generating temporary keypair..."
+    $EC2_TOOLS_DIR/ec2-create-keypair $KEYPAIR_NAME | sed 's/.*KEYPAIR.*//' > $KEYPAIR_NAME.pem
+    echo "Keypair generated."
+}
+
+create_temp_security_group () {
+    # Create temp security group
+    echo "Generating temporary security group..."
+    $EC2_TOOLS_DIR/ec2-create-group $SECURITY_GROUP_NAME -d "$SECURITY_GROUP_NAME"
+    echo "Security group generated."
+}
+
+authorize_port_22_for_ssh_access () {
+    echo "Authorizing port 22 on instance for SSH access..."
+    $EC2_TOOLS_DIR/ec2-authorize $SECURITY_GROUP_NAME -p 22
+}
+
 trap cleanup EXIT
 
 if [ "$#" -ne 2 ]; then
@@ -33,18 +62,9 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-# Generate temp key pair and save
-echo "Generating temporary keypair..."
-$EC2_TOOLS_DIR/ec2-create-keypair $KEYPAIR_NAME | sed 's/.*KEYPAIR.*//' > $KEYPAIR_NAME.pem
-echo "Keypair generated."
-
-# Create temp security group
-echo "Generating temporary security group..."
-$EC2_TOOLS_DIR/ec2-create-group $SECURITY_GROUP_NAME -d "$SECURITY_GROUP_NAME"
-echo "Security group generated."
-
-echo "Authorizing port 22 on instance for SSH access..."
-$EC2_TOOLS_DIR/ec2-authorize $SECURITY_GROUP_NAME -p 22
+generate_temp_keypair
+create_temp_security_group
+authorize_port_22_for_ssh_access
 
 echo "Starting EC2 instance..."
 INSTANCE_ID=$($EC2_TOOLS_DIR/ec2-run-instances \
@@ -64,18 +84,26 @@ PUBLIC_DNS=$($EC2_TOOLS_DIR/ec2-describe-instances $INSTANCE_ID \
 | grep "INSTANCE" | awk '{print $4'})
 
 chmod 400 $KEYPAIR_NAME.pem
-set -x
 
-# Try sshing certain number of times
+# Try for successful ssh connection
 tries="10"
+SUCCESSFUL_SSH=-1
 while [ $tries -ne 0 ]
 do
-    ssh -o StrictHostKeyChecking=no -i $KEYPAIR_NAME.pem $USERNAME@$PUBLIC_DNS \
-        "echo test > testfile" && break
+    run_ssh_cmd "echo 'Connection to remote server successful'" \
+        && SUCCESSFUL_SSH=0 && break
     tries=$[$tries-1]
     sleep 1
 done
-set +x
+
+# Run rest of ssh commands
+if [ $SUCCESSFUL_SSH -eq 0 ]
+then
+    run_ssh_cmd "echo 'dat thang' > testfile"
+
+    # Get output files
+    get_file_from_server_to_destination "testfile" /tmp
+fi
 
 echo "Terminating EC2 instance..."
 $EC2_TOOLS_DIR/ec2-terminate-instances $INSTANCE_ID
