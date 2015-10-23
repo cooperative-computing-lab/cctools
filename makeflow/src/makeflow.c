77030e5fe33dcc541236807322154ae393e60e13
NeilB879 <nab880@hotmail.com>
2015-10-23 10:05:47 -0400

    fixed whitespace issues

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -76,9 +76,9 @@ an example.
 
 #define MAX_REMOTE_JOBS_DEFAULT 100
 struct {
-    int local_mem;
-    int local_cores;
-    int local_disk;
+	int local_mem;
+	int local_cores;
+	int local_disk;
 } loc_info;
 
 static sig_atomic_t makeflow_abort_flag = 0;
@@ -464,9 +464,9 @@ static int makeflow_is_local_node(struct dag_node *n)
 }
 static int makeflow_can_alloc_local(struct dag_node *n)
 {
-	int mem_ok = 0;
-	int disk_ok = 0;
-	int cores_ok = 0;
+    int mem_ok = 0;
+    int disk_ok = 0;
+    int cores_ok = 0;
 	mem_ok = (loc_info.local_mem == -1 || !n->resources || n->resources->resident_memory >= loc_info.local_mem);
 	disk_ok = (loc_info.local_disk == -1 || !n->resources || n->resources->swap_memory >= loc_info.local_disk);
 	if(!n->resources)
@@ -1041,11 +1041,7 @@ int main(int argc, char *argv[])
 	char *work_queue_preferred_connection = NULL;
 	char *write_summary_to = NULL;
 	char *s;
-	char *log_dir = NULL;
-	char *log_format = NULL;
-
-	loc_info = malloc(sizeof(local_info));
-    loc_info.local_mem = 0;
+	loc_info.local_mem = 0;
 	loc_info.local_disk =0;
 	loc_info.local_cores=1;
 
@@ -1498,10 +1494,8 @@ int main(int argc, char *argv[])
 	}
 	if(!loc_info.local_cores)
 	{
-		//get how many cores we have
 		loc_info.local_cores=load_average_get_cpus();
 	}
-	// back to old code
 	if(explicit_local_jobs_max) {
 		local_jobs_max = explicit_local_jobs_max;
 	} else {
