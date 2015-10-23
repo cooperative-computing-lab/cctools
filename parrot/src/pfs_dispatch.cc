cooperative-computing-lab/cctools

commit 50200922bb94e4a11743561ac84ae5f1ba90b08b

Wait for all tracees before processing events.
We perform wait4 until there are no tracees left to wait for.  Previously, we
would wait for a process, handle the event, then repeat.  This caused problems
with Java where threads would get stuck in a race condition with
sched_yield/futex.

The reason I discovered this solution is due to strace(1). I've long wondered
why it chooses to wait for all tracees before processing events (you can see
this if you strace strace).  After again seeing the sched_yield infinite loop
again in #927, I recalled this peculiarity and decided to give the strace
approach a try.  It apparently fixes the problem. I couldn't find any
documentation on why strace does this.

Fixes #43.
Fixes #505.

parrot/src/pfs_main.cc
@@ -316,8 +316,6 @@ static void handle_event( pid_t pid, int status, struct rusage *usage )
 	struct pfs_process *p;
 	unsigned long message;
 
-	trace_this_pid = -1; /* now always trace any process at all */
-
 	p = pfs_process_lookup(pid);
 	if(!p) {
 		debug(D_PROCESS,"ignoring event %d for unknown pid %d",status,pid);
@@ -478,6 +476,12 @@ void write_rval(const char* message, int status) {
 	}
 
 }
+
+struct pfswait {
+	pid_t pid;
+	int status;
+	struct rusage usage;
+};
 
 int main( int argc, char *argv[] )
 {
@@ -1065,33 +1069,59 @@ int main( int argc, char *argv[] )
 	p->state = PFS_PROCESS_STATE_USER;
 	snprintf(p->name,sizeof(p->name),"%s",argv[optind]);
 
+	/* We perform wait4 until there are no tracees left to wait for.
+	 * Previously, we would wait for a process, handle the event, then repeat.
+	 * This caused problems with Java where threads would get stuck in a race
+	 * condition with sched_yield/futex.
+	 *
+	 * The reason I discovered this solution is due to strace(1). I've long
+	 * wondered why it chooses to wait for all tracees before processing events
+	 * (you can see this if you strace strace).  After again seeing the
+	 * sched_yield infinite loop again in #927, I recalled this peculiarity and
+	 * decided to give the strace approach a try.  It apparently fixes the
+	 * problem. I couldn't find any documentation on why strace does this.
+	 */
+
 	while(pfs_process_count()>0) {
-		while(1) {
+		std::vector<struct pfswait> pfswait;
+
+		if(trace_this_pid != -1)
+			debug(D_PROCESS, "Waiting for process %d\n", trace_this_pid);
+
+		while (1) {
+			struct pfswait p;
 			int flags = WUNTRACED|__WALL;
-			int status;
-			struct rusage usage;
-
-			if(trace_this_pid != -1)
-				debug(D_PROCESS, "Waiting for process %d\n", trace_this_pid);
-			pid = wait4(trace_this_pid,&status,flags,&usage);
+			if (pfswait.size() > 0)
+				flags |= WNOHANG;
+			p.pid = wait4(trace_this_pid, &p.status, flags, &p.usage);
 #if 0 /* Enable this for extreme debugging... */
-			debug(D_DEBUG, "%d = wait4(%d, %p, %d, %p)", (int)pid, (int)trace_this_pid, &status, flags, &usage);
+			debug(D_DEBUG, "%d = wait4(%d, %p, %d, %p)", (int)p.pid, (int)trace_this_pid, &p.status, flags, &p.usage);
 #endif
+			if (p.pid == -1 && errno == ECHILD) {
+				debug(D_FATAL, "No children to wait for? Cleaning up...");
+				pfs_process_kill_everyone(SIGKILL);
+				abort();
+			} else if (p.pid == 0 || p.pid == -1) {
+				break;
+			}
+			pfswait.push_back(p);
+		}
+		if (pfswait.size() == 0)
+			break;
 
-			if(pid == pfs_watchdog_pid) {
-				if (WIFEXITED(status) || WIFSIGNALED(status)) {
+		trace_this_pid = -1; /* reinitialize to wait for any process; handle_event may change this after clone is processed */
+
+		for (std::vector<struct pfswait>::iterator it = pfswait.begin(); it != pfswait.end(); ++it) {
+			if(it->pid == pfs_watchdog_pid) {
+				if (WIFEXITED(it->status) || WIFSIGNALED(it->status)) {
 					debug(D_NOTICE,"watchdog died unexpectedly; killing everyone");
 					pfs_process_kill_everyone(SIGKILL);
 					break;
 				}
-			} else if(pid>0) {
-				handle_event(pid,status,&usage);
 			} else {
-				break;
+				handle_event(it->pid,it->status,&it->usage);
 			}
 		}
-
-		if(pid==-1 && errno==ECHILD) break;
 	}
 
 	if(pfs_syscall_totals32) {

commit c890315e329fead2783b78789557566027287a2a

Immediately wait for tracee on wait barrier.
Commit 50200922bb94e4a11743561ac84ae5f1ba90b08b introduced an error where
events for other tracees would be handled after setting trace_this_pid. The
intent of trace_this_pid is to setup a barrier preventing handling of other
events until the current two stage operation completes (clone or open).

Now Parrot immediately waits again for a tracee that sets up such a barrier.
I've corrected the variable name to make this intent more clear.

Fixes #986.

parrot/src/pfs_dispatch.cc
@@ -103,7 +103,7 @@ extern char *pfs_false_uname;
 extern uid_t pfs_uid;
 extern gid_t pfs_gid;
 
-extern pid_t trace_this_pid;
+extern int wait_barrier;
 
 extern INT64_T pfs_syscall_count;
 extern INT64_T pfs_read_count;
@@ -182,7 +182,7 @@ static void divert_to_parrotfd( struct pfs_process *p, INT64_T fd, char *path, c
 
 	p->syscall_args_changed = 1;
 	p->syscall_parrotfd = fd;
-	trace_this_pid = p->pid; /* this handles two processes racing to create the same file, also see comment for pfs_table::setparrot. */
+	wait_barrier = 1; /* this handles two processes racing to create the same file, also see comment for pfs_table::setparrot. */
 }
 
 static void handle_parrotfd( struct pfs_process *p )
@@ -1331,7 +1331,7 @@ static void decode_syscall( struct pfs_process *p, int entering )
 				 * we can determine the child pid before seeing any events from
 				 * the child.
 				 */
-				trace_this_pid = p->pid;
+				wait_barrier = 1;
 			}
 			break;
 

parrot/src/pfs_dispatch64.cc
@@ -112,7 +112,7 @@ extern char *pfs_false_uname;
 extern uid_t pfs_uid;
 extern gid_t pfs_gid;
 
-extern pid_t trace_this_pid;
+extern int wait_barrier;
 
 extern INT64_T pfs_syscall_count;
 extern INT64_T pfs_read_count;
@@ -191,7 +191,7 @@ static void divert_to_parrotfd( struct pfs_process *p, INT64_T fd, char *path, c
 
 	p->syscall_args_changed = 1;
 	p->syscall_parrotfd = fd;
-	trace_this_pid = p->pid; /* this handles two processes racing to create the same file, also see comment for pfs_table::setparrot. */
+	wait_barrier = 1; /* this handles two processes racing to create the same file, also see comment for pfs_table::setparrot. */
 }
 
 static void handle_parrotfd( struct pfs_process *p )
@@ -1100,7 +1100,7 @@ static void decode_syscall( struct pfs_process *p, int entering )
 				 * we can determine the child pid before seeing any events from
 				 * the child.
 				 */
-				trace_this_pid = p->pid;
+				wait_barrier = 1;
 			}
 			break;
 

parrot/src/pfs_main.cc
@@ -71,7 +71,7 @@ int linux_major;
 int linux_minor;
 int linux_micro;
 
-pid_t trace_this_pid = -1;
+int wait_barrier = 0;
 
 int pfs_master_timeout = 300;
 struct file_cache *pfs_file_cache = 0;
@@ -500,6 +500,31 @@ struct pfswait {
 	int status;
 	struct rusage usage;
 };
+
+static int pfswait (struct pfswait *p, pid_t pid, int block)
+{
+	int flags = WUNTRACED|__WALL;
+	if (!block)
+		flags |= WNOHANG;
+	if (pid > 0)
+		debug(D_PROCESS, "waiting for blocking event from process %d\n", pid);
+	p->pid = wait4(pid, &p->status, flags, &p->usage);
+#if 0 /* Enable this for extreme debugging... */
+	debug(D_DEBUG, "%d = wait4(%d, %p, %d, %p)", (int)p->pid, pid, &p->status, flags, &p->usage);
+#endif
+	if (p->pid == -1) {
+		debug(D_DEBUG, "wait4: %s", strerror(errno));
+		if (errno == ECHILD) {
+			debug(D_FATAL, "No children to wait for? Cleaning up...");
+			pfs_process_kill_everyone(SIGKILL);
+			abort();
+		}
+		return 0;
+	} else if (p->pid == 0) {
+		return 0;
+	}
+	return 1;
+}
 
 int main( int argc, char *argv[] )
 {
@@ -1115,35 +1140,16 @@ int main( int argc, char *argv[] )
 	 */
 
 	while(pfs_process_count()>0) {
-		std::vector<struct pfswait> pfswait;
+		std::vector<struct pfswait> pevents;
+		struct pfswait p;
 
-		if(trace_this_pid != -1)
-			debug(D_PROCESS, "Waiting for process %d\n", trace_this_pid);
-
-		while (1) {
-			struct pfswait p;
-			int flags = WUNTRACED|__WALL;
-			if (pfswait.size() > 0)
-				flags |= WNOHANG;
-			p.pid = wait4(trace_this_pid, &p.status, flags, &p.usage);
-#if 0 /* Enable this for extreme debugging... */
-			debug(D_DEBUG, "%d = wait4(%d, %p, %d, %p)", (int)p.pid, (int)trace_this_pid, &p.status, flags, &p.usage);
-#endif
-			if (p.pid == -1 && errno == ECHILD) {
-				debug(D_FATAL, "No children to wait for? Cleaning up...");
-				pfs_process_kill_everyone(SIGKILL);
-				abort();
-			} else if (p.pid == 0 || p.pid == -1) {
-				break;
-			}
-			pfswait.push_back(p);
+		while (pfswait(&p, -1, !pevents.size())) {
+			pevents.push_back(p);
 		}
-		if (pfswait.size() == 0)
+		if (pevents.size() == 0)
 			break;
 
-		trace_this_pid = -1; /* reinitialize to wait for any process; handle_event may change this after clone is processed */
-
-		for (std::vector<struct pfswait>::iterator it = pfswait.begin(); it != pfswait.end(); ++it) {
+		for (std::vector<struct pfswait>::iterator it = pevents.begin(); it != pevents.end(); ++it) {
 			if(it->pid == pfs_watchdog_pid) {
 				if (WIFEXITED(it->status) || WIFSIGNALED(it->status)) {
 					debug(D_NOTICE,"watchdog died unexpectedly; killing everyone");
@@ -1151,7 +1157,11 @@ int main( int argc, char *argv[] )
 					break;
 				}
 			} else {
-				handle_event(it->pid,it->status,&it->usage);
+				p = *it;
+				do {
+					wait_barrier = 0; /* reinitialize */
+					handle_event(p.pid, p.status, &p.usage);
+				} while (wait_barrier && pfswait(&p, it->pid, 1));
 			}
 		}
 	}