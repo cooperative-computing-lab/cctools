235f8b1a50804945a5d4bd1dcd1b2d409d301207
NeilB879 <nab880@hotmail.com>
2015-10-23 11:32:03 -0400

    ;

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -1041,9 +1041,9 @@ int main(int argc, char *argv[])
 	char *work_queue_preferred_connection = NULL;
 	char *write_summary_to = NULL;
 	char *s;
-	loc_info.local_mem = -1;
-	loc_info.local_disk =-1;
-	loc_info.local_cores=1;
+	loc_info.local_mem =  -1;
+	loc_info.local_disk = -1;
+	loc_info.local_cores= 1;
 
 	s = getenv("MAKEFLOW_BATCH_QUEUE_TYPE");
 	if(s) {
@@ -1257,15 +1257,6 @@ int main(int argc, char *argv[])
 			case 'J':
 				explicit_remote_jobs_max = atoi(optarg);
 				break;
-			case '1':
-				loc_info.local_disk = atoi(optarg);
-				break;
-			case '2':
-				loc_info.local_mem = atoi(optarg);
-				break;
-			case '3':
-				loc_info.local_cores = atoi(optarg);
-				break;
 			case 'l':
 				logfilename = xxstrdup(optarg);
 				break;
@@ -1422,6 +1413,15 @@ int main(int argc, char *argv[])
 				return 1;
 			case 'X':
 				change_dir = optarg;
+				break;
+			case '1':
+				loc_info.local_disk = atoi(optarg);
+				break;
+			case '2':
+				loc_info.local_mem = atoi(optarg);
+				break;
+			case '3':
+				loc_info.local_cores = atoi(optarg);
 				break;
 		}
 	}
