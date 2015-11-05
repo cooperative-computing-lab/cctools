734c3000ac51a29928c6c4c8765bb6f9deb659bc
Charles Shinaver <bshinaver@gmail.com>
2015-11-05 03:13:05 -0500

    Added input files

--- run_ec2_test.sh
+++ run_ec2_test.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 TESTFILE="testfile"
+echo "test1" > in1
+echo "test2" > in2
 
 
 ./makeflow/src/makeflow -d batch -T amazon -l /dev/null <(cat <<EOF
-MAKEFLOW_INPUTS =
+MAKEFLOW_INPUTS = in1 in2
 MAKEFLOW_OUTPUTS =
 
-$TESTFILE:
-    echo "hi" >> $TESTFILE
+$TESTFILE: in1 in2
+    cat in2 > $TESTFILE && cat in1 >> $TESTFILE
+
 
 EOF)
 
 if [[ -f $TESTFILE ]];
 then
     echo "File generation: passed"
-    rm $TESTFILE
+    rm $TESTFILE in1 in2
 else
     echo "File generation: failed"
 fi
