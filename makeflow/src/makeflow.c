4927954c6ac6bef0b278d2df80155f56def2c32f
NeilB879 <nab880@hotmail.com>
2015-11-16 08:02:20 -0500

    cleanup the code, and added back variable declations

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -476,9 +476,9 @@ static int makeflow_can_alloc_local(struct dag_node *n)
 	int disk_ok = 0;
 	int cores_ok = 0;
 
-	mem_ok = (loc_info.local_mem == -1 || n->resources->resident_memory <= loc_info.local_mem);
+	mem_ok = (loc_info.local_mem == -1 || !n->resources || n->resources->resident_memory <= loc_info.local_mem);
 
-	disk_ok = (loc_info.local_disk == -1 || n->resources->workdir_footprint <= loc_info.local_disk);
+	disk_ok = (loc_info.local_disk == -1 || !n->resources || n->resources->workdir_footprint <= loc_info.local_disk);
 
 	cores_ok = (n->resources->cores <= loc_info.local_cores ||( n->resources->cores < 1 && loc_info.local_cores >= 1 ));
 	return (cores_ok && disk_ok && mem_ok);
@@ -489,51 +489,6 @@ static int makeflow_can_alloc_local(struct dag_node *n)
  * Takes the allocates the local machine resources
  * of the job given as a parameter.
  */
-static void makeflow_alloc_local(struct dag_node *n)
-{
-
-		loc_info.local_cores -= n->resources->cores;
-
-
-		if(loc_info.local_mem != -1)
-			loc_info.local_mem -= n->resources->resident_memory;
-
-		if(loc_info.local_disk != -1)
-			loc_info.local_disk -= n->resources->workdir_footprint;
-}
-/*
- * Reallocates the resources to the local machine after a local job
- * stops running.
- */
-static void makeflow_dealloc_local(struct dag_node *n)
-{
-		loc_info.local_cores += n->resources->cores;
-
-		if(loc_info.local_mem != -1)
-			loc_info.local_mem += n->resources->resident_memory;
-
-		if(loc_info.local_disk != -1)
-			loc_info.local_disk += n->resources->workdir_footprint;
-}
-
-static int makeflow_is_local_node(struct dag_node *n)
-{
-	return (batch_queue_type == BATCH_QUEUE_TYPE_LOCAL || (n->local_job && local_queue));
-}
-static int makeflow_can_alloc_local(struct dag_node *n)
-{
-	int mem_ok = 0;
-	int disk_ok = 0;
-	int cores_ok = 0;
-
-	mem_ok = (loc_info.local_mem == -1 || !n->resources || n->resources->resident_memory <= loc_info.local_mem);
-
-	disk_ok = (loc_info.local_disk == -1 || !n->resources || n->resources->workdir_footprint <= loc_info.local_disk);
-
-	cores_ok = (n->resources->cores <= loc_info.local_cores ||( n->resources->cores < 1 && loc_info.local_cores >= 1 ));
-	return (cores_ok && disk_ok && mem_ok);
-
-}
 static void makeflow_alloc_local(struct dag_node *n)
 {
 
@@ -559,6 +514,10 @@ static void makeflow_alloc_local(struct dag_node *n)
 			loc_info.local_disk -= n->resources->workdir_footprint;
 	}
 }
+/*
+ * Reallocates the resources to the local machine after a local job
+ * stops running.
+ */
 static void makeflow_dealloc_local(struct dag_node *n)
 {
 	if( (loc_info.local_disk == -1 && loc_info.local_mem == -1)) {
