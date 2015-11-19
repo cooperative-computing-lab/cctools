93f409dcc5201dae6e5a00454c2d0ccd8181f8d5
Charles Shinaver <bshinaver@gmail.com>
2015-11-19 18:58:36 -0500

    Modified ec2 tests to have no dependencies

--- run_ec2_test.sh
+++ run_ec2_test.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 TESTFILE="testfile"
-echo "test1" > in1
-echo "test2" > in2
-
 
 ./makeflow/src/makeflow -d batch -T amazon --amazon-credentials-filepath "$(pwd)/amazon-credentials" -l /dev/null <(cat <<EOF
-MAKEFLOW_INPUTS = in1 in2
+MAKEFLOW_INPUTS =
 MAKEFLOW_OUTPUTS =
 
-$TESTFILE: in1 in2
-    cat in2 > $TESTFILE && cat in1 >> $TESTFILE
+$TESTFILE:
+    echo "hi" >> $TESTFILE
 
 
 EOF)
@@ -18,7 +15,7 @@ EOF)
 if [[ -f $TESTFILE ]];
 then
     echo "File generation: passed"
-    rm $TESTFILE in1 in2
+    rm $TESTFILE
 else
     echo "File generation: failed"
 fi
