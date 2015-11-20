414b8c342eb0dc8c9848a91c7a93805c9ccbba56
Charles Shinaver <bshinaver@gmail.com>
2015-11-19 19:31:49 -0500

    Test script now tests dependencies, multiple recipes, etc

--- run_ec2_test.sh
+++ run_ec2_test.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-TESTFILE="testfile"
+echo "file 0" >> in1
 
 ./makeflow/src/makeflow -d batch -T amazon --amazon-credentials-filepath "$(pwd)/amazon-credentials" -l /dev/null <(cat <<EOF
-MAKEFLOW_INPUTS =
+MAKEFLOW_INPUTS = in1
 MAKEFLOW_OUTPUTS =
 
-$TESTFILE:
-    echo "hi" >> $TESTFILE
+final_file: file1 file2 file3
+    cat file1 file2 file3 > final_file
+
+file1: in1
+    cat in1 > file1
+
+file2:
+    echo "file 2" > file2
+
+file3:
+    echo "file 3" > file3
 
 
 EOF)
 
-if [[ -f $TESTFILE ]];
+if [[ -f final_file ]]
 then
     echo "File generation: passed"
-    rm $TESTFILE
+    rm final_file in1 file1 file2 file3
 else
     echo "File generation: failed"
 fi
