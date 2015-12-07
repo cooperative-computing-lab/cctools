d5eedf2036e512716d5926ec4f5bde89105c238d
Benjamin Tovar <btovar@nd.edu>
2015-12-07 14:45:41 -0500

    use minimum of worker, or top resources for unlabeled tasks

--- work_queue/src/work_queue.c
+++ work_queue/src/work_queue.c
@@ -4485,11 +4485,12 @@ char *work_queue_monitor_wrap(struct work_queue *q, struct work_queue_worker *w,
 
 	struct rmsummary limits;
 
+	/* use the minimum of worker, or top value. We take care of the -1 that means 'unspecified'. */
 	if(t->unlabeled) {
-		memcpy(&limits, t->rn, sizeof(struct rmsummary));
-		limits.cores  = w->resources->cores.total;
-		limits.memory = w->resources->memory.total;
-		limits.disk   = w->resources->disk.total;
+		memcpy(&limits, q->worker_top_resources, sizeof(struct rmsummary));
+		limits.cores  = limits.cores < -1 ? w->resources->cores.total : MIN(w->resources->cores.total, limits.cores);
+		limits.memory  = limits.memory < -1 ? w->resources->memory.total : MIN(w->resources->memory.total, limits.memory);
+		limits.disk  = limits.disk < -1 ? w->resources->disk.total : MIN(w->resources->disk.total, limits.disk);
 	} else {
 		memcpy(&limits, t->rn, sizeof(struct rmsummary));
 	}
