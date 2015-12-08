c928635cdbf1c5ccfe27123febde844f9e9b4160
Douglas Thain <dthain@nd.edu>
2015-12-08 14:41:44 -0500

    Fixed select_static to work with json.

--- deltadb/src/deltadb_select_static.c
+++ deltadb/src/deltadb_select_static.c
@@ -4,9 +4,13 @@ This software is distributed under the GNU General Public License.
 See the file COPYING for details.
 */
 
-#include "nvpair.h"
+#include "deltadb_stream.h"
+
+#include "jx.h"
+#include "jx_print.h"
+#include "jx_parse.h"
+
 #include "hash_table.h"
-#include "debug.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -26,49 +30,52 @@ struct argument {
 
 struct deltadb {
 	struct hash_table *table;
-	int static_variables_only;
 	struct argument *args;
 };
 
-struct deltadb * deltadb_create( const char *logdir )
+struct deltadb * deltadb_create()
 {
 	struct deltadb *db = malloc(sizeof(*db));
 	db->table = hash_table_create(0,0);
-	db->static_variables_only = 1;
 	db->args = NULL;
 	return db;
 }
 
-void deltadb_delete( struct deltadb *db )
+static int is_number(char const* p)
 {
-  // should delete all nvpairs in the table here
-	if(db->table) hash_table_delete(db->table);
-	//if(db->logfile) fclose(db->logfile);
-	free(db);
-}
-
-static int is_number(char const* p){
 	char* end;
 	strtod(p, &end);
-	if (end==NULL)
-		return 1;
-	return 0;
+	return !*end;
 }
 
-static int keep_object(struct argument *arg, const char *input)
+static int jx_is_number( struct jx * j )
+{
+	return j->type==JX_DOUBLE || j->type==JX_INTEGER;
+}
+
+static double jx_to_double( struct jx *j )
+{
+	if(j->type==JX_DOUBLE) return j->double_value;
+	return j->integer_value;
+}
+
+static int expr_is_true( struct argument *arg, struct jx *jvalue )
 {
 	char *operator = arg->operator;
 	int cmp;
-	if (is_number(arg->val) && is_number(input)){
-		char* end;
-		double in = strtod(input, &end);
-		double v = strtod(arg->val, &end);
+
+	/// XXX need to handle other combinations of values here
+
+	if (is_number(arg->val) && jx_is_number(jvalue) ) {
+		double in = jx_to_double(jvalue);
+		double v = atof(arg->val);
 		if (in<v) cmp = -1;
 		else if (in==v) cmp = 0;
 		else cmp = 1;
-		printf("%s cmp %s =%i\n",input,arg->val,cmp);
-	} else cmp = strcmp(input,arg->val);
-	//printf("%s strcmp %s =%i\n",input,arg->val,cmp);
+	} else {
+		cmp = strcmp(jvalue->string_value,arg->val);
+	}
+
 	if(strcmp(operator,"=")==0) {
 		if(cmp==0)
 			return 1;
@@ -97,134 +104,87 @@ static int keep_object(struct argument *arg, const char *input)
 	return 0;
 }
 
-
-/*
-Replay a given log file into the hash table, up to the given snapshot time.
-Return true if the stoptime was reached.
-*/
-
-#define NVPAIR_LINE_MAX 4096
-
-static int log_play( struct deltadb *db )
+static int object_matches( struct deltadb *db, struct jx *jobject )
 {
-	FILE *stream = stdin;
-	time_t current = 0;
-	struct nvpair *nv;
-	int line_number = 0;
-	struct hash_table *table = db->table;
+	struct argument *arg;
 
-	char line[NVPAIR_LINE_MAX];
-	char key[NVPAIR_LINE_MAX];
-	char name[NVPAIR_LINE_MAX];
-	char value[NVPAIR_LINE_MAX];
-	char oper;
-
-	int notime = 1;
-	while(fgets(line,sizeof(line),stream)) {
-		//debug(D_NOTICE,"Processed line: %s",line);
-
-		line_number += 1;
-
-		if (line[0]=='\n') return 0;
-
-		int n = sscanf(line,"%c %s %s %[^\n]",&oper,key,name,value);
-		if(n<1) continue;
-
-		switch(oper) {
-			case 'C':
-				nv = nvpair_create();
-				int res = nvpair_parse_stream(nv,stream);
-				nvpair_insert_string(nv, "key", key);
-				if (res>0){
-
-					if (db->static_variables_only==1){ //should this object be output?
-						struct argument *arg = db->args;
-						int keep = 0;
-						while (arg!=NULL){
-							const char *var = nvpair_lookup_string(nv,arg->param);
-							if ( var!=NULL && keep_object(arg,var) ){
-								keep = 1;
-								break;
-							}
-							arg = arg->next;
-						}
-						if (keep==1){
-							if (notime){
-								printf("T %lld\n",(long long)current);
-								notime = 0;
-							}
-							printf("C %s\n",key);
-							nvpair_print_text(nv,stdout);
-							hash_table_remove(table,key);
-							hash_table_insert(table,key,key);
-						}
-					} else {
-
-					}
-
-				}
-				nvpair_delete(nv);
-				break;
-			case 'D':
-				nv = hash_table_remove(table,key);
-				if(nv){
-					if (notime){
-						printf("T %lld\n",(long long)current);
-						notime = 0;
-					}
-					printf("%s",line);
-				}//nvpair_delete(nv);
-				break;
-			case 'U':
-				nv = hash_table_lookup(table,key);
-				if(nv){
-					if (notime){
-						printf("T %lld\n",(long long)current);
-						notime = 0;
-					}
-					printf("%s",line);
-				}//nvpair_insert_string(nv,name,value);
-				break;
-			case 'R':
-				nv = hash_table_lookup(table,key);
-				if(nv){
-					if (notime){
-						printf("T %lld\n",(long long)current);
-						notime = 0;
-					}
-					printf("%s",line);
-				}//nvpair_remove(nv,name);
-				break;
-			case 'T':
-				current = atol(key);
-				notime = 1;
-				break;
-			default:
-				debug(D_NOTICE,"corrupt log data[%i]: %s",line_number,line);
-				fflush(stderr);
-				break;
+	for(arg=db->args;arg;arg=arg->next) {
+		struct jx *jvalue = jx_lookup(jobject,arg->param);
+		if( jvalue && expr_is_true(arg,jvalue) ) {
+			return 1;
+			break;
 		}
 	}
+
 	return 0;
 }
 
-/*
-Play the log from start_time to end_time by opening the appropriate
-checkpoint file and working ahead in the various log files.
-*/
-
-static int parse_input( struct deltadb *db )
+int deltadb_create_event( struct deltadb *db, const char *key, struct jx *jobject )
 {
-	while(1) {
-		int keepgoing = log_play(db);
-		if(!keepgoing) break;
+	if(object_matches(db,jobject)) {
+		hash_table_insert(db->table,key,jobject);
+		char *str = jx_print_string(jobject);
+		printf("C %s %s\n",key,str);
+		free(str);
 	}
+	return 1;
+}
+
+int deltadb_delete_event( struct deltadb *db, const char *key )
+{
+	struct jx *jobject = hash_table_remove(db->table,key);
+	if(jobject) {
+		jx_delete(hash_table_remove(db->table,key));
+		printf("D %s\n",key);
+	}
+	return 1;
+}
+
+int deltadb_update_event( struct deltadb *db, const char *key, const char *name, struct jx *jvalue )
+{
+	struct jx *jobject;
+	jobject = hash_table_lookup(db->table,key);
+	if(jobject) {
+		struct jx *jname = jx_string(name);
+		jx_delete(jx_remove(jobject,jname));
+		jx_insert(jobject,jname,jvalue);
+
+		char *str = jx_print_string(jvalue);
+		printf("U %s %s %s\n",key,name,str);
+		free(str);
+	} else {
+		jx_delete(jvalue);
+	}
+	return 1;
+}
+
+int deltadb_remove_event( struct deltadb *db, const char *key, const char *name )
+{
+	struct jx *jobject;
+	jobject = hash_table_lookup(db->table,key);
+	if(jobject) {
+		struct jx *jname = jx_string(name);
+		jx_delete(jx_remove(jobject,jname));
+		jx_delete(jname);
+		printf("R %s %s\n",key,name);
+	}
+	return 1;
+}
+
+int deltadb_time_event( struct deltadb *db, time_t starttime, time_t stoptime, time_t current )
+{
+	printf("T %lld\n",(long long)current);
+	return 1;
+}
+
+int deltadb_post_event( struct deltadb *db, const char *line )
+{
 	return 1;
 }
 
 int main( int argc, char *argv[] )
 {
-	struct deltadb *db = deltadb_create(NULL);
+	struct deltadb *db = deltadb_create();
 
 	int i;
 	for (i=1; i<argc; i++){
@@ -238,23 +198,11 @@ int main( int argc, char *argv[] )
 		strncpy(arg.operator,delim,operator_size);
 		arg.operator[operator_size] = '\0';
 		delim[0] = '\0';
-
-		/*char *dop = strstr(arg.operator,"...");
-		if ( dop!=NULL ){
-			arg.dynamic = 1;
-			dop[0] = '\0';
-			db->static_variables_only = 0;
-		} else arg.dynamic = 0;*/
-
 		arg.next = db->args;
 		db->args = &arg;
 	}
 
-
-
-	parse_input(db);
-
-	deltadb_delete(db);
+	deltadb_process_stream(db,stdin,0,0);
 
 	return 0;
 }
