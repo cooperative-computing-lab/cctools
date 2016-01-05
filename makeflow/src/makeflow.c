013ffeaf0d3a617e82370da13388ac69d3569b09
NeilB879 <nab880@hotmail.com>
2016-01-05 09:37:59 -0500

    fixed error with print

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -1533,7 +1533,7 @@ exit(1);
 		struct dag_node *n;
 		for(n = d->nodes; n; n = n->next) {
 			if(makeflow_is_local_node(n) && !makeflow_can_alloc_local(n)) {
-				fprintf("Node %d didn't have enough resources to run with allocated resources", n->linenum);
+				fprintf(stderr, "Node %d didn't have enough resources to run with allocated resources", n->linenum);
 			}
 	}
 	if(tooBig)
