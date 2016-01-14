063e17a9d7155c51611e63913c5d945b76a4cf19
Nick Hazekamp <nhazekam@nd.edu>
2016-01-14 11:42:10 -0500

    Option to skip file check initially and in rerun

--- makeflow/src/makeflow_log.h
+++ makeflow/src/makeflow_log.h
@@ -25,6 +25,6 @@ void makeflow_log_completed_event( struct dag *d );
 void makeflow_log_state_change( struct dag *d, struct dag_node *n, int newstate );
 void makeflow_log_file_state_change( struct dag *d, struct dag_file *f, int newstate );
 void makeflow_log_gc_event( struct dag *d, int collected, timestamp_t elapsed, int total_collected );
-void makeflow_log_recover( struct dag *d, const char *filename, int verbose_mode, struct batch_queue *queue, makeflow_clean_depth clean_mode );
+void makeflow_log_recover( struct dag *d, const char *filename, int verbose_mode, struct batch_queue *queue, makeflow_clean_depth clean_mode, int skip_file_check );
 
 #endif
