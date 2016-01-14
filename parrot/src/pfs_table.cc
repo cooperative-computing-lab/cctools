cooperative-computing-lab/cctools

commit 7e1b16fb48aaf53b01229ca9545f72969688c260

Allow Parrot to close unneeded mmap fd.
If Parrot does not need to write through any changes to the mmap region, we
close the fd to free up the slot in the kernel table for Parrot.

Fixes #431.

parrot/src/pfs_table.cc
@@ -2484,34 +2484,11 @@ static int save_file_from_channel( pfs_file *file, pfs_size_t file_offset, pfs_s
 	return 1;
 }
 
-pfs_size_t pfs_table::mmap_create_object( pfs_file *file, pfs_size_t file_offset, pfs_size_t map_length, int prot, int flags )
+pfs_size_t pfs_table::mmap_create_object( pfs_file *file, pfs_size_t channel_offset, pfs_size_t map_length, pfs_size_t file_offset, int prot, int flags )
 {
-	pfs_size_t channel_offset;
-	pfs_ssize_t file_length;
-
-	file_length = file->get_size();
-	if(file_length<0) return -1;
-
-	if(!pfs_channel_lookup(file->get_name()->path,&channel_offset)) {
-
-		if(!pfs_channel_alloc(file->get_name()->path,file_length,&channel_offset)) {
-			errno = ENOMEM;
-			return -1;
-		}
-
-		debug(D_CHANNEL,"%s loading to channel %llx size %llx",file->get_name()->path,(long long)channel_offset,(long long)file_length);
-
-		if(!load_file_to_channel(file,file_length,channel_offset,1024*1024)) {
-			pfs_channel_free(channel_offset);
-			return -1;
-		}
-	} else {
-	  debug(D_CHANNEL,"%s cached at channel %llx",file->get_name()->path,(long long)channel_offset);
-	}
-
 	pfs_mmap *m;
 
-	m = new pfs_mmap( file, 0, channel_offset, map_length, file_offset, prot, flags );
+	m = new pfs_mmap(file, 0, channel_offset, map_length, file_offset, prot, flags);
 
 	m->next = mmap_list;
 	mmap_list = m;
@@ -2521,9 +2498,40 @@ pfs_size_t pfs_table::mmap_create_object( pfs_file *file, pfs_size_t file_offset
 
 pfs_size_t pfs_table::mmap_create( int fd, pfs_size_t file_offset, pfs_size_t map_length, int prot, int flags )
 {
+	pfs_file *file;
+	pfs_size_t channel_offset;
+	pfs_ssize_t file_length;
+
 	if( (fd<0) || (fd>=pointer_count) || (pointers[fd]==0) )
 		return (errno = EBADF, -1);
-	return mmap_create_object(pointers[fd]->file,file_offset,map_length,prot,flags);
+
+	file = pointers[fd]->file;
+	file_length = file->get_size();
+
+	if(file_length<0)
+		return (errno = ENODEV, -1);
+
+	/* FIXME we don't check the range because it's valid to mmap a file plus extra. However, we don't allocate space in the channel for this! */
+	//else if(!(file_offset < file_length)) /* beginning of range [off, off+len) */
+	//	return (errno = ENXIO, -1);
+	//else if(!((file_offset+map_length) <= file_length)) /* end of range [off, off+len) */
+	//	return (errno = ENXIO, -1);
+
+	if(!pfs_channel_lookup(file->get_name()->path,&channel_offset)) {
+		if(!pfs_channel_alloc(file->get_name()->path,file_length,&channel_offset))
+			return (errno = ENOMEM, -1);
+
+		debug(D_CHANNEL,"%s loading to channel %llx size %llx",file->get_name()->path,(long long)channel_offset,(long long)file_length);
+
+		if(!load_file_to_channel(file,file_length,channel_offset,1024*1024)) {
+			pfs_channel_free(channel_offset);
+			return -1;
+		}
+	} else {
+		debug(D_CHANNEL,"%s cached at channel %llx",file->get_name()->path,(long long)channel_offset);
+	}
+
+	return mmap_create_object(file, channel_offset, map_length, file_offset, prot, flags);
 }
 
 int pfs_table::mmap_update( pfs_size_t logical_addr, pfs_size_t channel_offset )
@@ -2560,12 +2568,7 @@ int pfs_table::mmap_delete( pfs_size_t logical_addr, pfs_size_t length )
 			// This will increase the reference count of both the file and the memory object.
 
 			if(logical_addr>m->logical_addr) {
-				mmap_create_object(
-					m->file,
-					m->file_offset,
-					logical_addr-m->logical_addr,
-					m->prot,
-					m->flags);
+				mmap_create_object(m->file, m->channel_offset, logical_addr-m->logical_addr, m->file_offset, m->prot, m->flags);
 				mmap_update(m->logical_addr,0);
 			}
 
@@ -2573,12 +2576,7 @@ int pfs_table::mmap_delete( pfs_size_t logical_addr, pfs_size_t length )
 			// This will increase the reference count of both the file and the memory object.
 
 			if((logical_addr+length) < (m->logical_addr+m->map_length)) {
-				mmap_create_object(
-					m->file,
-					m->file_offset+m->map_length-(m->logical_addr-logical_addr),
-					m->map_length - length - (logical_addr - m->logical_addr),
-					m->prot,
-					m->flags);
+				mmap_create_object(m->file, m->channel_offset, m->map_length-length-(logical_addr-m->logical_addr), m->file_offset+m->map_length-(m->logical_addr-logical_addr), m->prot, m->flags);
 				mmap_update(logical_addr+length,0);
 			}
 

parrot/src/pfs_mmap.h
@@ -7,7 +7,10 @@ See the file COPYING for details.
 #ifndef PFS_MMAP_H
 #define PFS_MMAP_H
 
+#include <sys/mman.h>
+
 #include <assert.h>
+#include <stdlib.h>
 
 #include "pfs_channel.h"
 #include "pfs_types.h"
@@ -17,14 +20,18 @@ class pfs_mmap {
 public:
 	pfs_mmap( pfs_file *_file, pfs_size_t _logical_addr, pfs_size_t _channel_offset, pfs_size_t _map_length, pfs_size_t _file_offset, int _prot, int _flags )
 	{
-		file = _file;
+		if(_flags&MAP_SHARED && _prot&PROT_WRITE)
+			file = _file; /* we only keep the reference if we need to write back */
+		else
+			file = NULL;
 		logical_addr = _logical_addr;
 		channel_offset = _channel_offset;
 		map_length = _map_length;
 		file_offset = _file_offset;
 		prot = _prot;
 		flags = _flags;
-		file->addref();
+		if(file)
+			file->addref();
 		pfs_channel_addref(channel_offset);
 	}
 
@@ -36,16 +43,19 @@ public:
 		file_offset = m->file_offset;
 		prot = m->prot;
 		flags = m->flags;
-		file->addref();
+		if(file)
+			file->addref();
 		pfs_channel_addref(channel_offset);
 	}
 
 	~pfs_mmap()
 	{
-		file->delref();
-		if(file->refs()<1) {
-			file->close();
-			delete file;
+		if(file) {
+			file->delref();
+			if(file->refs()<1) {
+				file->close();
+				delete file;
+			}
 		}
 		pfs_channel_free(channel_offset);
 	}
@@ -61,3 +71,5 @@ public:
 };
 
 #endif
+
+/* vim: set noexpandtab tabstop=4: */

parrot/src/pfs_table.h
@@ -122,10 +122,9 @@ public:
 	int	resolve_name( const char *caller, const char *cname, pfs_name *pname, bool do_follow_symlink = true, int depth = 0 );
 
 	/* mmap operations */
-	pfs_size_t mmap_create_object( pfs_file *file, pfs_size_t file_offset, pfs_size_t length, int prot, int flags );
 	pfs_size_t mmap_create( int fd, pfs_size_t file_offset, pfs_size_t length, int prot, int flags );
-	int	   mmap_update( pfs_size_t logical_address, pfs_size_t channel_address );
-	int	   mmap_delete( pfs_size_t logical_address, pfs_size_t length );
+	int	       mmap_update( pfs_size_t logical_address, pfs_size_t channel_address );
+	int	       mmap_delete( pfs_size_t logical_address, pfs_size_t length );
 	void       mmap_print();
 
 	pfs_file * open_object( const char *path, int flags, mode_t mode, int force_cache );
@@ -141,6 +140,8 @@ private:
 	void collapse_path( const char *short_path, char *long_path, int remove_dotdot );
 	void complete_path( const char *short_path, char *long_path );
 
+	pfs_size_t mmap_create_object( pfs_file *file, pfs_size_t channel_offset, pfs_size_t map_length, pfs_size_t file_offset, int prot, int flags );
+
 	int         pointer_count;
 	pfs_pointer **pointers;
 	int         *fd_flags;
@@ -151,5 +152,4 @@ private:
 
 #endif
 
-
-
+/* vim: set noexpandtab tabstop=4: */

commit e78b77343bbed2495b5c49f0c647a965e925abde

Fix segfault caused by NULL file handle.
Problem introduced by 7e1b16fb48aaf53b01229ca9545f72969688c260.

Fixes #1096.

parrot/src/pfs_table.cc
@@ -2494,16 +2494,21 @@ int pfs_table::mmap_delete( pfs_size_t logical_addr, pfs_size_t length )
 				// This will increase the reference count of both the file and the memory object.
 
 				if(logical_addr>m->logical_addr) {
-					mmap_create_object(m->file, m->channel_offset, logical_addr-m->logical_addr, m->file_offset, m->prot, m->flags);
-					mmap_update(m->logical_addr,0);
+					pfs_mmap *newmap = new pfs_mmap(m);
+					newmap->map_length = logical_addr-m->logical_addr;
+					newmap->next = *p;
+					*p = newmap;
 				}
 
 				// If there is a fragment left over after the unmap, add it as a new map
 				// This will increase the reference count of both the file and the memory object.
 
 				if((logical_addr+length) < (m->logical_addr+m->map_length)) {
-					mmap_create_object(m->file, m->channel_offset, m->map_length-length-(logical_addr-m->logical_addr), m->file_offset+m->map_length-(m->logical_addr-logical_addr), m->prot, m->flags);
-					mmap_update(logical_addr+length,0);
+					pfs_mmap *newmap = new pfs_mmap(m);
+					newmap->logical_addr = m->map_length-length-(logical_addr-m->logical_addr);
+					newmap->map_length = m->file_offset+m->map_length-(m->logical_addr-logical_addr);
+					newmap->next = *p;
+					*p = newmap;
 				}
 			}
 