d9f09a9695fc0a0a9a7b0ce339f1f02a20548568
nkremerh <nkremerh@nd.edu>
2016-01-20 16:45:31 -0500

    Adds help and version messages to disk_allocator

--- dttools/src/disk_allocator.c
+++ dttools/src/disk_allocator.c
@@ -10,16 +10,53 @@ See the file COPYING for details.
 #include <inttypes.h>
 #include <unistd.h>
 #include <string.h>
-
+#include "getopt.h"
 #include "disk_alloc.h"
 #include "stringtools.h"
+#include "cctools.h"
+
+static void show_help(const char *cmd) {
+
+	fprintf(stdout, "Use: %s [options]\n", cmd);
+	fprintf(stdout, "Where options are:\n");
+	fprintf(stdout, " %-30s This message\n", "-h,--help=<flag>");
+	fprintf(stdout, " %-30s Version\n", "-v,--version");
+	fprintf(stdout, " %-30s Loop device allocation\n", "create <target directory> <size (i.e. 100MB)> <filesystem>");
+	fprintf(stdout, " %-30s Loop device deallocation\n", "delete <target directory>");
+	fprintf(stdout, "\n");
+	return;
+}
 
 int main(int argc, char *argv[]) {
 
 	char *func = argv[1];
 	char *loc = argv[2];
-	char *fs = argv[3];
+	char *fs = argv[4];
 	int result;
+	signed char c;
+
+	static const struct option long_options[] = {
+		{"help", no_argument, 0, 'h'},
+		{"version", no_argument, 0, 'v'},
+		{0,0,0,0}
+	};
+
+	while((c = getopt_long(argc, argv, "h:v", long_options, NULL)) > -1) {
+		switch (c) {
+		case 'h':
+			show_help(argv[0]);
+			exit(0);
+			break;
+		case 'v':
+			cctools_version_print(stdout, argv[0]);
+			exit(EXIT_SUCCESS);	
+			break;
+		default:
+			show_help(argv[0]);
+			exit(1);
+			break;
+		}
+	}
 
 	if(strstr(func, "create") != NULL) {
 
@@ -56,3 +93,4 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 }
+
