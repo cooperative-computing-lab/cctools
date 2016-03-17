2a37d79d1f5587261e8617e8dc2def7b1e3e1730
Douglas Thain <dthain@nd.edu>
2016-03-17 11:02:28 -0400

    Updated jx_pretty_print to deal with newly added expression types.

--- dttools/src/jx_pretty_print.c
+++ dttools/src/jx_pretty_print.c
@@ -11,15 +11,17 @@ See the file COPYING for details.
 
 #define SPACES 2
 
-void jx_pretty_print_buffer( struct jx *j, buffer_t *b, int level, int key);
-
+static void jx_pretty_print_buffer( struct jx *j, buffer_t *b, int level );
 
 static void jx_pretty_print_pair( struct jx_pair *pair, buffer_t *b, int level)
 {
 	if(!pair) return;
 
-	jx_pretty_print_buffer(pair->key, b, level, /* key */ 1);
-	jx_pretty_print_buffer(pair->value, b, level+1, 0);
+	buffer_printf(b,"%*s", level*SPACES, "");
+
+	jx_pretty_print_buffer(pair->key, b, level );
+	buffer_printf(b,":");
+	jx_pretty_print_buffer(pair->value, b, level+1 );
 
 	if(pair->next) {
 		buffer_putstring(b,",\n");
@@ -29,30 +31,16 @@ static void jx_pretty_print_pair( struct jx_pair *pair, buffer_t *b, int level)
 	}
 }
 
-void jx_pretty_print_buffer( struct jx *j, buffer_t *b, int level, int key)
+static void jx_pretty_print_buffer( struct jx *j, buffer_t *b, int level )
 {
 	if(!j) return;
 
-	switch(j->type) {
-		case JX_NULL:
-		case JX_DOUBLE:
-		case JX_BOOLEAN:
-		case JX_INTEGER:
-		case JX_SYMBOL:
-		case JX_STRING:
-		case JX_ARRAY:
-			buffer_printf(b,"%*s", level*SPACES, "");
-			jx_print_buffer(j, b);
-			if(key)
-				buffer_printf(b,":");
-			break;
-		case JX_OBJECT:
-			buffer_printf(b,"\n%*s{\n", level*SPACES, "");
-			jx_pretty_print_pair(j->u.pairs, b, level+1);
-			buffer_printf(b,"%*s}", level*SPACES, "");
-			break;
-		default:
-			return;
+	if(j->type==JX_OBJECT) {
+		buffer_printf(b,"\n%*s{\n", level*SPACES, "");
+		jx_pretty_print_pair(j->u.pairs, b, level+1);
+		buffer_printf(b,"%*s}", level*SPACES, "");
+	} else {
+		jx_print_buffer(j, b);
 	}
 }
 
@@ -60,7 +48,7 @@ void jx_pretty_print_stream( struct jx *j, FILE *file )
 {
 	buffer_t buffer;
 	buffer_init(&buffer);
-	jx_pretty_print_buffer(j, &buffer, 0, 0);
+	jx_pretty_print_buffer(j, &buffer, 0 );
 	fprintf(file,"%s",buffer_tostring(&buffer));
 	buffer_free(&buffer);
 }
