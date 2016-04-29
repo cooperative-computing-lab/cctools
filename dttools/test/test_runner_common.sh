a98b5a754fd5c8f7d4045be907612ff0ac6065ea
Benjamin Tovar <btovar@nd.edu>
2016-04-29 12:45:33 -0400

    Assign cores, memory, and disk to worker in test.

--- dttools/test/test_runner_common.sh
+++ dttools/test/test_runner_common.sh
@@ -93,7 +93,7 @@ run_local_worker()
 		exit 1
 	fi
 	echo "Running worker."
-	if ! "$WORK_QUEUE_WORKER" --single-shot --timeout=10s --debug=all --debug-file="$log" localhost $(cat "$port_file"); then
+	if ! "$WORK_QUEUE_WORKER" --single-shot --timeout=10s --cores 1 --memory 250 --disk 250 --debug=all --debug-file="$log" localhost $(cat "$port_file"); then
 		echo "ERROR: could not start worker"
 		exit 1
 	fi
