2d4b55a12bf13cc26ed7ec42f2d38a539c9000c4
Patrick Donnelly <pdonnel3@nd.edu>
2016-05-04 22:16:40 -0400

    Add UNIXRC conversion and fix translation bug.
    CATCHUNIXIGNORE would wrongly leave rc as -1 when the error is ignored.

--- dttools/src/catch.h
+++ dttools/src/catch.h
@@ -27,6 +27,10 @@
 		}\
 	} while (0)
 
+#define RCUNIX(rc) (rc == 0 ? 0 : (errno = (int)rc, -1))
+
+#define UNIXRC(ux) ((ux) == -1 ? errno : 0)
+
 #define CATCHUNIX(expr) \
 	do {\
 		rc = (expr);\
@@ -40,14 +44,14 @@
 #define CATCHUNIXIGNORE(expr,err) \
 	do {\
 		rc = (expr);\
-		if (rc == -1 && errno != err) {\
+		if (rc == -1) {\
 			rc = errno;\
-			debug(D_DEBUG, "%s: %s:%d[%s] unix error: -1 (errno = %d) `%s'", __func__, __FILE__, __LINE__, CCTOOLS_SOURCE, (int)rc, strerror((int)rc));\
-			goto out;\
+			if (errno != err) {\
+				debug(D_DEBUG, "%s: %s:%d[%s] unix error: -1 (errno = %d) `%s'", __func__, __FILE__, __LINE__, CCTOOLS_SOURCE, (int)rc, strerror((int)rc));\
+				goto out;\
+			}\
 		}\
 	} while (0)
-
-#define RCUNIX(rc) (rc == 0 ? 0 : (errno = (int)rc, -1))
 
 #endif
 
