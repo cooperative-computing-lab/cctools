536430064ec01790ff1efc3ef17feb97dc282903
Tim Shaffer <tshaffe1@nd.edu>
2016-05-19 15:31:35 -0400

    Expose dag_close_over_*

--- makeflow/src/parser.h
+++ makeflow/src/parser.h
@@ -8,5 +8,7 @@ See the file COPYING for details.
 #define PARSER_H
 
 struct dag *dag_from_file(const char *filename);
+void dag_close_over_categories(struct dag *d);
+void dag_close_over_environment(struct dag *d);
 
 #endif
