8c7507e38c7bb91010c0efa4884245c4c3f117ad
Charles Zheng <charleszheng44@gmail.com>
2016-06-21 14:41:34 -0400

    initial commit for batch_job_mesos

---
+++ batch_job/src/batch_job_mesos.c
@@ -0,0 +1,167 @@
+#include "batch_job.h"
+#include "batch_job_internal.h"
+#include "debug.h"
+#include "process.h"
+#include "macros.h"
+#include "stringtools.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+static batch_job_id_t batch_job_mesos_submit (struct batch_queue *q, const char *cmd, const char *extra_input_files, const char *extra_output_files, struct jx *envlist, const struct rmsummary *resources )
+{
+	batch_job_id_t jobid;
+
+	fflush(NULL);
+	jobid = fork();
+	if(jobid > 0) {
+		debug(D_BATCH, "started process %" PRIbjid ": %s", jobid, cmd);
+		struct batch_job_info *info = malloc(sizeof(*info));
+		memset(info, 0, sizeof(*info));
+		info->submitted = time(0);
+		info->started = time(0);
+		itable_insert(q->job_table, jobid, info);
+		return jobid;
+	} else if(jobid < 0) {
+		debug(D_BATCH, "couldn't create new process: %s\n", strerror(errno));
+		return -1;
+	} else {
+		/** The following code works but would duplicates the current process because of the system() function.
+		int result = system(cmd);
+		if(WIFEXITED(result)) {
+			_exit(WEXITSTATUS(result));
+		} else {
+			_exit(1);
+		}*/
+
+		if(envlist) {
+			jx_export(envlist);
+		}
+
+		/** A note from "man system 3" as of Jan 2012:
+		 * Do not use system() from a program with set-user-ID or set-group-ID
+		 * privileges, because strange values for some environment variables
+		 * might be used to subvert system integrity. Use the exec(3) family of
+		 * functions instead, but not execlp(3) or execvp(3). system() will
+		 * not, in fact, work properly from programs with set-user-ID or
+		 * set-group-ID privileges on systems on which /bin/sh is bash version
+		 * 2, since bash 2 drops privileges on startup. (Debian uses a modified
+		 * bash which does not do this when invoked as sh.)
+		 */
+
+		execlp("sh", "sh", "-c", cmd, (char *) 0);
+		_exit(127);	// Failed to execute the cmd.
+	}
+	return -1;
+}
+
+static batch_job_id_t batch_job_mesos_wait (struct batch_queue * q, struct batch_job_info * info_out, time_t stoptime)
+{
+	while(1) {
+		int timeout;
+
+		if(stoptime > 0) {
+			timeout = MAX(0, stoptime - time(0));
+		} else {
+			timeout = 5;
+		}
+
+		struct process_info *p = process_wait(timeout);
+		if(p) {
+			struct batch_job_info *info = itable_remove(q->job_table, p->pid);
+			if(!info) {
+				process_putback(p);
+				return -1;
+			}
+
+			info->finished = time(0);
+			if(WIFEXITED(p->status)) {
+				info->exited_normally = 1;
+				info->exit_code = WEXITSTATUS(p->status);
+			} else {
+				info->exited_normally = 0;
+				info->exit_signal = WTERMSIG(p->status);
+			}
+
+			memcpy(info_out, info, sizeof(*info));
+
+			int jobid = p->pid;
+			free(p);
+			free(info);
+			return jobid;
+
+		} else if(errno == ESRCH || errno == ECHILD) {
+			return 0;
+		}
+
+		if(stoptime != 0 && time(0) >= stoptime)
+			return -1;
+	}
+}
+
+static int batch_job_mesos_remove (struct batch_queue *q, batch_job_id_t jobid)
+{
+	int status;
+
+	if(kill(jobid, SIGTERM) == 0) {
+		if(!itable_lookup(q->job_table, jobid)) {
+			debug(D_BATCH, "runaway process %" PRIbjid "?\n", jobid);
+			return 0;
+		} else {
+			debug(D_BATCH, "waiting for process %" PRIbjid, jobid);
+			waitpid(jobid, &status, 0);
+			return 1;
+		}
+	} else {
+		debug(D_BATCH, "could not signal process %" PRIbjid ": %s\n", jobid, strerror(errno));
+		return 0;
+	}
+
+}
+
+static int batch_queue_mesos_create (struct batch_queue *q)
+{
+	batch_queue_set_feature(q, "mesos_job_queue", NULL);
+	return 0;
+}
+
+batch_queue_stub_free(mesos);
+batch_queue_stub_port(mesos);
+batch_queue_stub_option_update(mesos);
+
+batch_fs_stub_chdir(mesos);
+batch_fs_stub_getcwd(mesos);
+batch_fs_stub_mkdir(mesos);
+batch_fs_stub_putfile(mesos);
+batch_fs_stub_stat(mesos);
+batch_fs_stub_unlink(mesos);
+
+const struct batch_queue_module batch_queue_mesos = {
+	BATCH_QUEUE_TYPE_MESOS,
+	"mesos",
+
+	batch_queue_mesos_create,
+	batch_queue_mesos_free,
+	batch_queue_mesos_port,
+	batch_queue_mesos_option_update,
+
+	{
+		batch_job_mesos_submit,
+		batch_job_mesos_wait,
+		batch_job_mesos_remove,
+	},
+
+	{
+		batch_fs_mesos_chdir,
+		batch_fs_mesos_getcwd,
+		batch_fs_mesos_mkdir,
+		batch_fs_mesos_putfile,
+		batch_fs_mesos_stat,
+		batch_fs_mesos_unlink,
+	},
+};
+
+/* vim: set noexpandtab tabstop=4: */
