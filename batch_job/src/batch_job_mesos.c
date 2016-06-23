0be646859130b47b8118728436724b14daad3d92
Charles Zheng <charleszheng44@gmail.com>
2016-06-23 16:07:20 -0400

    implement batch_job_mesos_wait()

--- batch_job/src/batch_job_mesos.c
+++ batch_job/src/batch_job_mesos.c
@@ -4,6 +4,8 @@
 #include "process.h"
 #include "macros.h"
 #include "stringtools.h"
+#include "path.h"
+#include "xxmalloc.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,18 +13,33 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define FILE_RUN_TASKS "task_to_run"
 #define FILE_FINISH_TASKS "finished_tasks"
 
+#define NUM_OF_TASKS 1024
+
 static int counter = 0;
+static int finished_task[NUM_OF_TASKS];
+
+static bool is_in_array(int a, int *int_array, int size) 
+{
+	int i = 0;
+	for(i = 0; i < size; i++) {
+		if(int_array[i] == a) {
+			return true;
+		}
+	}
+	return false;
+}
 
 static batch_job_id_t batch_job_mesos_submit (struct batch_queue *q, const char *cmd, const char *extra_input_files, const char *extra_output_files, struct jx *envlist, const struct rmsummary *resources )
 {
 	int task_id = ++counter;
 
-	debug(D_BATCH, "task %" PRIbjid " is ready", task_id);
+	debug(D_BATCH, "task %d is ready", task_id);
 	struct batch_job_info *info = malloc(sizeof(*info));
 	memset(info, 0, sizeof(*info));
 	info->submitted = time(0);
@@ -36,11 +53,38 @@ static batch_job_id_t batch_job_mesos_submit (struct batch_queue *q, const char 
 	} else {
 		fp = fopen(FILE_RUN_TASKS, "w+");
 	}
-		
-	fprintf(fp, "task_id %" PRIbjid "\n", task_id);
+
+	fprintf(fp, "task_id: %d\n", task_id);
 	fprintf(fp, "cmd: %s\n", cmd);
-	fprintf(fp, "Input files: %s\n", extra_input_files);
-	fprintf(fp, "Output files: %s\n\n", extra_output_files);
+
+	// Get the absolut path of each input file
+	
+	if (extra_input_files != NULL) {
+		fputs("input files: ", fp);
+		char *path_buf = path_getcwd();
+		char *pch;
+		char *input_files_cpy = xxstrdup(extra_input_files); 
+		pch = strtok(input_files_cpy, " ,");
+		char *tmp_fn_path;
+		char *tmp_fn_abs_path;
+		while(pch != NULL) {
+			if(pch[0] != '/') {
+				tmp_fn_path = string_combine(path_buf, "/");
+				tmp_fn_abs_path = string_combine(tmp_fn_path, pch);
+				fprintf(fp, "%s,", tmp_fn_abs_path);
+			} else {
+				fprintf(fp, "%s,", pch);
+			}	
+			pch = strtok (NULL, " ,");
+		}
+		free(path_buf);
+
+		fputs("\n", fp);
+	} else {
+		fputs("input files: \n", fp);
+	}
+		
+	fprintf(fp, "output files: %s\n", extra_output_files);
 
 	fclose(fp);
 
@@ -49,66 +93,71 @@ static batch_job_id_t batch_job_mesos_submit (struct batch_queue *q, const char 
 
 static batch_job_id_t batch_job_mesos_wait (struct batch_queue * q, struct batch_job_info * info_out, time_t stoptime)
 {
+
+	return 0;
+	/*
+	// read FILE_FINISH_TASKS and check if there is job finished
+	// remove the job from batch_queue->job_table 
+
+	struct stat oup_fn_stat;
+	int oup_fn_status = 0;
+	oup_fn_status = stat(FILE_FINISH_TASKS, &oup_fn_stat);
+	off_t oup_fn_size = oup_fn_stat.st_size;
+
+	// polling the FILE_FINISH_TASKS to check if there is
+	// new task finished
+	
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read_len;
+	FILE *fp;
+	fp = fopen(FILE_FINISH_TASKS, "r");
+
 	while(1) {
-		int timeout;
 
-		if(stoptime > 0) {
-			timeout = MAX(0, stoptime - time(0));
+		oup_fn_status = stat(FILE_FINISH_TASKS, &oup_fn_stat);
+		// if the file size has changed
+		if (oup_fn_stat.st_size - oup_fn_size > 0) {
+			char *task_id_ch;
+			char *task_stat_ch;
+			int task_id;
+			
+	    	while((read_len = getline(&line, &len, )) != -1) {
+				task_id_ch = strtok(line, " ");
+				task_id = atoi(task_id_ch);
+
+				// There is a new task finished
+				if(!is_in_array(task_id, finished_tasks)) {
+					struct batch_job_info *info = itable_remove(q->job_table, task_id);	
+					info->finished = time(0);
+					task_stat_ch = strtok(NULL, " ");
+
+					if (strcmp(task_stat_ch, "finished") == 0) {
+						info->exited_normally = 1;
+					} else {
+						info->exited_normally = 0;
+					}
+
+					memcpy(info_out, info, sizeof(*info));
+					free(info);
+					fclose(fp);
+					return task_id;
+				}
+			}
+
 		} else {
-			timeout = 5;
+			sleep(1);
 		}
 
-		struct process_info *p = process_wait(timeout);
-		if(p) {
-			struct batch_job_info *info = itable_remove(q->job_table, p->pid);
-			if(!info) {
-				process_putback(p);
-				return -1;
-			}
-
-			info->finished = time(0);
-			if(WIFEXITED(p->status)) {
-				info->exited_normally = 1;
-				info->exit_code = WEXITSTATUS(p->status);
-			} else {
-				info->exited_normally = 0;
-				info->exit_signal = WTERMSIG(p->status);
-			}
-
-			memcpy(info_out, info, sizeof(*info));
-
-			int jobid = p->pid;
-			free(p);
-			free(info);
-			return jobid;
-
-		} else if(errno == ESRCH || errno == ECHILD) {
-			return 0;
-		}
-
-		if(stoptime != 0 && time(0) >= stoptime)
-			return -1;
+		oup_fn_size = oup_fn_stat.st_size;	
 	}
+	*/
+
 }
 
 static int batch_job_mesos_remove (struct batch_queue *q, batch_job_id_t jobid)
 {
-	int status;
-
-	if(kill(jobid, SIGTERM) == 0) {
-		if(!itable_lookup(q->job_table, jobid)) {
-			debug(D_BATCH, "runaway process %" PRIbjid "?\n", jobid);
-			return 0;
-		} else {
-			debug(D_BATCH, "waiting for process %" PRIbjid, jobid);
-			waitpid(jobid, &status, 0);
-			return 1;
-		}
-	} else {
-		debug(D_BATCH, "could not signal process %" PRIbjid ": %s\n", jobid, strerror(errno));
-		return 0;
-	}
-
+	return 0;
 }
 
 static int batch_queue_mesos_create (struct batch_queue *q)
