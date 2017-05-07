667c832273449e1f5a81a22e108e7bd029988dbf
Charles Zheng <charleszheng44@gmail.com>
2017-05-06 22:08:58 -0400

    inital commit for k8s_v2

---
+++ batch_job/src/batch_job_k8s.c
@@ -0,0 +1,237 @@
+#include "batch_job.h"
+#include "batch_job_internal.h"
+#include "debug.h"
+#include "process.h"
+#include "macros.h"
+#include "stringtools.h"
+#include "uuid.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+static cctools_uuid_t *mf_uuid = NULL;
+static int count = 0;
+
+static const char *k8s_config_tmpl = "\
+{\n\
+    \"apiVersion\": \"v1\",\n\
+    \"kind\": \"Pod\",\n\
+    \"metadata\": {\n\
+        \"labels\": {\n\
+            \"app\": \"makeflow\"\n\
+        },\n\
+        \"name\": \"%s\"\n\
+    },\n\
+\n\
+    \"spec\": {\n\
+        \"containers\": [{\n\
+            \"name\": \"%s\",\n\
+            \"image\": \"centos\",\n\
+            \"command\": [\"/bin/bash\", \"-c\"],\n\
+            \"args\": [\"echo \\\"%d start_container\\\" > %s.log ; tail -f /dev/null \"]\n\
+        }],\n\
+        \"restartPolicy\": \"Never\"\n\
+    }\n\
+}\n\
+";
+
+static batch_job_id_t batch_job_k8s_submit (struct batch_queue *q, const char *cmd, const char *extra_input_files, const char *extra_output_files, struct jx *envlist, const struct rmsummary *resources )
+{
+	
+	if(mf_uuid == NULL) {
+		cctools_uuid_create (mf_uuid);
+	}
+
+	int job_id;
+	pid_t pid;
+	
+	job_id = count ++;
+	pid = fork();
+	if(pid > 0) {
+
+		debug(D_BATCH, "started process %d: %s", job_id, cmd);
+		struct batch_job_info *info = calloc(sizeof(*info));
+		info->submitted = time(0);
+		info->started = time(0);
+		itable_insert(q->job_table, job_id, info);
+		return job_id;
+
+	} else if(pid < 0) {
+
+		fatal("couldn't create new process: %s\n", strerror(errno));
+
+	} else {
+
+		if(envlist) {
+			jx_export(envlist);
+		}
+
+		/*
+	 	 * 1. kubectl create -f $mf_uuid-$count.json (create log)
+	     * 2. "kubectl get pods $mf_uuid-$count" is running
+	     * 3. kubectl cp extra_input_files $mf_uuid-$count:/
+	 	 */
+		
+		char *pod_id = string_format("%s-%d", mf_uuid->str, job_id);	
+		char *k8s_config_fn = string_format("%s.json", pod_id);
+
+		FILE *fd = fopen(k8s_config_fn, "w+");
+		fprintf(fd, k8s_config_tmpl, pod_id, pod_id, job_id, pod_id);
+		fclose(fd);
+		
+		char *cmd = string_format("batch_job_k8s_script.sh %s %d %s %s %s", 
+			pod_id, job_id, extra_input_files, cmd, extra_output_files)	
+
+		execlp("/bin/bash", "bash", "-c", cmd, (char *) 0);
+		_exit(errno);	// Failed to execute the cmd.
+	}
+	return -1;
+}
+
+// TODO
+static batch_job_id_t batch_job_k8s_wait (struct batch_queue * q, struct batch_job_info * info_out, time_t stoptime)
+{
+	
+	/*
+	 * There are 5 states for a k8s job
+	 * 1. pod_created
+	 * 2. inps_transferred
+     * 3. exec_success/exec_failed
+     * 4. oups_transferred
+	 * 5. job_done 
+	 */
+	
+	while(1) {
+		int timeout;
+
+		if(stoptime > 0) {
+			timeout = MAX(0, stoptime - time(0));
+		} else {
+			timeout = 5;
+		}
+
+		struct process_info *p = process_wait(timeout);
+		if(p) {
+			struct batch_job_info *info = itable_remove(q->job_table, p->pid);
+			if(!info) {
+				process_putback(p);
+				return -1;
+			}
+
+			info->finished = time(0);
+			if(WIFEXITED(p->status)) {
+				info->exited_normally = 1;
+				info->exit_code = WEXITSTATUS(p->status);
+			} else {
+				info->exited_normally = 0;
+				info->exit_signal = WTERMSIG(p->status);
+			}
+
+			memcpy(info_out, info, sizeof(*info));
+
+			int jobid = p->pid;
+			free(p);
+			free(info);
+			return jobid;
+
+		} else if(errno == ESRCH || errno == ECHILD) {
+			return 0;
+		}
+
+		if(stoptime != 0 && time(0) >= stoptime)
+			return -1;
+	}
+}
+
+static int batch_job_k8s_remove (struct batch_queue *q, batch_job_id_t jobid)
+{
+	int status;
+	
+	pid_t pid = fork();
+	char *pod_id = string_format("%s-%d", mf_uuid, (int)jobid);
+
+    if(pid > 0) {
+
+    	int status;
+
+    	debug(D_BATCH, "Trying to remove task %d by deleting pods %s.", 
+			jobid, pod_id);
+
+		// wait child process to complete
+
+		if(waitpid(pid, &status, 0) == -1) {
+			fatal("Failed to remove pods %s", pod_id);
+		}
+
+		if(WIFEXITED(status)) {
+			debug(D_BATCH, "Successfully delete pods %s", pod_id);
+		}
+		
+		free(pod_id);
+		return 0;
+
+    } else if(pid < 0) {
+
+		fatal("couldn't create new process: %s\n", strerror(errno));
+
+    } else {
+
+		char *cmd = string_format("kubectl delete pods %s", pod_id);
+
+		execlp("/bin/bash", "bash", "-c", cmd, (char *) 0);
+		_exit(errno);
+
+    }
+
+    return 1;	
+
+}
+
+static int batch_queue_k8s_create (struct batch_queue *q)
+{
+	strncpy(q->logfile, "k8s.log", sizeof(q->logfile));
+    batch_queue_set_feature(q, "batch_log_name", "%s.k8slog");
+    batch_queue_set_feature(q, "batch_log_transactions", "%s.tr");
+	return 0;
+}
+
+batch_queue_stub_free(k8s);
+batch_queue_stub_port(k8s);
+batch_queue_stub_option_update(k8s);
+
+batch_fs_stub_chdir(k8s);
+batch_fs_stub_getcwd(k8s);
+batch_fs_stub_mkdir(k8s);
+batch_fs_stub_putfile(k8s);
+batch_fs_stub_stat(k8s);
+batch_fs_stub_unlink(k8s);
+
+const struct batch_queue_module batch_queue_k8s = {
+	BATCH_QUEUE_TYPE_K8S,
+	"k8s",
+
+	batch_queue_k8s_create,
+	batch_queue_k8s_free,
+	batch_queue_k8s_port,
+	batch_queue_k8s_option_update,
+
+	{
+		batch_job_k8s_submit,
+		batch_job_k8s_wait,
+		batch_job_k8s_remove,
+	},
+
+	{
+		batch_fs_k8s_chdir,
+		batch_fs_k8s_getcwd,
+		batch_fs_k8s_mkdir,
+		batch_fs_k8s_putfile,
+		batch_fs_k8s_stat,
+		batch_fs_k8s_unlink,
+	},
+};
+
+/* vim: set noexpandtab tabstop=4: */
