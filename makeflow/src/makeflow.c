c5d0fbf61574bf86a13a5dcc8adc2451ee477bb4
Charles Zheng <charleszheng44@gmail.com>
2017-05-19 14:08:03 -0400

    debug k8s mode and works with minikube

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -1007,6 +1007,7 @@ static void makeflow_run( struct dag *d )
 			break;
 
 		if(dag_remote_jobs_running(d)) {
+
 			int tmp_timeout = 5;
 			jobid = batch_job_wait_timeout(remote_queue, &info, time(0) + tmp_timeout);
 			if(jobid > 0) {
@@ -1019,10 +1020,13 @@ static void makeflow_run( struct dag *d )
 		}
 
 		if(dag_local_jobs_running(d)) {
+			debug(D_MAKEFLOW_RUN, "==========local job running================");
 			time_t stoptime;
 			int tmp_timeout = 5;
 
 			if(dag_remote_jobs_running(d)) {
+
+				debug(D_MAKEFLOW_RUN, "**********remote running****************");
 				stoptime = time(0);
 			} else {
 				stoptime = time(0) + tmp_timeout;
@@ -1038,12 +1042,13 @@ static void makeflow_run( struct dag *d )
 		}
 
 		/* Make periodic report to catalog. */
-                timestamp_t now = timestamp_get();
-                if(catalog_reporting_on && (((now-last_time) > (60 * 1000 * 1000)) || first_report==1)){ //if we are in reporting mode, and if either it's our first report, or 1 min has transpired
+		//report to catalog
+		timestamp_t now = timestamp_get();
+		if(catalog_reporting_on && (((now-last_time) > (60 * 1000 * 1000)) || first_report==1)){ //if we are in reporting mode, and if either it's our first report, or 1 min has transpired
 			makeflow_catalog_summary(d, project,batch_queue_type,start);
 			last_time = now;
 			first_report = 0;
-                }
+		}
 
 		/* Rather than try to garbage collect after each time in this
 		 * wait loop, perform garbage collection after a proportional
