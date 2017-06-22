f0531c206748855339ed3b95799745a61d8bb596
Tim Shaffer <tshaffe1@nd.edu>
2017-06-22 15:27:40 -0400

    Save outputs of failed Makeflow rules

--- makeflow/src/makeflow.c
+++ makeflow/src/makeflow.c
@@ -93,6 +93,7 @@ an example.
 */
 
 #define MAX_REMOTE_JOBS_DEFAULT 100
+#define FAIL_DIR "makeflow.failed.%d"
 
 static sig_atomic_t makeflow_abort_flag = 0;
 static int makeflow_failed_flag = 0;
@@ -713,6 +714,91 @@ int makeflow_node_check_file_was_created(struct dag_node *n, struct dag_file *f)
 	return file_created;
 }
 
+static struct dag_file *makeflow_lookup_fail_file(
+		struct dag *d, struct batch_queue *q, const char *path) {
+	assert(d);
+	assert(q);
+	assert(path);
+	struct stat buf;
+	struct dag_file *f = hash_table_lookup(d->files, path);
+	if (f) {
+		if (f->type == DAG_FILE_TYPE_INPUT) {
+			debug(D_MAKEFLOW_RUN,
+					"skipping %s since it's specified as an input",
+					path);
+			return NULL;
+		}
+		return f;
+	} else {
+		if (!batch_fs_stat(q, path, &buf)) {
+			debug(D_MAKEFLOW_RUN,
+					"skipping %s since it already exists",
+					path);
+			return NULL;
+		}
+		return dag_file_lookup_or_create(d, path);
+	}
+}
+
+static int makeflow_prep_fail_dir(
+		struct dag *d, struct dag_node *n, struct batch_queue *q) {
+	assert(d);
+	assert(n);
+	assert(q);
+	int rc = 1;
+	char *faildir = string_format(FAIL_DIR, n->nodeid);
+	struct dag_file *f = makeflow_lookup_fail_file(d, q, faildir);
+	if (!f) goto FAILURE;
+
+	if (makeflow_clean_file(d, q, f, 1)) {
+		debug(D_MAKEFLOW_RUN, "Unable to clean failed output");
+		goto FAILURE;
+	}
+	if (batch_fs_mkdir(q, f->filename, 0755, 0)) {
+		debug(D_MAKEFLOW_RUN,
+				"Unable to create failed output directory");
+		goto FAILURE;
+	}
+
+	makeflow_log_file_state_change(d, f, DAG_FILE_STATE_COMPLETE);
+	fprintf(stderr, "rule %d failed, copying any outputs to %s\n",
+			n->nodeid, faildir);
+	rc = 0;
+FAILURE:
+	free(faildir);
+	return rc;
+}
+
+static int makeflow_clean_failed_file(struct dag *d, struct dag_node *n,
+		struct batch_queue *q, struct dag_file *f, int prep_failed,
+		int silent) {
+	assert(d);
+	assert(n);
+	assert(q);
+	assert(f);
+	if (!prep_failed) {
+		char *failout = string_format(
+				FAIL_DIR "/%s", n->nodeid, f->filename);
+		struct dag_file *o = makeflow_lookup_fail_file(d, q, failout);
+		if (o) {
+			if (batch_fs_putfile(q, f->filename, o->filename) < 0) {
+				debug(D_MAKEFLOW_RUN, "Failed to copy %s -> %s",
+						f->filename, o->filename);
+			} else {
+				debug(D_MAKEFLOW_RUN, "Copied %s -> %s",
+						f->filename, o->filename);
+				makeflow_log_file_state_change(
+						d, o, DAG_FILE_STATE_COMPLETE);
+			}
+		} else {
+			fprintf(stderr, "Skipping copy %s -> %s", f->filename,
+					failout);
+		}
+		free(failout);
+	}
+	return makeflow_clean_file(d, q, f, silent);
+}
+
 /*
 Mark the given task as completing, using the batch_job_info completion structure provided by batch_job.
 */
@@ -774,14 +860,21 @@ static void makeflow_node_complete(struct dag *d, struct dag_node *n, struct bat
 
 	if(job_failed) {
 		makeflow_log_state_change(d, n, DAG_NODE_STATE_FAILED);
+		int prep_failed = makeflow_prep_fail_dir(d, n, remote_queue);
+		if (prep_failed) {
+			fprintf(stderr, "rule %d failed, cannot copy outputs\n",
+					n->nodeid);
+		}
 
 		/* Clean files created in node. Clean existing and expected and record deletion. */
 		list_first_item(outputs);
 		while((f = list_next_item(outputs))) {
 			if(f->state == DAG_FILE_STATE_EXPECT) {
-				makeflow_clean_file(d, remote_queue, f, 1);
+				makeflow_clean_failed_file(d, n, remote_queue,
+						f, prep_failed, 1);
 			} else {
-				makeflow_clean_file(d, remote_queue, f, 0);
+				makeflow_clean_failed_file(d, n, remote_queue,
+						f, prep_failed, 0);
 			}
 		}
 
