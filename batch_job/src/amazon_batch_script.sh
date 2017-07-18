6b664428fdf45d42ef2d52aaab8f9218338c1971
ksweene3 <ksweene3@cclws07.cse.nd.edu>
2017-07-18 18:21:16 -0400

    Added the creation and cleanup scripts which creates the configure script. I then modified the batch_job_amazon_batch module to call the setup script if the configure script doesn't exist, and then read all the necessary information from the configure script. Everything get submitted and created, but jobs seem to be stuck at the runnable state, which my current theory is that it's related to the VPC, and subnet, and maybe the default values for the minimum number,max number,desired number of cores for the compute environment.

--- batch_job/src/amazon_batch_script.sh
+++ batch_job/src/amazon_batch_script.sh
@@ -23,29 +23,35 @@ echo "\"aws_id\":\"$aws_id\"," >> $outputfile
 echo "\"aws_key\":\"$aws_key\"," >> $outputfile
 echo "\"aws_reg\":\"$aws_reg\"," >> $outputfile
 
-echo create the vpc
+#echo create the vpc
 ec2_vpc_create_response="$(aws ec2 create-vpc --cidr-block 10.0.0.0/16)"
 ec2_vpc=$(python -c "import json; print json.loads('''$ec2_vpc_create_response''')['Vpc']['VpcId'];")
+echo $ec2_vpc
 
 echo "\"vpc\":\"$ec2_vpc\"," >> $outputfile
 
-echo create the subnet
-ec2_subnet_create_response="$(aws ec2 create-subnet --vpc-id=$ec2_vpc --cidr-block 10.0.1.0/24)"
+#echo create the subnet
+ec2_subnet_create_response="$(aws ec2 create-subnet --vpc-id $ec2_vpc --cidr-block 10.0.1.0/24)"
 ec2_subnet=$(python -c "import json; print json.loads('''$ec2_subnet_create_response''')['Subnet']['SubnetId'];")
+echo $ec2_subnet
+
+aws ec2 modify-subnet-attribute --subnet-id $ec2_subnet --map-public-ip-on-launch
 
 echo "\"subnet\":\"$ec2_subnet\"," >> $outputfile
 
-echo create the security group
+#echo create the security group
 ec2_security_group_name="makeflow_ccl_sec_group_$time"
 ec2_security_group_create_response="$(aws ec2 create-security-group --group-name $ec2_security_group_name --description "A Makeflow Security Group" --vpc-id $ec2_vpc)"
 ec2_security_group_id=$(python -c "import json; print json.loads('''$ec2_security_group_create_response''')['GroupId'];")
+echo $ec2_security_group_id
 
-echo "\"sec_group\":\"$ec2_security_group_name\"," >> $outputfile
+echo "\"sec_group\":\"$ec2_security_group_id\"," >> $outputfile
 
-echo creating the environment
+#echo creating the environment
 env_name="makeflow_ccl_env_$time"
 account_num_id=$(aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text) #got from stack overflow: "Quick Way to get AWS Account number from the cli tools?
-batch_compenv_create_response="$(aws --region=$aws_reg batch create-compute-environment --compute-environment-name $env_name --type MANAGED --state ENABLED --compute-resources type=EC2,minvCpus=$min_cpus,maxvCpus=$max_cpus,desiredvCpus=$cpus,instanceTypes=optimal,subnets=$ec2_subnet,securityGroupIds=$ec2_security_group_id,instanceRole=ecsInstanceRole --service-role=arn:aws:iam::$account_num_id:role/service-role/AWSBatchServiceRole)"
+env_output_response=$(aws batch create-compute-environment --compute-environment-name $env_name --type MANAGED --state ENABLED --compute-resources type=EC2,minvCpus=$min_cpus,maxvCpus=$max_cpus,desiredvCpus=$cpus,instanceTypes=optimal,subnets=$ec2_subnet,securityGroupIds=$ec2_security_group_id,instanceRole=ecsInstanceRole --service-role=arn:aws:iam::$account_num_id:role/service-role/AWSBatchServiceRole)
+#echo $batch_compenv_create_response
 
 env_done_check_output="$(aws batch describe-compute-environments --compute-environments $env_name)"
 env_done=$(python -c "import json; print json.loads('''$env_done_check_output''')['computeEnvironments'][0]['status'] == 'VALID'")
@@ -55,11 +61,12 @@ env_done_check_output="$(aws batch describe-compute-environments --compute-envir
 env_done=$(python -c "import json; print json.loads('''$env_done_check_output''')['computeEnvironments'][0]['status'] == 'VALID'")
 done
 
-echo "\"env_name\":[$env_name]," >> $outputfile
+echo "\"env_name\":\"$env_name\"," >> $outputfile
 
-echo creating the queue
+#echo creating the queue
 queue_name="makeflow_ccl_queue_$time"
 batch_queue_create_response="$(aws --region=$aws_reg batch create-job-queue --state=ENABLED --priority=1 --job-queue-name=$queue_name --compute-environment-order order=1,computeEnvironment=$env_name)"
+echo $batch_queue_create_response
 
 queue_done_check_output="$(aws batch describe-job-queues --job-queues $queue_name)"
 queue_done=$(python -c "import json; print json.loads('''$queue_done_check_output''')['jobQueues'][0]['status'] == 'VALID'")
@@ -69,6 +76,14 @@ queue_done_check_output="$(aws batch describe-job-queues --job-queues $queue_nam
 queue_done=$(python -c "import json; print json.loads('''$queue_done_check_output''')['jobQueues'][0]['status'] == 'VALID'")
 done
 
-echo "\"queue_name\":\"$queue_name\"" >> $outputfile
+echo "\"queue_name\":\"$queue_name\"," >> $outputfile
+
+#echo creating the bucket
+bucket_pre=$(python -c "print ''.join([chr(ord('a')+int(x)) for x in '$time'])")
+echo $bucket_pre 
+bucket_name="ccl$bucket_pre"
+make_bucket_response="$(aws s3 mb s3://$bucket_name)"
+
+echo "\"bucket\":\"$bucket_name\"" >> $outputfile
 
 echo "}" >> $outputfile
