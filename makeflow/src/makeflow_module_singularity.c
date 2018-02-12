1e848945871e97b4d612c7f2ee96cfcd4007bc2d
Nick Hazekamp <nhazekam@nd.edu>
2018-02-12 16:37:00 -0500

    Change over to wrapper

--- makeflow/src/makeflow_module_singularity.c
+++ makeflow/src/makeflow_module_singularity.c
@@ -6,17 +6,19 @@
 
 #include "stringtools.h"
 #include "xxmalloc.h"
+#include "debug.h"
+#include "batch_wrapper.h"
 
 #include "dag.h"
 #include "makeflow_gc.h"
+#include "makeflow_log.h"
 #include "makeflow_hook.h"
 
+#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 
 #define CONTAINER_SINGULARITY_SH "singularity.wrapper.sh"
-
-char *singularity_global_command = NULL;
 
 char *singularity_image = NULL;
 
@@ -35,67 +37,32 @@ static int create( struct jx *hook_args )
 
 static int destroy( struct dag *d )
 {
-	if(makeflow_clean_file(d, makeflow_get_remote_queue(), dag_file_lookup_or_create(d, CONTAINER_SINGULARITY_SH), 1))
+	if(makeflow_clean_file(d, makeflow_get_remote_queue(), dag_file_lookup_or_create(d, CONTAINER_SINGULARITY_SH)))
 		return MAKEFLOW_HOOK_SUCCESS;
 	return MAKEFLOW_HOOK_FAILURE;
 }
 
-static int dag_start(struct dag *d){
-	FILE *wrapper_fn = fopen(CONTAINER_SINGULARITY_SH, "w");
-
-    char* file_sans_compress = string_format("%s",singularity_image);
-
-    char* filedata;
-    //$@ passes in everything, thus this should be awesome and just say "singularity exect <contimg> ALL THE THINGS
-
-    if(strstr(singularity_image,".gz") != NULL){
-        filedata = string_format("%s\n%s\n%s\n",
-                                 "#!/bin/sh",
-                                 "tar -xzf $s",
-                                "singularity exec --home `pwd` %s \"$@\"");
-        file_sans_compress[strlen(singularity_image)-3] = '\0';
-        fprintf(wrapper_fn, filedata, singularity_image, file_sans_compress);
-    }else if(strstr(singularity_image,".xz") != NULL){
-        filedata = string_format("%s\n%s\n%s\n",
-                                 "#!/bin/sh",
-                                 "tar -xf %s",
-                                 "singularity exec --home `pwd` %s \"$@\"");
-        file_sans_compress[strlen(singularity_image)-3] = '\0';
-        fprintf(wrapper_fn, filedata, singularity_image, file_sans_compress);
-    }else if(strstr(singularity_image,".bz2") != NULL){
-        filedata = string_format("%s\n%s\n%s\n",
-                                 "#!/bin/sh",
-                                 "tar -xjf %s",
-                                 "singularity exec --home `pwd` %s \"$@\"");
-        file_sans_compress[strlen(singularity_image)-4] = '\0';
-        fprintf(wrapper_fn, filedata, singularity_image, file_sans_compress);
-    }else{
-        filedata = string_format("%s\n%s\n",
-                                 "#!/bin/sh",
-                                 "singularity exec --home `pwd` %s \"$@\"");
-        fprintf(wrapper_fn, filedata, singularity_image);
-    }
-	fclose(wrapper_fn);
-
-    chmod(CONTAINER_SINGULARITY_SH, 0755);
-
-    free(filedata);
-    free(file_sans_compress);
-
-	dag_file_lookup_or_create(d, singularity_image);
-	dag_file_lookup_or_create(d, CONTAINER_SINGULARITY_SH);
-
-	singularity_global_command = string_format("sh %s", CONTAINER_SINGULARITY_SH);
-
-	return MAKEFLOW_HOOK_SUCCESS;
-}
-
 static int node_submit(struct dag_node *n, struct batch_task *t){
+	struct batch_wrapper *wrapper = batch_wrapper_create();
+	batch_wrapper_prefix(wrapper, CONTAINER_SINGULARITY_SH);
+
+	char *cmd = string_format("singularity exec --home `pwd` %s \"$@\"", singularity_image);
+	batch_wrapper_cmd(wrapper, cmd);
+	free(cmd);
+
+	cmd = batch_wrapper_write(wrapper, t);
+	if(cmd){
+		batch_task_set_command(t, cmd);
+		struct dag_file *df = makeflow_hook_add_input_file(n->d, t, cmd, cmd);
+		debug(D_MAKEFLOW_HOOK, "Wrapper written to %s", df->filename);
+		makeflow_log_file_state_change(n->d, df, DAG_FILE_STATE_EXISTS);
+	} else {
+		debug(D_MAKEFLOW_HOOK, "Failed to create wrapper: errno %d, %s", errno, strerror(errno));
+		return MAKEFLOW_HOOK_FAILURE;
+	}
+	free(cmd);
 
 	makeflow_hook_add_input_file(n->d, t, singularity_image, NULL);
-	makeflow_hook_add_input_file(n->d, t, CONTAINER_SINGULARITY_SH, NULL);
-
-	batch_task_wrap_command(t, singularity_global_command);
 
 	return MAKEFLOW_HOOK_SUCCESS;
 }
@@ -104,8 +71,6 @@ struct makeflow_hook makeflow_hook_singularity = {
 	.module_name = "Singularity",
 	.create = create,
 	.destroy = destroy,
-
-	.dag_start = dag_start,
 
 	.node_submit = node_submit,
 };
