ae455aa6d43a34da5baaa1d0118cff573ff0ca40
ksweene3 <ksweene3@cclws07.cse.nd.edu>
2018-02-12 18:12:11 -0500

    added a lot of error checking code, so the script should be pretty robust in ensuring that everything it needs to work properly is set up!

--- batch_job/src/amazon_batch_script.sh
+++ batch_job/src/amazon_batch_script.sh
@@ -58,72 +58,112 @@ if [ -e $outputfile ]; then
 	exit -12
 fi
 
-echo "{" >> $outputfile
+
+#taken from professor Thain's makeflow_ec2_setup
+if which aws >/dev/null 2>&1
+then
+	echo "found aws, moving on to config file check"
+else
+	echo "aws not found in path, please include it"
+	exit -9
+fi
+
+if [ -f ~/.aws/config ]; then
+	echo "found config file, moving on to credentials file check"
+else
+	echo "aws not configured: please configure before running the script"
+	exit -10
+fi
+if [ -f ~/.aws/credentials ]; then
+	echo "found credentials file!"
+else
+	echo "aws not configured: please configure before running the script"
+	exit -10
+fi
+
+if aws batch list-jobs --job-queue HighPriority > /dev/null 2>&1
+then
+	echo "config and credentials configured correctly, proceeding to setup"
+else
+	echo "configuration and credentials files not properly set, please reconfig aws and try again"
+	exit -33
+fi
+
+check_arn_iam=$(aws --output json iam get-role --role-name AWSBatchServiceRole)
+#good_iam=$(python -c "print 'True' if 'CreateDate' in '$check_arn_iam' else print 'False'")
+if [[ $check_arn_iam = *"CreateDate"*  ]]; then
+	echo "credentials have IAM with AWSBatchServiceRole set up, good!"
+else
+	echo "current user doesn't have AWSBatchServiceRole attached to credientials: please fix in amazon console"
+	exit -44
+fi
 
 #Grabbing the necessary keys
-aws_id=$(python -c "import ConfigParser,os; config = ConfigParser.RawConfigParser(); config.readfp(open(os.path.expanduser('~/.aws/credentials'))); print config.get('default','aws_access_key_id')")
+aws_id=$(python -c "import ConfigParser,os; config = ConfigParser.RawConfigParser(); config.readfp(open(os.path.expanduser('~/.aws/credentials'))); print config.get('default','aws_access_key_id');")
 aws_key=$(python -c "import ConfigParser,os; config = ConfigParser.RawConfigParser(); config.readfp(open(os.path.expanduser('~/.aws/credentials'))); print config.get('default','aws_secret_access_key')")
 aws_reg=$(python -c "import ConfigParser,os; config = ConfigParser.RawConfigParser(); config.readfp(open(os.path.expanduser('~/.aws/config'))); print config.get('default','region')")
+
+echo "{" >> $outputfile
 
 echo "\"aws_id\":\"$aws_id\"," >> $outputfile
 echo "\"aws_key\":\"$aws_key\"," >> $outputfile
 echo "\"aws_reg\":\"$aws_reg\"," >> $outputfile
 
 #echo create the vpc
-ec2_vpc_create_response="$(aws ec2 create-vpc --cidr-block 10.0.0.0/16)"
+ec2_vpc_create_response="$(aws --output json ec2 create-vpc --cidr-block 10.0.0.0/16)"
 ec2_vpc=$(python -c "import json; print json.loads('''$ec2_vpc_create_response''')['Vpc']['VpcId'];")
-#ec2_vpc=$(aws ec2 describe-vpcs --query 'Vpcs[0].VpcId' --output text)
+#ec2_vpc=$(aws --output json ec2 describe-vpcs --query 'Vpcs[0].VpcId' --output text)
 echo "created vpc: $ec2_vpc"
 
-aws ec2 modify-vpc-attribute --vpc-id $ec2_vpc --enable-dns-hostnames
-#aws ec2 modify-vpc-attribute --vpc-id $ec2_vpc --enable-dns-support
+aws --output json ec2 modify-vpc-attribute --vpc-id $ec2_vpc --enable-dns-hostnames
+#aws --output json ec2 modify-vpc-attribute --vpc-id $ec2_vpc --enable-dns-support
 
 echo "\"vpc\":\"$ec2_vpc\"," >> $outputfile
 
 #echo create the subnet
-ec2_subnet_create_response="$(aws ec2 create-subnet --vpc-id $ec2_vpc --cidr-block 10.0.1.0/24)"
+ec2_subnet_create_response="$(aws --output json ec2 create-subnet --vpc-id $ec2_vpc --cidr-block 10.0.1.0/24)"
 ec2_subnet=$(python -c "import json; print json.loads('''$ec2_subnet_create_response''')['Subnet']['SubnetId'];")
-#ec2_subnet=$(aws ec2 describe-subnets --query 'Subnets[0].SubnetId' --output text)
+#ec2_subnet=$(aws --output json ec2 describe-subnets --query 'Subnets[0].SubnetId' --output text)
 echo "created subnet: $ec2_subnet"
 
-aws ec2 modify-subnet-attribute --subnet-id $ec2_subnet --map-public-ip-on-launch
+aws --output json ec2 modify-subnet-attribute --subnet-id $ec2_subnet --map-public-ip-on-launch
 
 echo "\"subnet\":\"$ec2_subnet\"," >> $outputfile
 
 #echo create the security group
 #ec2_security_group_name="makeflow_ccl_sec_group_$time"
-#ec2_security_group_create_response="$(aws ec2 create-security-group --group-name $ec2_security_group_name --description "A Makeflow Security Group" --vpc-id $ec2_vpc)"
+#ec2_security_group_create_response="$(aws --output json ec2 create-security-group --group-name $ec2_security_group_name --description "A Makeflow Security Group" --vpc-id $ec2_vpc)"
 #ec2_security_group_id=$(python -c "import json; print json.loads('''$ec2_security_group_create_response''')['GroupId'];")
-ec2_security_group_id=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values=$ec2_vpc --query 'SecurityGroups[0].GroupId' --output text)
+ec2_security_group_id=$(aws --output json ec2 describe-security-groups --filters Name=vpc-id,Values=$ec2_vpc --query 'SecurityGroups[0].GroupId' --output text)
 echo "created security group: $ec2_security_group_id"
 
-aws ec2 authorize-security-group-ingress --group-id $ec2_security_group_id --protocol all --port all
+aws --output json ec2 authorize-security-group-ingress --group-id $ec2_security_group_id --protocol all --port all
 
 echo "\"sec_group\":\"$ec2_security_group_id\"," >> $outputfile
 
 #create an internet gateway
-ec2_gateway=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
+ec2_gateway=$(aws --output json ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
 echo "created gateway: $ec2_gateway"
 
-aws ec2 attach-internet-gateway --vpc-id $ec2_vpc --internet-gateway-id $ec2_gateway
+aws --output json ec2 attach-internet-gateway --vpc-id $ec2_vpc --internet-gateway-id $ec2_gateway
 
-route_table=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=$ec2_vpc --query 'RouteTables[0].RouteTableId' --output text)
+route_table=$(aws --output json ec2 describe-route-tables --filters Name=vpc-id,Values=$ec2_vpc --query 'RouteTables[0].RouteTableId' --output text)
 echo "This is the route table: $route_table"
-should_be_true_json=$(aws ec2 create-route --route-table-id $route_table --destination-cidr-block 0.0.0.0/0 --gateway-id $ec2_gateway)
+should_be_true_json=$(aws --output json ec2 create-route --route-table-id $route_table --destination-cidr-block 0.0.0.0/0 --gateway-id $ec2_gateway)
 
 echo "\"gateway\":\"$ec2_gateway\"," >> $outputfile
 
 echo "creating the environment"
 env_name="makeflow_ccl_env_$time"
-account_num_id=$(aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text) #got from stack overflow: "Quick Way to get AWS Account number from the cli tools?
-env_output_response=$(aws batch create-compute-environment --compute-environment-name $env_name --type MANAGED --state ENABLED --compute-resources type=EC2,minvCpus=$min_cpus,maxvCpus=$max_cpus,desiredvCpus=$cpus,instanceTypes=optimal,subnets=$ec2_subnet,securityGroupIds=$ec2_security_group_id,instanceRole=ecsInstanceRole --service-role=arn:aws:iam::$account_num_id:role/service-role/AWSBatchServiceRole)
+account_num_id=$(aws --output json ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text) #got from stack overflow: "Quick Way to get AWS Account number from the cli tools?
+env_output_response=$(aws --output json batch create-compute-environment --compute-environment-name $env_name --type MANAGED --state ENABLED --compute-resources type=EC2,minvCpus=$min_cpus,maxvCpus=$max_cpus,desiredvCpus=$cpus,instanceTypes=optimal,subnets=$ec2_subnet,securityGroupIds=$ec2_security_group_id,instanceRole=ecsInstanceRole --service-role=arn:aws:iam::$account_num_id:role/service-role/AWSBatchServiceRole)
 #echo $batch_compenv_create_response
 
-env_done_check_output="$(aws batch describe-compute-environments --compute-environments $env_name)"
+env_done_check_output="$(aws --output json batch describe-compute-environments --compute-environments $env_name)"
 env_done=$(python -c "import json; print json.loads('''$env_done_check_output''')['computeEnvironments'][0]['status'] == 'VALID'")
 while [ "$env_done" != "True" ]
 do
-env_done_check_output="$(aws batch describe-compute-environments --compute-environments $env_name)"
+env_done_check_output="$(aws --output json batch describe-compute-environments --compute-environments $env_name)"
 env_done=$(python -c "import json; print json.loads('''$env_done_check_output''')['computeEnvironments'][0]['status'] == 'VALID'")
 done
 
@@ -131,14 +171,14 @@ echo "\"env_name\":\"$env_name\"," >> $outputfile
 
 #echo creating the queue
 queue_name="makeflow_ccl_queue_$time"
-batch_queue_create_response="$(aws --region=$aws_reg batch create-job-queue --state=ENABLED --priority=1 --job-queue-name=$queue_name --compute-environment-order order=1,computeEnvironment=$env_name)"
+batch_queue_create_response="$(aws --output json --region=$aws_reg batch create-job-queue --state=ENABLED --priority=1 --job-queue-name=$queue_name --compute-environment-order order=1,computeEnvironment=$env_name)"
 echo $batch_queue_create_response
 
-queue_done_check_output="$(aws batch describe-job-queues --job-queues $queue_name)"
+queue_done_check_output="$(aws --output json batch describe-job-queues --job-queues $queue_name)"
 queue_done=$(python -c "import json; print json.loads('''$queue_done_check_output''')['jobQueues'][0]['status'] == 'VALID'")
 while [ "$queue_done" != "True" ]
 do
-queue_done_check_output="$(aws batch describe-job-queues --job-queues $queue_name)"
+queue_done_check_output="$(aws --output json batch describe-job-queues --job-queues $queue_name)"
 queue_done=$(python -c "import json; print json.loads('''$queue_done_check_output''')['jobQueues'][0]['status'] == 'VALID'")
 done
 
@@ -149,7 +189,7 @@ echo "\"queue_name\":\"$queue_name\"," >> $outputfile
 bucket_pre=$(python -c "print ''.join([chr(ord('a')+int(x)) for x in '$time'])")
 echo "created bucket: ccl$bucket_pre" 
 bucket_name="ccl$bucket_pre"
-make_bucket_response="$(aws s3 mb s3://$bucket_name)"
+make_bucket_response="$(aws --output json s3 mb s3://$bucket_name)"
 
 echo "\"bucket\":\"$bucket_name\"" >> $outputfile
 
