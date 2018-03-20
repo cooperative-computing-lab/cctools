67674afc09694cd77b1a1334753efcba287d0cf0
Nick Hazekamp <nhazekam@nd.edu>
2018-03-20 16:29:49 -0400

    Use escaped cmd

--- makeflow/src/makeflow_module_singularity.c
+++ makeflow/src/makeflow_module_singularity.c
@@ -52,7 +52,7 @@ static int node_submit(struct dag_node *n, struct batch_task *t){
 
 	/* Assumes a /disk dir in the image. */
 	char *task_cmd = string_escape_shell(t->command);
-	char *cmd = string_format("singularity exec --home $(pwd) %s %s sh -c %s", singularity_opt, singularity_image, t->command);
+	char *cmd = string_format("singularity exec --home $(pwd) %s %s sh -c %s", singularity_opt, singularity_image, task_cmd);
 	free(task_cmd);
 	batch_wrapper_cmd(wrapper, cmd);
 	free(cmd);
