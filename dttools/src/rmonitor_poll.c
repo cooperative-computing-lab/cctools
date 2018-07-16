9ed0dbeea45f2e711de331f1671641b90f27cc10
Ben Tovar <btovar@nd.edu>
2018-07-16 08:57:37 -0400

    fix comments

--- dttools/src/rmonitor_poll.c
+++ dttools/src/rmonitor_poll.c
@@ -286,7 +286,7 @@ int rmonitor_get_start_time(pid_t pid, uint64_t *start_time)
 
 int rmonitor_get_cpu_time_usage(pid_t pid, struct rmonitor_cpu_time_info *cpu)
 {
-	/* /dev/proc/[pid]/stat */
+	/* /proc/[pid]/stat */
 
 	uint64_t kernel, user;
 
@@ -339,7 +339,7 @@ int rmonitor_get_loadavg(struct rmonitor_load_info *load)
 
 int rmonitor_get_mem_usage(pid_t pid, struct rmonitor_mem_info *mem)
 {
-	// /dev/proc/[pid]/status:
+	// /proc/[pid]/status:
 
 	FILE *fmem = open_proc_file(pid, "status");
 	if(!fmem)
@@ -431,7 +431,7 @@ static double rmonitor_mem_info_priority(void *item) {
 
 int rmonitor_get_mmaps_usage(pid_t pid, struct hash_table *maps)
 {
-	// /dev/proc/[pid]/smaps:
+	// /proc/[pid]/smaps:
 
 	FILE *fmem = open_proc_file(pid, "smaps");
 	if(!fmem)
@@ -642,7 +642,7 @@ void acc_sys_io_usage(struct rmonitor_io_info *acc, struct rmonitor_io_info *oth
 /* We compute the resident memory changes from mmap files. */
 int rmonitor_get_map_io_usage(pid_t pid, struct rmonitor_io_info *io)
 {
-	/* /dev/proc/[pid]/smaps */
+	/* /proc/[pid]/smaps */
 
 	uint64_t kbytes_resident_accum;
 	uint64_t kbytes_resident;
@@ -729,7 +729,7 @@ void acc_wd_usage(struct rmonitor_wdir_info *acc, struct rmonitor_wdir_info *oth
 
 char *rmonitor_get_command_line(pid_t pid)
 {
-	/* /dev/proc/[pid]/cmdline */
+	/* /proc/[pid]/cmdline */
 
 	FILE *fline = open_proc_file(pid, "cmdline");
 	if(!fline)
