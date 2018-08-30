c3f7efaab5fe3cc897416a8f0e883dd61cda1510
Tim Shaffer <tshaffe1@nd.edu>
2018-08-30 10:30:09 -0400

    Fill in functionality for reading ext images

--- parrot/src/pfs_service_ext.cc
+++ parrot/src/pfs_service_ext.cc
@@ -8,6 +8,7 @@ See the file COPYING for details.
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
@@ -36,10 +37,122 @@ extern "C" {
 	#include "xxmalloc.h"
 }
 
+#define LOOKUP_INODE(name, inode, follow, fail) { \
+	errcode_t rc; \
+	if (follow) { \
+		rc = ext2fs_namei_follow(fs, EXT2_ROOT_INO, EXT2_ROOT_INO, name, inode); \
+	} else { \
+		rc = ext2fs_namei(fs, EXT2_ROOT_INO, EXT2_ROOT_INO, name, inode); \
+	} \
+	if (rc == 0) { \
+		debug(D_EXT, "lookup %s -> inode %d", name, *inode); \
+	} else { \
+		debug(D_EXT, "lookup %s failed: %s", name, error_message(rc)); \
+		errno = fix_errno(rc); \
+		return fail; \
+	} \
+} while (false)
+
+#define READ_INODE(inode, buf, fail) { \
+	errcode_t rc = ext2fs_read_inode(fs, inode, buf); \
+	if (rc == 0) { \
+		debug(D_EXT, "read inode %d", inode); \
+	} else { \
+		debug(D_EXT, "read inode %d failed: %s", inode, error_message(rc)); \
+		return fail; \
+	} \
+} while (false)
+
+#define OPEN_FILE(inode, file, fail) { \
+	errcode_t rc = ext2fs_file_open(fs, inode, 0, file); \
+	if (rc == 0) { \
+		debug(D_EXT, "open inode %d -> file %p", inode, *file); \
+	} else { \
+		debug(D_EXT, "open inode %d failed: %s", inode, error_message(rc)); \
+		return fail; \
+	} \
+} while (false)
+
+#define CLOSE_FILE(file, fail) { \
+	errcode_t rc = ext2fs_file_close(file); \
+	if (rc == 0) { \
+		debug(D_EXT, "close file %p", file); \
+	} else { \
+		debug(D_EXT, "close file %p failed: %s", file, error_message(rc)); \
+		return fail; \
+	} \
+} while (false)
+
+static int fix_errno(errcode_t rc) {
+	if (((rc>>8) & ((1<<24) - 1)) == 0) {
+		// literal errno value
+		return rc;
+	}
+	// very rough translation
+	switch (rc) {
+		case 0: return 0;
+		case EXT2_ET_RO_FILSYS: return EROFS;
+		case EXT2_ET_SYMLINK_LOOP: return ELOOP;
+		case EXT2_ET_NO_MEMORY: return ENOMEM;
+		case EXT2_ET_UNSUPP_FEATURE: return ENOSYS;
+		case EXT2_ET_RO_UNSUPP_FEATURE: return ENOSYS;
+		case EXT2_ET_INVALID_ARGUMENT: return EINVAL;
+		case EXT2_ET_NO_DIRECTORY: return ENOENT;
+		case EXT2_ET_TOO_MANY_REFS: return EMLINK;
+		case EXT2_ET_FILE_NOT_FOUND: return ENOENT;
+		case EXT2_ET_FILE_RO: return EROFS;
+		case EXT2_ET_DIR_EXISTS: return EEXIST;
+		case EXT2_ET_UNIMPLEMENTED: return ENOSYS;
+		case EXT2_ET_FILE_EXISTS: return EEXIST;
+		case EXT2_ET_FILE_TOO_BIG: return EFBIG;
+		default: return EINVAL;
+	}
+}
+
+static void inode2stat(ext2_ino_t i, struct ext2_inode *b, struct pfs_stat *p) {
+	assert(i);
+	assert(p);
+
+	memset(p, 0, sizeof(*p));
+	p->st_dev = (dev_t) -1;
+	p->st_ino = i;
+	p->st_mode = b->i_mode;
+	p->st_uid = b->i_uid;
+	p->st_gid = b->i_gid;
+	p->st_size = b->i_size;
+	p->st_nlink = b->i_links_count;
+	p->st_blksize = 65336;
+	p->st_blocks = b->i_blocks;
+	p->st_atime = b->i_atime;
+	p->st_ctime = b->i_ctime;
+	p->st_mtime = b->i_mtime;
+}
+
+static int append_dirents(struct ext2_dir_entry *dirent, int offset, int blocksize, char *buf, void *p) {
+	char name[PATH_MAX];
+
+	assert(dirent);
+	assert(p);
+
+	pfs_dir *d = (pfs_dir *) p;
+	snprintf(name, dirent->name_len, "%s", dirent->name);
+	d->append(name);
+
+	return 0;
+}
+
 class pfs_file_ext : public pfs_file {
+private:
+	ext2_ino_t inode;
+	ext2_filsys fs;
+
 public:
-	pfs_file_ext(pfs_name *name) : pfs_file(name) {
-		last_offset = 0;
+	pfs_file_ext(pfs_name *name, ext2_ino_t inode, ext2_filsys fs)
+			: pfs_file(name)
+			, inode(inode)
+			, fs(fs) {
+		assert(name);
+		debug(D_EXT, "open %s (inode %d) -> %p", name->rest, inode, this);
 	}
 
 	virtual int canbenative(char *path, size_t len) {
@@ -47,82 +160,64 @@ public:
 	}
 
 	virtual int close() {
-		return -1;
+		debug(D_EXT, "close %p", this);
+		// not holding any resources open, so noop here
+		return 0;
 	}
 
 	virtual pfs_ssize_t read(void *data, pfs_size_t length, pfs_off_t offset) {
-		return -1;
-	}
+		ext2_file_t file;
+		unsigned size;
 
-	virtual pfs_ssize_t write(const void *data, pfs_size_t length, pfs_off_t offset) {
-		return -1;
+		assert(data);
+
+		debug(D_EXT, "read %" PRIi64 "B from %p at %" PRIi64, length, this, offset);
+		OPEN_FILE(inode, &file, -1);
+		errcode_t rc = ext2fs_file_llseek(file, offset, EXT2_SEEK_SET, NULL);
+		if (rc != 0) {
+			debug(D_EXT, "failed to seek to %" PRIi64 " in %p: %s", offset, this, error_message(rc));
+			errno = fix_errno(rc);
+			return -1;
+		}
+
+		rc = ext2fs_file_read(file, data, length, &size);
+		if (rc == 0) {
+			debug(D_EXT, "read %u/%" PRIi64 " bytes from file %p", size, length, file);
+		} else {
+			debug(D_EXT, "read file %p failed: %s", file, error_message(rc));
+			CLOSE_FILE(file, -1);
+			return -1;
+		}
+
+		CLOSE_FILE(file, -1);
+
+		return size;
+
 	}
 
 	virtual int fstat(struct pfs_stat *buf) {
-		return -1;
+		struct ext2_inode inode_buf;
+
+		assert(buf);
+
+		debug(D_EXT, "fstat %p", this);
+		READ_INODE(inode, &inode_buf, -1);
+		inode2stat(inode, &inode_buf, buf);
+
+		return 0;
 	}
 
 	virtual int fstatfs(struct pfs_statfs *buf) {
-		return -1;
-	}
+		assert(buf);
 
-	virtual int ftruncate(pfs_size_t length) {
-		return -1;
-	}
+		debug(D_EXT, "fstatfs %p", this);
+		pfs_service_emulate_statfs(buf);
 
-	virtual int fsync() {
-		return -1;
+		return 0;
 	}
-
-	virtual int fcntl(int cmd, void *arg) {
-		return -1;
-	}
-
-	virtual int fchmod(mode_t mode) {
-		return -1;
-	}
-
-	virtual int fchown(uid_t uid, gid_t gid) {
-		return -1;
-	}
-
-#if defined(HAS_SYS_XATTR_H) || defined(HAS_ATTR_XATTR_H)
-	virtual ssize_t fgetxattr(const char *name, void *data, size_t size) {
-		return -1;
-	}
-
-	virtual ssize_t flistxattr(char *list, size_t size) {
-		return -1;
-	}
-
-	virtual int fsetxattr(const char *name, const void *data, size_t size, int flags) {
-		return -1;
-	}
-
-	virtual int fremovexattr(const char *name) {
-		return -1;
-	}
-
-#endif
 
 	virtual int flock(int op) {
-		return -1;
-	}
-
-	virtual void *mmap() {
-		return NULL;
-	}
-
-	virtual pfs_ssize_t get_size() {
-		return -1;
-	}
-
-	virtual int get_real_fd() {
-		return -1;
-	}
-
-	virtual int get_local_name(char *n) {
-		strcpy(n, name.rest);
+		// just noop this
 		return 0;
 	}
 
@@ -137,9 +232,9 @@ private:
 	ext2_filsys fs;
 
 public:
-	pfs_service_ext(ext2_filsys handle, const char *img) {
+	pfs_service_ext(ext2_filsys fs, const char *img)
+			: fs(fs) {
 		image = strdup(img);
-		fs = handle;
 	}
 
 	~pfs_service_ext() {
@@ -152,79 +247,144 @@ public:
 	}
 
 	virtual pfs_file *open(pfs_name *name, int flags, mode_t mode) {
+		ext2_ino_t inode;
+		struct ext2_inode inode_buf;
+
 		assert(name);
+		if (mode&(O_WRONLY|O_RDWR)) {
+			errno = EROFS;
+			return NULL;
+		}
+
 		debug(D_EXT, "open %s %d %d", name->rest, flags, mode);
-		return NULL;
+		LOOKUP_INODE(name->rest, &inode, !(flags&O_NOFOLLOW), NULL);
+		READ_INODE(inode, &inode_buf, NULL);
+		if (S_ISLNK(inode_buf.i_mode)) {
+			errno = ELOOP;
+			return NULL;
+		}
+
+		pfs_file_ext *result = new pfs_file_ext(name, inode, fs);
+		debug(D_EXT, "open %s in image %s -> %p", name->rest, image, result);
+		return result;
 	}
 
 	virtual pfs_dir *getdir(pfs_name *name) {
-		assert(name);
-		debug(D_EXT, "getdir %s", name->rest);
-		return NULL;
-	}
+		ext2_ino_t inode;
+		struct ext2_inode inode_buf;
 
-	virtual int stat(pfs_name *name, struct pfs_stat *buf) {
 		assert(name);
-		assert(buf);
-		debug(D_EXT, "stat %s", name->rest);
-		return -1;
+
+		debug(D_EXT, "getdir %s", name->rest);
+		LOOKUP_INODE(name->rest, &inode, true, NULL);
+		READ_INODE(inode, &inode_buf, NULL);
+		if (!S_ISDIR(inode_buf.i_mode)) {
+			errno = ENOTDIR;
+			return NULL;
+		}
+		pfs_dir *result = new pfs_dir(name);
+		ext2fs_dir_iterate(fs, inode, 0, NULL, append_dirents, result);
+
+		return result;
 	}
 
 	virtual int statfs(pfs_name *name, struct pfs_statfs *buf) {
+		ext2_ino_t inode;
+
 		assert(name);
 		assert(buf);
+
 		debug(D_EXT, "statfs %s", name->rest);
-		return -1;
+		LOOKUP_INODE(name->rest, &inode, true, -1);
+		pfs_service_emulate_statfs(buf);
+
+		return 0;
+	}
+
+	virtual int stat(pfs_name *name, struct pfs_stat *buf) {
+		ext2_ino_t inode;
+		struct ext2_inode inode_buf;
+
+		assert(name);
+		assert(buf);
+
+		debug(D_EXT, "stat %s", name->rest);
+		LOOKUP_INODE(name->rest, &inode, true, -1);
+		READ_INODE(inode, &inode_buf, -1);
+		inode2stat(inode, &inode_buf, buf);
+
+		return 0;
 	}
 
 	virtual int lstat(pfs_name *name, struct pfs_stat *buf) {
+		ext2_ino_t inode;
+		struct ext2_inode inode_buf;
+
 		assert(name);
 		assert(buf);
+
 		debug(D_EXT, "lstat %s", name->rest);
-		return -1;
+		LOOKUP_INODE(name->rest, &inode, false, -1);
+		READ_INODE(inode, &inode_buf, -1);
+		inode2stat(inode, &inode_buf, buf);
+
+		return 0;
 	}
 
 	virtual int access(pfs_name *name, mode_t mode) {
+		ext2_ino_t inode;
+
 		assert(name);
+
 		debug(D_EXT, "access %s %d", name->rest, mode);
-		return -1;
+		LOOKUP_INODE(name->rest, &inode, true, -1);
+
+		// we don't do permission checks, so just go ahead
+		return 0;
 	}
 
 	virtual int readlink(pfs_name *name, char *buf, pfs_size_t bufsiz) {
+		ext2_ino_t inode;
+		ext2_file_t file;
+		struct ext2_inode inode_buf;
+		unsigned size;
+
 		assert(name);
 		assert(buf);
+
 		debug(D_EXT, "readlink %s", name->rest);
-		return -1;
-	}
+		LOOKUP_INODE(name->rest, &inode, false, -1);
 
-	virtual ssize_t getxattr(pfs_name *name, const char *attrname, void *data, size_t size) {
-		assert(name);
-		assert(attrname);
-		assert(data);
-		debug(D_EXT, "getxattr %s %s", name->rest, attrname);
-		return -1;
-	}
+		errcode_t rc = ext2fs_read_inode(fs, inode, &inode_buf);
+		if (rc == 0) {
+			debug(D_EXT, "read full inode %d", inode);
+		} else {
+			debug(D_EXT, "read full inode %d failed: %s", inode, error_message(rc));
+			return -1;
+		}
 
-	virtual ssize_t lgetxattr(pfs_name *name, const char *attrname, void *data, size_t size) {
-		assert(name);
-		assert(attrname);
-		assert(data);
-		debug(D_EXT, "lgetxattr %s %s", name->rest, attrname);
-		return -1;
-	}
+		if (!S_ISLNK(inode_buf.i_mode)) {
+			errno = EINVAL;
+			return -1;
+		}
+		OPEN_FILE(inode, &file, -1);
 
-	virtual ssize_t listxattr(pfs_name *name, char *list, size_t size) {
-		assert(name);
-		assert(list);
-		debug(D_EXT, "listxattr %s", name->rest);
-		return -1;
-	}
+		rc = ext2fs_file_read(file, buf, bufsiz, &size);
+		if (rc == 0) {
+			debug(D_EXT, "read %u/%" PRIu64 " bytes from file %p", size, bufsiz, file);
+		} else if (rc == EXT2_ET_SHORT_READ) {
+			debug(D_EXT, "short read on %p, inline link", file);
+			size = inode_buf.i_size;
+			memcpy(buf, inode_buf.i_block, size);
+		} else {
+			debug(D_EXT, "read file %p failed: %s", file, error_message(rc));
+			CLOSE_FILE(file, -1);
+			return -1;
+		}
 
-	virtual ssize_t llistxattr(pfs_name *name, char *list, size_t size) {
-		assert(name);
-		assert(list);
-		debug(D_EXT, "llistxattr %s", name->rest);
-		return -1;
+		CLOSE_FILE(file, -1);
+
+		return size;
 	}
 
 	virtual int is_seekable() {
