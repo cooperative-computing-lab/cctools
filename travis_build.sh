d8c25e5131f69b5933b2fe3099d5f49637cb56f8
Tim Shaffer <tshaffe1@nd.edu>
2018-09-19 14:30:17 -0400

    Don't try to use VC3 stuff outside of Docker images

--- travis_build.sh
+++ travis_build.sh
@@ -2,15 +2,13 @@
 
 set -ex
 
-COMMAND="./configure --strict --with-cvmfs-path /opt/libcvmfs --with-uuid-path /opt/uuid && make && make test"
-
 if [ -z "$DOCKER_IMAGE" ]; then
-    eval "$COMMAND"
+    ./configure --strict && make && make test
 else
     docker run \
         --privileged \
         --ulimit nofile=65536 \
         -v "$(pwd):/root" -w '/root' \
         "$DOCKER_IMAGE" \
-        /bin/sh -c "$COMMAND"
+        /bin/sh -c "./configure --strict --with-cvmfs-path /opt/libcvmfs --with-uuid-path /opt/uuid && make && make test"
 fi
