5808fe6f16470d19a2ac501c65bb76f86fe9bcdc
Tim Shaffer <tshaffe1@nd.edu>
2018-09-26 14:56:19 -0400

    Try releases

--- travis_build.sh
+++ travis_build.sh
@@ -5,10 +5,14 @@ set -ex
 if [ -z "$DOCKER_IMAGE" ]; then
     ./configure --strict && make && make test
 else
+    D=/tmp/cctools-${TRAVIS_COMMIT:0:8}-${DOCKER_IMAGE#cclnd/cctools-env:}
     docker run \
         --privileged \
         --ulimit nofile=65536 \
-        -v "$(pwd):/root" -w '/root' \
+        -v "$(pwd):/root" \
+        -v /tmp:/tmp \
+        -w '/root' \
         "$DOCKER_IMAGE" \
-        /bin/sh -c "./configure --strict --with-cvmfs-path /opt/libcvmfs --with-uuid-path /opt/uuid && make && make test"
+        /bin/sh -c "./configure --strict --prefix $D --with-cvmfs-path /opt/libcvmfs --with-uuid-path /opt/uuid && make install && make test"
+    tar -cz -C $(dirname $D) -f $D.tar.gz $(basename $D)
 fi
