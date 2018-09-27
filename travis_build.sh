e6d806b4ad367f786d5fac815a0fb935bb26396a
Tim Shaffer <tshaffe1@nd.edu>
2018-09-27 12:11:54 -0400

    Build tarball in Docker images

--- travis_build.sh
+++ travis_build.sh
@@ -2,13 +2,19 @@
 
 set -ex
 
+BUILD_ID="$(basename ${TRAVIS_TAG:-${TRAVIS_COMMIT:0:8}})"
+D="/tmp/cctools-$BUILD_ID-${DOCKER_IMAGE#cclnd/cctools-env:}"
+
 if [ -z "$DOCKER_IMAGE" ]; then
     ./configure --strict && make && make test
 else
     docker run \
         --privileged \
         --ulimit nofile=65536 \
-        -v "$(pwd):/root" -w '/root' \
+        -v "$(pwd):/root" \
+        -v /tmp:/tmp \
+        -w '/root' \
         "$DOCKER_IMAGE" \
-        /bin/sh -c "./configure --strict --with-cvmfs-path /opt/libcvmfs --with-uuid-path /opt/uuid && make && make test"
+        /bin/sh -c "./configure --strict --prefix $D --with-cvmfs-path /opt/libcvmfs --with-uuid-path /opt/uuid && make install && make test"
+    tar -cz -C $(dirname $D) -f $D.tar.gz $(basename $D)
 fi
