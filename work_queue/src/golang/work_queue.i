36d171eea565cbe37a43ded3ac92deddfa3e8575
chao zheng <charleszheng44@gmail.com>
2018-12-12 20:18:01 +0800

    rename the function using camelcase, which follows the Golang convention

--- work_queue/src/golang/work_queue.i
+++ work_queue/src/golang/work_queue.i
@@ -1,1 +1,35 @@
-../work_queue.i
+/* work_queue.i */
+%module work_queue
+
+%{
+	#include "debug.h"
+	#include "int_sizes.h"
+	#include "timestamp.h"
+	#include "work_queue.h"
+	#include "rmsummary.h"
+%}
+
+%typemap(in) off_t = int;
+
+/*Function name to camelcase which follows Golang convention*/
+%rename("%(camelcase)s") "";
+
+/* vdebug() takes va_list as arg but SWIG can't wrap such functions. */
+%ignore vdebug;
+%ignore debug;
+
+/* These return pointers to lists defined in list.h. We aren't
+ * wrapping methods in list.h and so ignore these. */
+%ignore work_queue_cancel_all_tasks;
+%ignore input_files;
+%ignore output_files;
+
+%include "stdint.i"
+%include "debug.h"
+%include "int_sizes.h"
+%include "timestamp.h"
+%include "work_queue.h"
+%include "rmsummary.h"
+%include "category.h"
+
+
--- work_queue/src/golang/work_queue_example.go
+++ work_queue/src/golang/work_queue_example.go
@@ -9,16 +9,19 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		log.Println("work_queue_example <file1> [file2] [file3] ...")
-		log.Println("Each file given on the command line will be compressed using a remote worker.")
+		log.Println(`Each file given on the command line will be compressed 
+using a remote worker.`)
 		return
 	}
 
 	gzipPath := "/bin/gzip"
 	if _, err := os.Stat(gzipPath); os.IsNotExist(err) {
-		log.Fatal("gzip was not found. Please modify the gzip_path variable accordingly. To determine the location of gzip, from the terminal type: which gzip (usual locations are /bin/gzip and /usr/bin/gzip)")
+		log.Fatal(`gzip was not found. Please modify the gzip_path variable 
+accordingly. To determine the location of gzip, from the terminal type: 
+which gzip (usual locations are /bin/gzip and /usr/bin/gzip)`)
 	}
 
-	q := Work_queue_create(9123)
+	q := WorkQueueCreate(9123)
 	if q == nil {
 		log.Fatal("couldn't listen on port 9123\n")
 	}
@@ -29,24 +32,30 @@ func main() {
 		outFile := fmt.Sprintf("%s.gz", os.Args[i])
 		command := fmt.Sprintf("./gzip < %s > %s", inFile, outFile)
 
-		t := Work_queue_task_create(command)
-		Work_queue_task_specify_file(t, gzipPath, "gzip", Work_queue_file_type_t(WORK_QUEUE_INPUT), Work_queue_file_flags_t(WORK_QUEUE_CACHE))
-		Work_queue_task_specify_file(t, inFile, inFile, Work_queue_file_type_t(WORK_QUEUE_INPUT), Work_queue_file_flags_t(WORK_QUEUE_NOCACHE))
-		Work_queue_task_specify_file(t, outFile, outFile, Work_queue_file_type_t(WORK_QUEUE_INPUT), Work_queue_file_flags_t(WORK_QUEUE_NOCACHE))
+		t := WorkQueueTaskCreate(command)
+		WorkQueueTaskSpecifyFile(t, gzipPath, "gzip",
+			Work_queue_file_type_t(WORKQUEUEINPUT),
+			Work_queue_file_flags_t(WORKQUEUECACHE))
+		WorkQueueTaskSpecifyFile(t, inFile, inFile,
+			Work_queue_file_type_t(WORKQUEUEINPUT),
+			Work_queue_file_flags_t(WORKQUEUENOCACHE))
+		WorkQueueTaskSpecifyFile(t, outFile, outFile,
+			Work_queue_file_type_t(WORKQUEUEINPUT),
+			Work_queue_file_flags_t(WORKQUEUENOCACHE))
 
-		taskID := Work_queue_submit(q, t)
+		taskID := WorkQueueSubmit(q, t)
 		log.Printf("submitted task (id# %d)\n", taskID)
 	}
 
 	log.Println("waiting for tasks to complete...")
-	for Work_queue_empty(q) == 0 {
-		completeTask := Work_queue_wait(q, 5)
+	for WorkQueueEmpty(q) == 0 {
+		completeTask := WorkQueueWait(q, 5)
 		if completeTask != nil {
 			log.Println("a task complete")
-			Work_queue_task_delete(completeTask)
+			WorkQueueTaskDelete(completeTask)
 		}
 	}
 
 	log.Println("all tasks complete!")
-	Work_queue_delete(q)
+	WorkQueueDelete(q)
 }
