faa95fe80fb05e8a4cd262cfff46b81e5304040e
Douglas Thain <dthain@nd.edu>
2019-01-28 10:09:18 -0500

    Added program to upgrade log files from U records to M records.

---
+++ deltadb/src/deltadb_upgrade_log.c
@@ -0,0 +1,116 @@
+/*
+Copyright (C) 2015- The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file COPYING for details.
+*/
+
+/*
+A limited-use program to upgrade (still-valid) log files
+in two ways that save space:
+1 - Adjacent U records are combined into one M record.
+2 - T records are reduced to one per minute.
+*/
+
+
+#include "jx.h"
+#include "jx_print.h"
+#include "jx_parse.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define LOG_LINE_MAX 4096
+
+void corrupt_data( const char *line, int lineno )
+{
+	fprintf(stderr,"ABORT: corrupt data line %d: %s\n",lineno,line);
+	exit(1);
+}
+
+
+int main( int argc, char *argv[] )
+{
+	if(argc!=3) {
+		fprintf(stderr,"use: %s <infile> <outfile>\n",argv[0]);
+		return 1;
+	}
+
+	FILE *input = fopen(argv[1],"r");
+	if(!input) {
+		fprintf(stderr,"couldn't open %s: %s\n",argv[1],strerror(errno));
+		return 1;
+	}
+
+	FILE *output = fopen(argv[2],"w");
+	if(!input) {
+		fprintf(stderr,"couldn't open %s: %s\n",argv[2],strerror(errno));
+		return 1;
+	}
+
+	char line[LOG_LINE_MAX] = "";
+	char key[LOG_LINE_MAX] = "";
+	char name[LOG_LINE_MAX] = "";
+	char value[LOG_LINE_MAX] = "";
+
+	char lastop = 0;
+	char lastkey[LOG_LINE_MAX] = "";
+
+	struct jx *merge = 0;
+	int lineno = 0;
+	long time;
+	long lasttime = 0;
+	long time_granularity = 60;
+
+	while(fgets(line,sizeof(line),input)) {
+		lineno++;
+		int emit_merge = 0;
+
+		if(line[0]=='U') {
+			if(sscanf(line,"U %s %s %[^\n]",key,name,value)==3) {
+				if(lastop=='U' && !strcmp(key,lastkey)) {
+					struct jx *jvalue = jx_parse_string(value);
+					if(!merge) merge = jx_object(0);
+					jx_insert(merge,jx_string(name),jvalue);
+				} else {
+					emit_merge = 1;
+				}
+				strcpy(lastkey,key);
+			} else {
+				corrupt_data(line,lineno);
+			}
+		} else {
+			emit_merge = 1;
+		}
+
+		if(emit_merge && merge) {
+			char *str = jx_print_string(merge);
+			fprintf(output,"M %s %s\n",key,str);
+			free(str);
+			jx_delete(merge);
+			merge = 0;
+		}
+
+		if(line[0]=='T') {
+			if(sscanf(line,"T %ld",&time)) {
+				if((time-lasttime)>time_granularity) {
+					fputs(line,output);
+					lasttime = time;
+				}
+			} else {
+				corrupt_data(line,lineno);
+			}
+		} else if(line[0]!='U') {
+			fputs(line,output);
+		}
+
+		lastop = line[0];
+	}
+
+	fclose(input);
+	fclose(output);
+
+	return 0;
+}
+
