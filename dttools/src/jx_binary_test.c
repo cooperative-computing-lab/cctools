cb41572852aa1681cefdc9b691af96d2faa09bc8
Douglas Thain <dthain@nd.edu>
2019-07-19 17:43:54 -0400

    JX Binary Representation (#2099)
    * First pass at JX binary encoding.
    
    * Use jx_string_nocopy to avoid an extra malloc/copy/free.
    
    * Make internal functions static.
    Remove unused encoding functions.
    Handle unexpected input types.
    
    * Added test program.
    
    * Fix bug -- objects not returned on read!
    
    * Boilerplate.
    
    * Implement integers 0/8/16/32/64 and strings 8/16/32.
    
    * Remove redundant breaks.
    
    * Clean up read/write integer routines to call jx_binary_read/write_data
    
    * String types have unsigned length.
    
    * Object types are unsigned.
    
    * Remove debug output.
    
    * malloc.h -> stdlib.h for portability
    
    * Cast printf format.
    
    * Doxygen.

---
+++ dttools/src/jx_binary_test.c
@@ -0,0 +1,69 @@
+/*
+Copyright (C) 2019- The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file COPYING for details.
+*/
+
+#include "jx_binary.h"
+#include "jx_parse.h"
+#include "jx_print.h"
+
+#include "timestamp.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#define TIMEIT( name, xxx )\
+{ \
+timestamp_t start = timestamp_get(); \
+xxx \
+timestamp_t end = timestamp_get(); \
+printf( "%s %lu us\n",name,(unsigned long)(end-start));	\
+}
+
+
+int main( int argc, char *argv[] )
+{
+	if(argc!=4) {
+		fprintf(stderr,"use: %s <source-text> <binary-file> <text-out>\n",argv[0]);
+		return 1;
+	}
+
+	FILE *textfile = fopen(argv[1],"r");
+	if(!textfile) {
+		fprintf(stderr,"couldn't open %s: %s\n",argv[1],strerror(errno));
+		return 1;
+	}
+
+	FILE *binaryfile = fopen(argv[2],"w");
+	if(!binaryfile) {
+		fprintf(stderr,"couldn't open %s: %s\n",argv[2],strerror(errno));
+		return 1;
+	}
+
+	FILE *textout = fopen(argv[3],"w");
+	if(!textout) {
+		fprintf(stderr,"couldn't open %s: %s\n",argv[3],strerror(errno));
+		return 1;
+	}
+
+
+	struct jx *j;
+	TIMEIT( "text    read", j = jx_parse_stream(textfile); fclose(textfile); )
+
+	TIMEIT( "binary write", jx_binary_write(binaryfile,j); fclose(binaryfile); )
+
+	jx_delete(j);
+
+	binaryfile = fopen(argv[2],"r");
+	if(!binaryfile) {
+		fprintf(stderr,"couldn't open %s: %s\n",argv[2],strerror(errno));
+		return 1;
+	}
+
+	TIMEIT( "binary read", j = jx_binary_read(binaryfile); fclose(binaryfile); )
+
+	TIMEIT( "text  write", jx_print_stream(j,textout); fclose(textout); )
+	return 0;
+}
