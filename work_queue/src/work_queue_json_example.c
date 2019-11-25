c610c48e9032a28441f2094a3a42b688f0d3b17e
Cami Carballo <ccarball@nd.edu>
2019-11-25 14:19:42 -0500

    create work_queue_json.h and example program

---
+++ work_queue/src/work_queue_json_example.c
@@ -0,0 +1,76 @@
+#include "work_queue_json.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+char * workqueue = "{ \"name\" : \"cami_wq\" , \"port\" : 1234 }";
+
+int main(int argc, char* argv[]){
+
+    struct work_queue *q;
+    struct work_queue_task *t;
+    int taskid;
+    char *task;
+    size_t len = 0;
+    ssize_t read;
+
+    if(argc < 2){
+        printf("./example <tasks_json>\n");
+        return 0;
+    }
+
+    q = work_queue_json_create(workqueue);
+    if(!q){
+        return 1;
+    }
+
+    /* read from tasks file and create a task for each line */
+    char * filename = argv[1];
+    FILE *fp = fopen(filename, "r");
+    if(!fp){
+        printf("cannot open file: %s\n", filename);
+        return 1;
+    }
+
+    while ((read = getline(&task, &len, fp)) != -1) {
+
+    	taskid = work_queue_json_submit(q, task);
+
+        if(taskid < 0){
+            return 1;
+        }
+    
+    	printf("submitted task (id# %d)\n", taskid);
+    
+    }     
+
+    fclose(fp);
+
+	printf("waiting for tasks to complete...\n");
+
+	while(!work_queue_empty(q)) {
+
+		t = work_queue_wait(q, 20);
+
+		if(t) {
+			printf("task (id# %d) complete: %s (return code %d)\n", t->taskid, t->command_line, t->return_status);
+
+            if(t->return_status != 0){
+                
+            }
+
+			work_queue_task_delete(t);
+		}
+
+	}
+
+	printf("all tasks complete!\n");
+
+	work_queue_delete(q);
+
+    return 0;
+
+}
