aba2373c94a00636f8337b16f371950e35c12b13
camicarballo <ccarball@nd.edu>
2019-12-02 10:58:57 -0500

    add function description and JSON task interface examples

--- work_queue/src/work_queue_json.c
+++ work_queue/src/work_queue_json.c
@@ -131,10 +131,7 @@ static int specify_files(int input, struct jx *files, struct work_queue_task *ta
 }
 
 
-/* WQTCREATE
- * takes in a JSON string and returns a work_queue struct
- */
-struct work_queue_task* work_queue_task_json_create(const char* str){
+static struct work_queue_task* create_task(const char* str){
 
     char *command_line;
     struct jx *input_files, *output_files;
@@ -200,8 +197,12 @@ struct work_queue_task* work_queue_task_json_create(const char* str){
 
 }
 
-/* WQCREATE
- * takes in a JSON string and returns a work_queue struct
+/** Create a new work_queue object.
+@param port The port number to listen on. If zero is specified, then the 
+port stored in <b>WORK_QUEUE_PORT</b> is used if available. If it isn't, or 
+-1 is specified, the first unused port between <b>WORK_QUEUE_LOW_PORT</b> 
+and <b>WORK_QUEUE_HIGH_PORT</b> (1024 and 32767 by default) is chosen.
+@return A new work queue, or null if it could not be created. 
  */
 struct work_queue* work_queue_json_create(const char* str){
 
@@ -267,14 +268,26 @@ struct work_queue* work_queue_json_create(const char* str){
 
 }
 
-/* SUBMIT
- *
- */
+/** Submit a task to a queue.
+Once a task is submitted to a queue, it is not longer under the user's 
+control and should not be inspected until returned via @ref work_queue_wait.
+Once returned, it is safe to re-submit the same take object via 
+@ref work_queue_submit.
+@param q A work queue object.
+@param str A JSON description of a task. 
+
+{ "command_line" : "<i>string</i>" , "output_files" : <i>array of objects with one object per output file</i> -> 
+[ { "local" : "<i>string</i>" , "remote" : "<i>string</i>" , "flags" : "<i>integer</i>" } ] , "input _files" : 
+<i>array of objects with one object per input file</i> -> [ { "local" : "<i>string</i>" , "remote" : 
+"<i>string</i>" , "flags" : "<i>integer</i>" } ] , "tag" : "<i>string</i>" }
+
+@return An integer taskid assigned to the submitted task.
+*/
 int work_queue_json_submit(struct work_queue *q, const char* str){
 
     struct work_queue_task * task;
 
-    task = work_queue_task_json_create(str);
+    task = create_task(str);
 
     if(task){
         return work_queue_submit(q, task);
@@ -285,22 +298,41 @@ int work_queue_json_submit(struct work_queue *q, const char* str){
 
 }
 
-/* WAIT
- *
- */
+/** Wait for a task to complete.
+@param q A work queue object.
+@param timeout The number of seconds to wait for a completed task before 
+returning. Use an integer time to set the timeout or the constant 
+@ref WORK_QUEUE_WAITFORTASK to block until a task has completed.
+@return A JSON description of the completed task or the
+ timeout was reached without a completed task, or there is completed child 
+process (call @ref process_wait to retrieve the status of the completed 
+child process).
+
+{ "command_line" : "<i>string</i>" , "tag" : "<i>string</i>" , "output" : "<i>string</i>" , "taskid" : 
+<i>integer</i> , "return_status" : <i>integer</i> }
+
+*/
 char* work_queue_json_wait(struct work_queue *q, int timeout){
 
     char *task;
     struct jx *j;
-    struct jx_pair *command_line, *taskid, *return_status;
+    struct jx_pair *command_line, *taskid, *return_status, *tag, *output;
 
     struct work_queue_task *t = work_queue_wait(q, timeout);
 
     command_line = jx_pair(jx_string("command_line"), jx_string(t->command_line), NULL);
     taskid = jx_pair(jx_string("taskid"), jx_integer(t->taskid), command_line);
     return_status = jx_pair(jx_string("return_status"), jx_integer(t->return_status), taskid);
-    
-    j = jx_object(return_status);
+
+    if (t->tag){
+        tag = jx_pair(jx_string("tag"), jx_string(t->tag), return_status);
+        output = jx_pair(jx_string("output"), jx_string(t->output), tag);
+    }
+    else {
+        output = jx_pair(jx_string("output"), jx_string(t->output), return_status);
+    }
+
+    j = jx_object(output);
 
     task = jx_print_string(j);
 
--- work_queue/src/work_queue_json.h
+++ work_queue/src/work_queue_json.h
@@ -3,7 +3,6 @@
 
 #include "work_queue.h"
 
-struct work_queue_task* work_queue_task_json_create(const char* str);
 struct work_queue* work_queue_json_create(const char* str);
 int work_queue_json_submit(struct work_queue *q, const char* str);
 char* work_queue_json_wait(struct work_queue *q, int timeout);
