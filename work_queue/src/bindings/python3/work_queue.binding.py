0699f2879860ab143a851935b334fd16935b019b
Benjamin Tovar <btovar@nd.edu>
2020-02-18 09:07:31 -0500

    Python interface for wq factory #2253 adds @trshaffer python wq factory interface
    separate between config file options and command line only options
    
    correctly handle "_" in attributes
    
    update to linter suggestions

--- work_queue/src/bindings/python3/work_queue.binding.py
+++ work_queue/src/bindings/python3/work_queue.binding.py
@@ -13,6 +13,12 @@
 
 import copy
 import os
+import json
+import errno
+import tempfile
+import subprocess
+import distutils.spawn
+
 
 def set_debug_flag(*flags):
     for flag in flags:
@@ -1366,6 +1372,217 @@ class WorkQueue(object):
             del(self._task_table[task_pointer.taskid])
             return task
         return None
+
+
+class Factory(object):
+    """Launch a Work Queue factory.
+
+    The command line arguments for `work_queue_factory` can be set for a
+    factory object (with dashes replaced with underscores). Creating a factory
+    object does not immediately launch it, so this is a good time to configure
+    the resources, number of workers, etc. Factory objects function as Python
+    context managers, so to indicate that a set of commands should be run with
+    a factory running, wrap them in a `with` statement. The factory will be
+    cleaned up automtically at the end of the block. You can also make
+    config changes to the factory while it is running. As an example,
+
+        # normal WQ setup stuff
+        workers = work_queue.Factory("sge", "myproject") 
+        workers.cores = 4
+        with workers:
+            # submit some tasks
+            workers.max_workers = 300
+            # got a pile of tasks, allow more workers
+        # any additional cleanup steps on the master
+    """
+
+    _command_line_options = [
+        "amazon-config",
+        "autosize",
+        "batch-options",
+        "batch-type",
+        "capacity",
+        "catalog",
+        "condor-requirements",
+        "config-file",
+        "cores",
+        "debug",
+        "debug-file",
+        "debug-file-size",
+        "disk",
+        "env",
+        "extra-options",
+        "factory-timeout",
+        "foremen-name",
+        "gpus",
+        "k8s-image",
+        "k8s-worker-image",
+        "max-workers",
+        "master-name",
+        "memory",
+        "mesos-master",
+        "mesos-path",
+        "mesos-preload",
+        "min-workers",
+        "password",
+        "run-factory-as-master",
+        "runos",
+        "scratch-dir",
+        "tasks-per-worker",
+        "timeout",
+        "worker-binary",
+        "workers-per-cycle",
+        "wrapper",
+        "wrapper-input",
+    ]
+
+    # subset of command line options that can be written to the configuration
+    # file, and therefore they can be changed once the factory is running.
+    _config_file_options = [
+        "autosize",
+        "capacity",
+        "cores",
+        "disk",
+        "factory-timeout",
+        "foremen-name",
+        "master-name",
+        "max-workers",
+        "memory",
+        "min-workers",
+        "tasks-per-worker",
+        "timeout",
+        "workers-per-cycle",
+        "condor-requirements",
+    ]
+
+    def __init__(
+            self, batch_type, master_name,
+            factory_binary=None, worker_binary=None,
+            log_file=os.devnull):
+        """Create a factory with the given batch_type and master name.
+
+        If factory_binary or worker_binary is not
+        specified, $PATH will be searched.
+        """
+        self._config_file = None
+        self._factory_proc = None
+        self._log_file = log_file
+
+        self._opts = {}
+
+        self._opts['batch-type'] = batch_type
+        self._opts['master-name'] = master_name
+        self._opts['worker-binary'] = self._find_exe(worker_binary, 'work_queue_worker')
+
+        self._factory_binary = self._find_exe(factory_binary, 'work_queue_factory')
+
+
+    def _find_exe(self, path, default):
+        if path is None:
+            out = distutils.spawn.find_executable(default)
+        else:
+            out = path
+        if out is None or not os.access(out, os.F_OK):
+            raise OSError(
+                errno.ENOENT,
+                'Command not found',
+                out or default)
+        if not os.access(out, os.X_OK):
+            raise OSError(
+                errno.EPERM,
+                os.strerror(errno.EPERM),
+                out)
+        return os.path.abspath(out)
+
+
+    def __getattr__(self, name):
+        if name[0] == '_':
+            # For names that start with '_', immediately return the attribute.
+            # If the name does not start with '_' we assume is a factory option.
+            return object.__getattribute__(self, name)
+
+        # original command line options use - instead of _. _ is required by
+        # the naming conventions of python (otherwise - is taken as 'minus')
+        name_with_hyphens = name.replace('_', '-')
+
+        if name_with_hyphens in Factory._command_line_options:
+            try:
+                return object.__getattribute__(self, '_opts')[name_with_hyphens]
+            except KeyError:
+                raise KeyError("{} is a valid factory attribute, but has not been set yet.".format(name))
+        else:
+            raise AttributeError("{} is not a supported option".format(name))
+
+
+    def __setattr__(self, name, value):
+        # original command line options use - instead of _. _ is required by
+        # the naming conventions of python (otherwise - is taken as 'minus')
+        name_with_hyphens = name.replace('_', '-')
+
+        if name[0] == '_':
+            # For names that start with '_', immediately set the attribute.
+            # If the name does not start with '_' we assume is a factory option.
+            object.__setattr__(self, name, value)
+        elif self._factory_proc:
+            # if factory is already running, only accept attributes that can
+            # changed dynamically
+            if name_with_hyphens in Factory._config_file_options:
+                self._opts[name_with_hyphens] = value
+                self._write_config()
+            elif name_with_hyphens in Factory._command_line_options:
+                raise AttributeError('{} cannot be changed once the factory is running.'.format(name))
+            else:
+                raise AttributeError("{} is not a supported option".format(name))
+        else:
+            if name_with_hyphens in Factory._command_line_options:
+                self._opts[name_with_hyphens] = value
+            else:
+                raise AttributeError("{} is not a supported option".format(name))
+
+    def _construct_command_line(self):
+        args = [self._factory_binary]
+        args += ['--config-file', self._config_file]
+        args += ["--{}={}".format(opt, self._opts[opt]) for opt in self._opts if opt not in Factory._config_file_options]
+
+        return args
+
+    def __enter__(self):
+        if self._factory_proc is not None:
+            raise RuntimeError('Factory was already started')
+        (tmp, self._config_file) = tempfile.mkstemp(
+            prefix='wq-factory-config-',
+            suffix='.json')
+        os.close(tmp)
+        self._write_config()
+        logfd = open(self._log_file, 'a')
+        devnull = open(os.devnull, 'w')
+        self._factory_proc = subprocess.Popen(
+            self._construct_command_line(),
+            stdin=devnull,
+            stdout=logfd,
+            stderr=logfd)
+        devnull.close()
+        logfd.close()
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self._factory_proc is None:
+            raise RuntimeError('Factory not yet started')
+        self._factory_proc.terminate()
+        self._factory_proc.wait()
+        self._factory_proc = None
+        os.unlink(self._config_file)
+        self._config_file = None
+
+    def _write_config(self):
+        if self._config_file is None:
+            return
+
+        opts_subset = dict([(opt, self._opts[opt]) for opt in self._opts if opt in Factory._config_file_options ])
+        with open(self._config_file, 'w') as f:
+            json.dump(opts_subset, f, indent=4)
+
 
 def rmsummary_snapshots(self):
     if self.snapshots_count < 1:
