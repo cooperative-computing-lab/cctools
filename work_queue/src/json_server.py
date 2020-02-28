6e3c4f6000090ee4635ec434a47d16d54dc1608f
camicarballo <ccarball@nd.edu>
2020-02-28 14:12:03 -0500

    fixed for wq_bwa_json example

--- work_queue/src/json_server.py
+++ work_queue/src/json_server.py
@@ -2,18 +2,22 @@
 #This software is distributed under the GNU General Public License.
 #See the file COPYING for details.
 
+import os
 import socket
 import json
+from time import sleep
+from subprocess import Popen
 
 class WorkQueueServer:
 
-    def __init__(self):
+    def __init__(self, workers):
         self.socket = socket.socket()
         self.id = 1
+        self.num_workers = workers
+        self.wq = None
 
     def send_recv(self, request):
         request = json.dumps(request)
-        request += "\n"
         self.send(request)
 
         response = self.recv()
@@ -22,14 +26,29 @@ class WorkQueueServer:
 
         return response
 
-    def connect(self, address, port):
-        self.socket.connect((address, port))
+    def connect(self, address, server_port, wq_port):
+        args = ['./work_queue_server', "%d" % server_port, "%d" % wq_port]#, '1>', '/dev/null', '2>&1']
+        self.server = Popen(args)
+
+        os.system("condor_submit_workers --cores 2 --memory 4000 --disk 10000 -M wq_bwa_json %d" % self.num_workers)
+
+        i = 1
+        while True:
+            try:
+                self.socket.connect((address, server_port))
+                break
+            except:
+                sleep(0.1*i)
+                i *= 2
 
     def send(self, msg):
         length = len(msg)
 
         total = 0
         sent = 0
+
+        self.socket.send("%d" % length)
+
         while total < length:
             sent = self.socket.send(msg[sent:])
             if sent == 0:
@@ -46,10 +65,12 @@ class WorkQueueServer:
                 break
 
         response = response[len(length):]
-        length = int(length)
-
-        while len(response) < length:
-            response += self.socket.rec(4096)
+        try:
+            length = int(length)
+            while len(response) < length:
+                response += self.socket.recv(4096)
+        except:
+            pass
         
         return response
 
@@ -84,17 +105,24 @@ class WorkQueueServer:
         return self.send_recv(request)
 
     def disconnect(self):
+        self.socket.close()
+        Popen.terminate(self.server)
+
+        os.system("condor_rm ccarball")
+
+    def wq_empty(self):
         request = {
             "jsonrpc" : "2.0",
-            "method" : "disconnect",
+            "method" : "empty",
             "id" : self.id,
-            "params" : None
+            "params" : ""
         }
 
         response = self.send_recv(request)
+        response = json.loads(response)
+        empty = response["result"]
 
-        self.socket.close()
-
-        return response
-
-
+        if empty == "Not Empty":
+            return False
+        else:
+            return True
