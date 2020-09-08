94d553f833481f08fcdff88dbd119165e4505edc
Douglas Thain <dthain@nd.edu>
2020-09-08 16:24:23 -0400

    Doc worker fields.

--- dataswarm/worker/dataswarm_worker.h
+++ dataswarm/worker/dataswarm_worker.h
@@ -2,17 +2,23 @@
 #define DATASWARM_WORKER_H
 
 struct dataswarm_worker {
+	// Network connection to the manager process.
 	struct link *manager_link;
 
-	// taskid -> dataswarm_task *
+	// Table mapping taskids to dataswarm_task objects.
 	struct hash_table *task_table;
 
+	// Path to top of workspace containing tasks and blobs.
 	char *workspace;
+
+	/***************************************************************/
+	/* Internal tuning parameters set in dataswarm_worker_create() */
+	/***************************************************************/
 
 	// Give up and reconnect if no message received after this time.
 	int idle_timeout;
 
-	//
+	// Abort a single message transmission if stuck for this long.
 	int long_timeout;
 
 	// Minimum time between connection attempts.
