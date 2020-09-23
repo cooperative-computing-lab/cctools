3e5f9f0e22ecab17dc13bb39cd41ca61b5a1c12d
Benjamin Tovar <btovar@nd.edu>
2020-09-23 10:02:15 -0400

    remove blob retrieved state

--- dataswarm/manager/dataswarm_blob_rep.h
+++ dataswarm/manager/dataswarm_blob_rep.h
@@ -7,9 +7,8 @@ typedef enum {
 	DS_BLOB_WORKER_STATE_NEW = 0,
 	DS_BLOB_WORKER_STATE_CREATED,
 	DS_BLOB_WORKER_STATE_PUT, DS_BLOB_WORKER_STATE_COPIED,
-	DS_BLOB_WORKER_STATE_GET,
-	DS_BLOB_WORKER_STATE_RETRIEVED,
 	DS_BLOB_WORKER_STATE_COMMITTED,
+	DS_BLOB_WORKER_STATE_GET,
 	DS_BLOB_WORKER_STATE_DELETED,
 } dataswarm_blob_worker_state_t;
 
@@ -19,7 +18,7 @@ struct dataswarm_blob_rep {
 	 * state, in_transition, and result represent the state of the blob in the
 	 * worker according to the manager according to the following invariants:
 	 *
-	 * 1) state always records the latest rpc succesfully completed.
+	 * 1) state always records the latest rpc successfully completed.
 	 * 2) result always records the result of the latest rpc, whether it has
 	 *    completed. If it has not completed, then result == DS_RESULT_PENDING.
 	 * 3) result == DS_RESULT_SUCCESS implies state == in_transition.
@@ -28,16 +27,12 @@ struct dataswarm_blob_rep {
 	 *    reached because of the error in result.
 	 * 5) state and in_transition are strictly monotonically increasing
 	 *    according to DS_BLOB_WORKER_STATE: NEW, CREATED, ((PUT or COPIED), COMMITTED) or
-	 *    (GET, RETRIEVED). DELETED may occur at any time after create.
+	 *    (COMMITTED, GET) or GET. DELETED may occur at any time after create.
 	 *
-	 * To get a blob there are two stages: GET which prompts the worker to
-	 * start sending the blob. The manager is free to do other things while
-	 * an in_transition GET has a result of DS_RESULT_PENDING. When it
-	 * becomes DS_RESULT_SUCCESS, then the in_transition becomes RETRIEVED.
-	 *
-	 * Note that RETRIEVED does not really represent an rpc, but the inflight
-	 * contents of the buffer. This is necessary, as the GET may succeed,
-	 * but the overall transfer may fail.
+	 * With GET, state and in_transition are immediately set to GET, as the
+	 * state of the blob in the worker does not change. result is set to
+	 * DS_RESULT_PENDING, and changed to DS_RESULT_SUCCESS once the file is
+	 * retrieved.
 	 *
 	 */
 
