b6dd702c3af277166d6e2cb2ec5c277f6bc078d5
Douglas Thain <dthain@nd.edu>
2020-09-29 11:54:25 -0400

    Indent.

--- dataswarm/manager/ds_file.c
+++ dataswarm/manager/ds_file.c
@@ -3,57 +3,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct ds_file * ds_file_create( struct jx *jfile )
+struct ds_file *ds_file_create(struct jx *jfile)
 {
 	struct ds_file *f = malloc(sizeof(*f));
-	memset(f,0,sizeof(*f));
+	memset(f, 0, sizeof(*f));
 
-	f->fileid = jx_lookup_string_dup(jfile,"file-id");
-	f->projectid = jx_lookup_string_dup(jfile,"project-id");
-    f->size = jx_lookup_integer(jfile,"size");
+	f->fileid = jx_lookup_string_dup(jfile, "file-id");
+	f->projectid = jx_lookup_string_dup(jfile, "project-id");
+	f->size = jx_lookup_integer(jfile, "size");
 
-	f->metadata = jx_lookup(jfile,"metadata");
+	f->metadata = jx_lookup(jfile, "metadata");
 	if(f->metadata) {
-        f->metadata = jx_copy(f->metadata);
-    }
+		f->metadata = jx_copy(f->metadata);
+	}
 
 	return f;
 
 }
 
-const char * ds_file_state_string( ds_file_state_t state )
+const char *ds_file_state_string(ds_file_state_t state)
 {
-	switch(state) {
-        case DS_FILE_PENDING: return "pending";
-        case DS_FILE_ALLOCATING: return "allocating";
-        case DS_FILE_MUTABLE: return "mutable";
-        case DS_FILE_COMMITTING: return "committing";
-        case DS_FILE_IMMUTABLE: return "immutable";
-        case DS_FILE_DELETING: return "deleting";
-        case DS_FILE_DELETED: return "deleted";
-		default: return "unknown";
+	switch (state) {
+	case DS_FILE_PENDING:
+		return "pending";
+	case DS_FILE_ALLOCATING:
+		return "allocating";
+	case DS_FILE_MUTABLE:
+		return "mutable";
+	case DS_FILE_COMMITTING:
+		return "committing";
+	case DS_FILE_IMMUTABLE:
+		return "immutable";
+	case DS_FILE_DELETING:
+		return "deleting";
+	case DS_FILE_DELETED:
+		return "deleted";
+	default:
+		return "unknown";
 	}
 }
 
-struct jx * ds_file_to_jx( struct ds_file *f )
+struct jx *ds_file_to_jx(struct ds_file *f)
 {
 	struct jx *jfile = jx_object(0);
-	if(f->fileid) jx_insert_string(jfile,"file-id",f->fileid);
-	if(f->projectid) jx_insert_string(jfile,"project-id",f->projectid);
-    if(f->metadata) jx_insert(jfile,jx_string("metadata"),jx_copy(f->metadata));
-    if(f->size) jx_insert(jfile,jx_string("size"),jx_integer(f->size));
-    jx_insert_string(jfile,"state",ds_file_state_string(f->state));
+	if(f->fileid)
+		jx_insert_string(jfile, "file-id", f->fileid);
+	if(f->projectid)
+		jx_insert_string(jfile, "project-id", f->projectid);
+	if(f->metadata)
+		jx_insert(jfile, jx_string("metadata"), jx_copy(f->metadata));
+	if(f->size)
+		jx_insert(jfile, jx_string("size"), jx_integer(f->size));
+	jx_insert_string(jfile, "state", ds_file_state_string(f->state));
 
 	return jfile;
 }
 
 
-void ds_file_delete( struct ds_file *f )
+void ds_file_delete(struct ds_file *f)
 {
-	if(!f) return;
-	if(f->projectid) free(f->projectid);
-	if(f->fileid) free(f->fileid);
-	if(f->metadata) jx_delete(f->metadata);
+	if(!f)
+		return;
+	if(f->projectid)
+		free(f->projectid);
+	if(f->fileid)
+		free(f->fileid);
+	if(f->metadata)
+		jx_delete(f->metadata);
 	free(f);
 }
-
--- dataswarm/manager/ds_file.h
+++ dataswarm/manager/ds_file.h
@@ -4,26 +4,26 @@
 #include "jx.h"
 
 typedef enum {
-    DS_FILE_PENDING,
-    DS_FILE_ALLOCATING,
-    DS_FILE_MUTABLE,
-    DS_FILE_COMMITTING,
-    DS_FILE_IMMUTABLE,
-    DS_FILE_DELETING,
+	DS_FILE_PENDING,
+	DS_FILE_ALLOCATING,
+	DS_FILE_MUTABLE,
+	DS_FILE_COMMITTING,
+	DS_FILE_IMMUTABLE,
+	DS_FILE_DELETING,
 	DS_FILE_DELETED
 } ds_file_state_t;
 
 struct ds_file {
 	char *fileid;
 	ds_file_state_t state;
-    int size;
-    char *projectid;
-    struct jx *metadata;
+	int size;
+	char *projectid;
+	struct jx *metadata;
 };
 
-struct ds_file * ds_file_create( struct jx *jfile );
-struct jx * ds_file_to_jx( struct ds_file *file );
-const char * ds_file_state_string( ds_file_state_t state );
-void ds_file_delete( struct ds_file *f );
+struct ds_file *ds_file_create(struct jx *jfile);
+struct jx *ds_file_to_jx(struct ds_file *file);
+const char *ds_file_state_string(ds_file_state_t state);
+void ds_file_delete(struct ds_file *f);
 
 #endif
