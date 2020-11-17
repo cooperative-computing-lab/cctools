cbf6dc1c5065256c4842adb7ab9e967e16ac39c7
Douglas Thain <dthain@nd.edu>
2020-11-17 16:21:26 -0500

    Added brief comment to ds_db.h

--- dataswarm/manager/ds_db.h
+++ dataswarm/manager/ds_db.h
@@ -3,6 +3,14 @@
 
 #include "ds_manager.h"
 
+/*
+Implements a trivial persistent database for tasks and files.
+At startup, all tasks and files are read into the manager hash tables.
+When a task or file is modified, call ds_db_commit_{task|file} to
+force its storage to local disk.
+*/
+
+
 void ds_db_commit_task( struct ds_manager *m, const char *taskid );
 void ds_db_commit_file( struct ds_manager *m, const char *fileid );
 
