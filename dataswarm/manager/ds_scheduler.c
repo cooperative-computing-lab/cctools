ca073d7dc52a1e05c8c836b5c89dd41134739186
Douglas Thain <dthain@nd.edu>
2020-11-20 10:26:16 -0500

    Some renaming and factoring to split out each action.

--- dataswarm/manager/ds_scheduler.c
+++ dataswarm/manager/ds_scheduler.c
@@ -23,8 +23,8 @@
 #include "ds_client_ops.h"
 #include "ds_file.h"
 
-//XXX change table setups?
-static bool check_replicas(struct ds_file *f, ds_blob_state_t state) {
+static int blobs_in_state( struct ds_file *f, ds_blob_state_t state )
+{
 	char *key;
 	struct ds_blob_rep *b;
 
@@ -37,41 +37,47 @@ static bool check_replicas(struct ds_file *f, ds_blob_state_t state) {
 	return true;
 }
 
-static void process_files(struct ds_manager *m) {
+static void ds_schedule_file( struct ds_manager *m, struct ds_file *f )
+{
+	switch (f->state) {
+		case DS_FILE_ALLOCATING:
+			if (blobs_in_state(f, DS_BLOB_RW)) {
+				f->state = DS_FILE_MUTABLE;
+			} else {
+				//XXX talk to workers to advance replica states
+			}
+			break;
+		case DS_FILE_COMMITTING:
+			if (blobs_in_state(f, DS_BLOB_RO)) {
+				f->state = DS_FILE_IMMUTABLE;
+			} else {
+				//XXX talk to workers to advance replica states
+			}
+			break;
+		case DS_FILE_DELETING:
+			if (blobs_in_state(f, DS_BLOB_DELETED)) {
+				f->state = DS_FILE_DELETED;
+			} else {
+				//XXX talk to workers to advance replica states
+			}
+			break;
+		case DS_FILE_PENDING:
+		case DS_FILE_MUTABLE:
+		case DS_FILE_IMMUTABLE:
+		case DS_FILE_DELETED:
+			// nothing to do, wait for the client
+			break;
+	}
+}
+
+static void schedule_all_files( struct ds_manager *m )
+{
 	char *key;
 	struct ds_file *f;
 
 	hash_table_firstkey(m->file_table);
 	while (hash_table_nextkey(m->file_table, &key, (void **) &f)) {
-		switch (f->state) {
-			case DS_FILE_ALLOCATING:
-				if (check_replicas(f, DS_BLOB_RW)) {
-					f->state = DS_FILE_MUTABLE;
-				} else {
-					//XXX talk to workers to advance replica states
-				}
-				break;
-			case DS_FILE_COMMITTING:
-				if (check_replicas(f, DS_BLOB_RO)) {
-					f->state = DS_FILE_IMMUTABLE;
-				} else {
-					//XXX talk to workers to advance replica states
-				}
-				break;
-			case DS_FILE_DELETING:
-				if (check_replicas(f, DS_BLOB_DELETED)) {
-					f->state = DS_FILE_DELETED;
-				} else {
-					//XXX talk to workers to advance replica states
-				}
-				break;
-			case DS_FILE_PENDING:
-			case DS_FILE_MUTABLE:
-			case DS_FILE_IMMUTABLE:
-			case DS_FILE_DELETED:
-				// nothing to do, wait for the client
-				break;
-		}
+		ds_schedule_file(m,f);
 	}
 }
 
@@ -85,20 +91,6 @@ static void fix_task(struct ds_task *t) {
 	//XXX inspect task, check retry count, etc.
 	// for now, just switch to a hard failure
 	t->result = DS_TASK_RESULT_ERROR;
-}
-
-static void schedule_task(struct ds_manager *m, struct ds_task *t) {
-	//XXX do some matching of tasks to workers
-
-	int r = rand() % hash_table_size(m->worker_table);
-	char *key;
-	void *w;
-	hash_table_firstkey(m->worker_table);
-	while (r >= 0) {
-		hash_table_nextkey(m->worker_table, &key, &w);
-		r--;
-	}
-	t->worker = xxstrdup(key);
 }
 
 static bool prepare_worker(struct ds_manager *m, struct ds_task *t) {
@@ -121,53 +113,79 @@ static bool prepare_worker(struct ds_manager *m, struct ds_task *t) {
 	return true;
 }
 
-void ds_scheduler( struct ds_manager *m )
+static char * choose_worker_for_task( struct ds_manager *m, struct ds_task *t )
+{
+	//XXX do some matching of tasks to workers
+
+	int r = rand() % hash_table_size(m->worker_table);
+	char *key;
+	void *w;
+	hash_table_firstkey(m->worker_table);
+	while (r >= 0) {
+		hash_table_nextkey(m->worker_table, &key, &w);
+		r--;
+	}
+	return xxstrdup(key);
+}
+
+static void schedule_task( struct ds_manager *m, struct ds_task *t )
+{
+	switch (t->state) {
+		case DS_TASK_ACTIVE:
+			// schedule/advance below
+			break;
+		case DS_TASK_DONE:
+		case DS_TASK_DELETED:
+			// nothing to do, wait for the client
+			return;
+		case DS_TASK_RUNNING:
+		case DS_TASK_DELETING:
+			// nothing to do, wait for the worker
+			return;
+	}
+
+	switch (t->result) {
+		case DS_TASK_RESULT_SUCCESS:
+			break;
+		case DS_TASK_RESULT_ERROR:
+		case DS_TASK_RESULT_UNDEFINED:
+			return;
+		case DS_TASK_RESULT_FIX:
+			fix_task(t);
+			return;
+		case DS_TASK_RESULT_AGAIN:
+			retry_task(t);
+			return;
+	}
+
+	if (!t->worker) {
+		t->worker = choose_worker_for_task(m,t);
+	}
+
+	if (!t->worker) {
+		// couldn't/didn't want to schedule the task this time around
+		return;
+	}
+
+	struct ds_worker_rep *w = hash_table_lookup(m->worker_table, t->worker);
+	if (!prepare_worker(m, t)) return;
+	ds_manager_add_task_to_worker(m, w, t->taskid);
+}
+
+static void schedule_all_tasks( struct ds_manager *m )
 {
 	char *key;
 	struct ds_task *t;
 
 	hash_table_firstkey(m->task_table);
 	while (hash_table_nextkey(m->task_table, &key, (void **) &t)) {
-		switch (t->state) {
-			case DS_TASK_ACTIVE:
-				// schedule/advance below
-				break;
-			case DS_TASK_DONE:
-			case DS_TASK_DELETED:
-				// nothing to do, wait for the client
-				continue;
-			case DS_TASK_RUNNING:
-			case DS_TASK_DELETING:
-				// nothing to do, wait for the worker
-				continue;
-		}
-
-		switch (t->result) {
-			case DS_TASK_RESULT_SUCCESS:
-				break;
-			case DS_TASK_RESULT_ERROR:
-			case DS_TASK_RESULT_UNDEFINED:
-				// nothing to do
-				continue;
-			case DS_TASK_RESULT_FIX:
-				fix_task(t);
-				continue;
-			case DS_TASK_RESULT_AGAIN:
-				retry_task(t);
-				continue;
-		}
-
-		if (!t->worker) {
-			schedule_task(m, t);
-		}
-		if (!t->worker) {
-			// couldn't/didn't want to schedule the task this time around
-			return;
-		}
-
-		struct ds_worker_rep *w = hash_table_lookup(m->worker_table, t->worker);
-		if (!prepare_worker(m, t)) return;
-		ds_manager_add_task_to_worker(m, w, key);
+		schedule_task(m,t);
 	}
+}
+
+void ds_scheduler( struct ds_manager *m )
+{
+	schedule_all_tasks(m);
+	schedule_all_files(m);
 }
 
