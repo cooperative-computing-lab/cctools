657fd915dc99834fe8ad85b58d8ca8284a8ab2c5
Benjamin Tovar <btovar@nd.edu>
2021-03-09 12:38:09 -0500

    adds osx

--- packaging/scripts/build.sh
+++ packaging/scripts/build.sh
@@ -4,7 +4,7 @@ set -ex
 BUILD_ID=$(basename "${GITHUB_REF:-${GITHUB_SHA:0:8}}")
 case "${OS_NAME}" in
     osx)
-    IMAGE_ID="x86_64-osx10.13"
+    IMAGE_ID="x86_64-osx10.15"
     ;;
     *)
     IMAGE_ID=$(basename "${DOCKER_IMAGE:-${BUILD_ID}}")
