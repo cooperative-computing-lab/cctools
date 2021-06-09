998b27f26c32aa7aba5c214d45e8885c88dbc281
Benjamin Tovar <btovar@nd.edu>
2021-06-09 07:50:21 -0400

    bug fix wq hungry (#2604)
    * fix whitespace
    
    * fix whitespace
    
    * correctly loop through ready list
    
    * use resources requested rather than allocated
    
    * assume tasks use at least one core
    
    This to prevent a workflow with unlabeled task to always return 'hungry'
    
    * remove check not needed for no workers

--- work_queue/src/work_queue.c
+++ work_queue/src/work_queue.c
@@ -6343,29 +6343,19 @@ int work_queue_hungry(struct work_queue *q)
 	struct work_queue_stats qstats;
 	work_queue_get_stats(q, &qstats);
 
-	//check if there's any workers joined from start
-	//if there's none, limit the number of ready tasks in queue to 10
-	//10 is chosen to be the default number of ready tasks in queue to keep queue efficient
-	if (qstats.workers_joined == 0){
-		if (qstats.tasks_waiting < 10){
-			return 1;
-		}  
-		return 0;
-	}
-	
-	//if number of ready tasks is less than 10, return true for more tasks in queue 
+	//if number of ready tasks is less than 10, return true for more tasks in queue
 	//10 is chosen to be the default number of ready tasks in queue to keep queue efficient
 	if (qstats.tasks_waiting < 10){
 		return 1;
 	}
-	
+
 	//get total available resources consumption (cores, memory, disk, gpus) of all workers of this manager
 	//available = total (all) - committed (actual in use)
 	int64_t workers_total_avail_cores 	= 0;
 	int64_t workers_total_avail_memory 	= 0;
 	int64_t workers_total_avail_disk 	= 0;
 	int64_t workers_total_avail_gpus 	= 0;
-	
+
 	workers_total_avail_cores 	= q->stats->total_cores - q->stats->committed_cores;
 	workers_total_avail_memory 	= q->stats->total_memory - q->stats->committed_memory;
 	workers_total_avail_disk 	= q->stats->total_disk - q->stats->committed_disk;
@@ -6378,21 +6368,28 @@ int work_queue_hungry(struct work_queue *q)
 	int64_t ready_task_gpus		= 0;
 
 	struct work_queue_task *t;
-	
-	list_first_item(q->ready_list); 
-	t = list_next_item(q->ready_list);
 
-	ready_task_cores 	+= t->resources_allocated->cores;
-	ready_task_memory 	+= t->resources_allocated->memory;
-	ready_task_disk 	+= t->resources_allocated->disk;
-	ready_task_gpus 	+= t->resources_allocated->gpus;	
+	int count = task_state_count(q, NULL, WORK_QUEUE_TASK_READY);
+
+	while(count > 0)
+	{
+		count--;
+		t = list_pop_head(q->ready_list);
+
+		ready_task_cores  += MAX(1,t->resources_requested->cores);
+		ready_task_memory += t->resources_requested->memory;
+		ready_task_disk   += t->resources_requested->disk;
+		ready_task_gpus   += t->resources_requested->gpus;
+
+		list_push_tail(q->ready_list, t);
+	}
 
 	//check possible limiting factors
 	//return false if required resources exceed available resources
 	if (ready_task_cores > workers_total_avail_cores){
 		return 0;
 	}
- 	if (ready_task_memory > workers_total_avail_memory){
+	if (ready_task_memory > workers_total_avail_memory){
 		return 0;
 	}
 	if (ready_task_disk > workers_total_avail_disk){
