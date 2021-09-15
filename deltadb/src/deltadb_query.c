27f552f9f9e767c79beb61eac9917ece52948fce
Douglas Thain <dthain@nd.edu>
2021-09-15 14:08:56 -0400

    Remove extraneous newline in reduction output.

--- deltadb/src/deltadb_query.c
+++ deltadb/src/deltadb_query.c
@@ -240,7 +240,7 @@ static void display_reduce_exprs( struct deltadb_query *query, time_t current )
 	list_first_item(query->reduce_exprs);
 	for(struct deltadb_reduction *r; (r = list_next_item(query->reduce_exprs));) {
 		char *str = deltadb_reduction_string(r);
-		fprintf(query->output_stream,"%s\n",str);
+		fprintf(query->output_stream,"%s",str);
 		free(str);
 	}
 
