e81a6fd54e0a8bb6b880985d8527bb4a5b02d1d1
Benjamin Tovar <btovar@nd.edu>
2021-09-20 14:58:17 -0400

    replace copy_direntry with copy_file_to_file so symlinks are copied to scratch

--- batch_job/src/work_queue_factory.c
+++ batch_job/src/work_queue_factory.c
@@ -9,7 +9,6 @@ See the file COPYING for details.
 #include "batch_job.h"
 #include "hash_table.h"
 #include "copy_stream.h"
-#include "copy_tree.h"
 #include "debug.h"
 #include "domain_name_cache.h"
 #include "envtools.h"
@@ -1497,7 +1496,7 @@ int main(int argc, char *argv[])
 	list_first_item(wrapper_inputs);
 	while((item = list_next_item(wrapper_inputs))) {
 		char *file_at_scratch_dir = string_format("%s/%s", scratch_dir, path_basename(item));
-		int result = copy_direntry(item, file_at_scratch_dir);
+		int result = copy_file_to_file(item, file_at_scratch_dir);
 		if(result < 0) {
 			fprintf(stderr,"work_queue_factory: Cannot copy wrapper input file %s to factory scratch directory %s:\n", item, file_at_scratch_dir);
 			fprintf(stderr,"%s\n", strerror(errno));
