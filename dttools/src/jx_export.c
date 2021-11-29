7508ea57c0a59b728810971288afd3ee4068103d
Douglas Thain <dthain@nd.edu>
2021-11-29 13:31:18 -0500

    Convert jx_export routines to operate on link instead of stream.

--- dttools/src/jx_export.c
+++ dttools/src/jx_export.c
@@ -13,6 +13,8 @@ See the file COPYING for details.
 #include <string.h>
 #include <stdio.h>
 
+void link_printf( struct link *l, time_t stoptime, const char *fmt, ... );
+
 static char * unquoted_string( struct jx *j )
 {
 	char *str;
@@ -43,92 +45,92 @@ void jx_export_shell( struct jx *j, FILE *stream )
 The old nvpair format simply has unquoted data following the key.
 */
 
-void jx_export_nvpair( struct jx *j, FILE *stream )
+void jx_export_nvpair( struct jx *j, struct link *l, time_t stoptime )
 {
 	struct jx_pair *p;
 	for(p=j->u.pairs;p;p=p->next) {
 		char *str = unquoted_string(p->value);
-		fprintf(stream,"%s %s\n",p->key->u.string_value,str);
+		link_printf(l,stoptime,"%s %s\n",p->key->u.string_value,str);
 		free(str);
 	}
-	fprintf(stream,"\n");
+	link_printf(l,stoptime,"\n");
 }
 
 /*
 The old classad format has quoted strings, symbols, booleans, integers, but not objects or arrays.  So, we quote the latter two types.  Individual ads are separated by newlines.
 */
 
-void jx_export_old_classads( struct jx *j, FILE *stream )
+void jx_export_old_classads( struct jx *j, struct link *l, time_t stoptime )
 {
 	struct jx_pair *p;
 	for(p=j->u.pairs;p;p=p->next) {
 		char *str = jx_print_string(p->value);
 		if(p->value->type==JX_OBJECT || p->value->type==JX_ARRAY) {
-			fprintf(stream,"%s = \"%s\"\n",p->key->u.string_value,str);
+			link_printf(l,stoptime,"%s = \"%s\"\n",p->key->u.string_value,str);
 		} else {
-			fprintf(stream,"%s = %s\n",p->key->u.string_value,str);
+			link_printf(l,stoptime,"%s = %s\n",p->key->u.string_value,str);
 		}
 		free(str);
 	}
-	fprintf(stream,"\n");
+	link_printf(l,stoptime,"\n");
 }
 
 /*
 For XML encoding, we use plain text for atomic types and tags to structure objects and arrays.
 */
 
-void jx_export_xml( struct jx *j, FILE *stream )
+void jx_export_xml( struct jx *j, struct link *l, time_t stoptime )
 {
 	struct jx_pair *p;
 	struct jx_item *i;
 
 	switch(j->type) {
 	case JX_NULL:
-		fprintf(stream,"null");
+		link_printf(l,stoptime,"null");
 		break;
 	case JX_BOOLEAN:
-		fprintf(stream,j->u.boolean_value?"true":"false");
+		link_printf(l,stoptime,j->u.boolean_value?"true":"false");
 		break;
 	case JX_INTEGER:
-		fprintf(stream,"%lld",(long long)j->u.integer_value);
+		link_printf(l,stoptime,"%lld",(long long)j->u.integer_value);
 		break;
 	case JX_DOUBLE:
-		fprintf(stream,"%lf",j->u.double_value);
+		link_printf(l,stoptime,"%lf",j->u.double_value);
 		break;
 	case JX_STRING:
-		fprintf(stream,"%s",j->u.string_value);
+		link_printf(l,stoptime,"%s",j->u.string_value);
 		break;
 	case JX_SYMBOL:
-		fprintf(stream,"%s",j->u.symbol_name);
+		link_printf(l,stoptime,"%s",j->u.symbol_name);
 		break;
 	case JX_OBJECT:
-		fprintf(stream,"<object>\n");
+		link_printf(l,stoptime,"<object>\n");
 		for(p=j->u.pairs;p;p=p->next) {
-			fprintf(stream,"<pair><key>%s</key>",p->key->u.string_value);
-			fprintf(stream,"<value>");
-			jx_export_xml(p->value,stream);
-			fprintf(stream,"</value></pair>");
+			link_printf(l,stoptime,"<pair><key>%s</key>",p->key->u.string_value);
+			link_printf(l,stoptime,"<value>");
+			jx_export_xml(p->value,l,stoptime);
+			link_printf(l,stoptime,"</value></pair>");
 		}
-		fprintf(stream,"</object>\n");
+		link_printf(l,stoptime,"</object>\n");
 		break;
 	case JX_ARRAY:
-		fprintf(stream,"<array>\n");
+		link_printf(l,stoptime,"<array>\n");
 		for (i = j->u.items; i; i = i->next) {
-			fprintf(stream, "<item>");
-			jx_export_xml(i->value, stream);
-			fprintf(stream,"</item>");
+			link_printf(l,stoptime, "<item>");
+			jx_export_xml(i->value,l,stoptime);
+			link_printf(l,stoptime,"</item>");
 		}
-		fprintf(stream,"</array>\n");
+		link_printf(l,stoptime,"</array>\n");
 		break;
 	case JX_OPERATOR:
-		fprintf(stream,"<expr>\n");
-		jx_print_stream(j,stream);
-		fprintf(stream,"</expr>\n");
+		link_printf(l,stoptime,"<expr>\n");
+		jx_print_link(j,l,stoptime);
+		link_printf(l,stoptime,"</expr>\n");
 		break;
 	case JX_ERROR:
-		fprintf(stream,"<error>\n");
-		jx_print_stream(j,stream);
-		fprintf(stream,"</error>\n");
+		link_printf(l,stoptime,"<error>\n");
+		jx_print_link(j,l,stoptime);
+		link_printf(l,stoptime,"</error>\n");
 		break;
 	}
 }
@@ -137,31 +139,31 @@ void jx_export_xml( struct jx *j, FILE *stream )
 New classads are quite similar to json, except that the use of [] and {} is reversed.
 */
 
-void jx_export_new_classads( struct jx *j, FILE *stream )
+void jx_export_new_classads( struct jx *j, struct link *l, time_t stoptime )
 {
 	struct jx_pair *p;
 	struct jx_item *i;
 
 	switch(j->type) {
 		case JX_OBJECT:
-			fprintf(stream,"[\n");
+			link_printf(l,stoptime,"[\n");
 			for(p=j->u.pairs;p;p=p->next) {
-				fprintf(stream,"%s=",p->key->u.string_value);
-				jx_print_stream(p->value,stream);
-				fprintf(stream,";\n");
+				link_printf(l,stoptime,"%s=",p->key->u.string_value);
+				jx_print_link(p->value,l,stoptime);
+				link_printf(l,stoptime,";\n");
 			}
-			fprintf(stream,"]\n");
+			link_printf(l,stoptime,"]\n");
 			break;
 		case JX_ARRAY:
-			fprintf(stream,"{\n");
+			link_printf(l,stoptime,"{\n");
 			for(i=j->u.items;i;i=i->next) {
-				jx_print_stream(i->value,stream);
-				if(i->next) fprintf(stream,",");
+				jx_print_link(i->value,l,stoptime);
+				if(i->next) link_printf(l,stoptime,",");
 			}
-			fprintf(stream,"}\n");
+			link_printf(l,stoptime,"}\n");
 			break;
 		default:
-			jx_print_stream(j,stream);
+			jx_print_link(j,l,stoptime);
 			break;
 	}
 }
@@ -180,48 +182,48 @@ static const char *align_string(struct jx_table *h)
 	}
 }
 
-void jx_export_html_solo(struct jx *j, FILE * stream)
+void jx_export_html_solo(struct jx *j, struct link *l, time_t stoptime )
 {
-	fprintf(stream, "<table bgcolor=%s>\n", COLOR_TWO);
-	fprintf(stream, "<tr bgcolor=%s>\n", COLOR_ONE);
+	link_printf(l,stoptime, "<table bgcolor=%s>\n", COLOR_TWO);
+	link_printf(l,stoptime, "<tr bgcolor=%s>\n", COLOR_ONE);
 
 	color_counter = 0;
 
 	struct jx_pair *p;
 	for(p=j->u.pairs;p;p=p->next) {
-		fprintf(stream, "<tr bgcolor=%s>\n", color_counter % 2 ? COLOR_ONE : COLOR_TWO);
+		link_printf(l,stoptime, "<tr bgcolor=%s>\n", color_counter % 2 ? COLOR_ONE : COLOR_TWO);
 		color_counter++;
-		fprintf(stream, "<td align=left><b>%s</b>\n", p->key->u.string_value);
+		link_printf(l,stoptime, "<td align=left><b>%s</b>\n", stoptime, p->key->u.string_value);
 		char *str = unquoted_string(p->value);
 		if(!strcmp(p->key->u.string_value, "url")) {
-			fprintf(stream, "<td align=left><a href=%s>%s</a>\n",str,str);
+			link_printf(l,stoptime, "<td align=left><a href=%s>%s</a>\n",str,str);
 		} else {
-			fprintf(stream, "<td align=left>%s\n",str);
+			link_printf(l,stoptime, "<td align=left>%s\n",str);
 		}
 		free(str);
 	}
-	fprintf(stream, "</table>\n");
+	link_printf(l,stoptime, "</table>\n");
 }
 
-void jx_export_html_header(FILE * s, struct jx_table *h)
+void jx_export_html_header( struct link *l, struct jx_table *h, time_t stoptime )
 {
-	fprintf(s, "<table bgcolor=%s>\n", COLOR_TWO);
-	fprintf(s, "<tr bgcolor=%s>\n", COLOR_ONE);
+	link_printf(l,stoptime,"<table bgcolor=%s>\n", COLOR_TWO);
+	link_printf(l,stoptime,"<tr bgcolor=%s>\n", COLOR_ONE);
 	while(h->name) {
-		fprintf(s, "<td align=%s><b>%s</b>\n", align_string(h), h->title);
+		link_printf(l,stoptime,"<td align=%s><b>%s</b>\n", align_string(h), h->title);
 		h++;
 	}
 	color_counter = 0;
 }
 
-void jx_export_html(struct jx *n, FILE * s, struct jx_table *h)
+void jx_export_html( struct jx *n, struct link *l, struct jx_table *h, time_t stoptime )
 {
-	jx_export_html_with_link(n, s, h, 0, 0);
+	jx_export_html_with_link(n, l, h, 0, 0, stoptime);
 }
 
-void jx_export_html_with_link(struct jx *n, FILE * s, struct jx_table *h, const char *linkname, const char *linktext)
+void jx_export_html_with_link( struct jx *n, struct link *l, struct jx_table *h, const char *linkname, const char *linktext, time_t stoptime )
 {
-	fprintf(s, "<tr bgcolor=%s>\n", color_counter % 2 ? COLOR_ONE : COLOR_TWO);
+	link_printf(l,stoptime,"<tr bgcolor=%s>\n", color_counter % 2 ? COLOR_ONE : COLOR_TWO);
 	color_counter++;
 	while(h->name) {
 		struct jx *value = jx_lookup(n,h->name);
@@ -231,18 +233,18 @@ void jx_export_html_with_link(struct jx *n, FILE * s, struct jx_table *h, const 
 		} else {
 			text = strdup("???");
 		}
-		fprintf(s, "<td align=%s>", align_string(h));
+		link_printf(l,stoptime,"<td align=%s>", align_string(h));
 		if(h->mode == JX_TABLE_MODE_URL) {
-			fprintf(s, "<a href=%s>%s</a>\n", text, text);
+			link_printf(l,stoptime,"<a href=%s>%s</a>\n", text, text);
 		} else if(h->mode == JX_TABLE_MODE_METRIC) {
 			char line[1024];
 			string_metric(atof(text), -1, line);
-			fprintf(s, "%sB\n", line);
+			link_printf(l,stoptime,"%sB\n", line);
 		} else {
 			if(linkname && !strcmp(linkname, h->name)) {
-				fprintf(s, "<a href=%s>%s</a>\n", linktext, text);
+				link_printf(l,stoptime,"<a href=%s>%s</a>\n", linktext, text);
 			} else {
-				fprintf(s, "%s\n", text);
+				link_printf(l,stoptime,"%s\n", text);
 			}
 		}
 		free(text);
@@ -250,7 +252,7 @@ void jx_export_html_with_link(struct jx *n, FILE * s, struct jx_table *h, const 
 	}
 }
 
-void jx_export_html_footer(FILE * s, struct jx_table *h)
+void jx_export_html_footer( struct link *l, struct jx_table *h, time_t stoptime )
 {
-	fprintf(s, "</table>\n");
+	link_printf(l,stoptime,"</table>\n");
 }
