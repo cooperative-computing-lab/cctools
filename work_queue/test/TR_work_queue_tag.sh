f9743e4850d497f6d4deaea1dee22a268a55870c
Benjamin Tovar <btovar@nd.edu>
2021-12-06 07:28:51 -0500

    wq: work_queue_wait_for_tag (#2749)
    * update tag comparator to take into account NULL tags
    
    * find tasks by state and tag
    
    * wq wait internal to use tags
    
    * adds work_queue_wait_for_tag
    
    * py bindings
    
    * adds TR_work_queue_tag.sh

---
+++ work_queue/test/TR_work_queue_tag.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -e
+
+. ../../dttools/test/test_runner_common.sh
+
+python=${CCTOOLS_PYTHON_TEST_EXEC}
+python_dir=${CCTOOLS_PYTHON_TEST_DIR}
+
+STATUS_FILE=wq.status
+PORT_FILE=wq.port
+
+
+check_needed()
+{
+	[ -n "${python}" ] || return 1
+}
+
+prepare()
+{
+	rm -f $STATUS_FILE
+	rm -f $PORT_FILE
+
+	return 0
+}
+
+run()
+{
+	# send wq to the background, saving its exit status.
+	(PYTHONPATH=$(pwd)/../src/bindings/${python_dir} ${python} wq_test_tag.py $PORT_FILE; echo $? > $STATUS_FILE) &
+
+	# wait at most 5 seconds for wq to find a port.
+	wait_for_file_creation $PORT_FILE 5
+
+	run_local_worker $PORT_FILE worker.log
+
+	# wait for wq to exit.
+	wait_for_file_creation $STATUS_FILE 5
+
+	# retrieve wq exit status
+	status=$(cat $STATUS_FILE)
+	if [ $status -ne 0 ]
+	then
+		exit 1
+	fi
+
+	exit 0
+}
+
+clean()
+{
+	rm -f $STATUS_FILE
+	rm -f $PORT_FILE
+
+	exit 0
+}
+
+
+dispatch "$@"
+
+# vim: set noexpandtab tabstop=4:
