3d1f54264df05ba3192c465b71715a79fe81cc6c
Benjamin Tovar <btovar@nd.edu>
2022-01-18 12:33:36 -0500

    Starch small fixes (#2780)
    * print help and quit when executable option is missing
    
    * adds starch configuration file example
    
    * Adds SFX_EXEC to change what command to execute in the package.
    
    The use case is for when several executables are added to the package.
    E.g.:
    
    starch -x date -x uname -c date example.sfx
    
    ./example.sfx
    Mon Jan 10 11:12:02 EST 2022
    
    SFX_EXEC=uname ./example.sfx
    Linux
    
    This also useful to run a shell with all the PATH and LD_LIBRARY_PATH
    variables set correctly to run a set of commands:
    
    SFX_EXEC=/bin/sh ./example.sfx
    sh$ which date
    /home/myuser/example.sfx.27441.dir/bin/date
    
    * Adds --sfx-help to show help message for sfx packages.
    
    E.g:
    starch -x date -c date example.sfx
    ./example.sfx --sfx-help
    Executing the sfx file by itself executes the command date
    
    environment variables to set packages options:
    SFX_EXEC=CMD Execute CMD instead of the default.
    SFX_EXTRACT_ONLY=1 Extract package, but do not run a command.
    SFX_KEEP=1 Do not remove extracted package when command finishes.
    SFX_DIR=DIR Extract to DIR, instead of a temporary directory. Implies
    SFX_KEEP=1
    
    * Correctly quote SFX_EXEC
    
    * cleanup TR_starch_extract_and_remove.sh test
    
    * make SFX_DIR available for commands

--- makeflow/test/TR_starch_extract_and_remove.sh
+++ makeflow/test/TR_starch_extract_and_remove.sh
@@ -7,13 +7,13 @@ tarfile=starch.tar.gz
 
 prepare()
 {
-	cd ..; tar czvf $tarfile src; cd -; mv ../$tarfile .
+	(cd .. && tar czvf $tarfile src) && mv ../$tarfile .
 	exit 0
 }
 
 run()
 {
-	../src/starch -v -x tar -x rm -c 'tar_test() { for f in $@; do if ! tar xvf $f; then exit 1; fi ; done; rm $@; }; tar_test' $sfxfile
+	../src/starch -v -x tar -x rm -c 'tar_test() { for f in "$@"; do if ! tar xvf $f; then exit 1; fi ; done; }; tar_test' $sfxfile
 	exec ./$sfxfile $tarfile
 }
 
