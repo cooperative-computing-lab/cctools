bdf5bba1857374b2fe3d5fdc65639808926e1187
Nick Locascio <nicoloc4545@gmail.com>
2022-01-24 10:19:59 -0500

    changed disk to gpus (#2789)
    Co-authored-by: Nicholas Locascio <nlocasci@crcfe02.crc.nd.edu>

--- work_queue/src/work_queue_status.c
+++ work_queue/src/work_queue_status.c
@@ -112,8 +112,8 @@ static struct jx_table manager_resource_headers[] = {
 {"cores_inuse",		"INUSE", 	    JX_TABLE_MODE_PLAIN, JX_TABLE_ALIGN_RIGHT, 6},
 {"memory_total",	"MEM(GB)",     JX_TABLE_MODE_GIGABYTES, JX_TABLE_ALIGN_RIGHT, 8},
 {"memory_inuse",	"INUSE", 	JX_TABLE_MODE_GIGABYTES, JX_TABLE_ALIGN_RIGHT, 8},
-{"disk_total",  	"DISK(GB)",  	JX_TABLE_MODE_GIGABYTES, JX_TABLE_ALIGN_RIGHT, 9},
-{"disk_inuse",		"INUSE", 	JX_TABLE_MODE_GIGABYTES, JX_TABLE_ALIGN_RIGHT, 9},
+{"gpus_total",  	"GPUS",  	JX_TABLE_MODE_PLAIN, JX_TABLE_ALIGN_RIGHT, 9},
+{"gpus_inuse",		"INUSE", 	JX_TABLE_MODE_PLAIN, JX_TABLE_ALIGN_RIGHT, 9},
 {NULL,NULL,0,0,0}
 };
 
