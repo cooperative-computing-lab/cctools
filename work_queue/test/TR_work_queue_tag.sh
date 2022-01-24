af54a7a5618412816990731de5ca0034ee714c25
Douglas Thain <dthain@nd.edu>
2022-01-24 14:52:29 -0500

    Wait for 15 seconds for various work queue programs to start. Making the (slow) github/OSX tests a little more resilient.

--- work_queue/test/TR_work_queue_tag.sh
+++ work_queue/test/TR_work_queue_tag.sh
@@ -28,13 +28,13 @@ run()
 	# send wq to the background, saving its exit status.
 	(PYTHONPATH=$(pwd)/../src/bindings/${python_dir} ${python} wq_test_tag.py $PORT_FILE; echo $? > $STATUS_FILE) &
 
-	# wait at most 5 seconds for wq to find a port.
-	wait_for_file_creation $PORT_FILE 5
+	# wait at most 15 seconds for wq to find a port.
+	wait_for_file_creation $PORT_FILE 15
 
 	run_local_worker $PORT_FILE worker.log
 
 	# wait for wq to exit.
-	wait_for_file_creation $STATUS_FILE 5
+	wait_for_file_creation $STATUS_FILE 15
 
 	# retrieve wq exit status
 	status=$(cat $STATUS_FILE)
