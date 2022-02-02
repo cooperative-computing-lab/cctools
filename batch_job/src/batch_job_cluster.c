27cbef93af019bde833737110ab0199249749c18
Benjamin Tovar <btovar@nd.edu>
2022-02-02 08:29:00 -0500

    remove extra space, sge mem

--- batch_job/src/batch_job_cluster.c
+++ batch_job/src/batch_job_cluster.c
@@ -165,7 +165,7 @@ static char *cluster_set_resource_string(struct batch_queue *q, const struct rms
 	} else if(q->type == BATCH_QUEUE_TYPE_SGE){
 		if(!ignore_mem && resources->memory > 0) {
 			const char *mem_type = batch_queue_get_option(q, "mem-type");
-			buffer_printf(&cluster_resources, " -l %s=%.0f M", mem_type ? mem_type : "h_vmem", resources->memory);
+			buffer_printf(&cluster_resources, " -l %s=%.0fM", mem_type ? mem_type : "h_vmem", resources->memory);
 		}
 		if(!ignore_time && resources->wall_time > 0) {
 			buffer_printf(&cluster_resources, " -l h_rt=00:%.0f:00", DIV_INT_ROUND_UP(resources->wall_time, 60));
