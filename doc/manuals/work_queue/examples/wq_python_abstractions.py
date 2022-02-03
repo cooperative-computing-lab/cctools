b21ee5cfd5f2e6fd770bf73627dd76f691fbbf9c
Brett Wiseman <bwisema3@nd.edu>
2022-02-02 21:27:50 -0500

    change function names to make more sense

--- doc/manuals/work_queue/examples/wq_python_abstractions.py
+++ doc/manuals/work_queue/examples/wq_python_abstractions.py
@@ -2,17 +2,17 @@
 import work_queue as wq
 
 # Example funtion for the Map Abstraction
-def Map(x):
+def dblfunc(x):
     return 2*x
 
 # Example function for the Pair Abstraction
 # Note: function must accept a tuple
-def Pair(p):
+def mulfunc(p):
     return p[0] * p[1]
 
 # Example function for the Tree Reduce Abstraction  
 # Note: function must accept a iterable
-def Tree(p):
+def maxfunc(p):
     return p[0] if p[0] > p[1] else p[1]
 
 
@@ -25,13 +25,13 @@ a = [1, 2, 3, 4]
 b = [2, 4, 6, 8]
 
 # Map
-results = q.map(Map, a, 1)
+results = q.map(dblfunc, a, 1)
 print(f'Map: {results}')
 
 # Pair
-results = q.pair(Pair, a, b, 2)
+results = q.pair(mulfunc, a, b, 2)
 print(f'Pair: {results}')
 
 # Tree reduce
-results = q.tree_reduce(Tree, b, 2)
+results = q.tree_reduce(maxfunc, b, 2)
 print(f'Tree: {results}')
