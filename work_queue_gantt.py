ec60755a0d7007e36186b7bc3d704302db3e239e
Guanchao “Samuel” Huang <58419736+sghuang19@users.noreply.github.com>
2022-06-02 15:56:02 -0400

    Refactor regex

--- work_queue_gantt.py
+++ work_queue_gantt.py
@@ -34,84 +34,94 @@ def parse_tasks(logfile):
     Parse the logfile as a table.
     | taskid | category | wait_time | start_time | end_time | info |
     """
-    
+
     tasks = pd.DataFrame(columns=[
-        "taskid", "category", "wait_time", "start_time", "end_time", "info"
+        "taskid",
+        "category",
+        "wait_time",
+        "start_time",
+        "end_time",
+        "info"
     ])
     tasks.set_index("taskid")
-    # TODO: switch from nested json to flatten table. 
+
     with open(logfile, 'r') as log:
-        p_taskid    = re.compile(r"^[^#].*TASK (\d+) (WAITING|RUNNING|DONE) ")
-        p_info      = re.compile(r" (\{.*\})?$")
-        p_category  = re.compile(r"WAITING (\w+)")
-        p_time      = re.compile(r"^\d+")
-        min_time    = 0
+        p_task = re.compile(r"(?P<time>\d+) \s+ "
+                            r"(?P<pid>\d+) \s+ TASK \s+ "
+                            r"(?P<taskid>\d+) \s+ "
+                            r"(?P<state>\S+) \s+ "
+                            r"(?P<state_args>.+)", re.X)
+        min_time = 0
 
         for line in log:
-            m = p_taskid.findall(line)
+            m = p_task.match(line)
             if not m:
                 continue
-            m, = m
-            taskid  = int(m[0])
-            status  = m[1]
-            time    = int(p_time.findall(line)[0][:10])
+            time   = int(m.group("time"))
+            taskid = m.group("taskid")
+            state  = m.group("state")
 
-            if status == "WAITING":
+            if state == "WAITING":
                 if not min_time:
                     min_time = time
                 tasks.loc[taskid] = {
-                    "taskid"        : taskid,
-                    "category"      : p_category.findall(line)[0],
-                    "wait_time"     : time - min_time,
-                    "start_time"    : -1,
-                    "end_time"      : -1,
-                    "info"          : ""
+                    "taskid"     : taskid,
+                    "category"   : m.group("state_args").split()[0],
+                    "wait_time"  : time - min_time,
+                    "start_time" : -1,
+                    "end_time"   : -1,
+                    "info"       : ""
                 }
-            elif status == "RUNNING":
+            elif state == "RUNNING":
                 tasks.loc[taskid, "start_time"] = time - min_time
-            elif status == "DONE":
+            elif state == "DONE":
                 tasks.loc[taskid, "end_time"]   = time - min_time
-                tasks.loc[taskid, "info"]       = p_info.findall(line)
-   
-    print(tasks) 
+                tasks.loc[taskid, "info"]       = m.group("state_args")
+
+    tasks["order"] = tasks["start_time"].rank(method="first")
     return tasks
 
 
-def get_times(tasks): 
+def get_times(tasks):
     taskids     = list(tasks.keys())
-    wait_times  = [tasks[taskid]["wait_time"]   for taskid in taskids]
-    start_times = [tasks[taskid]["start_time"]  for taskid in taskids]
-    end_times   = [tasks[taskid]["end_time"]    for taskid in taskids]
-    return taskids, wait_times, start_times, end_times    
+    wait_times  = [tasks[taskid]["wait_time"]  for taskid in taskids]
+    start_times = [tasks[taskid]["start_time"] for taskid in taskids]
+    end_times   = [tasks[taskid]["end_time"]   for taskid in taskids]
+    return taskids, wait_times, start_times, end_times
 
 
 def plot(tasks, outfile):
-    taskids, start_times, end_times  = get_times(tasks)
+    taskids, start_times, end_times = get_times(tasks)
     durations = [end_times[i] - start_times[i] for i in range(len(tasks))]
     fig, ax = plt.subplots()
     ax.barh(taskids, durations, left=start_times)
     plt.savefig(outfile, format='svg')
     plt.show()
-    
+
 
 def plot_interactive(tasks):
     # taskids, wait_times, start_times, end_times = get_times(tasks)
     # print(start_times)
     # num_tasks = len(taskids)
-    
-    categories  = tasks["category"].unique() 
-    colors      = palette[len(categories)]
-    p = figure(title="Tasks Lifetime",
-               x_axis_label="time", y_axis_label="task id",
-               sizing_mode="stretch_width")
+
+    categories = tasks["category"].unique()
+    colors     = palette[len(categories)]
+    p = figure(
+        title="Tasks Lifetime",
+        x_axis_label="time", y_axis_label="task id",
+        sizing_mode="stretch_width"
+    )
     tasks = tasks.sort_values(by="start_time")
-    offset = 0
+
     for i, category in enumerate(categories):
         # TODO write this as a iterator
         # Plot the bars based on the time they started
         tasks_cat = tasks[tasks["category"] == category]
-        p.hbar(y=range(offset, offset + len(tasks_cat)), left=tasks_cat["start_time"], right=tasks_cat["end_time"], color=colors[i])
-        offset += len(tasks_cat)
+        p.hbar(
+            y=tasks_cat["order"],
+            left=tasks_cat["start_time"], right=tasks_cat["end_time"],
+            color=colors[i]
+        )
     show(p)
 
 
@@ -123,7 +133,7 @@ def main():
 
     arguments = sys.argv[1:]
     while arguments:
-        argument = arguments.pop(0);
+        argument = arguments.pop(0)
         if argument[0] == '-':
             if argument == '-h':
                 usage(0)
@@ -144,7 +154,7 @@ def main():
         plot_interactive(tasks)
     else:
         plot(tasks, outfile)
-    
+
+
 if __name__ == '__main__':
     main()
-
