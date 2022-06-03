2f288a1c216da903826a22b3493b51dc47495dbf
Guanchao “Samuel” Huang <58419736+sghuang19@users.noreply.github.com>
2022-06-03 09:53:14 -0400

    Fix mem and time conversion

--- work_queue/src/work_queue.c
+++ work_queue/src/work_queue.c
@@ -7561,8 +7561,8 @@ static void write_transaction_transfer(struct work_queue *q, struct work_queue_w
 	}
 	buffer_printf(&B,  "%d",  t->taskid);
 	buffer_printf(&B, " %s",  w->workerid);
-	buffer_printf(&B, " %d", size_in_bytes >> 10);
-	buffer_printf(&B, " %f",  time_in_usecs / 1E6);
+	buffer_printf(&B, " %f", size_in_bytes / ((double) MEGABYTE));
+	buffer_printf(&B, " %f",  time_in_usecs / ((double) USECOND));
 	buffer_printf(&B, " %s",  filename);
 
 	write_transaction(q, buffer_tostring(&B));
