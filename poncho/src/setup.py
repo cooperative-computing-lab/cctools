0f5093e9813faf1265ca7bc9fb1b099452645e97
David-Simonetti-ND <dsimone2@nd.edu>
2022-06-06 10:47:59 -0400

    Added creation of poncho python module

---
+++ poncho/src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name='poncho',
+        version='1.0.0',
+        description='packaging tools for python applications',
+        packages=['poncho'],
+        install_requires=['dill']
+
+)
