f4067abccb4bb84aa685083b08e82ccbeefe82b0
Guanchao “Samuel” Huang <58419736+sghuang19@users.noreply.github.com>
2022-06-14 11:50:52 -0400

    Include cache information in the transfer transaction (#2897)
    * Update arguments of func transfer trans log
    
    * Add cache_flag
    
    * Update func write_transaction_transfer usage
    
    * Use work_queue_file_type_t

--- work_queue/src/work_queue.c
+++ work_queue/src/work_queue.c
@@ -344,7 +344,7 @@ static void write_transaction(struct work_queue *q, const char *str);
 static void write_transaction_task(struct work_queue *q, struct work_queue_task *t);
 static void write_transaction_category(struct work_queue *q, struct category *c);
 static void write_transaction_worker(struct work_queue *q, struct work_queue_worker *w, int leaving, worker_disconnect_reason reason_leaving);
-static void write_transaction_transfer(struct work_queue *q, struct work_queue_worker *w, struct work_queue_task *t, size_t size_in_bytes, int time_in_usecs, char* filename, work_queue_file_type_t type);
+static void write_transaction_transfer(struct work_queue *q, struct work_queue_worker *w, struct work_queue_task *t, struct work_queue_file *f, size_t size_in_bytes, int time_in_usecs, work_queue_file_type_t type);
 static void write_transaction_worker_resources(struct work_queue *q, struct work_queue_worker *w);
 
 static void delete_feature(struct work_queue_task *t, const char *name);
@@ -1404,7 +1404,7 @@ static work_queue_result_code_t get_output_file( struct work_queue *q, struct wo
 
 		debug(D_WQ, "%s (%s) sent %.2lf MB in %.02lfs (%.02lfs MB/s) average %.02lfs MB/s", w->hostname, w->addrport, total_bytes / 1000000.0, sum_time / 1000000.0, (double) total_bytes / sum_time, (double) w->total_bytes_transferred / w->total_transfer_time);
 
-        write_transaction_transfer(q, w, t, total_bytes, sum_time, f->remote_name, WORK_QUEUE_OUTPUT);
+        write_transaction_transfer(q, w, t, f, total_bytes, sum_time, WORK_QUEUE_OUTPUT);
 	}
 
 	// If the transfer was successful, make a record of it in the cache.
@@ -3389,7 +3389,7 @@ static work_queue_result_code_t send_input_file(struct work_queue *q, struct wor
 		q->stats->bytes_sent += total_bytes;
 
 		// Write to the transaction log.
-		write_transaction_transfer(q, w, t, total_bytes, elapsed_time, f->remote_name, WORK_QUEUE_INPUT);
+		write_transaction_transfer(q, w, t, f, total_bytes, elapsed_time, WORK_QUEUE_INPUT);
 
 		// Avoid division by zero below.
 		if(elapsed_time==0) elapsed_time = 1;
@@ -7563,20 +7563,16 @@ static void write_transaction_worker_resources(struct work_queue *q, struct work
 }
 
 
-static void write_transaction_transfer(struct work_queue *q, struct work_queue_worker *w, struct work_queue_task *t, size_t size_in_bytes, int time_in_usecs, char* filename, work_queue_file_type_t type){
+static void write_transaction_transfer(struct work_queue *q, struct work_queue_worker *w, struct work_queue_task *t, struct work_queue_file *f, size_t size_in_bytes, int time_in_usecs, work_queue_file_type_t type) {
 	struct buffer B;
 	buffer_init(&B);
 	buffer_printf(&B, "TRANSFER ");
-	if (type == WORK_QUEUE_INPUT){
-		buffer_printf(&B, "INPUT ");
-	}
-	else{
-		buffer_printf(&B, "OUTPUT ");
-	}
-	buffer_printf(&B,  "%d", t->taskid);
+	buffer_printf(&B, type == WORK_QUEUE_INPUT ? "INPUT":"OUTPUT");
+	buffer_printf(&B, " %d", t->taskid);
+	buffer_printf(&B, " %d", f->flags & WORK_QUEUE_CACHE);
 	buffer_printf(&B, " %f", size_in_bytes / ((double) MEGABYTE));
 	buffer_printf(&B, " %f", time_in_usecs / ((double) USECOND));
-	buffer_printf(&B, " %s", filename);
+	buffer_printf(&B, " %s", f->remote_name);
 
 	write_transaction(q, buffer_tostring(&B));
 	buffer_free(&B);
@@ -7600,7 +7596,7 @@ int work_queue_specify_transactions_log(struct work_queue *q, const char *logfil
 		fprintf(q->transactions_logfile, "# time manager_pid TASK taskid RUNNING worker_address (FIRST_RESOURCES|MAX_RESOURCES) {resources_allocated}\n");
 		fprintf(q->transactions_logfile, "# time manager_pid TASK taskid WAITING_RETRIEVAL worker_address\n");
 		fprintf(q->transactions_logfile, "# time manager_pid TASK taskid (RETRIEVED|DONE) (SUCCESS|SIGNAL|END_TIME|FORSAKEN|MAX_RETRIES|MAX_WALLTIME|UNKNOWN|RESOURCE_EXHAUSTION) exit_code {limits_exceeded} {resources_measured}\n");
-		fprintf(q->transactions_logfile, "# time manager_pid TRANSFER (INPUT|OUTPUT) taskid sizeinmb walltime filename\n");
+		fprintf(q->transactions_logfile, "# time manager_pid TRANSFER (INPUT|OUTPUT) taskid cache_flag sizeinmb walltime filename\n");
 		fprintf(q->transactions_logfile, "\n");
 
 		write_transaction(q, "MANAGER START");
