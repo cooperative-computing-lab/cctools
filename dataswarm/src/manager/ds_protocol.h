cd9aceb5538b00d859d355a4b5f6690125b03863
Douglas Thain <dthain@nd.edu>
2022-08-11 15:52:55 -0400

    Protocol back to version 1.

--- dataswarm/src/manager/ds_protocol.h
+++ dataswarm/src/manager/ds_protocol.h
@@ -14,19 +14,10 @@ This file should not be installed and should only be included by .c files.
 #ifndef DS_PROTOCOL_H
 #define DS_PROTOCOL_H
 
-/* 4: added invalidate-file message, for cache management.    */
-/* 5: added wall_time, end_time messages, for task maximum running time. */
-/* 6: worker only report total, max, and min resources. */
-/* 7: added category message */
-/* 8: worker send feature message. */
-/* 9: recursive send/recv and filename encoding. */
-/* 10: added coprocess message. */
-/* 11: added url/command as a file source, added cache-update/invalidate */
+#define DS_PROTOCOL_VERSION 1
 
-#define DS_PROTOCOL_VERSION 11
-
-#define DS_LINE_MAX 4096       /**< Maximum length of a work queue message line. */
-#define DS_POOL_NAME_MAX 128   /**< Maximum length of a work queue pool name. */
+#define DS_LINE_MAX 4096       /**< Maximum length of a ds message line. */
+#define DS_PROJECT_NAME_MAX 128   /**< Maximum length of a ds project name. */
 #define WORKER_WORKSPACE_NAME_MAX 2048   /**< Maximum length of a work queue worker's workspace name. */
 
 #define DS_PROTOCOL_FIELD_MAX 256
