6fe7299c2e5a938f295e7410b3e424b10c57bd24
Benjamin Tovar <btovar@nd.edu>
2022-08-16 13:17:36 -0400

    wq ssl: remove typo

--- work_queue/src/bindings/python3/work_queue.binding.py
+++ work_queue/src/bindings/python3/work_queue.binding.py
@@ -1130,7 +1130,7 @@ class WorkQueue(object):
                 prefix='cert')
         os.close(tmp)
 
-        cmd=f"openssl req -x509 -newkey rsa:4096 -keyout {key} -out {cert} x-sha256 -days 365 -nodes -batch".split()
+        cmd=f"openssl req -x509 -newkey rsa:4096 -keyout {key} -out {cert} -sha256 -days 365 -nodes -batch".split()
 
         output=""
         try:
