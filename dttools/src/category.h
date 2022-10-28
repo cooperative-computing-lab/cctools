0adc08a194086b78cf756e929f603843fbef9019
Thanh Son Phung <tphung@nd.edu>
2022-10-28 14:42:41 -0400

    add modes for fast and exhaustive bucketing

--- dttools/src/category.h
+++ dttools/src/category.h
@@ -22,7 +22,9 @@ typedef enum {
 	CATEGORY_ALLOCATION_FIRST = 0,       /**< No automatic allocation, or using first step value of the two-step policy. */
 	CATEGORY_ALLOCATION_AUTO  = 0,       /**< Same as FIRST, FIRST is deprecated */
 	CATEGORY_ALLOCATION_MAX,             /**< Using max of category. (2nd step of two-step policy) */
-	CATEGORY_ALLOCATION_ERROR            /**< No valid resources could be found. (E.g., after 2nd step fails) */
+	CATEGORY_ALLOCATION_ERROR,            /**< No valid resources could be found. (E.g., after 2nd step fails) */
+    CATEGORY_ALLOCATION_MODE_FAST_BUCKETING,
+    CATEGORY_ALLOCATION_MODE_EXHAUSTIVE_BUCKETING
 } category_allocation_t;
 
 
