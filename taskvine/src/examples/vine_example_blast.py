2e2bf5c59d47f3a48152b4159b284dbfc07c8b09
JosephDuggan7 <89431217+JosephDuggan7@users.noreply.github.com>
2022-11-04 11:32:19 -0400

    Translated taskvine examples to Python (#3044)
    * Translated taskvine examples to Python
    
    * fixed translated examples
    
    * fixed translated examples
    
    * remove originally written in C comment
    
    * do not unpack query.file
    
    * accept convert path from command line
    
    * print temporary output from watched files
    
    * fix whitespace
    
    * whitespace
    
    * remove unused imports
    
    * fix bare except
    
    * fix comparison to 0
    
    Co-authored-by: Joe Duggan <jduggan5@crcfe01.crc.nd.edu>
    Co-authored-by: Benjamin Tovar <btovar@nd.edu>

---
+++ taskvine/src/examples/vine_example_blast.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2022- The University of Notre Dame
+# This software is distributed under the GNU General Public License.
+# See the file COPYING for details.
+
+# This example shows some of the data handling features of taskvine.
+# It performs a BLAST search of the "Landmark" model organism database.
+# It works by constructing tasks that download the blast executable
+# and landmark database from NCBI, and then performs a short query.
+
+# The query is provided by a string (but presented to the task as a file.)
+# Both the downloads are automatically unpacked, cached, and shared
+# with all the same tasks on the worker.
+
+import taskvine as vine
+import sys
+
+query_string = """>P01013 GENE X PROTEIN (OVALBUMIN-RELATED)
+QIKDLLVSSSTDLDTTLVLVNAIYFKGMWKTAFNAEDTREMPFHVTKQESKPVQMMCMNNSFNVATLPAE
+KMKILELPFASGDLSMLVLLPDEVSDLERIEKTINFEKLTEWTNPNTMEKRRVKVYLPQMKIEEKYNLTS
+VLMALGMTDLFIPSANLTGISSAESLKISQAVHGAFMELSEDGIEMAGSTGVIEDIKHSPESEQFRADHP
+FLFLIKHNPTNTIVYFGRYWSP"""
+
+blast_url = "https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz"
+
+landmark_url = "https://ftp.ncbi.nlm.nih.gov/blast/db/landmark.tar.gz"
+
+if __name__ == "__main__":
+    try:
+        m = vine.Manager()
+    except IOError as e:
+        print("couldn't create manager:", e.errno)
+        sys.exit(1)
+    print("listening on port", m.port)
+
+    m.enable_debug_log("manager.log")
+    m.set_scheduler(vine.VINE_SCHEDULE_FILES)
+
+    for i in range(10):
+        t = vine.Task(
+            "blastdir/ncbi-blast-2.13.0+/bin/blastp -db landmark -query query.file"
+        )
+
+        t.add_input_buffer(query_string, "query.file", cache=True)
+        t.add_input_url(blast_url, "blastdir", flags=vine.VINE_UNPACK, cache=True)
+        t.add_input_url(landmark_url, "landmark", flags=vine.VINE_UNPACK, cache=True)
+        t.set_env_var("BLASTDB", value="landmark")
+
+        task_id = m.submit(t)
+
+        print("submitted task (id# " + str(task_id) + "):", t.command)
+
+    print("waiting for tasks to complete...")
+
+    while not m.empty():
+        t = m.wait(5)
+        if t:
+            r = t.result
+            id = t.id
+
+            if r == vine.VINE_RESULT_SUCCESS:
+                print("task", id, "output:", t.std_output)
+            else:
+                print("task", id, "failed:", t.result_string)
+
+    print("all tasks complete!")
---
+++ taskvine/src/examples/vine_example_watch.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2022- The University of Notre Dame
+# This software is distributed under the GNU General Public License.
+# See the file COPYING for details.
+
+# This example program shows the behavior of the VINE_WATCH flag.
+
+# If a task produces output to a file incrementally as it runs,
+# it can be helpful to see that output piece by piece as it
+# is produced. By simply adding the VINE_WATCH flag to the output
+# of the program, taskvine will periodically check for output
+# and return it to the manager while each task runs.  When the
+# task completes, any remaining output is fetched.
+
+# This example runs several instances of the task named
+# vine_example_watch_trickle.sh, which gradually produces output
+# every few seconds.  While running the manager program, open
+# up another terminal, and observe that files output.0, output.1,
+# etc are gradually produced throughout the run.
+
+import taskvine as vine
+import sys
+
+if __name__ == "__main__":
+    try:
+        m = vine.Manager()
+    except IOError as e:
+        print("couldn't create manager:", e.errno)
+        sys.exit(1)
+    print("listening on port", m.port)
+
+    m.enable_debug_log("manager.log")
+    n = 3
+
+    for i in range(n):
+        output = "output." + str(i)
+        t = vine.Task("./vine_example_watch_trickle.sh > output")
+        t.add_input_file(
+            "vine_example_watch_trickle.sh", "vine_example_watch_trickle.sh", cache=True
+        )
+        t.add_output_file(output, "output", flags=vine.VINE_WATCH)
+        t.set_cores(1)
+        m.submit(t)
+
+    print("Waiting for tasks to complete...")
+
+    while not m.empty():
+        t = m.wait(5)
+        if t:
+            r = t.result
+            id = t.id
+
+            if r == vine.VINE_RESULT_SUCCESS:
+                print("task", id, "output:", t.std_output)
+            else:
+                print("task", id, "failed:", t.result_string)
+
+        for i in range(n):
+            try:
+                with open(f"output.{i}") as f:
+                    print(f"output.{i}:\n{f.readlines()}\n")
+            except IOError:
+                pass
+
+    print("All tasks complete!")
