326086083e760877e578cba22db00835e7bd4be3
Thanh Son Phung <tphung@nd.edu>
2022-11-15 13:27:39 -0500

    fix typo in test script

--- dttools/test/TR_bucketing.sh
+++ dttools/test/TR_bucketing.sh
@@ -16,7 +16,7 @@ run()
     ../src/bucketing_manager_test -greedy > /dev/null
     val3=$?
     ../src/bucketing_manager_test -exhaust > /dev/null
-    val4=?
+    val4=$?
     (test $val1 = 0) && (test $val2 = 0) && (test $val3 = 0) && (test $val4 = 0)
     return $?
 }
