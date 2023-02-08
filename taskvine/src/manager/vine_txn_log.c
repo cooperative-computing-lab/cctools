edee5493f604c261fba48c91a7ca1cb7473961c5
Benjamin Tovar <btovar@nd.edu>
2023-02-08 13:25:43 -0500

    remove redundant if

--- taskvine/src/manager/vine_txn_log.c
+++ taskvine/src/manager/vine_txn_log.c
@@ -19,6 +19,7 @@ See the file COPYING for details.
 
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
 
 void vine_txn_log_write(struct vine_manager *q, const char *str)
 {
@@ -75,31 +76,27 @@ void vine_txn_log_write_task(struct vine_manager *q, struct vine_task *t)
 		buffer_printf(&B, " %s ", vine_result_string(t->result));
 		buffer_printf(&B, " %d ", t->exit_code);
 
-		if(t->resources_measured) {
-			if(t->result == VINE_RESULT_RESOURCE_EXHAUSTION) {
-				rmsummary_print_buffer(&B, t->resources_measured->limits_exceeded, 1);
-				buffer_printf(&B, " ");
-			}
-			else {
-				// no limits broken, thus printing an empty dictionary
-				buffer_printf(&B, " {} ");
-			}
+        assert(t->resources_measured);
+        if(t->result == VINE_RESULT_RESOURCE_EXHAUSTION) {
+            rmsummary_print_buffer(&B, t->resources_measured->limits_exceeded, 1);
+            buffer_printf(&B, " ");
+        }
+        else {
+            // no limits broken, thus printing an empty dictionary
+            buffer_printf(&B, " {} ");
+        }
 
-			struct jx *m = rmsummary_to_json(t->resources_measured, /* only resources */ 1);
-			jx_insert(m, jx_string("size_input_mgr"), jx_arrayv(jx_double(t->bytes_sent/((double) MEGABYTE)), jx_string("MB"), NULL));
-			jx_insert(m, jx_string("size_output_mgr"), jx_arrayv(jx_double(t->bytes_received/((double) MEGABYTE)), jx_string("MB"), NULL));
+        struct jx *m = rmsummary_to_json(t->resources_measured, /* only resources */ 1);
+        jx_insert(m, jx_string("size_input_mgr"), jx_arrayv(jx_double(t->bytes_sent/((double) MEGABYTE)), jx_string("MB"), NULL));
+        jx_insert(m, jx_string("size_output_mgr"), jx_arrayv(jx_double(t->bytes_received/((double) MEGABYTE)), jx_string("MB"), NULL));
 
-			jx_insert(m, jx_string("time_input_mgr"), jx_arrayv(jx_double((t->time_when_commit_end - t->time_when_commit_start)/((double) ONE_SECOND)), jx_string("s"), NULL));
-			jx_insert(m, jx_string("time_output_mgr"), jx_arrayv(jx_double((t->time_when_done - t->time_when_retrieval)/((double) ONE_SECOND)), jx_string("s"), NULL));
+        jx_insert(m, jx_string("time_input_mgr"), jx_arrayv(jx_double((t->time_when_commit_end - t->time_when_commit_start)/((double) ONE_SECOND)), jx_string("s"), NULL));
+        jx_insert(m, jx_string("time_output_mgr"), jx_arrayv(jx_double((t->time_when_done - t->time_when_retrieval)/((double) ONE_SECOND)), jx_string("s"), NULL));
 
-			jx_insert(m, jx_string("time_worker_start"), jx_arrayv(jx_double(t->time_workers_execute_last_start/((double) ONE_SECOND)), jx_string("s"), NULL));
-			jx_insert(m, jx_string("time_worker_end"), jx_arrayv(jx_double(t->time_workers_execute_last_end/((double) ONE_SECOND)), jx_string("s"), NULL));
-			jx_print_buffer(m, &B);
-			jx_delete(m);
-		} else {
-			// no resources measured, one empty dictionary for limits broken, other for resources.
-			buffer_printf(&B, " {} {}");
-		}
+        jx_insert(m, jx_string("time_worker_start"), jx_arrayv(jx_double(t->time_workers_execute_last_start/((double) ONE_SECOND)), jx_string("s"), NULL));
+        jx_insert(m, jx_string("time_worker_end"), jx_arrayv(jx_double(t->time_workers_execute_last_end/((double) ONE_SECOND)), jx_string("s"), NULL));
+        jx_print_buffer(m, &B);
+        jx_delete(m);
 	} else {
 		struct vine_worker_info *w = t->worker;
 		if(w) {
