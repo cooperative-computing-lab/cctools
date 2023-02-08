4a5e08ed94dc575599165f903367deb864cf93ed
Douglas Thain <dthain@nd.edu>
2023-02-08 15:06:32 -0500

    Add vine_worker.h for prototypes of public things in vine_worker.c (#3105)
    send_cache_update -> vine_worker_send_cache_update
    send_cache_invalid -> vine_worker_send_cache_invalid
    symlinks_enabled -> vine_worker_symlinks_enabled

---
+++ taskvine/src/worker/vine_worker.h
@@ -0,0 +1,12 @@
+#ifndef VINE_WORKER_H
+#define VINE_WORKER_H
+
+#include "timestamp.h"
+#include "link.h"
+
+void vine_worker_send_cache_update( struct link *manager, const char *cachename, int64_t size, timestamp_t transfer_time );
+void vine_worker_send_cache_invalid( struct link *manager, const char *cachename, const char *message );
+
+extern int vine_worker_symlinks_enabled;
+
+#endif
