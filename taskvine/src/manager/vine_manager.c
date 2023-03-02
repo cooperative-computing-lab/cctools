d62c19d92f67d65d1a20706ee0a9e72b00d779a4
Colin Thomas <33940547+colinthomas-z80@users.noreply.github.com>
2023-03-01 20:57:25 -0500

    Vine: Peer Transfer Flags in vine_file (#3173)
    * add transfer file flags to type vine_file
    
    * revise naming

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -2686,7 +2686,7 @@ static int vine_manager_transfer_capacity_available(struct vine_manager *q, stru
 
 		/* If not, then search for an available peer to provide it. */
 		/* Provide a substitute file object to describe the peer. */
-		if(m->file->type != VINE_MINI_TASK) 
+		if(m->file->share_peer_mode != VINE_PEER_NOSHARE) 
 		{
 			if((peer = vine_file_replica_table_find_worker(q, m->file->cached_name)))
 			{
