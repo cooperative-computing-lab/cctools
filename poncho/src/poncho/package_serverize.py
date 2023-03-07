32808f0e71f33a71506dd654e64fd9fd786f23d6
David-Simonetti-ND <dsimone2@nd.edu>
2023-03-07 17:45:18 -0500

    Moved network code from string to functions

--- poncho/src/poncho/package_serverize.py
+++ poncho/src/poncho/package_serverize.py
@@ -18,223 +18,217 @@ import inspect
 
 shebang = "#! /usr/bin/env python3\n\n"
 
-library_network_code = \
-'''
-import socket
-import json
-import os
-import sys
-import threading
-import queue
+def library_network_code():
+    import json
+    import os
+    import sys
+    import threading
+    import queue
 
-def remote_execute(func):
-    def remote_wrapper(event, q=None):
-        if q:
-            event = json.loads(event)
-        kwargs = event["fn_kwargs"]
-        args = event["fn_args"]
-        try:
-            response = {
-                "Result": func(*args, **kwargs),
-                "StatusCode": 200
-            }
-        except Exception as e:
-            response = { 
-                "Result": str(e),
-                "StatusCode": 500 
-            }
-        if not q:
-            return response
-        q.put(response)
-    return remote_wrapper
-    
-read, write = os.pipe() 
-
-def send_configuration(config):
-    config_string = json.dumps(config)
-    config_cmd = f"{len(config_string) + 1}\\n{config_string}\\n"
-    sys.stdout.write(config_cmd)
-    sys.stdout.flush()
-
-def main():
-    config = {
-        "name": name(),
-    }
-    send_configuration(config)
-    while True:
-        while True:
-            # wait for message from worker about what function to execute
-            line = input()
-            print(f"Network function received task", file=sys.stderr, flush=True)
-            if len(line) >= 0:
-                function_name, event_size = line.split(" ")
-                if event_size:
-                    # receive the bytes containing the event and turn it into a string
-                    event_str = input()
-                    if len(event_str) != int(event_size):
-                        print(event_str, len(event_str), event_size, file=sys.stderr)
-                        print("Size of event does not match what was sent: exiting", file=sys.stderr)
-                        exit(0)
-                    # turn the event into a python dictionary
-                    event = json.loads(event_str)
-                    # see if the user specified an execution method
-                    exec_method = event.get("remote_task_exec_method", None)
-                    if exec_method == "thread":
-                        # create a forked process for function handler
-                        q = queue.Queue()
-                        p = threading.Thread(target=globals()[function_name], args=(event_str, q))
-                        p.start()
-                        p.join()
-                        response = json.dumps(q.get())
-                    elif exec_method == "direct":
-                        response = json.dumps(globals()[function_name](event))
-                    else:
-                        p = os.fork()
-                        if p == 0:
-                            response =globals()[function_name](event)
-                            os.write(write, json.dumps(response).encode("utf-8"))
-                            os._exit(0)
-                        elif p < 0:
-                            print('Network function: unable to fork', file=sys.stderr)
-                            response = { 
-                                "Result": "unable to fork",
-                                "StatusCode": 500 
-                            }
-                        else:
-                            chunk = os.read(read, 65536).decode("utf-8")
-                            all_chunks = [chunk]
-                            while (len(chunk) >= 65536):
-                                chunk = os.read(read, 65536).decode("utf-8")
-                                all_chunks.append(chunk)
-                            response = "".join(all_chunks)
-                            os.waitid(os.P_PID, p, os.WEXITED)
-
-                    print(response, flush=True)
-
-                    break
-            else:
-                print("Network function could not read from worker\\n", file=sys.stderr)
-    return 0
-
-'''
-
-wq_network_code = \
-'''
-import socket
-import json
-import os
-import sys
-import threading
-import queue
-def remote_execute(func):
-    def remote_wrapper(event, q=None):
-        if q:
-            event = json.loads(event)
-        kwargs = event["fn_kwargs"]
-        args = event["fn_args"]
-        try:
-            response = {
-                "Result": func(*args, **kwargs),
-                "StatusCode": 200
-            }
-        except Exception as e:
-            response = { 
-                "Result": str(e),
-                "StatusCode": 500 
-            }
-        if not q:
-            return response
-        q.put(response)
-    return remote_wrapper
-    
-read, write = os.pipe() 
-def send_configuration(config):
-    config_string = json.dumps(config)
-    config_cmd = f"{len(config_string) + 1}\\n{config_string}\\n"
-    sys.stdout.write(config_cmd)
-    sys.stdout.flush()
-def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        # modify the port argument to be 0 to listen on an arbitrary port
-        s.bind(('localhost', 0))
-    except Exception as e:
-        s.close()
-        print(e)
-        exit(1)
-    # information to print to stdout for worker
-    config = {
-            "name": name(),
-            "port": s.getsockname()[1],
-            }
-    send_configuration(config)
-    while True:
-        s.listen()
-        conn, addr = s.accept()
-        print('Network function: connection from {}'.format(addr), file=sys.stderr)
-        while True:
-            # peek at message to find newline to get the size
-            event_size = None
-            line = conn.recv(100, socket.MSG_PEEK)
-            eol = line.find(b'\\n')
-            if eol >= 0:
-                size = eol+1
-                # actually read the size of the event
-                input_spec = conn.recv(size).decode('utf-8').split()
-                function_name = input_spec[0]
-                task_id = int(input_spec[1])
-                event_size = int(input_spec[2])
+    def remote_execute(func):
+        def remote_wrapper(event, q=None):
+            if q:
+                event = json.loads(event)
+            kwargs = event["fn_kwargs"]
+            args = event["fn_args"]
             try:
-                if event_size:
-                    # receive the bytes containing the event and turn it into a string
-                    event_str = conn.recv(event_size).decode("utf-8")
-                    # turn the event into a python dictionary
-                    event = json.loads(event_str)
-                    # see if the user specified an execution method
-                    exec_method = event.get("remote_task_exec_method", None)
-                    print('Network function: recieved event: {}'.format(event), file=sys.stderr)
-                    os.chdir(f"t.{task_id}")
-                    if exec_method == "thread":
-                        # create a forked process for function handler
-                        q = queue.Queue()
-                        p = threading.Thread(target=globals()[function_name], args=(event_str, q))
-                        p.start()
-                        p.join()
-                        response = json.dumps(q.get()).encode("utf-8")
-                    elif exec_method == "direct":
-                        response = json.dumps(globals()[function_name](event)).encode("utf-8")
-                    else:
-                        p = os.fork()
-                        if p == 0:
-                            response =globals()[function_name](event)
-                            os.write(write, json.dumps(response).encode("utf-8"))
-                            os._exit(0)
-                        elif p < 0:
-                            print('Network function: unable to fork', file=sys.stderr)
-                            response = { 
-                                "Result": "unable to fork",
-                                "StatusCode": 500 
-                            }
-                        else:
-                            chunk = os.read(read, 65536).decode("utf-8")
-                            all_chunks = [chunk]
-                            while (len(chunk) >= 65536):
-                                chunk = os.read(read, 65536).decode("utf-8")
-                                all_chunks.append(chunk)
-                            response = "".join(all_chunks).encode("utf-8")
-                            os.waitid(os.P_PID, p, os.WEXITED)
-                    response_size = len(response)
-                    size_msg = "{}\\n".format(response_size)
-                    # send the size of response
-                    conn.sendall(size_msg.encode('utf-8'))
-                    # send response
-                    conn.sendall(response)
-                    os.chdir("..")
-                    break
+                response = {
+                    "Result": func(*args, **kwargs),
+                    "StatusCode": 200
+                }
             except Exception as e:
-                print("Network function encountered exception ", str(e), file=sys.stderr)
-    return 0
-'''
+                response = { 
+                    "Result": str(e),
+                    "StatusCode": 500 
+                }
+            if not q:
+                return response
+            q.put(response)
+        return remote_wrapper
+        
+    read, write = os.pipe() 
+
+    def send_configuration(config):
+        config_string = json.dumps(config)
+        config_cmd = f"{len(config_string) + 1}\n{config_string}\n"
+        sys.stdout.write(config_cmd)
+        sys.stdout.flush()
+
+    def main():
+        config = {
+            "name": name(),
+        }
+        send_configuration(config)
+        while True:
+            while True:
+                # wait for message from worker about what function to execute
+                line = input()
+                print(f"Network function received task", file=sys.stderr, flush=True)
+                if len(line) >= 0:
+                    function_name, event_size = line.split(" ")
+                    if event_size:
+                        # receive the bytes containing the event and turn it into a string
+                        event_str = input()
+                        if len(event_str) != int(event_size):
+                            print(event_str, len(event_str), event_size, file=sys.stderr)
+                            print("Size of event does not match what was sent: exiting", file=sys.stderr)
+                            exit(0)
+                        # turn the event into a python dictionary
+                        event = json.loads(event_str)
+                        # see if the user specified an execution method
+                        exec_method = event.get("remote_task_exec_method", None)
+                        if exec_method == "thread":
+                            # create a forked process for function handler
+                            q = queue.Queue()
+                            p = threading.Thread(target=globals()[function_name], args=(event_str, q))
+                            p.start()
+                            p.join()
+                            response = json.dumps(q.get())
+                        elif exec_method == "direct":
+                            response = json.dumps(globals()[function_name](event))
+                        else:
+                            p = os.fork()
+                            if p == 0:
+                                response =globals()[function_name](event)
+                                os.write(write, json.dumps(response).encode("utf-8"))
+                                os._exit(0)
+                            elif p < 0:
+                                print('Network function: unable to fork', file=sys.stderr)
+                                response = { 
+                                    "Result": "unable to fork",
+                                    "StatusCode": 500 
+                                }
+                            else:
+                                chunk = os.read(read, 65536).decode("utf-8")
+                                all_chunks = [chunk]
+                                while (len(chunk) >= 65536):
+                                    chunk = os.read(read, 65536).decode("utf-8")
+                                    all_chunks.append(chunk)
+                                response = "".join(all_chunks)
+                                os.waitid(os.P_PID, p, os.WEXITED)
+
+                        print(response, flush=True)
+
+                        break
+                else:
+                    print("Network function could not read from worker\n", file=sys.stderr)
+        return 0
+
+def wq_network_code():
+    import socket
+    import json
+    import os
+    import sys
+    import threading
+    import queue
+    def remote_execute(func):
+        def remote_wrapper(event, q=None):
+            if q:
+                event = json.loads(event)
+            kwargs = event["fn_kwargs"]
+            args = event["fn_args"]
+            try:
+                response = {
+                    "Result": func(*args, **kwargs),
+                    "StatusCode": 200
+                }
+            except Exception as e:
+                response = { 
+                    "Result": str(e),
+                    "StatusCode": 500 
+                }
+            if not q:
+                return response
+            q.put(response)
+        return remote_wrapper
+        
+    read, write = os.pipe() 
+    def send_configuration(config):
+        config_string = json.dumps(config)
+        config_cmd = f"{len(config_string) + 1}\n{config_string}\n"
+        sys.stdout.write(config_cmd)
+        sys.stdout.flush()
+    def main():
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            # modify the port argument to be 0 to listen on an arbitrary port
+            s.bind(('localhost', 0))
+        except Exception as e:
+            s.close()
+            print(e)
+            exit(1)
+        # information to print to stdout for worker
+        config = {
+                "name": name(),
+                "port": s.getsockname()[1],
+                }
+        send_configuration(config)
+        while True:
+            s.listen()
+            conn, addr = s.accept()
+            print('Network function: connection from {}'.format(addr), file=sys.stderr)
+            while True:
+                # peek at message to find newline to get the size
+                event_size = None
+                line = conn.recv(100, socket.MSG_PEEK)
+                eol = line.find(b'\n')
+                if eol >= 0:
+                    size = eol+1
+                    # actually read the size of the event
+                    input_spec = conn.recv(size).decode('utf-8').split()
+                    function_name = input_spec[0]
+                    task_id = int(input_spec[1])
+                    event_size = int(input_spec[2])
+                try:
+                    if event_size:
+                        # receive the bytes containing the event and turn it into a string
+                        event_str = conn.recv(event_size).decode("utf-8")
+                        # turn the event into a python dictionary
+                        event = json.loads(event_str)
+                        # see if the user specified an execution method
+                        exec_method = event.get("remote_task_exec_method", None)
+                        print('Network function: recieved event: {}'.format(event), file=sys.stderr)
+                        os.chdir(f"t.{task_id}")
+                        if exec_method == "thread":
+                            # create a forked process for function handler
+                            q = queue.Queue()
+                            p = threading.Thread(target=globals()[function_name], args=(event_str, q))
+                            p.start()
+                            p.join()
+                            response = json.dumps(q.get()).encode("utf-8")
+                        elif exec_method == "direct":
+                            response = json.dumps(globals()[function_name](event)).encode("utf-8")
+                        else:
+                            p = os.fork()
+                            if p == 0:
+                                response =globals()[function_name](event)
+                                os.write(write, json.dumps(response).encode("utf-8"))
+                                os._exit(0)
+                            elif p < 0:
+                                print('Network function: unable to fork', file=sys.stderr)
+                                response = { 
+                                    "Result": "unable to fork",
+                                    "StatusCode": 500 
+                                }
+                            else:
+                                chunk = os.read(read, 65536).decode("utf-8")
+                                all_chunks = [chunk]
+                                while (len(chunk) >= 65536):
+                                    chunk = os.read(read, 65536).decode("utf-8")
+                                    all_chunks.append(chunk)
+                                response = "".join(all_chunks).encode("utf-8")
+                                os.waitid(os.P_PID, p, os.WEXITED)
+                        response_size = len(response)
+                        size_msg = "{}\n".format(response_size)
+                        # send the size of response
+                        conn.sendall(size_msg.encode('utf-8'))
+                        # send response
+                        conn.sendall(response)
+                        os.chdir("..")
+                        break
+                except Exception as e:
+                    print("Network function encountered exception ", str(e), file=sys.stderr)
+        return 0
 
 default_name_func = \
 '''def name():
@@ -278,9 +272,12 @@ def create_library_code(path, funcs, dest, version):
 		output_file.write(f"{import_module}\n")
 	# write network code into it
 	if version == "work_queue":
-		output_file.write(wq_network_code)
+		raw_source_fnc = wq_network_code
 	elif version == "taskvine":
-		output_file.write(library_network_code)
+		raw_source_fnc = library_network_code
+	raw_source_code = inspect.getsource(raw_source_fnc)
+	network_code = "\n".join([line[4:] for line in raw_source_code.split("\n")[1:]])
+	output_file.write(network_code)
 	# write name function code into it
 	output_file.write(f"{name_source_code}\n")
 	# iterate over every function the user requested and attempt to put it into the library code
@@ -350,6 +347,6 @@ def serverize_library_from_code(path, functions, name):
         temp_source_file.write("".join([inspect.getsource(fnc) for fnc in functions]))
         temp_source_file.write(f"def name():\n\treturn '{name}'")
     # create the final library code from that temporary file
-    create_library_code(tmp_library_path, [fnc.__name__ for fnc in functions], path + "/library_code.py")
+    create_library_code(tmp_library_path, [fnc.__name__ for fnc in functions], path + "/library_code.py", "taskvine")
     # and pack it into an environment
     pack_library_code(path + "/library_code.py", path + "/library_env.tar.gz")
