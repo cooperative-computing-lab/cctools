bf7bd3f5b3cfab72de98d082b33f0537b53da4f2
Benjamin Tovar <btovar@nd.edu>
2023-04-04 10:31:26 -0400

    add test

---
+++ taskvine/test/TR_vine_auto_modes.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+set -e
+
+. ../../dttools/test/test_runner_common.sh
+
+import_config_val CCTOOLS_PYTHON_TEST_EXEC
+import_config_val CCTOOLS_PYTHON_TEST_DIR
+
+export PYTHONPATH=$(pwd)/../src/bindings/${CCTOOLS_PYTHON_TEST_DIR}:$PYTHONPATH
+
+STATUS_FILE=vine.status
+
+check_needed()
+{
+    [ -n "${CCTOOLS_PYTHON_TEST_EXEC}" ] || return 1
+}
+
+prepare()
+{
+    rm -f $STATUS_FILE
+
+    return 0
+}
+
+run()
+{
+    ${CCTOOLS_PYTHON_TEST_EXEC} auto_modes.py
+    echo $? > $STATUS_FILE
+
+    wait_for_file_creation $STATUS_FILE 30
+
+    # retrieve exit status
+    status=$(cat $STATUS_FILE)
+    if [ $status -ne 0 ]
+    then
+        # display log files in case of failure.
+        logfile=$(latest_vine_debug_log)
+        if [ -f ${logfile}  ]
+        then
+            echo "manager log:"
+            cat ${logfile}
+        fi
+
+        exit 1
+    fi
+
+    exit 0
+}
+
+clean()
+{
+    rm -f $STATUS_FILE
+    rm -rf vine-run-info
+
+    exit 0
+}
+
+
+dispatch "$@"
