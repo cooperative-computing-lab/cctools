adc82ab2ebf2ab310f21cd0091dfe8d154f20ab3
David-Simonetti-ND <dsimone2@nd.edu>
2023-04-14 14:03:46 -0400

    Fix for wq as well

--- poncho/src/poncho/package_serverize.py
+++ poncho/src/poncho/package_serverize.py
@@ -198,7 +198,7 @@ def wq_network_code():
                                     chunk = os.read(read, max_read).decode("utf-8")
                                     all_chunks.append(chunk)
                                 response = "".join(all_chunks).encode("utf-8")
-                                os.waitid(os.P_PID, p, os.WEXITED)
+                                os.waitpid(p, 0)
                         response_size = len(response)
                         size_msg = "{}\n".format(response_size)
                         # send the size of response
