77838481314a2825819a60f3366a1bf6085928fc
Benjamin Tovar <btovar@nd.edu>
2023-04-26 19:40:23 -0400

    make all __init__.py the same
    In this way it does not matter when then get rewritten
    when copying them to ndcctools

--- taskvine/src/bindings/python3/ndcctools/__init__.py
+++ taskvine/src/bindings/python3/ndcctools/__init__.py
@@ -8,11 +8,3 @@
 # Package to encompass the python API of all CCTools.
 # See the <a href=http://cctools.readthedocs.io/en/latest>Manual</a> for complete documentation.
 #
-# - @ref ndcctools.taskvine
-
-
-from . import taskvine
-
-__all__ = [
-    "taskvine",
-]
