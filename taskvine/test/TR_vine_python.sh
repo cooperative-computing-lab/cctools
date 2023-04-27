5a2804d92927331bb9140de3f1b70eefd34defbd
Benjamin Tovar <btovar@nd.edu>
2023-04-27 10:50:49 -0400

    update vine tests

--- taskvine/test/TR_vine_python.sh
+++ taskvine/test/TR_vine_python.sh
@@ -7,7 +7,7 @@ set -e
 import_config_val CCTOOLS_PYTHON_TEST_EXEC
 import_config_val CCTOOLS_PYTHON_TEST_DIR
 
-export PYTHONPATH=$(pwd)/../src/bindings/${CCTOOLS_PYTHON_TEST_DIR}:$PYTHONPATH
+export PYTHONPATH=$(pwd)/../../test_support/python_modules/${CCTOOLS_PYTHON_TEST_DIR}:$PYTHONPATH
 
 STATUS_FILE=vine.status
 PORT_FILE=vine.port
