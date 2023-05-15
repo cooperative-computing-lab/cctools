c0902417f4d95f64138dbcc1ef8112d996336951
Colin Thomas <cthomas0687@gmail.com>
2023-05-15 15:31:14 -0400

    back to ordered struct

--- dttools/src/rmsummary.h
+++ dttools/src/rmsummary.h
@@ -25,6 +25,11 @@ COPYING for details.
 
 struct rmsummary
 {
+	double cores;
+	double gpus;
+	double memory;
+	double disk;
+	
 	char    *category;
 	char    *command;
 	char    *taskid;
@@ -37,18 +42,14 @@ struct rmsummary
 	double start;
 	double end;
 
-	double cores;
 	double cores_avg;
-	double gpus;
 
 	double wall_time;
 	double cpu_time;
 
-	double memory;
 	double virtual_memory;
 	double swap_memory;
 
-	double disk;
 	double bytes_read;
 	double bytes_written;
 
