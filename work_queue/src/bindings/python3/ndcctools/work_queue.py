00097def1442836cb40d07f5f8ecc4280e521f44
Colin Thomas <33940547+colinthomas-z80@users.noreply.github.com>
2023-06-09 15:02:13 -0400

    Vine: fixed number of scheduling attempts  (#3377)
    * add scheduling depth to taskvine
    
    * use list rotate in taskvine
    
    * update doc

--- work_queue/src/bindings/python3/ndcctools/work_queue.py
+++ work_queue/src/bindings/python3/ndcctools/work_queue.py
@@ -1850,6 +1850,7 @@ class WorkQueue(object):
     # - "category-steady-n-tasks" Set the number of tasks considered when computing category buckets.
     # - "hungry-minimum" Mimimum number of tasks to consider queue not hungry. (default=10)
     # - "wait-for-workers" Mimimum number of workers to connect before starting dispatching tasks. (default=0)
+    # - "attempt-schedule-depth" The amount of tasks to attempt scheduling on each pass of send_one_task in the main loop. (default=100)
     # - "wait_retrieve_many" Parameter to alter how work_queue_wait works. If set to 0, work_queue_wait breaks out of the while loop whenever a task changes to WORK_QUEUE_TASK_DONE (wait_retrieve_one mode). If set to 1, work_queue_wait does not break, but continues recieving and dispatching tasks. This occurs until no task is sent or recieved, at which case it breaks out of the while loop (wait_retrieve_many mode). (default=0)
     # - "monitor-interval" Parameter to change how frequently the resource monitor records resource consumption of a task in a times series, if this feature is enabled. See @ref enable_monitoring_full.
     # @param value The value to set the parameter to.
