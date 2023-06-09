2c5172a8404e8750156c6ab8420905f020613988
Colin Thomas <cthomas0687@gmail.com>
2023-06-09 10:55:47 -0400

    update doc

--- taskvine/src/bindings/python3/ndcctools/taskvine/manager.py
+++ taskvine/src/bindings/python3/ndcctools/taskvine/manager.py
@@ -800,6 +800,7 @@ class Manager(object):
     # - "hungry-minimum" Mimimum number of tasks to consider manager not hungry. (default=10)
     # - monitor-interval Maximum number of seconds between resource monitor measurements. If less than 1, use default (5s).
     # - "wait-for-workers" Mimimum number of workers to connect before starting dispatching tasks. (default=0)
+    # - "attempt-schedule-depth" The amount of tasks to attempt scheduling on each pass of send_one_task in the main loop. (default=100)
     # - "wait_retrieve_many" Parameter to alter how vine_wait works. If set to 0, cvine.vine_wait breaks out of the while loop whenever a task changes to "task_done" (wait_retrieve_one mode). If set to 1, vine_wait does not break, but continues recieving and dispatching tasks. This occurs until no task is sent or recieved, at which case it breaks out of the while loop (wait_retrieve_many mode). (default=0)
     # - "monitor-interval" Parameter to change how frequently the resource monitor records resource consumption of a task in a times series, if this feature is enabled. See @ref enable_monitoring.
     # @param value The value to set the parameter to.
