04fa618487edd436ce288f094a80d8d2805a4934
Douglas Thain <dthain@nd.edu>
2023-07-31 11:48:27 -0400

    Add packaging

--- packaging/build-conda/build.sh
+++ packaging/build-conda/build.sh
@@ -23,7 +23,7 @@ CONDA_BASE=$(conda info --base)
 . $CONDA_BASE/etc/profile.d/conda.sh
 
 # Install conda developer dependencies first:
-conda create -y -n cctools-dev -c conda-forge --strict-channel-priority python=3 gcc_linux-64 gxx_linux-64 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle
+conda create -y -n cctools-dev -c conda-forge --strict-channel-priority python=3 gcc_linux-64 gxx_linux-64 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle packaging
 conda activate cctools-dev
 
 # Configure and build in the normal way:
