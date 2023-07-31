ebe8d9e6d9db0a87b494a4d16132b6f51d3279c3
Douglas Thain <dthain@nd.edu>
2023-07-31 11:26:09 -0400

    Add build from conda developer dependencies.

---
+++ packaging/build-conda/build.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+set -xe
+
+# Save the dir from which the script was called
+ORG_DIR=$(pwd)
+
+# Find cctools src directory
+CCTOOLS_SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
+
+# Ensure we end up in the directory we started regardless of how the script
+# ends.
+function finish {
+    cd ${ORG_DIR}
+}
+trap finish EXIT
+
+# Install conda developer dependencies first:
+unset PYTHONPATH
+conda create -y -n cctools-dev -c conda-forge --strict-channel-priority python=3 gcc_linux-64 gxx_linux-64 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle
+conda activate cctools-dev
+
+# Configure and build in the normal way:
+./configure --debug --strict "$@"
+[[ -f config.mk ]] && make clean
+echo === Contents of config.mk ===
+cat config.mk
+
+make
+
+make install
+
+if ! make test
+then
+    echo === Contents of cctools.test.fail ===
+    cat cctools.test.fail
+    exit 1
+else
+    exit 0
+fi
