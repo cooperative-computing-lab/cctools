660f30b2100490121ef939f6f2c75562d8acb578
Douglas Thain <dthain@nd.edu>
2023-08-07 17:16:02 -0400

    Clean up function call example to run multiple tasks and check results.

--- taskvine/src/examples/vine_example_function_call.py
+++ taskvine/src/examples/vine_example_function_call.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+
+# This example shows how to install a library of functions once
+# as a LibraryTask, and then invoke that library remotely by
+# using FunctionCall tasks.
+
 import ndcctools.taskvine as vine
 import json
 import argparse
+
+# The library will consist of the following two functions:
 
 def divide(dividend, divisor):
     import math
@@ -25,6 +32,8 @@ def main():
 
     q = vine.Manager(9123)
 
+    print(f"TaskVine manager listening on port {q.port}")
+
     args = parser.parse_args()
 
     if args.disable_peer_transfers:
@@ -32,15 +41,23 @@ def main():
     else:
         q.enable_peer_transfers()
 
-    function_lib = q.create_library_from_functions('test-library', divide, double)
-    q.install_library(function_lib)
+    print("Creating library from functions...")
 
-    for i in range(1,100): 
+    libtask = q.create_library_from_functions('test-library', divide, double)
+    q.install_library(libtask)
+
+    print("Submitting function call tasks...")
+    
+    tasks = 100
+
+    for i in range(0,tasks): 
         s_task = vine.FunctionCall('test-library', 'divide', 2, 2**2)
         q.submit(s_task)
     
         s_task = vine.FunctionCall('test-library', 'double', 3)
         q.submit(s_task)
+
+    print("Waiting for results...")
 
     total_sum = 0
     x = 0
@@ -48,8 +65,16 @@ def main():
         t = q.wait(5)
         if t:
             x = t.output 
-        total_sum += x
-    assert total_sum == divide(2, 2**2) + double(3)
+            total_sum += x
+            print(f"task {t.id} completed with result {x}")
+
+    # Check that we got the right result.
+    expected = tasks * ( divide(2, 2**2) + double(3) )
+
+    print(f"Total:    {total_sum}")
+    print(f"Expected: {expected}")
+
+    assert total_sum == expected
 
 if __name__ == '__main__':
     main()
