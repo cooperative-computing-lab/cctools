39dc259ad7883f52c58474d3bd4907b2e057e386
Thanh Son Phung <tphung@nd.edu>
2023-08-31 14:59:03 -0400

    format

--- taskvine/src/worker/vine_process.c
+++ taskvine/src/worker/vine_process.c
@@ -320,7 +320,7 @@ pid_t vine_process_execute(struct vine_process *p)
 	/* Start the performance clock just prior to forking the task. */
 	p->execution_start = timestamp_get();
 	p->pid = fork();
-	
+
 	if (p->pid > 0) {
 		// Make child process the leader of its own process group. This allows
 		// signals to also be delivered to processes forked by the child process.
@@ -405,7 +405,7 @@ pid_t vine_process_execute(struct vine_process *p)
 
 			_exit(0);
 		}
-		/* For process types other than library, set up file desciptors. 
+		/* For process types other than library, set up file desciptors.
 		 * The library will use the input_fd and output_fd to talk to the manager instead. */
 		if (p->type != VINE_PROCESS_TYPE_LIBRARY) {
 			int result = dup2(input_fd, STDIN_FILENO);
@@ -415,17 +415,16 @@ pid_t vine_process_execute(struct vine_process *p)
 			result = dup2(output_fd, STDOUT_FILENO);
 			if (result < 0)
 				fatal("could not dup output to stdout: %s", strerror(errno));
-			
+
 			result = dup2(error_fd, STDERR_FILENO);
 			if (result < 0)
 				fatal("could not dup error to stderr: %s", strerror(errno));
-		
+
 			/* Close redundant file descriptors.
 			 * Note that output_fd is the same as error_fd so it's only closed once. */
 			close(input_fd);
-			close(output_fd);	// no need to close error_fd.
-		}
-		else {
+			close(output_fd); // no need to close error_fd.
+		} else {
 			int result = dup2(error_fd, STDERR_FILENO);
 			if (result < 0)
 				fatal("could not dup error to stderr: %s", strerror(errno));
@@ -451,10 +450,10 @@ pid_t vine_process_execute(struct vine_process *p)
 		 * the command line so it requires a special execl. */
 		if (p->type != VINE_PROCESS_TYPE_LIBRARY) {
 			execl("/bin/sh", "sh", "-c", p->task->command_line, (char *)0);
-		}
-		else {
-			char* final_command = string_format("%s --input-fd %d --output-fd %d", p->task->command_line, input_fd, output_fd);
-			execl("/bin/sh", "sh", "-c", final_command, (char*) 0);
+		} else {
+			char *final_command = string_format(
+					"%s --input-fd %d --output-fd %d", p->task->command_line, input_fd, output_fd);
+			execl("/bin/sh", "sh", "-c", final_command, (char *)0);
 		}
 		_exit(127); // Failed to execute the cmd.
 	}
