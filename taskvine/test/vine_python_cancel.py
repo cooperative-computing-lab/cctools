faa644193ff304c9b40d19e23eeec69109bbc58b
Douglas Thain <dthain@nd.edu>
2023-09-19 09:55:36 -0400

    - Fix number of tasks in test. - Check actual status for cancellation.

--- taskvine/test/vine_python_cancel.py
+++ taskvine/test/vine_python_cancel.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python3
 
-# taskvine python binding tests
-# tests for submission and cancel of tasks
+# Test of task cancellation in Python.
+# Submit 10 tasks that sleep for 10 seconds.
+# After 3 seconds, cancel all of them.
+# Wait for task return and verify that status was cancelled.
 
 import sys
 import ndcctools.taskvine as vine
 
 ntasks = 10
-tasks_done = 0
+ntasks_cancelled = 0
 
 port_file = None
 try:
@@ -24,7 +26,7 @@ with open(port_file, "w") as f:
 
 print("Submitting {} tasks...".format(ntasks))
     
-for i in range(1,ntasks):
+for i in range(0,ntasks):
     t = vine.Task("sleep 10")
     t.set_cores(1)
     q.submit(t)
@@ -41,7 +43,16 @@ while not q.empty():
     t = q.wait(10)
     if t:
         print("Task {} completed with status {}".format(t.id,t.result))
-        tasks_done+=1
-                            
+        if t.result=="cancelled":
+                ntasks_cancelled+=1
+
+                
+if ntasks_cancelled==ntasks:
+    print("Success: {} of {} tasks cancelled\n".format(ntasks_cancelled,ntasks))
+    sys.exit(0)
+else:
+    print("Failure: {} of {} tasks cancelled\n".format(ntasks_cancelled,ntasks))
+    sys.exit(1)
+    
 
               
