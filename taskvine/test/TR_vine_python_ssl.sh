cce67a7b0c44c45260393ea0789041684b3cc5b6
Douglas Thain <dthain@nd.edu>
2023-09-20 14:11:49 -0400

    Test for cloudpickle and conda-pack in Python tests, to avoid problems (#3502)
    (temporarily) on mac builds where these modules are not present.

--- taskvine/test/TR_vine_python_ssl.sh
+++ taskvine/test/TR_vine_python_ssl.sh
@@ -19,6 +19,11 @@ check_needed()
 {
 	[ -n "${CCTOOLS_PYTHON_TEST_EXEC}" ] || return 1
 	[ "${CCTOOLS_OPENSSL_AVAILABLE}" = yes ] || return 1
+
+	# In some limited build circumstances (e.g. macos build on github),
+	# poncho doesn't work due to lack of conda-pack or cloudpickle
+	"${CCTOOLS_PYTHON_TEST_EXEC}" -c "import conda_pack" || return 1
+	"${CCTOOLS_PYTHON_TEST_EXEC}" -c "import cloudpickle" || return 1
 }
 
 prepare()
