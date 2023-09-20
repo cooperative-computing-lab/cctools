0343a7971925712b9d3f5d1e2be4441060a130f7
Thanh Son Phung <tphung@nd.edu>
2023-09-20 14:54:56 -0400

    Vine: Fixing Library Task (#3490)
    * fix wait lib startup
    
    * fix 1st input invoke
    
    * fix 2nd input invoke
    
    * add cloudpickle 1
    
    * add cloudpickle 2
    
    * add cloudpickle 3
    
    * bytes to python
    
    * clean 1
    
    * clean 2
    
    * clean last and format
    
    * delay library check
    
    * format
    
    * lint
    
    * remove python2 note
    
    * remove libraries_table
    
    * fix check lib ready
    
    * assume cloudpickle
    
    * skip python cancel

--- taskvine/src/bindings/python3/ndcctools/taskvine/task.py
+++ taskvine/src/bindings/python3/ndcctools/taskvine/task.py
@@ -12,7 +12,6 @@ from . import cvine
 from .file import File
 
 import copy
-import json
 import os
 import shutil
 import sys
@@ -20,7 +19,7 @@ import tempfile
 import textwrap
 import uuid
 import weakref
-
+import cloudpickle
 
 ##
 # @class ndcctools.taskvine.task.Task
@@ -481,8 +480,6 @@ class Task(object):
     # @endcode
     @property
     def output(self):
-        if (isinstance(self, FunctionCall)):
-            return json.loads(cvine.vine_task_get_stdout(self._task))['Result']
         return cvine.vine_task_get_stdout(self._task)
 
     ##
@@ -709,13 +706,7 @@ class Task(object):
 # The class represents a Task specialized to execute remote Python code.
 #
 
-try:
-    import cloudpickle
-    pythontask_available = True
-except Exception:
-    # Note that the intended exception here is ModuleNotFoundError.
-    # However, that type does not exist in Python 2
-    pythontask_available = False
+
 
 
 class PythonTask(Task):
@@ -727,9 +718,6 @@ class PythonTask(Task):
     # @param args	arguments used in function to be executed by task
     # @param kwargs	keyword arguments used in function to be executed by task
     def __init__(self, func, *args, **kwargs):
-        if not pythontask_available:
-            raise RuntimeError("PythonTask is not available. The cloudpickle module is missing.")
-
         self._pp_run = None
         self._output_loaded = False
         self._output = None
@@ -966,7 +954,9 @@ class FunctionCall(Task):
         self._event = {}
         self._event["fn_kwargs"] = kwargs
         self._event["fn_args"] = args
+        self.set_time_max(900)     # maximum run time for function calls is 900s by default.
         self.needs_library(library_name)
+        self.output_buffer = None
 
     ##
     # Finalizes the task definition once the manager that will execute is run.
@@ -977,8 +967,10 @@ class FunctionCall(Task):
     # @param manager Manager to which the task was submitted
     def submit_finalize(self, manager):
         super().submit_finalize(manager)
-        f = manager.declare_buffer(json.dumps(self._event))
+        f = manager.declare_buffer(cloudpickle.dumps(self._event))
         self.add_input(f, "infile")
+        self.output_buffer = manager.declare_buffer(cache=False, peer_transfer=False)
+        self.add_output(self.output_buffer, "outfile")
 
     ##
     # Specify function arguments. Accepts arrays and dictionaries. This
@@ -1001,6 +993,15 @@ class FunctionCall(Task):
             print("Error, vine_exec_method must either be fork or direct, choosing fork by default")
             remote_task_exec_method = "fork"
         self._event["remote_task_exec_method"] = remote_task_exec_method
+
+    @property
+    def output(self):
+        output = cloudpickle.loads(self.output_buffer.contents())
+        if output['Success']:
+            return output['Result']
+        else:
+            return output['Reason']
+
 
 ##
 # \class LibraryTask
