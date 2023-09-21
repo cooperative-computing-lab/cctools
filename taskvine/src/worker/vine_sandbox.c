33eac3ae92dfc5011eb8103a2240f94c15854ea9
Colin Thomas <cthomas0687@gmail.com>
2023-09-21 12:27:18 -0400

    check created directory size

--- taskvine/src/worker/vine_sandbox.c
+++ taskvine/src/worker/vine_sandbox.c
@@ -163,8 +163,18 @@ static int stage_output_file(struct vine_process *p, struct vine_mount *m, struc
 	if (result) {
 		struct stat info;
 		if (stat(cache_path, &info) == 0) {
-			vine_cache_addfile(cache, info.st_size, info.st_mode, f->cached_name);
-			vine_worker_send_cache_update(manager, f->cached_name, info.st_size, 0, 0);
+			if (S_ISDIR(info.st_mode)) { 
+				struct path_disk_size_info *state = NULL;
+				path_disk_size_info_get_r(cache_path, -1, &state);
+				int64_t measured_size   = state->last_byte_size_complete;
+				
+				vine_cache_addfile(cache, measured_size, info.st_mode, f->cached_name);
+				vine_worker_send_cache_update(manager, f->cached_name, measured_size, 0, 0);
+				path_disk_size_info_delete_state(state);
+			} else {
+				vine_cache_addfile(cache, info.st_size, info.st_mode, f->cached_name);
+				vine_worker_send_cache_update(manager, f->cached_name, info.st_size, 0, 0);
+			}
 		} else {
 			// This seems implausible given that the rename/copy succeded, but we still have to check...
 			debug(D_VINE, "output: failed to stat %s: %s", cache_path, strerror(errno));
