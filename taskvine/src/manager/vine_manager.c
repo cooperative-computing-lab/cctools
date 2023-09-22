ab1590460013920970bbf7a3a42b2c12ca8f372d
Douglas Thain <dthain@nd.edu>
2023-09-22 15:23:40 -0400

    Take same path on INPUT_MISSING and FORSAKEN.

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -1090,17 +1090,16 @@ static int fetch_output_from_worker(struct vine_manager *q, struct vine_worker_i
 
 	switch (t->result) {
 	case VINE_RESULT_INPUT_MISSING:
-		/* If inputs were missing, the worker didn't run the task, so don't bother with outputs. */
+	case VINE_RESULT_FORSAKEN:
+		/* If the worker didn't run the task don't bother fetching outputs. */
 		result = VINE_SUCCESS;
 		break;
-	case VINE_RESULT_FORSAKEN:
-		reap_task_from_worker(q, w, t, VINE_TASK_RETRIEVED);
-		return VINE_SUCCESS;
-		break;
 	case VINE_RESULT_RESOURCE_EXHAUSTION:
+		/* On resource exhaustion, just get the monitor files to figure out what happened. */
 		result = vine_manager_get_monitor_output_file(q, w, t);
 		break;
 	default:
+		/* Otherwise get all of the output files. */
 		result = vine_manager_get_output_files(q, w, t);
 		break;
 	}
