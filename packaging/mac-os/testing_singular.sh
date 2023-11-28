697d464c355f2a0331d6ada83efb5d9949008b21
Jonathan Brockett <Jbrockett747@gmail.com>
2023-11-28 13:47:14 -0500

    added sleep to wait for process maybe

--- packaging/mac-os/testing_singular.sh
+++ packaging/mac-os/testing_singular.sh
@@ -3,8 +3,10 @@ cd makeflow/test
 conda activate cctools-build
 ./TR_makeflow_001_dirs_01.sh check_needed
 ./TR_makeflow_001_dirs_01.sh prepare
+
 ./TR_makeflow_001_dirs_01.sh run
 # ./TR_makeflow_001_dirs_01.sh clean
+sleep 5
 ls
 ls mydir
 cat mydir/1.txt
