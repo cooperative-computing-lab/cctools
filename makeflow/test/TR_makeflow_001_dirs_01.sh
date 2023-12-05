8d2f3744341f20d00e00fa5091b00567e71c2a36
Jonathan Brockett <Jbrockett747@gmail.com>
2023-12-05 10:45:58 -0500

    echos

--- makeflow/test/TR_makeflow_001_dirs_01.sh
+++ makeflow/test/TR_makeflow_001_dirs_01.sh
@@ -6,7 +6,7 @@
 MAKE_FILE=dirs/testcase.subdir.01.makeflow
 
 PRODUCTS="mydir/1.txt mydir/2.txt"
-
+echo "HERE1"
 . ./makeflow_dirs_test_common.sh
 
 dispatch "$@"
--- makeflow/test/makeflow_dirs_test_common.sh
+++ makeflow/test/makeflow_dirs_test_common.sh
@@ -21,22 +21,22 @@ run()
 {
 	# send makeflow to the background, saving its exit status.
 	(../src/makeflow -d all -T vine -Z $PORT_FILE  $MAKE_FILE; echo $? > $STATUS_FILE) &
-
+    echo "HERE2"
 	# wait at most 5 seconds for makeflow to find a port.
 	wait_for_file_creation $PORT_FILE 5
-
+    echo "HERE3"
 	run_taskvine_worker $PORT_FILE worker.log
-
+    echo "HERE4"
 	# wait for makeflow to exit.
 	wait_for_file_creation $STATUS_FILE 10
-
+    echo "HERE5"
 	# retrieve makeflow exit status
 	status=`cat $STATUS_FILE`
 	if [ $status -ne 0 ]
 	then
 		exit 1
 	fi
-
+    echo "HERE6"
 	# verify that makeflow created the required files from
 	# $MAKE_FILE
 	for file in $PRODUCTS
