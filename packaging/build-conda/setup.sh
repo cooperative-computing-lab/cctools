df45535cf744b7acb81181f12129d2fedb6fb93c
Douglas Thain <dthain@nd.edu>
2023-12-18 13:45:04 -0500

    Add conda setup to bashrc.

--- packaging/build-conda/setup.sh
+++ packaging/build-conda/setup.sh
@@ -8,10 +8,16 @@ then
     mkdir -p ~/miniconda3
     curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o ~/miniconda3/miniconda.sh
     bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    # This adds the conda configuration to bash_profile, which is executed once at login.
     ~/miniconda3/bin/conda init bash
+    # Now we pull in the configuration.
     source ~/.bash_profile
+    # But we also have to setup Conda in every normal shell as well:
+    echo ". $HOME/miniconda3/etc/profile.d/conda.sh" >> $HOME/.bashrc
     conda install -n base conda-libmamba-solver
-    conda create --name cctools-build --yes --quiet --channel conda-forge --strict-channel-priority --solver=libmamba python=3 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle packaging
+    # Remove Linux specific packages from environment.
+    grep -v linux environment.yml > environment.macos.yml
+    conda env create --name cctools-build --quiet --file environment.macos.yml
 else
     conda env create --name cctools-build --quiet --file environment.yml
 fi
