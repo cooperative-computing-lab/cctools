a028717db1ec17a56671c0ffa16653eb170c1a26
Douglas Thain <dthain@nd.edu>
2023-12-18 13:33:28 -0500

    bashrc -> bash_profile

--- packaging/build-conda/setup.sh
+++ packaging/build-conda/setup.sh
@@ -9,7 +9,7 @@ then
     curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o ~/miniconda3/miniconda.sh
     bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
     ~/miniconda3/bin/conda init bash
-    source ~/.bashrc
+    source ~/.bash_profile
     conda install -n base conda-libmamba-solver
     conda create --name cctools-build --yes --quiet --channel conda-forge --strict-channel-priority --solver=libmamba python=3 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle packaging
 else
