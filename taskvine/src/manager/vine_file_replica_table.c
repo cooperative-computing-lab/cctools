14db23876e4289d7bb9000546b91213b33de37ec
Colin Thomas <33940547+colinthomas-z80@users.noreply.github.com>
2023-12-20 09:49:09 -0500

    Vine: temp file replication (#3564)
    * WIP temp replication
    
    * format
    
    * some cleanup
    
    * bump protocol, better distribution
    
    * better replica transfer balancing
    
    * default replica count to 0
    
    * format
    
    * random distribution, remove some debug
    
    * cache transfer interface changes, puturl_now
    
    * format
    
    * flag etiquette
    
    * manual update

--- taskvine/src/manager/vine_file_replica_table.c
+++ taskvine/src/manager/vine_file_replica_table.c
@@ -60,6 +60,46 @@ struct vine_worker_info *vine_file_replica_table_find_worker(struct vine_manager
 	return 0;
 }
 
+// return a set of up to q->temp_replica_count workers that do not have the file cachename
+// and are not on the same host as worker w.
+struct vine_worker_info **vine_file_replica_table_find_replication_targets(
+		struct vine_manager *q, struct vine_worker_info *w, const char *cachename, int *count)
+{
+	char *id;
+	struct vine_worker_info *peer;
+	struct vine_file_replica *remote_info;
+
+	int found = 0;
+	struct vine_worker_info **workers = malloc(sizeof(struct vine_worker_info) * (q->temp_replica_count));
+
+	// some random distribution
+	int skip_workers = (rand() % hash_table_size(q->worker_table)) + 1;
+	HASH_TABLE_ITERATE(q->worker_table, id, peer)
+	{
+		if (found == q->temp_replica_count)
+			break;
+		if (skip_workers--)
+			continue;
+		if (!peer->transfer_port_active)
+			continue;
+
+		char *peer_addr = string_format("worker://%s:%d", peer->transfer_addr, peer->transfer_port);
+		if (!(remote_info = hash_table_lookup(peer->current_files, cachename)) &&
+				(strcmp(w->hostname, peer->hostname))) {
+			if ((vine_current_transfers_worker_in_use(q, peer_addr) < q->worker_source_max_transfers) &&
+					(vine_current_transfers_dest_in_use(q, peer) <
+							q->worker_source_max_transfers)) {
+				debug(D_VINE, "found replication target : %s", peer->transfer_addr);
+				workers[found] = peer;
+				found++;
+			}
+		}
+		free(peer_addr);
+	}
+	*count = found;
+	return workers;
+}
+
 /*
 Count number of replicas of a file in the system.
 XXX Note that this implementation is another inefficient linear search.
--- taskvine/src/manager/vine_manager_put.c
+++ taskvine/src/manager/vine_manager_put.c
@@ -211,6 +211,36 @@ may be an estimate at this point and will be updated by return
 message once the object is actually loaded into the cache.
 */
 
+vine_result_code_t vine_manager_put_url_now(struct vine_manager *q, struct vine_worker_info *w, const char *source,
+		const char *cachename, long long size)
+{
+	char source_encoded[VINE_LINE_MAX];
+	char cached_name_encoded[VINE_LINE_MAX];
+
+	url_encode(source, source_encoded, sizeof(source_encoded));
+	url_encode(cachename, cached_name_encoded, sizeof(cached_name_encoded));
+
+	char *transfer_id = vine_current_transfers_add(q, w, cachename);
+	int result = vine_manager_send(q,
+			w,
+			"puturl_now %s %s %lld %o %s\n",
+			source_encoded,
+			cached_name_encoded,
+			size,
+			0777,
+			transfer_id);
+
+	free(transfer_id);
+	return result;
+}
+
+/*
+Send a url to generate a cached file,
+if it has not already been cached there.  Note that the length
+may be an estimate at this point and will be updated by return
+message once the object is actually loaded into the cache.
+*/
+
 static vine_result_code_t vine_manager_put_url(
 		struct vine_manager *q, struct vine_worker_info *w, struct vine_task *t, struct vine_file *f)
 {
