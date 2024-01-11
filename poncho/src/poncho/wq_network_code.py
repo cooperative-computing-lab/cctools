cf7b2545e5a78a987d2bec4195bb653042d83878
Douglas Thain <dthain@nd.edu>
2024-01-11 14:45:25 -0500

    Fix up stream types correctly.

--- poncho/src/poncho/wq_network_code.py
+++ poncho/src/poncho/wq_network_code.py
@@ -10,19 +10,20 @@ def wq_network_code():
     import json
     import os
     import sys
+    import io
 
-    # Send a message on an I/O stream by sending the message length and then the (string) message.
+    # Send a message on a binary I/O stream by sending the message length and then the (string) message.
     def send_message( stream, data ):
         size = len(data)
         size_msg = "{}\n".format(size)
-        stream.write(size_msg.encode('utf-8'))
+        stream.write(size_msg)
         stream.write(data)
 
-    # Receive a standard message from an I/O stream by reading length and then returning the (string) message
+    # Receive a standard message from a binary I/O stream by reading length and then returning the (string) message
     def recv_message( stream ):
         line = stream.readline()
         length = int(line)
-        return stream.read(length).decode('utf-8')
+        return stream.read(length)
 
     # Decorator for remotely execution functions to package things as json.
     def remote_execute(func):
@@ -66,28 +67,23 @@ def wq_network_code():
 
         # Create pipe for communication with child process
         rpipe, wpipe = os.pipe()
-        rpipestream = fdopen("rb",rpipe)
+        rpipestream = os.fdopen(rpipe,"r")
 
         while True:
             s.listen()
             conn, addr = s.accept()
+            connstream = conn.makefile("rw",encoding="utf-8")
             print('Network function: connection from {}'.format(addr), file=sys.stderr)
             while True:
-                # peek at message to find newline to get the size
-                event_size = None
-                line = conn.recv(4096, socket.MSG_PEEK)
-                eol = line.find(b'\n')
-                if eol >= 0:
-                    size = eol+1
-                    # actually read the size of the event
-                    input_spec = conn.recv(size).decode('utf-8').split()
-                    function_name = input_spec[0]
-                    task_id = int(input_spec[1])
-                    event_size = int(input_spec[2])
+                line = connstream.readline()
+                input_spec = line.split()
+                function_name = input_spec[0]
+                task_id = int(input_spec[1])
+                event_size = int(input_spec[2])
                 try:
                     if event_size:
                         # receive the bytes containing the event and turn it into a string
-                        event_str = conn.recv(event_size).decode("utf-8")
+                        event_str = connstream.read(event_size)
                         # turn the event into a python dictionary
                         event = json.loads(event_str)
                         # see if the user specified an execution method
@@ -99,7 +95,7 @@ def wq_network_code():
                             p = os.fork()
                             if p == 0:
                                 response = globals()[function_name](event)
-                                wpipestream = fdopen(wpipe,"wb")
+                                wpipestream = os.fdopen(wpipe,"w")
                                 send_message(wpipestream,json.dumps(response))
                                 wpipestream.flush()
                                 os._exit(0)
@@ -117,7 +113,8 @@ def wq_network_code():
                                 os.waitpid(p, 0)
 
                         # Send response string back to parent worker process.
-                        send_message(conn,response)
+                        send_message(connstream,response)
+                        connstream.flush()
                         break
                 except Exception as e:
                     print("Network function encountered exception ", str(e), file=sys.stderr)
@@ -125,7 +122,8 @@ def wq_network_code():
                         'Result': f'network function encountered exception {e}',
                         'Status Code': 500
                     }
-                    send_message(conn,json.dumps(response))
+                    send_message(connstream,json.dumps(response))
+                    connstream.flush()
                 finally:
                     os.chdir(abs_working_dir)
         return 0
