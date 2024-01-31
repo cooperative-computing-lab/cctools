c31c14155a049c00936cb0f770a14d757e8e9fbf
Douglas Thain <dthain@nd.edu>
2024-01-31 11:34:50 -0500

    Update work_queue_factory to accommodate new behavior of HTCondor: check for invalid use of AFS, rather than requiring use of /tmp.

--- batch_job/src/work_queue_factory.c
+++ batch_job/src/work_queue_factory.c
@@ -129,6 +129,10 @@ struct hash_table *features_table = NULL;
 
 //0 means the container image does not container work_queue_worker binary
 int k8s_worker_image = 0;
+
+// Disable the check for invalid use of AFS with HTCondor.
+static int disable_afs_check = 0;
+
 /*
 In a signal handler, only a limited number of functions are safe to
 invoke, so we construct a string and emit it with a low-level write.
@@ -1179,6 +1183,7 @@ static void show_help(const char *cmd)
 
 	printf("\nOptions specific to batch systems:\n");
 	printf(" %-30s Generic batch system options.\n", "-B,--batch-options=<options>");
+	printf(" %-30s Disable check for use of AFS with HTCondor.\n", "--disable-afs-check");
 	printf(" %-30s Specify Amazon config file.\n", "--amazon-config");
 	printf(" %-30s Set requirements for the workers as Condor jobs.\n", "--condor-requirements");
 	printf(" %-30s Host name of mesos manager node..\n", "--mesos-master");
@@ -1216,7 +1221,8 @@ enum{   LONG_OPT_CORES = 255,
 		LONG_OPT_PARENT_DEATH,
 		LONG_OPT_PONCHO_ENV,
 		LONG_OPT_USE_SSL,
-		LONG_OPT_FACTORY_NAME
+		LONG_OPT_FACTORY_NAME,
+		LONG_OPT_DISABLE_AFS_CHECK,
 	};
 
 static const struct option long_options[] = {
@@ -1232,6 +1238,7 @@ static const struct option long_options[] = {
 	{"debug", required_argument, 0, 'd'},
 	{"debug-file", required_argument, 0, 'o'},
 	{"debug-file-size", required_argument, 0, 'O'},
+	{"disable-afs-check", no_argument, 0, LONG_OPT_DISABLE_AFS_CHECK },
 	{"disk",   required_argument,  0,  LONG_OPT_DISK},
 	{"env", required_argument, 0, LONG_OPT_ENVIRONMENT_VARIABLE},
 	{"extra-options", required_argument, 0, 'E'},
@@ -1479,6 +1486,9 @@ int main(int argc, char *argv[])
 			case LONG_OPT_FACTORY_NAME:
 				factory_name = xxstrdup(optarg);
 				break;
+			case LONG_OPT_DISABLE_AFS_CHECK:
+				disable_afs_check = 1;
+				break;
 			default:
 				show_help(argv[0]);
 				return EXIT_FAILURE;
@@ -1562,14 +1572,22 @@ int main(int argc, char *argv[])
 	/*
 	Careful here: most of the supported batch systems expect
 	that jobs are submitting from a single shared filesystem.
-	Changing to /tmp only works in the case of Condor.
+	In general, we will put log files into a subdir of the
+	current working directory, with a unique name to separate
+	factory instances.
+
+	However, HTCondor has two constraints:
+	1 - Recent versions of HTCondor insist upon the user log
+	being written to a file under $HOME, for reasons unknown.
+	It will emit errors at submit time if this happens.
+
+	2 - Condor cannot easily deal with files submitted from
+	an AFS home directory, without making things world writeable.
+	We will complain about that here.
 	*/
+
 	if(!scratch_dir) {
-		const char *scratch_parent_dir = ".";
-		if(batch_queue_type==BATCH_QUEUE_TYPE_CONDOR) {
-			scratch_parent_dir = system_tmp_dir(NULL);
-		}
-		scratch_dir = string_format("%s/wq-factory-%d", scratch_parent_dir, getuid());
+		scratch_dir = string_format("wq-factory-%d",getuid());
 	}
 
 	if(!create_dir(scratch_dir,0777)) {
@@ -1577,7 +1595,21 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	if(batch_queue_type==BATCH_QUEUE_TYPE_CONDOR && !disable_afs_check) {
+		char *absolute_scratch_dir = realpath(scratch_dir,0);
+		if(!absolute_scratch_dir) {
+			fprintf(stderr,"work_queue_factory: couldn't get full path of %s: %s\n",scratch_dir,strerror(errno));
+			return 1;
+		}
 
+		if(!strncmp(absolute_scratch_dir,"/afs",4)) {
+			fprintf(stderr,"work_queue_factory: The scratch directory is '%s'\n", absolute_scratch_dir);
+			fprintf(stderr,"This won't work because Condor is not able to write to files in AFS.\n");
+			fprintf(stderr,"Please use --scratch-dir to choose a different scratch directory.\n");
+			return 1;
+		}
+	}
+		
 	const char *item = NULL;
 	list_first_item(wrapper_inputs);
 	while((item = list_next_item(wrapper_inputs))) {
