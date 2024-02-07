6bb7ad9bc870e36451e528b79a3fd24471b90b18
JinZhou5042 <jzhou24@nd.edu>
2024-02-07 14:12:06 -0500

    future files

---
+++ taskvine/test/vine_python_future_pythontask.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import sys
+import ndcctools.taskvine as vine
+
+port_file = None
+try:
+    port_file = sys.argv[1]
+except IndexError:
+    sys.stderr.write("Usage: {} PORTFILE\n".format(sys.argv[0]))
+    raise
+
+
+# Define a function to invoke remotely
+def my_sum(x, y, negate=False):
+    from operator import add, mul
+
+    f = 1
+    if negate:
+        f = -1
+    s = mul(f, add(x, y))
+    return s
+
+
+def main():
+    # Create Executor
+    executor = vine.Executor(port=9123, manager_name='vine_matrtix_build_test', factory=False)
+    print("listening on port {}".format(executor.manager.port))
+    with open(port_file, "w") as f:
+        f.write(str(executor.manager.port))
+
+
+    # Submit several tasks for execution:
+    print("submitting tasks...")
+
+    t1 = executor.future_pythontask(my_sum, 7, 4)
+    t1.set_cores(1)
+    a = executor.submit(t1)
+
+    t2 = executor.future_pythontask(my_sum, a, a)
+    t2.set_cores(1)
+    b = executor.submit(t2)
+
+    t3 = executor.future_pythontask(my_sum, a, b)
+    t3.set_cores(1)
+    c = executor.submit(t3)
+
+    print("waiting for result...")
+
+    res = t3.output()
+    print(f"t3 output = {c.result()}")
+
+    a = 7 + 4
+    b = a + a
+    c = a + b
+    assert res == c
+
+
+if __name__ == '__main__':
+    main()
+
+
+# vim: set sts=4 sw=4 ts=4 expandtab ft=python:
