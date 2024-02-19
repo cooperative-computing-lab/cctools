d572e8b3d5cbffb6fdeae8501b1e13eaebd48445
Douglas Thain <dthain@nd.edu>
2024-02-19 10:46:33 -0500

    typo: matrix

--- taskvine/test/vine_python_futures.py
+++ taskvine/test/vine_python_futures.py
@@ -23,7 +23,7 @@ def my_sum(x, y, negate=False):
 
 
 # Create Executor
-executor = vine.Executor(port=9123, manager_name='vine_matrtix_build_test', factory=False)
+executor = vine.Executor(port=9123, manager_name='vine_matrix_build_test', factory=False)
 print("listening on port {}".format(executor.manager.port))
 with open(port_file, "w") as f:
     f.write(str(executor.manager.port))
