fa8682212136ae696f054fb871f2a11b0d0cc462
Benjamin Tovar <btovar@nd.edu>
2024-04-27 21:45:09 -0400

    vine: do not duplicate library if not needed

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -4529,14 +4529,13 @@ struct vine_task *send_library_to_worker(struct vine_manager *q, struct vine_wor
 		return 0;
 	}
 
-	/* Duplicate the original task */
-	struct vine_task *t = vine_task_copy(original);
-
 	/* Check if this library task can fit in this worker. */
-	if (!check_worker_against_task(q, w, t)) {
-		vine_task_delete(t);
+	if (!check_worker_against_task(q, w, original)) {
 		return 0;
 	}
+
+	/* Duplicate the original task */
+	struct vine_task *t = vine_task_copy(original);
 
 	/* Give it a unique taskid if library fits the worker. */
 	t->task_id = q->next_task_id++;
