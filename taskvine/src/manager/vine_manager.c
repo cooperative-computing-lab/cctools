adfe082ef90e1bc6b1f6270510103933df2220ee
Ben Tovar <btovar@nd.edu>
2024-05-09 01:28:08 +0000

    vine: remove lib tasks from retrieved list when using tags

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -3561,21 +3561,18 @@ static int shutdown_drained_workers(struct vine_manager *q)
 
 /* Comparator function for checking if a task matches a given tag. */
 
-static int task_tag_comparator(void *t, const void *r)
+static int task_tag_comparator(struct vine_task *t, const char *tag)
 {
 
-	struct vine_task *task_in_manager = t;
-	const char *task_tag = r;
-
-	if (!task_in_manager->tag && !task_tag) {
+	if (!t->tag && !tag) {
 		return 1;
 	}
 
-	if (!task_in_manager->tag || !task_tag) {
+	if (!t->tag || !tag) {
 		return 0;
 	}
 
-	return !strcmp(task_in_manager->tag, task_tag);
+	return !strcmp(t->tag, tag);
 }
 
 /*
@@ -4440,20 +4437,6 @@ const char *vine_result_string(vine_result_t result)
 	return str;
 }
 
-static struct vine_task *task_state_any_with_tag(struct vine_manager *q, vine_task_state_t state, const char *tag)
-{
-	struct vine_task *t;
-	uint64_t task_id;
-	ITABLE_ITERATE(q->tasks, task_id, t)
-	{
-		if (t->state == state && task_tag_comparator((void *)t, (void *)tag)) {
-			return t;
-		}
-	}
-
-	return NULL;
-}
-
 static int task_state_count(struct vine_manager *q, const char *category, vine_task_state_t state)
 {
 	struct vine_task *t;
@@ -4774,12 +4757,28 @@ struct vine_task *find_task_to_return(struct vine_manager *q, const char *tag, i
 		struct vine_task *t = NULL;
 
 		if (tag) {
-			t = task_state_any_with_tag(q, VINE_TASK_RETRIEVED, tag);
-		} else if (task_id >= 0) {
-			struct vine_task *temp = itable_lookup(q->tasks, task_id);
-			if (temp->state == VINE_TASK_RETRIEVED) {
-				t = temp;
+			struct vine_task *temp = NULL;
+			int tasks_to_consider = list_size(q->retrieved_list);
+			while (tasks_to_consider > 0) {
+				tasks_to_consider--;
+				temp = list_peek_head(q->ready_list);
+				// a small hack, if task is not standard we accepted it so it can be deleted below.
+				if (temp->type != VINE_TASK_TYPE_STANDARD || task_tag_comparator(temp, tag)) {
+					// temp points to head of list
+					t = list_pop_head(q->retrieved_list);
+					break;
+				} else {
+					list_rotate(q->retrieved_list);
+				}
 			}
+		} else if (task_id >= 0) {
+			// XXX: library tasks are never removed!
+			struct vine_task *temp = itable_lookup(q->tasks, task_id);
+			if (!temp || temp->state != VINE_TASK_RETRIEVED) {
+				break;
+			}
+			t = temp;
+			list_remove(q->retrieved_list, t);
 		} else if (list_size(q->retrieved_list) > 0) {
 			t = list_pop_head(q->retrieved_list);
 		}
