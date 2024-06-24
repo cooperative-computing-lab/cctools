ed13a04414e9124742d9de10c349212240e1b60d
Douglas Thain <dthain@nd.edu>
2024-06-24 09:31:01 -0400

    Remove batch_job_chirp

--- batch_job/src/batch_job.c
+++ batch_job/src/batch_job.c
@@ -21,7 +21,6 @@ See the file COPYING for details.
 extern const struct batch_queue_module batch_queue_amazon;
 extern const struct batch_queue_module batch_queue_lambda;
 extern const struct batch_queue_module batch_queue_amazon_batch;
-extern const struct batch_queue_module batch_queue_chirp;
 extern const struct batch_queue_module batch_queue_cluster;
 extern const struct batch_queue_module batch_queue_condor;
 extern const struct batch_queue_module batch_queue_local;
@@ -51,15 +50,12 @@ static struct batch_queue_module batch_queue_unknown = {
 	{NULL, NULL, NULL, NULL, NULL, NULL, NULL},
 };
 
-#define BATCH_JOB_SYSTEMS "local, vine, wq, condor, sge, pbs, lsf, torque, moab, mpi, slurm, chirp, amazon, amazon-batch, lambda, mesos, k8s, dryrun"
+#define BATCH_JOB_SYSTEMS "local, vine, wq, condor, sge, pbs, lsf, torque, moab, mpi, slurm, amazon, amazon-batch, lambda, mesos, k8s, dryrun"
 
 const struct batch_queue_module * const batch_queue_modules[] = {
 	&batch_queue_amazon,
 	&batch_queue_amazon_batch,
 	&batch_queue_lambda,
-#ifdef CCTOOLS_WITH_CHIRP
-	&batch_queue_chirp,
-#endif
 #ifdef CCTOOLS_WITH_MPI
         &batch_queue_mpi,
 #endif
