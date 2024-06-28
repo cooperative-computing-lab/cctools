d19440a149e844baa6b9a83d731b5340382ddc12
Md Saiful Islam <saifulislamplabon@gmail.com>
2024-06-28 00:53:18 -0400

    Added timeline visulization

--- shepherd/src/shepherd_viz.py
+++ shepherd/src/shepherd_viz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import os
-
 from config_loader import load_and_preprocess_config
 
+import os
 import argparse
 import json
-import sys
+import matplotlib.pyplot as plt
+import numpy as np
 from graphviz import Digraph, Source
 
 
@@ -24,8 +24,43 @@ def render_dot(dot_source, output_filename='workflow_visualization', output_form
     src.render(output_filename)
 
 
-def generate_state_times_graph(state_transition, output_prefix, output_format='png'):
-    pass
+def generate_state_times_graph(state_transition_times, output_prefix, output_format='png', fig_size=(15, 12)):
+    sorted_state_transition_times = dict(
+        sorted(state_transition_times.items(), key=lambda x: x[1]['started'], reverse=True))
+
+    unique_services = list(sorted_state_transition_times.keys())
+    unique_states = set(
+        {state for times in state_transition_times.values() for state in times.keys() if state != 'initialized'})
+
+    state_colors = plt.cm.tab20(np.linspace(0, 1, len(unique_states)))
+    color_map = dict(zip(unique_states, state_colors))
+
+    fig, ax = plt.subplots(figsize=fig_size)
+
+    for idx, (service, state_times) in enumerate(sorted_state_transition_times.items()):
+        sorted_states = sorted(state_times.items(), key=lambda x: x[1])
+        previous_time = sorted_states[1][1]  # skipping initialized state
+
+        previous_state = sorted_states[1][0]
+
+        for state, state_time in sorted_states[2:]:
+            duration = state_time - previous_time
+            ax.broken_barh([(previous_time, duration)], (idx - 0.4, 0.8), facecolors=color_map[previous_state],
+                           label=previous_state)
+            previous_time = state_time
+            previous_state = state
+
+    handles, labels = ax.get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    ax.legend(by_label.values(), by_label.keys())
+    ax.set_yticks(range(len(unique_services)))
+    ax.set_yticklabels(unique_services)
+    ax.set_xlabel('Time (seconds)')
+    ax.grid(True)
+
+    filename = output_prefix + '_timeline.' + output_format
+
+    plt.savefig(filename)
 
 
 def generate_state_transition_graph(config, state_transition, output_prefix, output_format='png'):
