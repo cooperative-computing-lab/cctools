262fb70d8ff41cea1fdbbe71b325f3a76938eb9e
Colin Thomas <cthomas0687@gmail.com>
2024-11-05 09:51:43 -0500

    correct uuid generation for temp names

--- taskvine/src/manager/vine_file.c
+++ taskvine/src/manager/vine_file.c
@@ -15,6 +15,7 @@ See the file COPYING for details.
 #include "stringtools.h"
 #include "timestamp.h"
 #include "unlink_recursive.h"
+#include "uuid.h"
 #include "xxmalloc.h"
 
 #include <errno.h>
@@ -249,9 +250,10 @@ struct vine_file *vine_file_temp_no_peers()
 {
 	// temp files are always cached at workers until explicitely removed.
 	vine_cache_level_t cache = VINE_CACHE_LEVEL_WORKFLOW;
-	uid_t uid = getuid();
+	cctools_uuid_t uuid;
+	cctools_uuid_create(&uuid);
 
-	char *name = string_format("temp-local-%u", uid);
+	char *name = string_format("temp-local-%s", uuid.str);
 	return vine_file_create(name, 0, 0, 0, VINE_FILE, 0, cache, VINE_UNLINK_WHEN_DONE);
 	free(name);
 }
