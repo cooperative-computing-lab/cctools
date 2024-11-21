8fb091ec44f91b1d4d49bb8b044d826a45658315
Thanh Son Phung <tphung@nd.edu>
2024-11-21 09:58:16 -0500

    change var name

--- taskvine/test/TR_vine_python_serverless.sh
+++ taskvine/test/TR_vine_python_serverless.sh
@@ -11,9 +11,9 @@ export PYTHONPATH=$(pwd)/../../test_support/python_modules/${CCTOOLS_PYTHON_TEST
 
 STATUS_FILE=vine.status
 PORT_FILE=vine.port
-TEST_FILE=vine_python_serverless.py
-TEST_INPUT_FILE=${TEST_FILE}.input
-TEST_OUTPUT_FILE=${TEST_FILE}.output
+PYTHON_SCRIPT=vine_python_serverless.py
+TEST_INPUT_FILE=${PYTHON_SCRIPT}.input
+TEST_OUTPUT_FILE=${PYTHON_SCRIPT}.output
 
 check_needed()
 {
@@ -43,7 +43,7 @@ prepare()
 
 run()
 {
-	( ${CCTOOLS_PYTHON_TEST_EXEC} ${TEST_FILE} $PORT_FILE; echo $? > $STATUS_FILE ) &
+	( ${CCTOOLS_PYTHON_TEST_EXEC} ${PYTHON_SCRIPT} $PORT_FILE; echo $? > $STATUS_FILE ) &
 
 	# wait at most 15 seconds for vine to find a port.
 	wait_for_file_creation $PORT_FILE 15
