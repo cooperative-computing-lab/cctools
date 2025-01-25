43948292819cf2041427d2902879108b576cb3c3
JinZhou5042 <jzhou24@nd.edu>
2025-01-25 01:19:50 -0500

    vine: typo

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -2938,7 +2938,7 @@ static vine_result_code_t commit_task_to_worker(struct vine_manager *q, struct v
 	vine_result_code_t result = VINE_SUCCESS;
 
 	/* Kill unused libraries on this worker to reclaim resources. */
-	/* Matches assumption in vine_schedule.c:check_available_resources() */
+	/* Matches assumption in vine_schedule.c:check_worker_have_enough_resources() */
 	kill_empty_libraries_on_worker(q, w, t);
 
 	/* If this is a function needing a library, dispatch the library. */
