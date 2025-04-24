40cddbe079efb851693aee09c652d5c06b925684
Benjamin Tovar <btovar@nd.edu>
2025-04-24 12:18:40 -0400

    vine: change `workers_with_complete_tasks` from table to list. (#4137)
    * remove unusued function
    
    * workers_with_completed_tasks to list
    
    * comment
    
    * add comments
    
    * whitespace

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -508,6 +508,20 @@ static int handle_transfer_hostport(struct vine_manager *q, struct vine_worker_i
 	return VINE_MSG_PROCESSED;
 }
 
+static void register_waiting_retrieval(struct vine_manager *q, struct vine_worker_info *w)
+{
+	/* Here we increment the counter of tasks ready to be retrieved at the worker.
+	 * In case no other task at the worker was ready to be retrieved, we add it
+	 * to the list for the manager to eventually process in the wait loop.
+	 * The counter is decremented as tasks are retrieved. */
+
+	w->tasks_waiting_retrieval += 1;
+	if (w->tasks_waiting_retrieval == 1) {
+		/* if this is the first task waiting retrieval, then tell manager about it. */
+		list_push_tail(q->workers_with_complete_tasks, xxstrdup(w->hashkey));
+	}
+}
+
 static vine_result_code_t get_completion_result(struct vine_manager *q, struct vine_worker_info *w, const char *line)
 {
 	if (!q || !w || !line)
@@ -623,7 +637,7 @@ static vine_result_code_t get_completion_result(struct vine_manager *q, struct v
 			c->min_vine_sandbox = sandbox_used;
 		}
 
-		hash_table_insert(q->workers_with_complete_tasks, w->hashkey, w);
+		register_waiting_retrieval(q, w);
 	}
 
 	/* Finally update data structures to reflect the completion. */
@@ -1055,7 +1069,6 @@ void vine_manager_remove_worker(struct vine_manager *q, struct vine_worker_info 
 
 	hash_table_remove(q->worker_table, w->hashkey);
 	hash_table_remove(q->workers_with_watched_file_updates, w->hashkey);
-	hash_table_remove(q->workers_with_complete_tasks, w->hashkey);
 
 	if (q->transfer_temps_recovery) {
 		recall_worker_lost_temp_files(q, w);
@@ -3561,20 +3574,6 @@ static int send_one_task(struct vine_manager *q)
 }
 
 /*
-get available results from a worker. This is typically used for signaling watched files.
-*/
-int get_results_from_worker(struct vine_manager *q, struct vine_worker_info *w)
-{
-	/* get available results from the worker, bail out if that also fails. */
-	vine_result_code_t r = get_available_results(q, w);
-	if (r != VINE_SUCCESS) {
-		handle_worker_failure(q, w);
-		return 0;
-	}
-	return 1;
-}
-
-/*
 Finding a worker that has tasks waiting to be retrieved, then fetch the outputs
 of those tasks. Returns the number of tasks received.
 */
@@ -3593,23 +3592,25 @@ static int receive_tasks_from_worker(struct vine_manager *q, struct vine_worker_
 		max_to_receive = itable_size(w->current_tasks);
 	}
 
-	/* Reset the available results table now that the worker is removed */
-	hash_table_remove(q->workers_with_complete_tasks, w->hashkey);
-	hash_table_firstkey(q->workers_with_complete_tasks);
-
 	/* Now consider all tasks assigned to that worker .*/
 	ITABLE_ITERATE(w->current_tasks, task_id, t)
 	{
 		/* If the task is waiting to be retrieved... */
 		if (t->state == VINE_TASK_WAITING_RETRIEVAL) {
+			if (tasks_received >= max_to_receive) {
+				/* but we are over the max_to_receive, then re-add worker to list to be processed later. */
+				list_push_head(q->workers_with_complete_tasks, xxstrdup(w->hashkey));
+				break;
+			}
+
 			/* Attempt to fetch it. */
 			if (fetch_outputs_from_worker(q, w, task_id)) {
 				/* If it was fetched, update stats and keep going. */
 				tasks_received++;
 
-				if (tasks_received >= max_to_receive) {
-					break;
-				}
+				/* Pair the increment of register_waiting_retrieval with
+				 * a decrement as the tasks has been retrieved. */
+				w->tasks_waiting_retrieval--;
 			} else {
 				/* But if the fetch failed, the worker is no longer vaild, bail out. */
 				return tasks_received;
@@ -4055,7 +4056,7 @@ struct vine_manager *vine_ssl_create(int port, const char *key, const char *cert
 	q->stats_measure = calloc(1, sizeof(struct vine_stats));
 
 	q->workers_with_watched_file_updates = hash_table_create(0, 0);
-	q->workers_with_complete_tasks = hash_table_create(0, 0);
+	q->workers_with_complete_tasks = list_create();
 
 	// The poll table is initially null, and will be created
 	// (and resized) as needed by build_poll_table.
@@ -4408,7 +4409,8 @@ void vine_delete(struct vine_manager *q)
 	list_delete(q->waiting_retrieval_list);
 	list_delete(q->retrieved_list);
 	hash_table_delete(q->workers_with_watched_file_updates);
-	hash_table_delete(q->workers_with_complete_tasks);
+	list_clear(q->workers_with_complete_tasks, free);
+	list_delete(q->workers_with_complete_tasks);
 
 	list_clear(q->task_info_list, (void *)vine_task_info_delete);
 	list_delete(q->task_info_list);
@@ -5236,13 +5238,27 @@ static struct vine_task *vine_wait_internal(struct vine_manager *q, int timeout,
 		int retrieved_this_cycle = 0;
 		BEGIN_ACCUM_TIME(q, time_receive);
 		do {
-			char *key;
-			struct vine_worker_info *w;
+			char *key = NULL;
+			struct vine_worker_info *w = NULL;
 
 			// consider one worker at a time
+			while (list_size(q->workers_with_complete_tasks) > 0) {
+				key = list_pop_head(q->workers_with_complete_tasks);
+				w = hash_table_lookup(q->worker_table, key);
+				free(key);
 
-			hash_table_firstkey(q->workers_with_complete_tasks);
-			if (hash_table_nextkey(q->workers_with_complete_tasks, &key, (void **)&w)) {
+				if (w) {
+					// found a worker that is still connected.
+					break;
+				} else {
+					// if the worker is not in the worker_table anymore,
+					// this means that it disconnected before we got to it
+					// in the workers_with_complete_tasks list. Elements
+					// of that list are only removed in the pop above.
+				}
+			}
+
+			if (w) {
 				int retrieved_from_worker = receive_tasks_from_worker(q, w, retrieved_this_cycle);
 				retrieved_this_cycle += retrieved_from_worker;
 				events += retrieved_from_worker;
