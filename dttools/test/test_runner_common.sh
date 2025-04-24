2b127c674513fb79de52a2dcda5fa50561810083
Benjamin Tovar <btovar@nd.edu>
2025-04-24 10:46:17 -0400

    fix gdb wrapper expression

--- dttools/test/test_runner_common.sh
+++ dttools/test/test_runner_common.sh
@@ -6,7 +6,7 @@ export CC=${CC:-gcc}
 WORK_QUEUE_WORKER=$(cd "$(dirname "$0")/../../work_queue/src/"; pwd)/work_queue_worker
 TASKVINE_WORKER=$(cd "$(dirname "$0")/../../taskvine/src/worker"; pwd)/vine_worker
 
-export GDB_WRAPPER="gdb --batch -ex run -ex 'bt 10' -ex exit --args "
+export GDB_WRAPPER="gdb --batch -ex run -ex bt -ex exit --args "
 
 # Obtain a config value from the master config file.
 
