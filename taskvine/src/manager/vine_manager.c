9013a06164ce044e1647c71c91ec46bcdd45c814
Benjamin Tovar <btovar@nd.edu>
2025-05-28 12:40:07 -0400

    revert incorrect changes to vine_manager.c

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -875,21 +875,22 @@ static void cleanup_worker_files(struct vine_manager *q, struct vine_worker_info
 		return;
 	}
 
-	struct vine_file_replica *replica = NULL;
 	char *cached_name = NULL;
-	HASH_TABLE_ITERATE(w->current_files, cached_name, replica)
-	{
-		// make a copy of the cached name because the file may be deleted from the file tables,
-		// which corrupts the cached_name given from HASH_TABLE_ITERATE.
-		char *cached_name_copy = xxstrdup(cached_name);
-		struct vine_file *f = hash_table_lookup(q->file_table, cached_name_copy);
+	char **cached_names = hash_table_keys_array(w->current_files);
+
+	struct vine_file_replica *replica = NULL;
+
+	int i = 0;
+	while ((cached_name = cached_names[i])) {
+		i++;
+		struct vine_file *f = hash_table_lookup(q->file_table, cached_name);
 
 		// check that the manager actually knows about that file, as the file
 		// may correspond to a cache-update of a file that has not been declared
 		// yet.
 		if (!f || !delete_worker_file(q, w, f->cached_name, f->cache_level, VINE_CACHE_LEVEL_WORKFLOW)) {
-			if (cached_name_copy) {
-				replica = vine_file_replica_table_remove(q, w, cached_name_copy);
+			if (cached_name) {
+				replica = vine_file_replica_table_remove(q, w, cached_name);
 			}
 
 			if (replica) {
@@ -905,8 +906,9 @@ static void cleanup_worker_files(struct vine_manager *q, struct vine_worker_info
 				}
 			}
 		}
-		free(cached_name_copy);
 	}
+
+	hash_table_free_keys_array(cached_names);
 }
 
 /* Remove all tasks and other associated state from a given worker. */
