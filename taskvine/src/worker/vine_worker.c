b94360b810e3b18f4a5886ad6543b0e5334f5c67
Jin Zhou <jzhou24@condorfe.crc.nd.edu>
2025-07-03 17:07:56 -0400

    vine: send_cache_invalid_for_pending_transfer

--- taskvine/src/worker/vine_worker.c
+++ taskvine/src/worker/vine_worker.c
@@ -1044,6 +1044,17 @@ static int do_unlink(struct link *manager, const char *path)
 	int result = 0;
 
 	if (path_within_dir(cached_path, workspace->workspace_dir)) {
+		/* Manager's replica state machine expects a response to every unlink message.
+		 * When arrive here, if not READY or FAILED (which have already sent messages),
+		 * send cache-invalid to prevent the replica state from getting stuck in DELETING. */
+		vine_cache_status_t status = vine_cache_ensure(cache_manager, path);
+		
+		if (status != VINE_CACHE_STATUS_READY && status != VINE_CACHE_STATUS_FAILED) {
+			char *error_message = string_format("File '%s' unlinked in status %d without completion.", path, status);
+			vine_worker_send_cache_invalid(manager, path, error_message);
+			free(error_message);
+		}
+
 		vine_cache_remove(cache_manager, path, manager);
 		result = 1;
 	} else {
