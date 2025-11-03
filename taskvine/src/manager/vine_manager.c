a4587792fff2555445228b1d0091cab24ff98215
Jin Zhou <jzhou24@condorfe.crc.nd.edu>
2025-11-03 10:26:52 -0500

    vine: return count pruned in vine_prune_file

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -6438,15 +6438,17 @@ Should be invoked by the application when a file will never
 be needed again, to free up available space.
 */
 
-void vine_prune_file(struct vine_manager *m, struct vine_file *f)
+int vine_prune_file(struct vine_manager *m, struct vine_file *f)
 {
 	if (!f) {
-		return;
+		return 0;
 	}
 
 	if (!m) {
-		return;
+		return 0;
 	}
+
+	int pruned_replica_count = 0;
 
 	/* delete all of the replicas present at remote workers. */
 	struct set *source_workers = hash_table_lookup(m->file_worker_table, f->cached_name);
@@ -6456,6 +6458,7 @@ void vine_prune_file(struct vine_manager *m, struct vine_file *f)
 			for (int i = 0; workers_array[i] != NULL; i++) {
 				struct vine_worker_info *w = (struct vine_worker_info *)workers_array[i];
 				delete_worker_file(m, w, f->cached_name, 0, 0);
+				pruned_replica_count++;
 			}
 			set_free_values_array(workers_array);
 		}
@@ -6463,6 +6466,8 @@ void vine_prune_file(struct vine_manager *m, struct vine_file *f)
 
 	/* also remove from the replication table. */
 	hash_table_remove(m->temp_files_to_replicate, f->cached_name);
+
+	return pruned_replica_count;
 }
 
 /*
