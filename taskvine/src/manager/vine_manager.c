ed1fc2368cbf0c341cd90570541fbebfa11212c1
Jin Zhou <jzhou24@condorfe.crc.nd.edu>
2025-11-05 00:12:13 -0500

    remove unrelated code

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -418,12 +418,6 @@ static vine_msg_code_t handle_cache_update(struct vine_manager *q, struct vine_w
 			f->state = VINE_FILE_STATE_CREATED;
 			f->size = size;
 
-			/* If the replica's type was a URL, it means the manager expected the destination worker to download it
-			 * from elsewhere. Now that it's physically present, we can resolve its type back to the original */
-			if (replica->type == VINE_URL) {
-				replica->type = f->type;
-			}
-
 			/* If a TEMP file, replicate or shift disk load as needed. */
 			if (f->type == VINE_TEMP) {
 				if (q->temp_replica_count > 1) {
