ee853cba22e8a2fd49a8136c782cb3460224bb26
Thanh Son Phung <tphung@nd.edu>
2025-12-18 12:40:00 -0500

    try factory period of 10s

--- batch_job/src/work_queue_factory.c
+++ batch_job/src/work_queue_factory.c
@@ -72,7 +72,7 @@ static int work_queue_status_timeout = 30;
 
 static const char *catalog_host = 0;
 
-static int factory_period = 30; // in seconds
+static int factory_period = 10; // in seconds
 
 static int workers_min = 5;
 static int workers_max = 100;
