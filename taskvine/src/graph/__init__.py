fe470361630b6492d67381262c5fe7f555fdf833
Jin Zhou <jzhou24@condorfe.crc.nd.edu>
2026-01-06 14:57:33 -0500

    Add example target to Makefile for running example_blueprint

---
+++ taskvine/src/graph/__init__.py
@@ -0,0 +1,10 @@
+"""High-level blueprint graph interfaces."""
+
+from .high_level import BlueprintFile, BlueprintGraph, ReturnRef
+
+__all__ = [
+    "BlueprintFile",
+    "BlueprintGraph",
+    "ReturnRef",
+]
+
