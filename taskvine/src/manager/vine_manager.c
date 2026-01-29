7c0ddb88e51f68c184f093eaf93ab1760701689d
Benjamin Tovar <btovar@nd.edu>
2026-01-29 11:46:16 -0500

    vine: reset waiting retrieval like running task (fall through)

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -3909,6 +3909,10 @@ static void reset_task_to_state(struct vine_manager *q, struct vine_task *t, vin
 		change_task_state(q, t, new_state);
 		break;
 
+	case VINE_TASK_WAITING_RETRIEVAL:
+		/* do the same for run vs waiting retrieval, only difference is waiting_retrieval_list */
+		list_remove(q->waiting_retrieval_list, t);
+		/* fall through */
 	case VINE_TASK_RUNNING:
 		/* t->worker must be set if in RUNNING state */
 		assert(w);
@@ -3922,11 +3926,6 @@ static void reset_task_to_state(struct vine_manager *q, struct vine_task *t, vin
 
 		/* change_task_state() happened inside reap_task_from_worker */
 
-		break;
-
-	case VINE_TASK_WAITING_RETRIEVAL:
-		list_remove(q->waiting_retrieval_list, t);
-		change_task_state(q, t, new_state);
 		break;
 
 	case VINE_TASK_RETRIEVED:
