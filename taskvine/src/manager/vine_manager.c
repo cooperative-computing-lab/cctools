f03530f6dbedc75f0daf7f8af2e9f2e1db4dcbfd
Benjamin Tovar <btovar@nd.edu>
2026-02-10 13:46:29 -0500

    vine: protect cleanup for files only known to the worker

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -956,9 +956,12 @@ static void cleanup_worker_files(struct vine_manager *q, struct vine_worker_info
 		if (removed_replica) {
 			vine_file_replica_delete(removed_replica);
 		}
+
 		/* consider if this replica needs recovery because of worker removal */
-		if (q->immediate_recovery && f->type == VINE_TEMP && !vine_temp_exists_somewhere(q, f)) {
-			vine_manager_consider_recovery_task(q, f, f->recovery_task);
+		if (f) {
+			if (q->immediate_recovery && f->type == VINE_TEMP && !vine_temp_exists_somewhere(q, f)) {
+				vine_manager_consider_recovery_task(q, f, f->recovery_task);
+			}
 		}
 	}
 
