4ab6d31da29999dd4b2c3f00151165326ca27172
Douglas Thain <dthain@nd.edu>
2026-03-18 10:12:49 -0400

    Change interface and comments from "cores" to "units".

--- taskvine/src/worker/vine_xpus.c
+++ taskvine/src/worker/vine_xpus.c
@@ -1,4 +1,4 @@
-/*OA
+/*
 Copyright (C) 2022- The University of Notre Dame
 This software is distributed under the GNU General Public License.
 See the file COPYING for details.
@@ -9,34 +9,36 @@ See the file COPYING for details.
 #include "debug.h"
 
 struct vine_xpus {
-	const char *name;
-	/* Total number of initialized cores */
-	int count;
-	/* Array tracks which task is assigned to each core. */
-	int *core_to_task;
+	const char *name;	// name of units: cpus, gpus, etc.
+	int count;		// number of available units
+	int *unit_to_task;	// array indicating task assigned to each unit.
 };
+
+/*
+Create a new tracker for units of type "name".
+*/
 
 struct vine_xpus *vine_xpus_create(const char *name, int count)
 {
 	struct vine_xpus *x = malloc(sizeof(*x));
 	x->name = strdup(name);
 	x->count = count;
-	x->core_to_task = calloc(count, sizeof(int));
+	x->unit_to_task = calloc(count, sizeof(int));
 	return x;
 }
 
 /*
-Display the cores associated with each task.
+Display the units associated with each task to the debug log
 */
 
 void vine_xpus_debug(struct vine_xpus *x)
 {
 	buffer_t b;
 	buffer_init(&b);
-	buffer_putfstring(&b, "%s Assigned to Tasks: [ ", x->name);
+	buffer_putfstring(&b, "%s assigned to tasks: [ ", x->name);
 	int i;
 	for (i = 0; i < x->count; i++) {
-		buffer_putfstring(&b, "%d ", x->core_to_task[i]);
+		buffer_putfstring(&b, "%d ", x->unit_to_task[i]);
 	}
 	buffer_putfstring(&b, " ]");
 	debug(D_VINE, "%s", buffer_tostring(&b));
@@ -44,22 +46,22 @@ void vine_xpus_debug(struct vine_xpus *x)
 }
 
 /*
-Free all of the cores associated with this taskid.
+Free all of the units associated with this taskid.
 */
 
 void vine_xpus_free(struct vine_xpus *x, int taskid)
 {
 	int i;
 	for (i = 0; i < x->count; i++) {
-		if (x->core_to_task[i] == taskid) {
-			x->core_to_task[i] = 0;
+		if (x->unit_to_task[i] == taskid) {
+			x->unit_to_task[i] = 0;
 		}
 	}
 }
 
 /*
-Allocate n specific cores to the given task.
-This assumes the total number of cores has been
+Allocate n specific units to the given task.
+This assumes the total number of units has been
 accurately tracked: this function will fatal()
 if not enough are available.
 */
@@ -68,21 +70,21 @@ void vine_xpus_alloc(struct vine_xpus *x, int n, int taskid)
 {
 	int i;
 	for (i = 0; i < x->count && n > 0; i++) {
-		if (x->core_to_task[i] == 0) {
-			x->core_to_task[i] = taskid;
+		if (x->unit_to_task[i] == 0) {
+			x->unit_to_task[i] = taskid;
 			n--;
 		}
 	}
 
 	if (n > 0)
-		fatal("vine_xpus_allocate: accounting error: ran out of cores to assign!");
+		fatal("vine_xpus_allocate: accounting error: ran out of %ss to assign!",x->name);
 
 	vine_xpus_debug(x);
 }
 
 /*
-Return a string representing the cores allocated to taskid.
-For example, if cores 1 and 3 are allocated, return "1,3"
+Return a string representing the units allocated to taskid.
+For example, if units 1 and 3 are allocated, return "1,3"
 This string must be freed after use.
 */
 
@@ -93,7 +95,7 @@ char *vine_xpus_to_string(struct vine_xpus *x, int taskid)
 	buffer_t b;
 	buffer_init(&b);
 	for (i = 0; i < x->count; i++) {
-		if (x->core_to_task[i] == taskid) {
+		if (x->unit_to_task[i] == taskid) {
 			if (first) {
 				first = 0;
 			} else {
