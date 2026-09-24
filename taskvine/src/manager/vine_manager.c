9b54d5c9915999e7e476d8389557b7de5690332b
Benjamin Tovar <btovar@nd.edu>
2026-09-24 13:30:22 -0400

    lint

--- taskvine/src/manager/vine_manager.c
+++ taskvine/src/manager/vine_manager.c
@@ -456,7 +456,7 @@ static vine_msg_code_t handle_cache_update(struct vine_manager *q, struct vine_w
 		vine_txn_log_write_cache_update(q, w, size, transfer_time, start_time, cachename);
 
 		/* process_replica_on_eventt may have deleted the replica,
-		 * so we check here again and return if there's nothing 
+		 * so we check here again and return if there's nothing
 		 * more to update */
 		replica = vine_file_replica_table_lookup(w, cachename);
 		if (!replica) {
